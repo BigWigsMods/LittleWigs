08af1dff948f4681a9a8a0d4d05ffb2800903926
funkydude <funkydude@users.noreply.github.com>
2007-05-04 04:12:49 +0000

    LittleWigs: TempestKeep/Laj: making use of :format

--- TempestKeep/Laj.lua
+++ TempestKeep/Laj.lua
@@ -16,14 +16,14 @@ L:RegisterTranslations("enUS", function() return {
 	allergic = "Allergic Reaction",
 	allergic_desc = "Warn for Allergic Reaction",
 	allergic_trigger = "^([^%s]+) ([^%s]+) afflicted by Allergic Reaction",
-	allergic_warn = " is Allergic!",
+	allergic_warn = "%s is Allergic!",
 } end )
 
 L:RegisterTranslations("koKR", function() return {
 	allergic = "알레르기 반응",
 	allergic_desc = "알레르기 반응에 대한 알림",
 	allergic_trigger = "^([^|;%s]*)(.*)알레르기 반응에 걸렸습니다.", -- check
-	allergic_warn = " 알레그기!",
+	allergic_warn = "%s 알레그기!",
 } end )
 
 ----------------------------------
@@ -45,7 +45,7 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 function mod:OnEnable()
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "CheckAllergic")
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "CheckAllergic")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "CheckAllergic")	
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "CheckAllergic")
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
 end
 
@@ -59,6 +59,6 @@ function mod:CheckAllergic(msg)
 		if player == L2["you"] and type == L2["are"] then
 			player = UnitName("player")
 		end
-		if self.db.profile.allergic then self:Message(player .. L["allergic_warn"], "Important") end
+		if self.db.profile.allergic then self:Message(L["allergic_warn"]:format(player), "Important") end
 	end
 end
