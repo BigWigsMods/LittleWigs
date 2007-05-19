77372837aa20bd751ca723aec47404ff9e1f2c7b
saroz <saroz@users.noreply.wowace.com>
2007-05-19 17:04:37 +0000

    LittleWigs: Skyriss: Warning for that nasty Mind Rend

--- TempestKeep/Skyriss.lua
+++ TempestKeep/Skyriss.lua
@@ -22,6 +22,11 @@ L:RegisterTranslations("enUS", function() return {
 	split_desc = "Warn when Harbinger Skyriss splits",
 	split_trigger = "^We span the universe, as countless as the stars!$",
 	split_warning = "%s has split.",
+	
+	mr = "Mind Rend",
+	mr_desc = "Warn for Mind Rend",
+	mr_trigger = "^([^%s]+) ([^%s]+) afflicted by Mind Rend.$",
+	mr_warning = "Mind Rend: %s",
 } end )
 
 L:RegisterTranslations("koKR", function() return {
@@ -40,7 +45,7 @@ mod.partyContent = true
 mod.otherMenu = "Tempest Keep"
 mod.zonename = AceLibrary("Babble-Zone-2.2")["The Arcatraz"]
 mod.enabletrigger = boss 
-mod.toggleoptions = {"mc", "split", "bosskill"}
+mod.toggleoptions = {"mc", "mr", "split", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -50,6 +55,11 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 function mod:OnEnable()
 	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")	
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_HOSTILEPLAYER_DAMAGE")	
+	
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "MrEvent")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "MrEvent")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "MrEvent")
+	
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
 end
 
@@ -57,8 +67,21 @@ end
 --      Event Handlers      --
 ------------------------------
 
+function mod:MrEvent(msg)
+	if not self.db.profile.mr then return end
+	
+	local player, type = select(3, msg:find(L["mr_trigger"]))
+	if player and type then
+		if player == L2["you"] and type == L2["are"] then
+			player = UnitName("player")
+		end
+		self:Message(L["mr_warning"]:format(player), "Important")
+	end
+end
+
 function mod:CHAT_MSG_SPELL_PERIODIC_HOSTILEPLAYER_DAMAGE(msg)
 	if not self.db.profile.mc then return end
+	
 	local player, type = select(3, msg:find(L["mc_trigger"]))
 	if player and type then
 		if player == L2["you"] and type == L2["are"] then
