5f76c6c8b826d0f04347412090f1d3f2a81acaac
ulic75 <ulic75@gmail.com>
2007-10-19 18:05:04 +0000

    LittleWigs: HF/Omor - change trigger pattern - updated deDE trigger

--- HellfireCitadel/Omor.lua
+++ HellfireCitadel/Omor.lua
@@ -16,7 +16,7 @@ L:RegisterTranslations("enUS", function() return {
 	aura = "Treacherous Aura", -- needs to be exactly what it's called in game.
 	aura_heroic = "Bane of Treachery",
 	aura_desc = "Announce who has the Trecherous Aura",
-	aura_trigger = "^([^%s]+) ([^%s]+) afflicted by ([^%t]+)%.$",
+	aura_trigger = "^(%S+) (%S+) afflicted by (.+)%.$",
 	aura_warning = "%s has %s!",
 	aura_bar = "%s: %s",
 
@@ -28,7 +28,7 @@ L:RegisterTranslations("frFR", function() return {
 	aura = "Aura traîtresse",
 	aura_heroic = "Plaie de traîtrise",
 	aura_desc = "Préviens quand un joueur subit les effets de l'Aura/Plaie traîtresse.",
-	aura_trigger = "^([^%s]+) ([^%s]+) les effets .* ([^%t]+)%.$",
+	aura_trigger = "^(%S+) (%S+) les effets (.+)%.$",
 	aura_warning = "%s a %s !",
 	aura_bar = "%s : %s",
 
@@ -45,6 +45,7 @@ L:RegisterTranslations("koKR", function() return {
 	icon = "전술 표시",
 	icon_desc = "배반의 오라에 걸린 사람에게 전술 표시를 지정합니다. (승급자 이상 권한 요구)",
 } end)
+
 --Chinese Translation: 月色狼影@CWDG
 --CWDG site: http://Cwowaddon.com
 --无疤者奥摩尔
@@ -52,7 +53,7 @@ L:RegisterTranslations("zhCN", function() return {
 	aura = "背叛光环",
 	aura_heroic = "背叛者之祸",
 	aura_desc = "当中了背叛光环发出警报",
-	aura_trigger = "^([^%s]+)受([^%s]+)了([^%t]+)效果的影响。$",
+	aura_trigger = "^(%S+)受(%S+)了(.+)效果的影响。$",
 	aura_warning = "%s 中了 %s!",
 	aura_bar = "%s: %s",
 
@@ -76,7 +77,7 @@ L:RegisterTranslations("deDE", function() return {
    aura = "Verr\195\164terische Aura",
    aura_heroic = "Bann des Verrats",
    aura_desc = "Ank\195\188ndigung wer die Verr\195\164terische Aura hat",
-   aura_trigger = "^([^%s]+) ([^%s]+) betroffen von ([^%t]+)%.$",
+   aura_trigger = "^(%S+) (%S+) von (.+) betroffen%.$",
    aura_warning = "%s hat %s!",
    aura_bar = "%s: %s",
 
