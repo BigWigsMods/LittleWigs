ef1baae46b0639db109bb972684fc8490756f590
cwdg <cwdg@users.noreply.wowace.com>
2007-11-15 08:46:14 +0000

    LittleWigs: update zhCN local

--- CoT/BlackMorass.lua
+++ CoT/BlackMorass.lua
@@ -154,6 +154,8 @@ L:RegisterTranslations("zhCN", function() return {
 
 	disable_trigger = "我们会胜利的。这只是个……时间问题。",
 	disable_warning = "%s 获救了",
+	death_trigger = "(.+)死亡了。",
+	reset_trigger = "No! Damn this feeble, mortal coil!
 
 	-- Bosses
 	frenzy = "埃欧努斯 - 狂乱",
