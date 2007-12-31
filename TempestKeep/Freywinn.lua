528278841c45355044c2787b97f3f0646b136de8
ulic75 <ulic75@gmail.com>
2007-12-31 23:44:16 +0000

    LittleWigs: If you add a new trigger for one language you must add it for all.

--- TempestKeep/Freywinn.lua
+++ TempestKeep/Freywinn.lua
@@ -23,7 +23,8 @@ L:RegisterTranslations("enUS", function() return {
 L:RegisterTranslations("zhTW", function() return {
 	tranq = "寧靜",
 	tranq_desc = "費瑞衛恩施放寧靜時發出警報",
-	tranq_trigger = "自然順從我的意志……",
+	tranq_trigger1 = "自然順從我的意志……",
+	tranq_trigger2 = "Endorel anuminor!", --needs localization
 	tranq_warning = "費瑞衛恩施放寧靜了!",
 	tranqfade_warning = "5 秒後寧靜消散!",
 } end )
@@ -31,7 +32,8 @@ L:RegisterTranslations("zhTW", function() return {
 L:RegisterTranslations("frFR", function() return {
 	tranq = "Tranquillité ",
 	tranq_desc = "Préviens quand Freywinn lance sa Tranquillité.",
-	tranq_trigger = "Nature bends to my will....", -- à traduire
+	tranq_trigger1 = "Nature bends to my will....", -- à traduire
+	tranq_trigger2 = "Endorel anuminor!", --needs localization
 	tranq_warning = "Tranquilité incanté !",
 	tranqfade_warning = "Fin de Tranquillité dans ~5 sec. !",
 } end )
@@ -39,7 +41,8 @@ L:RegisterTranslations("frFR", function() return {
 L:RegisterTranslations("koKR", function() return {
 	tranq = "평온",
 	tranq_desc = "평온에 대한 경고",
-	tranq_trigger = "자연의 힘이 내 손안에 있다...", -- check
+	tranq_trigger1 = "자연의 힘이 내 손안에 있다...", -- check
+	tranq_trigger2 = "Endorel anuminor!", --needs localization
 	tranq_warning = "평온 시전!",
 	tranqfade_warning = "약 5초 이내 평온 사라짐!",
 } end )
@@ -47,12 +50,12 @@ L:RegisterTranslations("koKR", function() return {
 L:RegisterTranslations("deDE", function() return {
 	tranq = "Gelassenheit",
 	tranq_desc = "Warnt vor Gelassenheit",
-	tranq_trigger = "Die Natur unterwirft sich meinem Willen.",
+	tranq_trigger1 = "Die Natur unterwirft sich meinem Willen.",
+	tranq_trigger2 = "Endorel anuminor!", --needs localization	
 	tranq_warning = "Gelassenheit wird gecastet!",
 	tranqfade_warning = "Gelassenheit schwindet in ~5s!",
 } end )
 
---高级植物学家弗雷温
 L:RegisterTranslations("zhCN", function() return {
 	tranq = "宁静",
 	tranq_desc = "宁静警报",
@@ -87,7 +90,7 @@ end
 ------------------------------
 
 function mod:CHAT_MSG_MONSTER_YELL(msg)
-	if self.db.profile.tranq and (msg == L["tranq_trigger1"] or msg == L["tranq_trigger2"] )then
+	if self.db.profile.tranq and (msg == L["tranq_trigger1"] or msg == L["tranq_trigger2"])then
 		self:Message(L["tranq_warning"], "Important")
 		self:DelayedMessage(10, L["tranqfade_warning"], "Attention")
 		self:Bar(L["tranq"], 15, "Spell_Nature_Tranquility")
