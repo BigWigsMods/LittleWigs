a05aa430d760c7202764ea7aee1a8034ce195175
funkydude <funkydude@users.noreply.github.com>
2008-03-30 15:06:26 +0000

    LittleWigs: MagistersTerrace/Vexallus: cleanup

--- MagistersTerrace/Vexallus.lua
+++ MagistersTerrace/Vexallus.lua
@@ -5,10 +5,6 @@
 local boss = BB["Vexallus"]
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 
-local pName = UnitName("player")
-local fmt = string.format
-local db = nil
-
 ----------------------------
 --      Localization      --
 ----------------------------
@@ -17,12 +13,12 @@ L:RegisterTranslations("enUS", function() return {
 	cmd = "Vexallus",
 
 	adds = "Pure Energy",
-	adds_desc = "Warn when Pure Energy is discharged",
+	adds_desc = "Warn when Pure Energy is discharged.",
 	adds_message = "Pure Energy discharged!",
 	adds_trigger = "discharges pure energy!",
 
 	feedback = "Energy Feedback",
-	feedback_desc = "Warn when someone gets the Energy Feedback debuff",
+	feedback_desc = "Warn when someone gets the Energy Feedback debuff.",
 	feedback_you = "Energy Feedback on YOU!",
 	feedback_other = "Energy Feedback on %s!",
 } end )
@@ -83,7 +79,7 @@ local mod = BigWigs:NewModule(boss)
 mod.partyContent = true
 mod.zonename = BZ["Magisters' Terrace"]
 mod.enabletrigger = boss 
-mod.toggleoptions = {"adds","feedback","bosskill"}
+mod.toggleoptions = {"adds", "feedback", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -91,14 +87,12 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 ------------------------------
 
 function mod:OnEnable()
-	self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE","AddSpawned")
+	self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE", "AddSpawned")
 
 	self:AddCombatListener("SPELL_AURA_APPLIED", "Feedback", 44335)
 	self:AddCombatListener("SPELL_AURA_APPLIED_DOSE", "FeedbackDose", 44335)
 	self:AddCombatListener("SPELL_AURA_REMOVED", "FeedbackRemove", 44335)
 	self:AddCombatListener("UNIT_DIED", "GenericBossDeath")
-
-	db = self.db.profile
 end
 
 ------------------------------
@@ -106,34 +100,32 @@ end
 ------------------------------
 
 function mod:AddSpawned(msg)
-	if db.adds and msg:find(L["adds_trigger"]) then
+	if self.db.profile.adds and msg:find(L["adds_trigger"]) then
 		self:Message(L["adds_message"], "Important")
 	end
 end
 
-function mod:Feedback(player, spellId)
-	if db.feedback then
+function mod:Feedback(player, spellID)
+	if self.db.profile.feedback then
 		local other = fmt(L["feedback_other"], player)
-		if player == pName then
-			self:Message(L["feedback_you"], "Personal", true, "Alert", nil, spellId)
-			self:Message(other, "Attention", nil, nil, true)
+		if UnitIsUnit(player, "player") then
+			self:LocalMessage(L["feedback_you"], "Personal", spellID, "Alert")
+			self:WideMessage(other)
 		else
-			self:Message(other, "Attention", nil, nil, nil, spellId)
+			self:IfMessage(other, "Attention", spellID)
 		end
 		self:Bar(other, 30, spellID)
 	end
 end
 
-function mod:FeedbackDose(player, spellId)
-	if db.feedback then
-		local other = fmt(L["feedback_other"], player)
-		self:TriggerEvent("BigWigs_StopBar", self, other)
-		self:Bar(other, 30, spellID)
+function mod:FeedbackDose(player, spellID)
+	if self.db.profile.feedback then
+		self:Bar(fmt(L["feedback_other"], player), 30, spellID)
 	end
 end
 
 function mod:FeedbackRemove(player)
-	if db.feedback then
+	if self.db.profile.feedback then
 		self:TriggerEvent("BigWigs_StopBar", self, fmt(L["feedback_other"], player))
 	end
 end
