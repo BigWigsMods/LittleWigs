b5f0b0282bb85fa0921871572a79332e9c67b950
7destiny <7destiny@users.noreply.wowace.com>
2008-06-25 07:16:09 +0000

    LittleWigs: - CFR/Ahune: Fixed emerge -> stand - added koKR locale

--- Coilfang/Ahune.lua
+++ Coilfang/Ahune.lua
@@ -14,15 +14,27 @@ local standing = nil
 L:RegisterTranslations("enUS", function() return {
 	cmd = "Ahune",
 	
-	emerge = "Emerge",
-	emerge_desc = "Announce and show a bar when Ahune emerges",
-	emerge_message = "Ahune Emerged",
-	emerge_soon = "Emerge Soon",
+	stand = "Emerge",
+	stand_desc = "Announce and show a bar when Ahune emerges",
+	stand_message = "Ahune Emerged",
+	stand_soon = "Emerge Soon",
 
 	submerge = "Submerge",
 	submerge_desc = "Announce and show a bar when Ahune submerges",
 	submerge_message = "Ahune Submerged",
 	sumberge_soon = "Submerge Soon",
+} end )
+
+L:RegisterTranslations("koKR", function() return {
+	stand = "등장",
+	stand_desc = "강화된 아훈에 대한 바와 알림입니다.",
+	stand_message = "아훈 등장",
+	stand_soon = "잠시 후 아훈 등장",
+
+	submerge = "약화",
+	submerge_desc = "아훈의 약화에 대한 바와 알림입니다.",
+	submerge_message = "아훈 약화 - 얼어붙은 핵",
+	sumberge_soon = "잠시 후 약화!",
 } end )
 
 ----------------------------------
@@ -35,7 +47,7 @@ mod.otherMenu = "Coilfang Reservoir"
 mod.zonename = BZ["The Slave Pens"]
 mod.enabletrigger = boss
 mod.guid = 25740
-mod.toggleoptions = {"emerge", "submerge", "bosskill"}
+mod.toggleoptions = {"stand", "submerge", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
