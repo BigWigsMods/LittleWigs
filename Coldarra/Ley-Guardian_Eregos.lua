070e03a8c37b50ad6aa1dd6bdb4bc00b7f63083f
tomsommer <tomsommer@users.noreply.wowace.com>
2008-11-21 23:28:49 +0000

    - Warn for Planar Shift and Enraged Assault (options are drycode)

--- Coldarra/Ley-Guardian_Eregos.lua
+++ Coldarra/Ley-Guardian_Eregos.lua
@@ -12,6 +12,16 @@ local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 
 L:RegisterTranslations("enUS", function() return {
 	cmd = "Eregos",
+	
+	planarshift = "Planar Shift",
+	planarshift_desc = "Warns for Planar Shift",
+	planarshift_message = "Planar Shift!",
+	planarshift_expire_warning = "Planar Shift ends in 5 sec!",
+	
+	enragedassault = "Enraged Assault",
+	enragedassault_desc = "Warns for Enraged Assault",
+	enragedassault_message = "Enraged Assault!",
+	
 	log = "|cffff0000"..boss.."|r: This boss needs data, please consider turning on your /combatlog or transcriptor and submit the logs.",
 } end )
 
@@ -22,14 +32,12 @@ L:RegisterTranslations("frFR", function() return {
 } end )
 
 L:RegisterTranslations("zhTW", function() return {
-	log = "|cffff0000"..boss.."|r：缺乏數據，請考慮開啟戰斗記錄（/combatlog）或 Transcriptor 記錄并提交戰斗記錄，謝謝！",
 } end )
 
 L:RegisterTranslations("deDE", function() return {
 } end )
 
 L:RegisterTranslations("zhCN", function() return {
-	log = "|cffff0000"..boss.."|r：缺乏数据，请考虑开启战斗记录（/combatlog）或 Transcriptor 记录并提交战斗记录，谢谢！",
 } end )
 
 L:RegisterTranslations("ruRU", function() return {
@@ -46,7 +54,7 @@ mod.otherMenu = "Coldarra"
 mod.zonename = BZ["The Oculus"]
 mod.enabletrigger = {boss} 
 mod.guid = 27656
-mod.toggleoptions = {"bosskill"}
+mod.toggleoptions = {"planarshift", "enragedassault", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -55,6 +63,8 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 function mod:OnEnable()
 	BigWigs:Print(L["log"])
+	self:AddCombatListener("SPELL_AURA_APPLIED", "PlanarShift", 51162)
+	self:AddCombatListener("SPELL_AURA_APPLIED", "EnragedAssault", 51170)
 	self:AddCombatListener("UNIT_DIED", "BossDeath")
 end
 
@@ -62,3 +72,13 @@ end
 --      Event Handlers      --
 ------------------------------
 
+function mod:PlanarShift(_, spellId, _, _, spellName)
+	self:IfMessage(L["planarshift_message"], "Important", spellId)
+	self:Bar(spellName, 18, spellId)
+	self:DelayedMessage(13, L["planarshift_expire_warning"], "Urgent")
+end
+
+function mod:EnragedAssault(player, spellId, _, _, spellName)
+	self:IfMessage(L["enragedassault_message"], "Important", spellId)
+	self:Bar(spellName, 12, spellId)
+end
