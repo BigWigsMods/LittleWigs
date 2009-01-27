ce89565ed668bd720ae2f977024ed043fda01fa6
ulic75 <ulic75@gmail.com>
2009-01-27 16:12:37 +0000

    Personal notification for Eck Residue

--- Zul'Drak/Eck_the_Ferocious.lua
+++ Zul'Drak/Eck_the_Ferocious.lua
@@ -5,12 +5,18 @@
 local boss = BB["Eck the Ferocious"]
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 
+local pName = UnitName("player")
+
 ----------------------------
 --      Localization      --
 ----------------------------
 
 L:RegisterTranslations("enUS", function() return {
 	cmd = "Eck",
+
+	residue = "Eck Residue (Achievement)",
+	residue_desc = "Announce when you have the Eck Residue debuff.",
+	residue_message = "Eck Residue on You",
 } end )
 
 L:RegisterTranslations("koKR", function() return {
@@ -41,7 +47,7 @@ mod.otherMenu = "Zul'Drak"
 mod.zonename = BZ["Gundrak"]
 mod.enabletrigger = boss 
 mod.guid = 29932
-mod.toggleoptions = {"bosskill"}
+mod.toggleoptions = {"residue", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -49,6 +55,7 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 ------------------------------
 
 function mod:OnEnable()
+	self:AddCombatListener("SPELL_AURA_APPLIED", "Residue", 55817)
 	self:AddCombatListener("UNIT_DIED", "BossDeath")
 end
 
@@ -56,3 +63,8 @@ end
 --      Event Handlers      --
 ------------------------------
 
+function mod:Residue(player, spellId)
+	if self.db.profile.residue and player == pName then
+		self:LocalMessage(L["residue_message"], "Positive", spellId, "Info")
+	end
+end
