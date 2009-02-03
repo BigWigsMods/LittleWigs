bff0dd984d19edfb4bed3c4e039de7d4cd73609d
ulic75 <ulic75@gmail.com>
2009-02-03 23:34:13 +0000

    Update to use web translations

--- Stratholme/Mal'Ganis.lua
+++ Stratholme/Mal'Ganis.lua
@@ -11,112 +11,26 @@ local sleepDuration
 --      Localization      --
 ----------------------------
 
-L:RegisterTranslations("enUS", function() return {
-	cmd = "Mal'Ganis",
+L:RegisterTranslations("enUS", function() return --@localization(locale="enUS", namespace="Stratholme/Mal_Ganis", format="lua_table", handle-unlocalized="ignore")@
+end )
 
-	sleep = "Sleep",
-	sleep_desc = "Warn for who is put to sleep.",
+L:RegisterTranslations("koKR", function() return --@localization(locale="koKR", namespace="Stratholme/Mal_Ganis", format="lua_table", handle-unlocalized="ignore")@
+end )
 
-	sleepBar = "Sleep Bar",
-	sleepBar_desc = "Show a bar for the duration of the sleep.",
+L:RegisterTranslations("frFR", function() return --@localization(locale="frFR", namespace="Stratholme/Mal_Ganis", format="lua_table", handle-unlocalized="ignore")@
+end )
 
-	sleep_message = "Sleep: %s",
+L:RegisterTranslations("zhTW", function() return --@localization(locale="zhTW", namespace="Stratholme/Mal_Ganis", format="lua_table", handle-unlocalized="ignore")@
+end )
 
-	vampTouch = "Vampiric Touch",
-	vampTouch_desc = "Warn when Mal'Ganis gains Vampiric Touch.",
-	vampTouch_message = "Mal'Ganis gains Vampiric Touch",
-	
-	vampTouchBar = "Vampiric Touch Bar",
-	vampTouchBar_desc = "Display a bar for the duration of Mal'Ganis Vampiric Touch.",
-} end )
+L:RegisterTranslations("deDE", function() return --@localization(locale="deDE", namespace="Stratholme/Mal_Ganis", format="lua_table", handle-unlocalized="ignore")@
+end )
 
-L:RegisterTranslations("koKR", function() return {
-	sleep = "수면",
-	sleep_desc = "수면에 걸린 플레이어를 알립니다.",
+L:RegisterTranslations("zhCN", function() return --@localization(locale="zhCN", namespace="Stratholme/Mal_Ganis", format="lua_table", handle-unlocalized="ignore")@
+end )
 
-	sleepBar = "수면 바",
-	sleepBar_desc = "수면이 지속되는 바를 표시합니다.",
-
-	sleep_message = "수면: %s",
-
-	vampTouch = "흡혈의 손길",
-	vampTouch_desc = "말가니스의 흡혈의 손길 획득을 알립니다.",
-	vampTouch_message = "말가니스 흡혈의 손길 획득",
-	
-	vampTouchBar = "흡혈의 손길 바",
-	vampTouchBar_desc = "말가니스의 흡혈의 손길이 지속되는 바를 표시합니다.",
-} end )
-
-L:RegisterTranslations("frFR", function() return {
-} end )
-
-L:RegisterTranslations("zhTW", function() return {
-	sleep = "催眠術",
-	sleep_desc = "當玩家中了催眠術時發出警報。",
-
-	sleepBar = "催眠術計時條",
-	sleepBar_desc = "當催眠術持續時顯示計時條。",
-
-	sleep_message = "催眠術：>%s<！",
-
-	vampTouch = "吸血之觸",
-	vampTouch_desc = "當瑪爾加尼斯獲得吸血之觸時發出警報。",
-	vampTouch_message = "瑪爾加尼斯獲得：>吸血之觸<！",
-	
-	vampTouchBar = "吸血之觸計時條",
-	vampTouchBar_desc = "當瑪爾加尼斯的吸血之觸持續時顯示計時條。",
-} end )
-
-L:RegisterTranslations("deDE", function() return {
-	sleep = "Schlaf",
-	sleep_desc = "Warnung wer von Schlaf betroffen ist.",
-	
-	sleepBar = "Schlaf-Anzeie",
-	sleepBar_desc = "Eine Leiste f\195\188r die Dauer von Schlaf anzeigen.",
-
-	sleep_message = "Schlaf: %s",
-
-	vampTouch = "Vampirber\195\188hrung",
-	vampTouch_desc = "Warnung wenn Mal'Ganis die Vampirber\195\188hrung erh\195\164lt.",
-	vampTouch_message = "Mal'Ganis erh\195\164lt Vampirber\195\188hrung!",
- 
-	vampTouchBar = "Vampirber\195\188hrung-Anzeige",
-	vampTouchBar_desc = "Eine Leiste f\195\188r die Dauer von Vampirber\195\188hrung anzeigen.",	
-} end )
-
-L:RegisterTranslations("zhCN", function() return {
-	sleep = "沉睡",
-	sleep_desc = "当玩家中了沉睡时发出警报。",
-
-	sleepBar = "沉睡计时条",
-	sleepBar_desc = "当沉睡持续时显示计时条。",
-
-	sleep_message = "沉睡：>%s<！",
-
-	vampTouch = "吸血鬼之触",
-	vampTouch_desc = "当玛尔加尼斯获得吸血鬼之触时发出警报。",
-	vampTouch_message = "玛尔加尼斯获得：>吸血鬼之触<！",
-	
-	vampTouchBar = "吸血鬼之触计时条",
-	vampTouchBar_desc = "当玛尔加尼斯的吸血鬼之触持续时显示计时条。",
-} end )
-
-L:RegisterTranslations("ruRU", function() return {
-	sleep = "Сон",
-	sleep_desc = "Сообщать о том кто усыплен.",
-
-	sleepBar = "Полоса сна",
-	sleepBar_desc = "Отображать полосу продолжительности сна.",
-
-	sleep_message = "Усыплен: %s",
-
-	vampTouch = "Прикосновение вампира",
-	vampTouch_desc = "Сообщать когда Мал'Ганис получает Прикосновение вампира.",
-	vampTouch_message = "Мал'Ганис получил Прикосновение вампира!",
-	
-	vampTouchBar = "Полоса прикосновения вампира",
-	vampTouchBar_desc = "Отображать полосу продолжительности прикосновения вампира Мал'Ганиса.",
-} end )
+L:RegisterTranslations("ruRU", function() return --@localization(locale="ruRU", namespace="Stratholme/Mal_Ganis", format="lua_table", handle-unlocalized="ignore")@
+end )
 
 ----------------------------------
 --      Module Declaration      --
