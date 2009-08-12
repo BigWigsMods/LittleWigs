4c0d453d1ba60e19c30442e2f99a505d16ee6825
yoshimo <yoshimo@users.noreply.github.com>
2009-08-12 15:16:19 +0000

    Try a localeindependent deathtrigger & add local warning for desecration

--- Icecrown/Black_Knight.lua
+++ Icecrown/Black_Knight.lua
@@ -10,16 +10,13 @@ mod.otherMenu = "Icecrown"
 mod.zonename = BZ["Trial of the Champion"]
 mod.enabletrigger = boss
 mod.guid = 35451
-mod.toggleoptions = {"explode", "explodeBar", "bosskill"}
+mod.toggleoptions = {"explode", "explodeBar", "desecration", "bosskill"}
 
 --------------------------------
 --        Localization        --
 --------------------------------
 
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
-
--- Translators: please update the translations on wowace web based
--- translations, these local ones will go away
 
 L:RegisterTranslations("enUS", function() return {
 	cmd = "The Black Knight",
@@ -29,6 +26,8 @@ L:RegisterTranslations("enUS", function() return {
 	explode_desc = "Warn when The Black Knight is going to shatter his ghouls.",
 	explode_message = "Casting Ghoul Explode!",
 	death_trigger = "Nein! Ich darf nicht... wieder... versagen...";-- needs the english version 
+	desecration = "Desecration",
+	desecration_desc = "Warns when you're standing in the desecration",
 }
 end )
 
@@ -39,6 +38,8 @@ L:RegisterTranslations("deDE", function() return {
 	explode_desc = "Warnen wenn der Schwarze Ritter seine Ghule zerschmettert.",
 	explode_message = "Zaubert Ghoulexplosion!",
 	death_trigger = "Nein! Ich darf nicht... wieder... versagen...", -- needs check
+	desecration = "Entweihung",
+	desecration_desc = "Warnt wenn du auf entweihtem Boden stehst.",
 }
 end )
 
@@ -72,13 +73,18 @@ L:RegisterTranslations("ruRU", function() return {
 }
 end )
 
+
 ----------------------------------
 --        Initialization        --
 ----------------------------------
 
 function mod:OnEnable()
 	self:AddCombatListener("SPELL_CAST_START", "explode", 67886, 51874, 47496, 67729, 67751)
-	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+	self:AddCombatListener("SPELL_AURA_APPLIED", "desecration", 67781, 67876)
+	--self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+	self:AddCombatListener("UNIT_DIED", "Deaths")
+
+	deaths = 0
 end
 
 ----------------------------------
@@ -94,8 +100,25 @@ function mod:explode(_, spellId, _, _, spellName)
 	end
 end
 
-function mod:CHAT_MSG_MONSTER_YELL(msg)
+--[[function mod:CHAT_MSG_MONSTER_YELL(msg)
 	if msg == L["death_trigger"] then
 		self:BossDeath(nil, self.guid)
 	end
+end]]--
+
+function mod:Deaths(_, guid)
+	if not self.db.profile.bosskill then return end
+	guid = tonumber((guid):sub(-12,-7),16)
+	if guid == self.guid then
+		deaths = deaths + 1
+	end
+	if deaths == 3 then
+		self:BossDeath(_, guid)
+	end
 end
+
+function mod:desecration(player)
+	if player == pName and self.db.profile.desecration then
+		self:LocalMessage(L["desecration"], "Personal", 67781, "Alarm")
+	end
+end	
