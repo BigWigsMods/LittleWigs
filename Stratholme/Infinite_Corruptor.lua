72c9791387e743d18f98dfacb8efb3bd603d93aa
ulic75 <ulic75@gmail.com>
2009-08-27 23:05:00 +0000

    Warn for Corrupting Blight, useful for healers.

--- Stratholme/Infinite_Corruptor.lua
+++ Stratholme/Infinite_Corruptor.lua
@@ -10,7 +10,7 @@ mod.otherMenu = "Caverns of Time"
 mod.zonename = BZ["The Culling of Stratholme"]
 mod.enabletrigger = boss 
 mod.guid = 32273
-mod.toggleOptions = {"bosskill"}
+mod.toggleOptions = {"blight","blightBar","bosskill"}
 
 ----------------------------------
 --         Localization         --
@@ -18,43 +18,52 @@ mod.toggleOptions = {"bosskill"}
 
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 
-L:RegisterTranslations("enUS", function() return {
-	cmd = "Corruptor",
-	log = "|cffff0000"..boss.."|r: This boss needs data, please consider turning on your /combatlog or transcriptor and submit the logs.",
-} end )
+L:RegisterTranslations("enUS", function() return --@localization(locale="enUS", namespace="Stratholme/Infinite_Corruptor", format="lua_table", handle-unlocalized="ignore")@
+end )
 
-L:RegisterTranslations("koKR", function() return {
-	log = "|cffff0000"..boss.."|r: 해당 보스의 데이터가 필요합니다. 채팅창에 /전투기록 , /대화기록 을 입력하여 기록된 데이터를 보내주시기 바랍니다.",
-} end )
+L:RegisterTranslations("deDE", function() return --@localization(locale="deDE", namespace="Stratholme/Infinite_Corruptor", format="lua_table", handle-unlocalized="ignore")@
+end )
 
-L:RegisterTranslations("frFR", function() return {
-} end )
+L:RegisterTranslations("esES", function() return --@localization(locale="esES", namespace="Stratholme/Infinite_Corruptor", format="lua_table", handle-unlocalized="ignore")@
+end )
 
-L:RegisterTranslations("zhTW", function() return {
-	log = "|cffff0000"..boss.."|r：缺乏數據，請考慮開啟戰鬥記錄（/combatlog）或 Transcriptor 記錄並提交戰鬥記錄，謝謝！",
-} end )
+L:RegisterTranslations("esMX", function() return --@localization(locale="esMX", namespace="Stratholme/Infinite_Corruptor", format="lua_table", handle-unlocalized="ignore")@
+end )
 
-L:RegisterTranslations("deDE", function() return {
-} end )
+L:RegisterTranslations("frFR", function() return --@localization(locale="frFR", namespace="Stratholme/Infinite_Corruptor", format="lua_table", handle-unlocalized="ignore")@
+end )
 
-L:RegisterTranslations("zhCN", function() return {
-	log = "|cffff0000"..boss.."|r：缺乏数据，请考虑开启战斗记录（/combatlog）或 Transcriptor 记录并提交战斗记录，谢谢！",
-} end )
+L:RegisterTranslations("koKR", function() return --@localization(locale="koKR", namespace="Stratholme/Infinite_Corruptor", format="lua_table", handle-unlocalized="ignore")@
+end )
 
-L:RegisterTranslations("ruRU", function() return {
-	log = "|cffff0000"..boss.."|r: Для этого босса необходимы правильные данные. Пожалуйста, включите запись логов (команда /combatlog) или установите аддон transcriptor, и пришлите получившийся файл (или оставьте ссылку на файл в комментариях на curse.com).",
-} end )
+L:RegisterTranslations("ruRU", function() return --@localization(locale="ruRU", namespace="Stratholme/Infinite_Corruptor", format="lua_table", handle-unlocalized="ignore")@
+end )
+
+L:RegisterTranslations("zhCN", function() return --@localization(locale="zhCN", namespace="Stratholme/Infinite_Corruptor", format="lua_table", handle-unlocalized="ignore")@
+end )
+
+L:RegisterTranslations("zhTW", function() return --@localization(locale="zhTW", namespace="Stratholme/Infinite_Corruptor", format="lua_table", handle-unlocalized="ignore")@
+end )
 
 ----------------------------------
 --        Initialization        --
 ----------------------------------
 
 function mod:OnEnable()
+	self:AddCombatListener("SPELL_AURA_APPLIED", "Blight", 60588)
+
 	self:AddCombatListener("UNIT_DIED", "BossDeath")
-	BigWigs:Print(L["log"])
 end
 
 ----------------------------------
 --        Event Handlers        --
 ----------------------------------
 
+function mod:Blight(player, spellId)
+	if self.db.profile.blight then
+		self:IfMessage(L["blight_message"]:format(player), "Important", spellId)
+	end
+	if self.db.profile.blightBar then
+		self:Bar(L["blight_message"]:format(player), 120, spellId)
+	end
+end
