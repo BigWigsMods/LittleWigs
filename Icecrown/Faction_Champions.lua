a0fa2393f21ce92f4fe94566ea6a7e8f59a8af07
Adavak <adavak@adavak.com>
2009-09-10 16:25:49 +0000

    LittleWigs: - Coliseum zhCN/zhTW update.

--- Icecrown/Faction_Champions.lua
+++ Icecrown/Faction_Champions.lua
@@ -62,6 +62,43 @@ L:RegisterTranslations("deDE", function() return {
 	arelas = "Arelas Hellstern",
 } end )
 
+L:RegisterTranslations("zhCN", function() return {
+	shaman_hex = "萨满祭司 - Hex of Mending",
+	shaman_hex_desc = "当玩家中了Hex of Mending诅咒时发出警报。",
+	shaman_hex_message = "Hex of Mending：>%s<！",
+	shaman_healingwave = "萨满祭司 - 治疗波",
+	shaman_healingwave_desc = "当萨满祭司正在施放治疗波时发出警报。",
+
+	mage_poly = "法师 - 变形术",
+	mage_poly_desc = "当玩家中了变形术时发出警报。",
+	mage_poly_message = "变形术：>%s<！",
+
+	rogue_poison = "潜行者 - 毒药",
+	rogue_poison_desc = "当玩家站在毒药里时发出警报。",
+	rogue_poison_message = ">你< 毒药！",
+
+	jaeren = "Jaeren Sunsworn",
+	arelas = "Arelas Brightstar",
+} end )
+
+L:RegisterTranslations("zhTW", function() return {
+	shaman_hex = "薩滿 - 癒合妖術",
+	shaman_hex_desc = "當玩家中了癒合妖術詛咒時發出警報。",
+	shaman_hex_message = "癒合妖術：>%s<！",
+	shaman_healingwave = "薩滿 - 治療波",
+	shaman_healingwave_desc = "當薩滿正在施放治療波時發出警報。",
+
+	mage_poly = "法師 - 變形術",
+	mage_poly_desc = "當玩家中了變形術時發出警報。",
+	mage_poly_message = "變形術：>%s<！",
+
+	rogue_poison = "盜賊 - 毒藥",
+	rogue_poison_desc = "當玩家站在毒藥里時發出警報。",
+	rogue_poison_message = ">你< 毒藥！",
+
+	jaeren = "傑倫·日誓",
+	arelas = "亞芮拉斯·亮星",
+} end )
 ------------------------------
 --      Initialization      --
 ------------------------------
