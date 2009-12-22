59e7938d676003749064c9c106586d194addf4f7
ulic75 <ulic75@gmail.com>
2009-12-22 15:00:16 +0000

    Arguments were made, add the icon for Overlord's Brand

--- Frozen_Halls/Scourgelord_Tyrannus.lua
+++ Frozen_Halls/Scourgelord_Tyrannus.lua
@@ -7,7 +7,7 @@ mod.partyContent = true
 mod.otherMenu = "The Frozen Halls"
 mod:RegisterEnableMob(36658, 36661)
 mod.toggleOptions = {
-	69172, -- Overlords Brand
+	{69172, "ICON"}, -- Overlords Brand
 	{69275, "ICON", "FLASHSHAKE"}, -- Mark of Rimefang
 	69629, -- Unholy Power
 	"bosskill",
@@ -25,6 +25,7 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "Brand", 69172)
 	self:Log("SPELL_AURA_APPLIED", "Mark", 69275)
 	self:Log("SPELL_AURA_REMOVED", "MarkRemoved", 69275)
+	self:Log("SPELL_AURA_REMOVED", "BrandRemoved", 69172)
 	self:Log("SPELL_AURA_APPLIED", "Power", 69629, 69167)
 	self:Death("Win", 36658)
 end
@@ -35,6 +36,7 @@ end
 function mod:Brand(player, spellId, _, _, spellName)
 	self:TargetMessage(69172, spellName, player, "Personal", spellId, "Alert")
 	self:Bar(69172, player..": "..spellName, 8, spellId)
+	self:SecondaryIcon(69172, player)
 end
 
 function mod:Mark(player, spellId, _, _, spellName)
@@ -46,6 +48,10 @@ end
 
 function mod:MarkRemoved()
 	self:PrimaryIcon(69275, false)
+end
+
+function mod:BrandRemoved()
+	self:SecondaryIcon(69172, false)
 end
 
 function mod:Power(_, spellId, _, _, spellName)
