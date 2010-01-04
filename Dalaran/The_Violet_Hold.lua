14d2a0e7b2b2b3df298d4c6d33e0fd49f1bd6c90
Wouter Commandeur <wouter@ctlaltdel.nl>
2010-01-04 07:00:11 +0000

    improve portal handling for violet hold, ulic/yoshimo please fix this in the trsnslator thing as well.

--- Dalaran/The_Violet_Hold.lua
+++ Dalaran/The_Violet_Hold.lua
@@ -15,9 +15,12 @@ local L = mod:NewLocale("enUS", true)
 if L then
 --@do-not-package@
 L["portals"] = "Portals"
+L["next_portal"] = "Portal %d"
+L["portal_opened"] = "Portal %d opened"
 L["portals_desc"] = "Information about portals after a boss dies."
-L["portal_message15s"] = "%s in ~15 seconds!"
-L["portal_message95s"] = "%s in ~95 seconds!"--@end-do-not-package@
+L["portal_message15s"] = "Portal %d in ~15 seconds!"
+L["portal_message120s"] = "Portal %d in ~120 seconds!"
+L["portal_message95s"] = "Portal %d in ~95 seconds!"--@end-do-not-package@
 --@localization(locale="enUS", namespace="Dalaran/The_Violet_Hold", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 L = mod:GetLocale()
@@ -25,8 +28,10 @@ mod.displayName = L["portals"]
 
 -------------------------------------------------------------------------------
 --  Initialization
+local lastportal = 0
 
 function mod:OnBossEnable()
+	self:RegisterEvent("UPDATE_WORLD_STATES")
 	self:Death("Deaths", 29315,29316,29313,29266,29312,29314,32226,32230,32231,32234,32235,32237)
 	self:Death("Disable", 31134)
 end
@@ -35,7 +40,26 @@ end
 --  Event Handlers
 
 function mod:Deaths()
-	self:Message("portals", L["portal_message95s"], "Attention", "INV_Misc_ShadowEgg")
-	self:DelayedMessage("portals", 80, L["portal_message15s"], "Attention", "INV_Misc_ShadowEgg")
-	self:Bar("portals", L["next_portal"], 95, "INV_Misc_ShadowEgg")
+	self:Message("portals", L["portal_message95s"]:format(lastportal+1), "Attention", "INV_Misc_ShadowEgg")
+	self:DelayedMessage("portals", 80, L["portal_message15s"]:format(lastportal+1), "Attention", "INV_Misc_ShadowEgg")
+	self:Bar("portals", L["next_portal"]:format(lastportal+1), 95, "INV_Misc_ShadowEgg")
+end
+
+
+function mod:UPDATE_WORLD_STATES()
+	local text = select(3, GetWorldStateUIInfo(2))
+	if not text then return end
+	local portal = tonumber(text:match("([0-9]+)/18")) or 0
+	if portal < lastportal then -- wiped probably
+		lastportal = 0
+	elseif portal > lastportal then
+		self:SendMessage("BigWigs_StopBar", self, L["next_portal"]:format(lastportal+1))
+		self:CancelDelayedMessage(L["portals_message15s"]:format(lastportal+1))
+		if portal ~= 6 and portal ~= 12 and portal ~= 18 then
+			self:Message("portals", L["portal_opened"]:format(portal), "Important", "INV_Misc_ShadowEgg")
+			self:Bar("portals", L["next_portal"]:format(portal+1), 120, "INV_Misc_ShadowEgg")
+			self:DelayedMessage("portals", 105, L["portal_message15s"]:format(portal+1), "Attention", "INV_Misc_ShadowEgg")
+		end
+		lastportal = portal
+	end
 end
