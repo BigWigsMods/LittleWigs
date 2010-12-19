4da5a4c305ef71e9f565287ee3ba0bd588ed944a
funkydude <funkydude@users.noreply.github.com>
2010-12-19 18:40:36 +0000

    Stonecore/Ozruk: drycoded warning for shatter

--- Stonecore/Ozruk.lua
+++ Stonecore/Ozruk.lua
@@ -8,25 +8,10 @@ mod.partyContent = true
 mod:RegisterEnableMob(42188)
 mod.toggleOptions = {
 	78939, -- Elementium Bulwark
+	92662, -- Shatter
 	80467, -- Enrage
 	"bosskill",
 }
-
--------------------------------------------------------------------------------
---  Locals
-
-local enraged
-
--------------------------------------------------------------------------------
---  Localization
-
-local L = mod:NewLocale("enUS", true)
-if L then
---@do-not-package@
-L["engage_trigger"] = "None may pass into the World's Heart!"--@end-do-not-package@
---@localization(locale="enUS", namespace="Stonecore/Ozruk", format="lua_additive_table", handle-unlocalized="ignore")@
-end
-L = mod:GetLocale()
 
 -------------------------------------------------------------------------------
 --  Initialization
@@ -34,17 +19,13 @@ L = mod:GetLocale()
 function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "Bulwark", 78939, 92659)
 	self:Log("SPELL_AURA_REMOVED", "BulwarkRemoved", 78939, 92659)
-
 	self:Log("SPELL_AURA_APPLIED", "Enraged", 80467)
+	self:Log("SPELL_CAST_START", "Paralyze", 92426)
+	self:Log("SPELL_CAST_START", "Shatter", 78807, 92662)
+
 	self:RegisterEvent("UNIT_HEALTH")
 
-	self:Yell("Engage", L["engage_trigger"])
-
 	self:Death("Win", 42188)
-end
-
-function mod:OnEngage()
-	enraged = nil
 end
 
 -------------------------------------------------------------------------------
@@ -63,20 +44,24 @@ end
 
 function mod:UNIT_HEALTH(_, unit)
 	if unit ~= "boss1" then return end --if it's not the boss, don't do anything
-	if enraged then
-		self:UnregisterEvent("UNIT_HEALTH")
-		return
-	end
 	if UnitName(unit) == self.displayName then
 		local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
-		if hp <= 27 and not enraged then
-			self:Message(80467, LibStub("AceLocale-3.0"):GetLocale("Little Wigs: Common")["soon"]:format(GetSpellInfo(80467)), "Attention", 80467)
-			enraged = true
+		if hp < 27 and hp > 21 then
+			self:Message(80467, LW_CL["soon"]:format(GetSpellInfo(80467)), "Attention", 80467)
+			self:UnregisterEvent("UNIT_HEALTH")
 		end
 	end
 end
 
 function mod:Enraged(_, spellId, _, _, spellName)
-	self:Message(80467, spellName, "Important", spellId, "Long")
+	self:Message(80467, spellName, "Positive", spellId, "Long")
+end
+
+function mod:Paralyze(_, _, _, _, spellName)
+	self:Message(92662, spellName.." - "..LW_CL["soon"]:format(GetSpellInfo(92662)), "Attention", 92662, "Alert")
+end
+
+function mod:Shatter(_, spellId, _, _, spellName)
+	self:Bar(92662, spellName, 2.5, spellId)
 end
 
