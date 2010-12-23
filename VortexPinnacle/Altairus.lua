790de667a0c29f132f418c47edc40a847cf2d5aa
Wouter Commandeur <wouter@ctlaltdel.nl>
2010-12-23 22:54:39 +0000

    I think this was intended here instead of UnitTarget("boss1")

--- VortexPinnacle/Altairus.lua
+++ VortexPinnacle/Altairus.lua
@@ -42,6 +42,6 @@ end
 
 function mod:Breath(_, spellId, _, _, spellName)
 	self:Bar(88308, LW_CL["next"]:format(spellName), 12, spellId)
-	self:TargetMessage(88308, spellName, UnitTarget("boss1"), "Urgent", spellId)
+	self:TargetMessage(88308, spellName, UnitName("boss1target"), "Urgent", spellId)
 end
 
