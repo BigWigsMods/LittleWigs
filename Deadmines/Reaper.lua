f2f0ef6a7c26574b1dfa944b0f5867aa59bc028c
funkydude <funkydude@users.noreply.github.com>
2011-01-11 20:25:06 +0000

    Remove fixate as it's somewhat useless, change "safety" message to enrage so people know what it is, change bars to "Next ability".

--- Deadmines/Reaper.lua
+++ Deadmines/Reaper.lua
@@ -5,17 +5,12 @@ local mod = BigWigs:NewBoss("Foe Reaper 5000", "The Deadmines")
 if not mod then return end
 mod.partyContent = true
 mod:RegisterEnableMob(43778)
-mod.toggleOptions = {91830, 88481, 88495, 91720, "bosskill"}
-mod.optionHeaders = {
-	[91830] = "Adds",
-	[88481] = "general",
-}
+mod.toggleOptions = {88481, 88495, 91720, "bosskill"}
 
 -------------------------------------------------------------------------------
 --  Initialization
 
 function mod:OnBossEnable()
-	self:Log("SPELL_CAST_SUCCESS", "Fixate", 91830)
 	self:Log("SPELL_CAST_START", "Overdrive", 88481)
 	self:Log("SPELL_CAST_START", "Harvest", 88495)
 	self:Log("SPELL_CAST_SUCCESS", "Safety", 91720)
@@ -30,7 +25,7 @@ function mod:VerifyEnable()
 end
 
 function mod:OnEngage()
-	self:Bar(88481, GetSpellInfo(88481), 10, 88481)
+	self:Bar(88481, LW_CL["next"]:format(GetSpellInfo(88481)), 10, 88481)
 end
 
 -------------------------------------------------------------------------------
@@ -38,22 +33,15 @@ end
 
 function mod:Overdrive(_, spellId, _, _, spellName)
 	self:Message(88481, spellName, "Important", spellId, "Alarm")
-	self:Bar(88481, spellName, 53, spellId)
+	self:Bar(88481, LW_CL["next"]:format(spellName), 53, spellId)
 end
 
 function mod:Harvest(_, spellId, _, _, spellName)
-	self:Message(88495, spellName, "Attention", spellId, "Alert")
-	self:Bar(88495, spellName, 56, spellId)
+	self:TargetMessage(88495, spellName, UnitName("boss1target"), "Urgent", spellId)
+	self:Bar(88495, LW_CL["next"]:format(spellName), 56, spellId)
 end
 
 function mod:Safety(_, spellId, _, _, spellName)
-	self:Message(91720, spellName, "Long", spellId, "Info")
-end
-
-function mod:Fixate(player, spellId, _, _, spellName)
-	if UnitIsUnit(player, "player") then
-		self:LocalMessage(91830, LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Common")["you"]:format(spellName), "Personal", spellId)
-		self:Bar(91830, spellName, 10, spellId)
-	end
+	self:Message(91720, GetSpellInfo(5229), "Long", spellId, "Info") --Enrage
 end
 
