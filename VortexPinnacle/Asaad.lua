e5671609bb92926b95294c85eee69489b1c282c5
ulic75 <ulic75@gmail.com>
2011-01-15 18:25:08 +0000

    Attempt at adding an accurate Static Cling warning (Ticket #40)

--- VortexPinnacle/Asaad.lua
+++ VortexPinnacle/Asaad.lua
@@ -8,8 +8,13 @@ if not mod then return end
 mod.partyContent = true
 mod:RegisterEnableMob(43875)
 mod.toggleOptions = {
+	87618, -- Static Cling
 	86930, -- Supremacy of the Storm
 	"bosskill",
+}
+mod.optionHeaders = {
+	[87618] = "heroic",
+	[86930] = "general",
 }
 
 -------------------------------------------------------------------------------
@@ -19,6 +24,8 @@ function mod:OnBossEnable()
 	-- Supremacy of the Storm is one we care about, but when he starts casting
 	-- the grounding field is a good time to warn
 	self:Log("SPELL_AURA_APPLIED", "Storm", 86911)
+	self:Log("SPELL_CAST_START", "Cling", 93993) -- Actually scan for Chain Lightning as it comes first
+	
 
 	self:Death("Win", 43875)
 end
@@ -32,3 +39,8 @@ function mod:Storm()
 	self:Message(86930, LW_CL["seconds"]:format(storm, 10), "Urgent", 86930)
 end
 
+function mod:Cling()
+	local cling = GetSpellInfo(87618)
+	self:Bar(87618, "~"..cling, 4.5, 87618)
+	self:Message(87618, LW_CL["seconds"]:format(cling, 4.5), "Alert", 87618)
+end
