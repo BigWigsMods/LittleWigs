70b75f544a9a79677ff1c4ac4a334e07f193abea
mojosdojo <mojosdojo@users.noreply.github.com>
2011-01-21 11:42:03 +0000

    Shadowfang/Springvale: Add Empowerment, as it should be interrupted, and Word of Shame for the healers. Switch colors around.

--- Shadowfang/Springvale.lua
+++ Shadowfang/Springvale.lua
@@ -7,7 +7,9 @@ mod.partyContent = true
 mod:RegisterEnableMob(4278)
 mod.toggleOptions = {
 	93687, -- Desecration
+	93844, -- Empowerment
 	93736, -- Shield of the Perfidious
+	93852, -- Word of Shame
 	"bosskill",
 }
 
@@ -16,7 +18,9 @@ mod.toggleOptions = {
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "Desecration", 93687)
+	self:Log("SPELL_CAST_START", "Empowerment", 93844)
 	self:Log("SPELL_AURA_APPLIED", "Shield", 93736)
+	self:Log("SPELL_AURA_APPLIED", "Word", 93852)
 
 	self:Death("Win", 4278)
 end
@@ -29,10 +33,18 @@ end
 --  Event Handlers
 
 function mod:Desecration(_, spellId, _, _, spellName)
-	self:Message(93687, spellName, "Important", spellId)
+	self:Message(93687, spellName, "Attention", spellId)
+end
+
+function mod:Empowerment(_, spellId, _, _, spellName)
+	self:Message(93844, spellName, "Important", spellId, "Alarm")
 end
 
 function mod:Shield(_, spellId, _, _, spellName)
-	self:Message(93736, spellName, "Positive", spellId)
+	self:Message(93736, spellName, "Urgent", spellId)
+end
+
+function mod:Word(player, spellId, _, _, spellName)
+	self:TargetMessage(93852, spellName, player, "Info", spellId)
 end
 
