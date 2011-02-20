c86b8fcdfef95b31941a77b3a2b1878b0eb81217
ulic75 <ulic75@gmail.com>
2011-02-20 20:40:07 +0000

    Static Cling doesn't always happen right after chain lightning anymore

--- VortexPinnacle/Asaad.lua
+++ VortexPinnacle/Asaad.lua
@@ -23,8 +23,7 @@ function mod:OnBossEnable()
 	-- Supremacy of the Storm is one we care about, but when he starts casting
 	-- the grounding field is a good time to warn:
 	self:Log("SPELL_AURA_APPLIED", "Storm", 86911)
-	-- Actually scan for Chain Lightning as it comes first:
-	self:Log("SPELL_CAST_START", "Cling", 93993)
+	self:Log("SPELL_CAST_START", "Cling", 87618)
 
 	self:Death("Win", 43875)
 end
@@ -40,6 +39,7 @@ end
 
 function mod:Cling()
 	local cling = GetSpellInfo(87618)
-	self:Bar(87618, "~"..cling, 4.5, 87618)
-	self:Message(87618, LW_CL["seconds"]:format(cling, 4.5), "Attention", 87618)
+	self:Bar(87618, "~"..cling, 1.25, 87618)
+	self:Message(87618, LW_CL["seconds"]:format(cling, 1.25), "Attention", 87618)
 end
+
