77df5581a3b1de78ad3a13016033011f34356fb8
martinc <martinc@users.noreply.wowace.com>
2011-05-08 01:57:57 +0000

    Venoxis: add duration bar for Venom Withdrawl Venoxis: only 2 Breath of Hethiss, so don't warn for 3rd Venoxis: add timer for next Bloodvenom, Bloodvenom duration

--- ZulGurub/Venoxis.lua
+++ ZulGurub/Venoxis.lua
@@ -10,6 +10,7 @@ mod.toggleOptions = {
 	96509, -- Breath of Hethiss
 	96466, -- Whispers of Hethiss
 	96842, -- Bloodvenom
+	96653, -- Venom Withdrawl (triggered by 97354)
 	"bosskill",
 }
 
@@ -17,6 +18,7 @@ mod.toggleOptions = {
 --  Locals
 
 local linkTargets = mod:NewTargetList()
+local breath = 2
 
 -------------------------------------------------------------------------------
 --  Initialization
@@ -28,6 +30,8 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "Whisper", 96466)
 	self:Log("SPELL_AURA_REMOVED", "WhisperRemoved", 96466)
 	self:Log("SPELL_CAST_START", "Bloodvenom", 96842)
+	self:Log("SPELL_AURA_APPLIED", "Blessing", 97354)
+	self:Log("SPELL_AURA_REMOVED", "BlessingRemoved", 97354)
 
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 
@@ -64,25 +68,43 @@ function mod:LinkRemoved()
 end
 
 function mod:Breath(_, spellId, _, _, spellName)
+	breath = breath - 1
 	self:Message(96509, spellName, "Important", spellId)
-	self:Bar(96509, LW_CL["next"]:format(spellName), 12, spellId)
+	if (breath > 0) then
+		self:Bar(96509, LW_CL["next"]:format(spellName), 12, spellId)
+	end
+end
+
+function mod:Whisper(player, spellId, _, _, spellName)
+	if UnitInParty(player) then
+		self:Message(96466, LW_CL["casting"]:format(spellName), "Urgent", spellId, "Alert")
+		self:Bar(96466, spellName..": "..player, 8, spellId)
+	end
+end
+
+function mod:WhisperRemoved(player, _, _, _, spellName)
+	if UnitInParty(player) then
+		self:SendMessage("BigWigs_StopBar", self, spellName..": "..player)
+	end
 end
 
 do
-	function mod:Whisper(player, spellId, _, _, spellName)
-		if UnitInParty(player) then
-			self:TargetMessage(96466, spellName, player, "Attention", spellId, "Alert")
-			self:Bar(96466, spellName..": "..player, 8, spellId)
-		end
+	local function BloodvenomActive()
+		mod:Bar(96842, GetSpellInfo(96842), 14, 96842)
 	end
-	function mod:WhisperRemoved(player, _, _, _, spellName)
-		if UnitInParty(player) then
-			self:SendMessage("BigWigs_StopBar", self, spellName..": "..player)
-		end
+
+	function mod:Bloodvenom(_, spellId, _, _, spellName)
+		self:Message(96842, LW_CL["casting"]:format(spellName), "Important", spellId, "Alert")
+		self:ScheduleTimer(BloodvenomActive, 3)
 	end
 end
 
-function mod:Bloodvenom(_, spellId, _, _, spellName)
-	self:Message(96842, spellName, "Important", spellId, "Alert")
+function mod:Blessing(_, spellId, _, _, spellName)
+	breath = 2
+	self:Bar(96842, LW_CL["next"]:format(GetSpellInfo(96842)), 38, 96842)
+	self:Bar(96509, LW_CL["next"]:format(GetSpellInfo(96509)), 5.5, 96509)
 end
 
+function mod:BlessingRemoved(_, spellId, _, _, spellName)
+	self:Bar(96653, GetSpellInfo(96653), 10, 96653)
+end
