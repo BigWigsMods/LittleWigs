434f3b7918907c1f38a4ff4c5d0609738a2ca9a6
ulic75 <ulic75@gmail.com>
2011-10-15 22:08:04 +0000

    Added Pulverize & Throw Totem

--- EndTime/Baine.lua
+++ EndTime/Baine.lua
@@ -6,6 +6,8 @@ if not mod then return end
 mod.partyContent = true
 mod:RegisterEnableMob(54431)
 mod.toggleOptions = {
+	{"ej:4140", "ICON"},-- Pulverize
+	"ej:4141", -- Throw Totem
 	"bosskill",
 }
 
@@ -15,7 +17,8 @@ mod.toggleOptions = {
 local L = mod:NewLocale("enUS", true)
 if L then
 --@do-not-package@
---@end-do-not-package@
+L["totemDrop"] = "Totem dropped"
+L["totemThrow"] = "Totem thrown by %s"--@end-do-not-package@
 --@localization(locale="enUS", namespace="EndTime/Baine", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 L = mod:GetLocale()
@@ -24,8 +27,43 @@ L = mod:GetLocale()
 --  Initialization
 
 function mod:OnBossEnable()
+	self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE", "Pulvarize")
+	self:RegisterEvent("SPELL_SUMMON", "TotemDown")
+	self:Log("SPELL_AURA_REMOVED", "TotemUp", 101614)
 	self:Death("Win", 54431)
+end
+
+function mod:OnEngage()
+	self:Bar(101626, LW_CL["next"]:format(GetSpellInfo(101626)), 25, 101626)
 end
 
 -------------------------------------------------------------------------------
 --  Event Handlers
+
+do
+	local pulverize = GetSpellInfo(101626)
+	local function clearIcon()
+		mod:PrimaryIcon("ej:4140")
+	end
+	function mod:Blitz(_, msg, _, _, _, player)
+		if msg:find(pulverize) then
+			if player then
+				self:TargetMessage("ej:4140", pulverize, player, "Important", 101626, "Alert")
+				self:PrimaryIcon("ej:4140", player)
+				self:ScheduleTimer(clearIcon, 4)
+			else
+				self:Message("ej:4140", pulverize, "Important", 101626, "Alert")
+			end
+		end
+	end
+end
+
+function mod:TotemDown()
+	mod:Message("ej:4141", L["totemDrop"], "Important", 101614, "Alarm")
+	self:Bar("ej:4141", L["totemDrop"], 20, 101614)
+end
+
+function mod:TotemUp(player)
+	mod:Message("ej:4141", L["totemThrow"]:format(player), "Positive", 101614, "Info")
+	self:SendMessage("BigWigs_StopBar", self, L["totemDrop"])
+end
