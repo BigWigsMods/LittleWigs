d566788e0f42eb03b257606feb23f3191224b765
funkydude <funkydude@users.noreply.github.com>
2011-12-01 17:53:59 +0000

    some kind of Mannoroth module

--- WellOfEternity/MannorothAndVaro'then.lua
+++ WellOfEternity/MannorothAndVaro'then.lua
@@ -1,31 +1,25 @@
--------------------------------------------------------------------------------
---  Module Declaration
+--------------------------------------------------------------------------------
+-- Module Declaration
+--
 
-local mod = BigWigs:NewBoss("Mannoroth and Varo'then", 816)
+local mod = BigWigs:NewBoss("Mannoroth and Varo'then", 816, 292)
 if not mod then return end
 mod.partyContent = true
-mod:RegisterEnableMob(54431) -- XXX Not correct
-mod.toggleOptions = {
-	"bosskill",
-}
+mod:RegisterEnableMob(54969, 55419) --Mannoroth, Varo'then
+mod.toggleOptions = {"bosskill"}
 
--------------------------------------------------------------------------------
---  Localization
-
-local L = mod:NewLocale("enUS", true)
-if L then
---@do-not-package@
---@end-do-not-package@
---@localization(locale="enUS", namespace="HourOfTwilight/Asira", format="lua_additive_table", handle-unlocalized="ignore")@
-end
-L = mod:GetLocale()
-
--------------------------------------------------------------------------------
---  Initialization
+--------------------------------------------------------------------------------
+-- Initialization
+--
 
 function mod:OnBossEnable()
-	self:Death("Win", 54431)  -- XXX Not correct
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+	self:Death("Win", 54969)
 end
 
--------------------------------------------------------------------------------
---  Event Handlers
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
+
+
+--There is some fire on the ground, warn if you step on it? Can't find it in my log.
