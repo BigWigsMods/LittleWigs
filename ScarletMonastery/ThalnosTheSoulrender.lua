061cfbe21330dbbe067315f71cbc578a9cf78692
funkydude <funkydude@users.noreply.github.com>
2012-10-14 16:49:47 +0000

    ThalnosTheSoulrender: fix this up

--- ScarletMonastery/ThalnosTheSoulrender.lua
+++ ScarletMonastery/ThalnosTheSoulrender.lua
@@ -12,7 +12,7 @@ mod:RegisterEnableMob(59789)
 
 local L = mod:NewLocale("enUS", true)
 if L then
-	L.engage_yell = "yell"
+	L.engage_yell = "My endless agony shall be yours, as well!"
 
 	L.spirit, L.spirit_desc = EJ_GetSectionInfo(5865)
 	L.spirit_icon = 115289
@@ -29,15 +29,12 @@ end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "SpiritGaleCast", 115289)
-	self:Log("SPELL_DAMAGE", "SpiritGaleYou", 115289)
+	self:Log("SPELL_AURA_APPLIED", "SpiritGaleYou", 115291)
+	self:Log("SPELL_INTERRUPT", "SpiritGaleStopped", "*")
 
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 
 	self:Death("Win", 59789)
-end
-
-function mod:OnEngage()
-
 end
 
 --------------------------------------------------------------------------------
@@ -53,6 +50,12 @@ function mod:SpiritGaleYou(player, spellId, _, _, spellName)
 	if UnitIsUnit(player, "player") then
 		self:LocalMessage("spirit", CL["underyou"]:format(spellName), "Personal", spellId, "Alert")
 		self:FlashShake("spirit")
+	end
+end
+
+function mod:SpiritGaleStopped(_, _, _, secSpellId, _, secSpellName)
+	if secSpellId == 115289 then
+		self:SendMessage("BigWigs_StopBar", self, CL["cast"]:format(secSpellName))
 	end
 end
 
