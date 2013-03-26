7cabeb9e3d885360450afc1858ffa3c51b0559bd
funkydude <funkydude@users.noreply.github.com>
2013-03-26 15:29:18 +0000

    remove stray comma

--- BlackTempleSolo/Kanrethad.lua
+++ BlackTempleSolo/Kanrethad.lua
@@ -14,7 +14,7 @@ local L = mod:NewLocale("enUS", true)
 if L then
 	L.summons = "Summons"
 	L.debuffs = "Debuffs"
-	L.start_say = "BEHOLD", -- BEHOLD! I have truly mastered the fel energies of this world! The demonic power I now command... It is indescribable, unlimited, OMNIPOTENT!
+	L.start_say = "BEHOLD" -- BEHOLD! I have truly mastered the fel energies of this world! The demonic power I now command... It is indescribable, unlimited, OMNIPOTENT!
 	L.win_say = "Jubeka" -- Jubeka?! What are you...?!
 end
 L = mod:GetLocale()
