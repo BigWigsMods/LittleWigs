3cda92f2951759f2b83f5cccddf86f08a919b24c
funkydude <funkydude@users.noreply.github.com>
2014-11-24 00:14:52 +0000

    TheEverbloom/AncientProtectors: Add auto-marking to mark the kill order, but mainly to supplement/clarify the potentially large amount of warnings. Shortens boss names and shows raid icons. Replace "Rapid Tides" cast message with a buff gained message. (You shouldn't interrupt this, interrupt the heal, and dispel this.)

--- WoD/TheEverbloom/AncientProtectors.lua
+++ WoD/TheEverbloom/AncientProtectors.lua
@@ -12,6 +12,7 @@ mod:RegisterEnableMob(83894, 83892, 83893) -- Dulhu, Life Warden Gola, Earthshap
 --
 
 local deathCount = 0
+local golaHasDied = false
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -19,7 +20,12 @@ local deathCount = 0
 
 local L = mod:NewLocale("enUS", true)
 if L then
-	
+	L[83892] = "|cFF00CCFFGola|r"
+	L[83893] = "|cFF00CC00Telu|r"
+
+	L.custom_on_automark = "Auto-Mark Bosses"
+	L.custom_on_automark_desc = "Automatically mark Gola with a {rt8} and Telu with a {rt7}, requires promoted or leader."
+	L.custom_on_automark_icon = 8
 end
 L = mod:GetLocale()
 
@@ -35,15 +41,16 @@ function mod:GetOptions()
 		167977, -- Bramble Patch
 		168383, -- Slash
 		168520, -- Shaper's Fortitude
+		"custom_on_automark",
 		"bosskill",
 	}
 end
 
 function mod:OnBossEnable()
-	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
 
 	self:Log("SPELL_CAST_START", "RevitalizingWaters", 168082)
-	self:Log("SPELL_CAST_START", "RapidTides", 168105)
+	self:Log("SPELL_AURA_APPLIED", "RapidTides", 168105)
 
 	self:Log("SPELL_CAST_START", "Briarskin", 168041)
 	self:Log("SPELL_AURA_APPLIED", "BramblePatch", 167977)
@@ -57,24 +64,44 @@ end
 
 function mod:OnEngage()
 	deathCount = 0
+	golaHasDied = false
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
+	self:CheckBossStatus()
+	if self.db.profile.custom_on_automark then
+		for i = 1, 3 do
+			local unit = ("boss%d"):format(i)
+			local id = self:MobId(UnitGUID(unit))
+			if id == 83892 and not golaHasDied then
+				SetRaidTarget(unit, 8)
+			elseif id == 83893 then
+				SetRaidTarget(unit, golaHasDied and 8 or 7)
+			end
+		end
+	end
+end
+
 -- Life Warden Gola
 function mod:RevitalizingWaters(args)
-	self:Message(args.spellId, "Urgent", "Warning", CL.casting:format(args.spellName))
+	local raidIcon = CombatLog_String_GetIcon(args.sourceRaidFlags)
+	self:Message(args.spellId, "Urgent", "Warning", CL.other:format(raidIcon.. L[83892], CL.casting:format(self:SpellName(31730)))) -- 31730 = "Heal"
 end
 
 function mod:RapidTides(args)
-	self:Message(args.spellId, "Important", "Alarm", CL.casting:format(args.spellName))
+	local raidIcon = CombatLog_String_GetIcon(args.destRaidFlags)
+	local name = L[self:MobId(args.destGUID)] or args.destName
+	self:Message(args.spellId, "Important", self:Dispeller("magic", true) and "Alarm", CL.other:format(args.spellName, raidIcon..name))
 end
 
 -- Earthshaper Telu
 function mod:Briarskin(args)
-	self:Message(args.spellId, "Attention", "Alert", CL.casting:format(args.spellName))
+	local raidIcon = CombatLog_String_GetIcon(args.sourceRaidFlags)
+	self:Message(args.spellId, "Attention", "Alert", CL.other:format(raidIcon.. L[83893], CL.casting:format(args.spellName)))
 end
 
 function mod:BramblePatch(args)
@@ -90,14 +117,19 @@ end
 
 -- General
 function mod:ShapersFortitude(args)
-	self:TargetMessage(args.spellId, args.destName, "Attention")
-	self:Bar(args.spellId, 8)
+	local raidIcon = CombatLog_String_GetIcon(args.destRaidFlags)
+	local name = L[self:MobId(args.destGUID)] or args.destName
+	self:Message(args.spellId, "Attention", nil, CL.other:format(args.spellName, raidIcon..name))
+	self:Bar(args.spellId, 8, CL.other:format(self:SpellName(111923), raidIcon..name)) -- 111923 = "Fortitude"
 end
 
 function mod:Deaths(args)
 	deathCount = deathCount + 1
 	if deathCount > 2 then
 		self:Win()
+	elseif args.mobId == 83892 then
+		golaHasDied = true
+		self:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
 	end
 end
 
