63064701bbe5ca2594d8c240dd041c73bd1e5687
funkydude <funkydude@users.noreply.github.com>
2014-11-24 01:12:38 +0000

    Auchindoun/Kaathar: Fix Consecrated Light duration, add Sanctified Strike warning & timer, add Sanctified Ground under you warning.

--- WoD/Auchindoun/Kaathar.lua
+++ WoD/Auchindoun/Kaathar.lua
@@ -8,6 +8,12 @@ if not mod then return end
 mod:RegisterEnableMob(75839)
 
 --------------------------------------------------------------------------------
+-- Locals
+--
+
+local strikeCount = 0
+
+--------------------------------------------------------------------------------
 -- Localization
 --
 
@@ -25,6 +31,8 @@ function mod:GetOptions()
 	return {
 		153002, -- Holy Shield
 		{153006, "FLASH"}, -- Consecrated Light
+		152954, -- Sanctified Strike
+		153430, -- Sanctified Ground
 		"bosskill",
 	}
 end
@@ -34,12 +42,17 @@ function mod:OnBossEnable()
 
 	self:Log("SPELL_CAST_START", "HolyShield", 153002)
 	self:Log("SPELL_CAST_START", "ConsecratedLight", 153006)
+	self:Log("SPELL_AURA_REMOVED", "ConsecratedLightOver", 153006)
+	self:Log("SPELL_CAST_START", "SanctifiedStrike", 152954)
+	self:Log("SPELL_AURA_APPLIED", "SanctifiedGround", 153430)
 
 	self:Death("Win", 75839)
 end
 
 function mod:OnEngage()
+	strikeCount = 0
 	self:CDBar(153002, 30.5) -- Holy Shield
+	self:CDBar(152954, 7.3) -- Sanctified Strike
 end
 
 --------------------------------------------------------------------------------
@@ -53,13 +66,31 @@ do
 	function mod:HolyShield(args)
 		self:GetBossTarget(printTarget, 0.4, args.sourceGUID)
 		self:CDBar(args.spellId, 47)
-		self:Bar(153006, 7) -- Consecrated Light
+		self:Bar(153006, 7.4) -- Consecrated Light
 	end
 end
 
 function mod:ConsecratedLight(args)
 	self:Message(args.spellId, "Important", "Warning")
-	self:Bar(args.spellId, 12, CL.cast:format(args.spellName))
+	self:Bar(args.spellId, self:Difficulty() == 1 and 12 or 8, CL.cast:format(args.spellName))
 	self:Flash(args.spellId)
+end
+
+function mod:ConsecratedLightOver()
+	self:CDBar(152954, 10.2) -- Sanctified Strike
+end
+
+function mod:SanctifiedStrike(args)
+	strikeCount = strikeCount + 1
+	self:Message(args.spellId, "Important")
+	if strikeCount < 3 then
+		self:CDBar(args.spellId, 8.5)
+	end
+end
+
+function mod:SanctifiedGround(args)
+	if self:Me(args.destGUID) then
+		self:Message(args.spellId, "Personal", "Alarm", CL.underyou:format(args.spellName))
+	end
 end
 
