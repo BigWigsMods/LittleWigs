4ca16937084c5397b7bf6017b9aace2e3902510e
funkydude <funkydude@users.noreply.github.com>
2014-11-25 14:10:57 +0000

    Kyrak: Add Eruption tank warning & fixation cd.

--- WoD/UpperBlackrockSpire/Kyrak.lua
+++ WoD/UpperBlackrockSpire/Kyrak.lua
@@ -26,6 +26,7 @@ function mod:GetOptions()
 		161199, -- Debilitating Fixation
 		161203, -- Rejuvenating Serum
 		161288, -- Vileblood Serum
+		{155037, "TANK"}, -- Eruption
 		"bosskill",
 	}
 end
@@ -35,6 +36,7 @@ function mod:OnBossEnable()
 
 	self:Log("SPELL_CAST_START", "DebilitatingFixation", 161199)
 	self:Log("SPELL_CAST_START", "RejuvenatingSerumIncoming", 161203)
+	self:Log("SPELL_CAST_START", "Eruption", 155037)
 	self:Log("SPELL_CAST_SUCCESS", "RejuvenatingSerum", 161203)
 	self:Log("SPELL_AURA_APPLIED", "VilebloodSerum", 161288)
 
@@ -47,10 +49,16 @@ end
 
 function mod:DebilitatingFixation(args)
 	self:Message(args.spellId, "Attention", "Alert", CL.casting:format(args.spellName))
+	self:CDBar(args.spellId, 20) -- 20-23
 end
 
 function mod:RejuvenatingSerumIncoming(args)
 	self:Message(args.spellId, "Urgent", "Long", CL.incoming:format(args.spellName))
+end
+
+function mod:Eruption(args)
+	local raidIcon = CombatLog_String_GetIcon(args.sourceRaidFlags)
+	self:Message(args.spellId, "Important", "Info", raidIcon.. args.spellName)
 end
 
 function mod:RejuvenatingSerum(args)
