b7ffc36ac01285c7aa88982dd35c3b00c9ad5a9a
funkydude <funkydude@users.noreply.github.com>
2015-02-21 13:16:07 +0000

    Skyreach/Rukhran: Tweak for wowNext/6.2

--- WoD/Skyreach/Rukhran.lua
+++ WoD/Skyreach/Rukhran.lua
@@ -26,7 +26,7 @@ function mod:GetOptions()
 		{153794, "TANK"}, -- Pierce Armor
 		153810, -- Summon Solar Flare
 		159382, -- Quills
-		{176544, "FLASH"}, -- Fixate
+		{167757, "FLASH"}, -- Fixate
 		"bosskill",
 	}
 end
@@ -36,6 +36,7 @@ function mod:OnBossEnable()
 
 	self:RegisterEvent("RAID_BOSS_WHISPER")
 
+	--self:Log("SPELL_AURA_APPLIED", "Fixate", 167757) -- XXX wowNext/6.2
 	self:Log("SPELL_CAST_START", "PierceArmor", 153794)
 	self:Log("SPELL_CAST_START", "SummonSolarFlare", 153810)
 	self:Log("SPELL_CAST_START", "Quills", 159382)
@@ -51,10 +52,17 @@ end
 -- Event Handlers
 --
 
+--[[ XXX wowNext/6.2
+function mod:Fixate(args)
+	self:Message(args.spellId, "Personal", "Alarm", CL.you:format(args.spellName))
+	self:Flash(args.spellId)
+end
+]]
+
 function mod:RAID_BOSS_WHISPER()
 	-- RAID_BOSS_WHISPER#|TInterface\\Icons\\ability_fixated_state_red:20|tA Solar Flare has |cFFFF0000|Hspell:176544|h[Fixated]|h|r on you! If it reaches you it will |cFFFF0000|Hspell:153828|h[Explode]|h|r!#Solar Flare#1#true
-	self:Message(176544, "Personal", "Alarm", CL.you:format(self:SpellName(176544)))
-	self:Flash(176544)
+	self:Message(167757, "Personal", "Alarm", CL.you:format(self:SpellName(167757)))
+	self:Flash(167757)
 end
 
 function mod:PierceArmor(args)
