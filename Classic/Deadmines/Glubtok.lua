b2a3547ae0e5a870170fe0facad57521e232444b
funkydude <funkydude@users.noreply.github.com>
2015-04-21 00:43:17 +0000

    Update Deadmines.

---
+++ Classic/Deadmines/Glubtok.lua
@@ -0,0 +1,41 @@
+
+--------------------------------------------------------------------------------
+-- Module declaration
+--
+
+local mod, CL = BigWigs:NewBoss("Glubtok", 756, 89)
+if not mod then return end
+mod:RegisterEnableMob(47162)
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		"stages",
+	}
+end
+
+function mod:OnBossEnable()
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+
+	self:Death("Win", 47162)
+end
+
+function mod:OnEngage()
+	self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", nil, "boss1")
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
+
+function mod:UNIT_HEALTH_FREQUENT(unit)
+	local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
+	if hp < 56 then
+		self:UnregisterUnitEvent("UNIT_HEALTH_FREQUENT", unit)
+		self:Message("stages", "Neutral", nil, CL.soon:format(CL.phase:format(2)), false)
+	end
+end
+
