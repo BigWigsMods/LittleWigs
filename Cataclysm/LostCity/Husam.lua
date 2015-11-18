522ce5afc28e297af255b3a02de3886587914f05
funkydude <funkydude@users.noreply.github.com>
2015-11-18 14:43:29 +0000

    Add missing LostCity/Husam module, fix module ordering in GUI.

---
+++ Cataclysm/LostCity/Husam.lua
@@ -0,0 +1,53 @@
+
+--------------------------------------------------------------------------------
+-- Module declaration
+--
+
+local mod, CL = BigWigs:NewBoss("General Husam", 747, 117)
+if not mod then return end
+mod:RegisterEnableMob(44577)
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		83445, -- Shockwave
+		91263, -- Detonate Traps
+		83113, -- Bad Intentions
+	}
+end
+
+function mod:OnBossEnable()
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+
+	self:Log("SPELL_CAST_START", "Shockwave", 83445)
+	self:Log("SPELL_CAST_START", "DetonateTraps", 91263)
+	self:Log("SPELL_CAST_SUCCESS", "BadIntentions", 83113)
+	self:Death("Win", 44577)
+end
+
+function mod:OnEngage()
+	self:CDBar(83445, 18) -- Shockwave
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
+
+function mod:Shockwave(args)
+	self:Message(args.spellId, "Important", "Info")
+	self:Bar(args.spellId, 5, CL.casting:format(args.spellName))
+	self:CDBar(args.spellId, 40)
+end
+
+function mod:DetonateTraps(args)
+	self:Message(args.spellId, "Urgent", "Alarm")
+end
+
+function mod:BadIntentions(args)
+	self:TargetMessage(args.spellId, args.destName, "Attention", "Alert")
+	self:CDBar(args.spellId, 25)
+end
+
