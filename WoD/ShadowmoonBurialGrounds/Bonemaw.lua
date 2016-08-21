fa1d66db3a7d33b82b18d891205f71ab80d8115f
funkydude <funkydude@users.noreply.github.com>
2016-08-21 02:01:36 +0000

    WoD/ShadowmoonBurialGrounds/Bonemaw: Revert change to emote

--- WoD/ShadowmoonBurialGrounds/Bonemaw.lua
+++ WoD/ShadowmoonBurialGrounds/Bonemaw.lua
@@ -32,7 +32,7 @@ end
 function mod:OnBossEnable()
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 
-	self:Emote("InhaleInc", "153804")
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "InhaleIncUnitEvent", "boss1")
 	self:Log("SPELL_CAST_SUCCESS", "Inhale", 153804)
 	self:Log("SPELL_CAST_START", "BodySlam", 154175)
 
@@ -42,6 +42,14 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:InhaleIncUnitEvent(_, _, _, _, spellId)
+	if spellId == 154868 then
+		-- Unit event is 1s faster than emote, but only works for first Inhale, so register Emote after that.
+		self:InhaleInc()
+		self:ScheduleTimer("Emote", 5, "InhaleInc", "153804")
+	end
+end
 
 function mod:InhaleInc()
 	self:Message(153804, "Urgent", "Warning", CL.incoming:format(self:SpellName(153804)))
