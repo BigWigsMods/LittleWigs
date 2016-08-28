62bd4027d84edde0abbe3e8e1e38dfe26c4a9195
Ontyftw <Ontyftw@users.noreply.github.com>
2016-08-28 03:44:02 +0200

    Add Court of Stars (#2)

--- Legion/CourtOfStars/PatrolCaptainGerdo.lua
+++ Legion/CourtOfStars/PatrolCaptainGerdo.lua
@@ -1,3 +1,5 @@
+--TO DO
+--Fix timers
 
 --------------------------------------------------------------------------------
 -- Module Declaration
@@ -9,32 +11,59 @@ mod:RegisterEnableMob(104215)
 --mod.engageId = 1868
 
 --------------------------------------------------------------------------------
--- Locals
---
-
-
---------------------------------------------------------------------------------
 -- Initialization
 --
 
 function mod:GetOptions()
 	return {
-		153764, -- Claws of Argus
-		{153392, "FLASH", "ICON", "PROXIMITY"}, -- Curtain of Flame
+		207261, -- Resonant Slash
+		219488, -- Streetsweeper
+		207278, -- Arcane Lockdown
+		207806, -- Signal Beacon
+		207815, -- Flask of the Solemn Night
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 
+	self:Log("SPELL_CAST_START", "ResonantSlash", 207261)
+	self:Log("SPELL_CAST_SUCCESS", "Streetsweeper", 219488)
+	self:Log("SPELL_CAST_START", "ArcaneLockdown", 207278)
+	self:Log("SPELL_CAST_START", "SignalBeacon", 207806)
+	self:Log("SPELL_CAST_START", "FlaskoftheSolemnNight", 207815)
+
 	self:Death("Win", 104215)
 end
 
 function mod:OnEngage()
-
+	--self:CDBar(??, ??)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:ResonantSlash(args)
+	self:Message(args.spellId, "Urgent", "Alarm")
+	--self:CDBar(args.spellId, ??)
+end
+
+function mod:Streetsweeper(args)
+	self:Message(args.spellId, "Important", "Info")
+	--self:CDBar(args.spellId, ??)
+end
+
+function mod:ArcaneLockdown(args)
+	self:Message(args.spellId, "Attention", "Long", CL.incoming:format(args.spellName))
+	--self:CDBar(args.spellId, ??)
+end
+
+function mod:SignalBeacon(args)
+	self:Message(args.spellId, "Attention", "Alert")
+	--self:CDBar(args.spellId, ??)
+end
+
+function mod:FlaskoftheSolemnNight(args)
+	self:Message(args.spellId, "Attention", "Info")
+end
--- Legion/CourtOfStars/TalixaeFlamewreath.lua
+++ Legion/CourtOfStars/TalixaeFlamewreath.lua
@@ -1,3 +1,6 @@
+--TO DO
+--Fix timers
+--maybe some sort of dispel warning when people reach x stacks of Withering Soul?
 
 --------------------------------------------------------------------------------
 -- Module Declaration
@@ -12,6 +15,7 @@ mod:RegisterEnableMob(104217)
 -- Locals
 --
 
+local burningIntensityCount = 0
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -19,22 +23,43 @@ mod:RegisterEnableMob(104217)
 
 function mod:GetOptions()
 	return {
-		153764, -- Claws of Argus
-		{153392, "FLASH", "ICON", "PROXIMITY"}, -- Curtain of Flame
+		207881, -- Infernal Eruption
+		207906, -- Burning Intensity
+		208165, -- Withering Soul
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 
+	self:Log("SPELL_CAST_START", "InfernalEruption", 207881)
+	self:Log("SPELL_CAST_START", "BurningIntensity", 207906)
+	self:Log("SPELL_CAST_START", "WitheringSoul", 208165)
+
 	self:Death("Win", 104217)
 end
 
 function mod:OnEngage()
-
+	burningIntensityCount = 0
+	--self:CDBar(??, ??)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:InfernalEruption(args)
+	self:Message(args.spellId, "Urgent", "Long")
+	--self:CDBar(args.spellId, ??)
+end
+
+function mod:BurningIntensity(args)
+	burningIntensityCount = burningIntensityCount + 1
+	self:Message(args.spellId, "Important", "Info", CL.count:format(args.spellName, burningIntensityCount))
+	--self:CDBar(args.spellId, ??)
+end
+
+function mod:WitheringSoul(args)
+	self:Message(args.spellId, "Attention", "Alert")
+	--self:CDBar(args.spellId, ??)
+end
