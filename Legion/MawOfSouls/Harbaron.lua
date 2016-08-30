261c4a4f4f785fef0129c5acb7d76603ca6daaa3
Saccarab <akaya2@binghamton.edu>
2016-08-31 00:44:36 +0300

    Add MoS
    Maw of Souls ADDED

--- Legion/MawOfSouls/Harbaron.lua
+++ Legion/MawOfSouls/Harbaron.lua
@@ -9,32 +9,54 @@ mod:RegisterEnableMob(96754)
 --mod.engageId = 1823
 
 --------------------------------------------------------------------------------
--- Locals
---
-
-
---------------------------------------------------------------------------------
 -- Initialization
 --
 
 function mod:GetOptions()
 	return {
-		153764, -- Claws of Argus
-		{153392, "FLASH", "ICON", "PROXIMITY"}, -- Curtain of Flame
+		 194216, -- Cosmic Scythe
+		 194231, -- Summon Shackled Servitor
+		 194668, -- Nether Rip
+		 194325, -- Fragment
+		 {198551, "SAY"}, -- FragmentApplied
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
-
+	self:Log("SPELL_CAST_START", "ShackledServitor", 194231) --cd varies between 23-26
+	self:Log("SPELL_CAST_START", "NetherRip", 194668)
+	self:Log("SPELL_CAST_START", "Fragment", 194325)
+	self:Log("SPELL_AURA_APPLIED", "FragmentApplied", 198551) -- aura apply id
 	self:Death("Win", 96754)
 end
 
 function mod:OnEngage()
-
+	self:Bar(194216, 3.6) -- Cosmic scythe
+   self:CDBar(194231, 8) -- Shackled
+   self:CDBar(194668, 12.5) -- nether rip
+   self:CDBar(194325, 18) -- fragment
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+function mod:Fragment(args)
+	self:Bar(args.spellId, 30)
+end
 
+function mod:FragmentApplied(args)
+	if self:Me(args.destGUID) then
+		self:Say(args.spellId)
+		self:Message(args.spellId, "Attention", "Warning", CL.incoming:format(args.spellName))
+	end
+end
+
+function mod:NetherRip(args)
+	self:CDBar(194668, 13.5)
+end
+
+function mod:ShackledServitor(args)
+	self:CDBar(194231, 24)
+	self:Message(args.spellId, "Attention", "Info", CL.incoming:format(args.spellName))
+end
--- Legion/MawOfSouls/Ymiron.lua
+++ Legion/MawOfSouls/Ymiron.lua
@@ -7,34 +7,59 @@ local mod, CL = BigWigs:NewBoss("Ymiron", 1042, 1502)
 if not mod then return end
 mod:RegisterEnableMob(96756)
 --mod.engageId = 1822
-
 --------------------------------------------------------------------------------
--- Locals
---
 
-
---------------------------------------------------------------------------------
 -- Initialization
 --
-
 function mod:GetOptions()
 	return {
-		153764, -- Claws of Argus
-		{153392, "FLASH", "ICON", "PROXIMITY"}, -- Curtain of Flame
+		193977, -- Winds of the Northrend
+		193211, -- Dark Slash
+		193364, -- Screams of the dead
+		193460, -- Bane
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
-
+	self:Log("SPELL_CAST_START", "Winds", 193977)
+   self:Log("SPELL_CAST_START", "DarkSlash", 193211)
+   self:Log("SPELL_CAST_START", "Screams", 193364)
+   self:Log("SPELL_CAST_START", "Bane", 193460)
+	self:Log("SPELL_CAST_START", "Arise", 193566)
 	self:Death("Win", 96756)
 end
 
 function mod:OnEngage()
-
+	self:Bar(193211, 3.5) -- Dark Slash
+	self:CDBar(193364, 5.9) -- Screams
+	self:CDBar(193977, 15.1) -- Winds
+	self:CDBar(193460, 22.1) -- Bane
+	self:CDBar(193566, 41.2) -- Arise
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+function mod:Winds(args)
+	self:Bar(args.spellId, 29)
+	self:Message(args.spellId, "Attention", "Warning", CL.incoming:format(args.spellName))
+end
 
+function mod:DarkSlash(args)
+	self:Bar(args.spellId, 15.5)
+	self:Message(args.spellId, "Alert", "Info", CL.incoming:format(args.spellName))
+end
+
+function mod:Screams(args)
+	self:Bar(args.spellId, 31)
+	self:Message(args.spellId, "Alert", "Warning", CL.incoming:format(args.spellName))
+end
+
+function mod:Bane(args)
+	self:Bar(args.spellId, 59)
+end
+
+function mod:Arise(args)
+	self:Message(args.spellId, "Attention", "Warning", CL.incoming:format(args.spellName))
+end
