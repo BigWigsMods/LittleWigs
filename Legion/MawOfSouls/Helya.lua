2e1217b4c638b1eb7df577f794fd290b33b12fc8
Ali San Kaya <akaya2@binghamton.edu>
2016-09-02 18:26:05 +0300

    Add Maw of Souls (#3)
    [skip ci]

--- Legion/MawOfSouls/Helya.lua
+++ Legion/MawOfSouls/Helya.lua
@@ -11,30 +11,70 @@ mod:RegisterEnableMob(96759)
 --------------------------------------------------------------------------------
 -- Locals
 --
-
-
+local firstTorrent = 0
+local afterCorrupted = 1
 --------------------------------------------------------------------------------
 -- Initialization
 --
 
 function mod:GetOptions()
 	return {
-		153764, -- Claws of Argus
-		{153392, "FLASH", "ICON", "PROXIMITY"}, -- Curtain of Flame
+		197517, -- Taint of the sea
+		{197262, "SAY"}, -- TaintApplied
+		198495, -- Torrent
+		227233, -- Corrupted Below
+		196947, -- Submerged
+		202088, -- BrackwaterBarrage
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
-
+	self:Log("SPELL_AURA_APPLIED", "TaintApplied", 197262) -- aura apply id
+	self:Log("SPELL_CAST_START", "CorruptedBelow", 227233)
+	self:Log("SPELL_CAST_START", "Submerged", 196947)
+	self:Log("SPELL_CAST_START", "BrackwaterBarrage", 202088)
+	self:Log("SPELL_CAST_START", "Torrent", 198495)
 	self:Death("Win", 96759)
 end
 
 function mod:OnEngage()
-
+	afterCorrupted = 0
+	firstTorrent = 0
+ 	self:CDBar(202088, 40.5) -- Barrage
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+function mod:TaintApplied(args)
+	self:TargetMessage(args.spellId, args.destGUID, "Attention", "Alert", nil, nil, self:Dispeller())
+	if self:Me(args.destGUID) then
+		self:Say(args.spellId)
+	end
+end
 
+function mod:Submerged(args)
+	self:StopBar(198495)
+	firstTorrent = 0
+end
+
+function mod:Torrent(args)
+	if afterCorrupted == 0 then
+		self:CDBar(198495, 11)
+	else
+		self:CDBar(198495, 13.5)
+	end
+
+	if firstTorrent == 0 then
+		self:CDBar(20585, 59) -- Wisp Icon for intermission phase
+	end
+
+	firstTorrent = 1
+	afterCorrupted = 0
+	self:Message(args.spellId, "Important", "Warning", CL.incoming:format(args.spellName))
+end
+
+function mod:CorruptedBelow(args)
+	afterCorrupted = 1
+end
