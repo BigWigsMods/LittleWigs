c1db49182d674ec8abc1e083effbc0f26f022f69
funkydude <funkydude@users.noreply.github.com>
2016-09-04 00:38:51 +0100

    NeltharionsLair/Rokmora: Add Choking Dust warning

--- Legion/NeltharionsLair/Rokmora.lua
+++ Legion/NeltharionsLair/Rokmora.lua
@@ -16,12 +16,17 @@ function mod:GetOptions()
 	return {
 		188169, -- Razor Shards
 		188114, -- Shatter
+		192800, -- Choking Dust
 	}
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "RazorShards", 188169)
 	self:Log("SPELL_CAST_START", "Shatter", 188114)
+
+	self:Log("SPELL_AURA_APPLIED", "ChokingDustDamage", 192800)
+	self:Log("SPELL_PERIODIC_DAMAGE", "ChokingDustDamage", 192800)
+	self:Log("SPELL_PERIODIC_MISSED", "ChokingDustDamage", 192800)
 end
 
 function mod:OnEngage()
@@ -34,12 +39,23 @@ end
 --
 
 function mod:RazorShards(args)
-	self:Message(args.spellId, "orange", "Warning", CL.incoming:format(args.spellName))
-	self:CDBar(args.spellId, 29)
+	self:Message(args.spellId, "Urgent", "Warning", CL.incoming:format(args.spellName))
+	self:CDBar(args.spellId, 29) -- pull:25.6, 29.1, 29.9
 end
 
 function mod:Shatter(args)
-	self:Message(args.spellId, "yellow", "Alert")
-	self:CDBar(args.spellId, 24)
+	self:Message(args.spellId, "Attention", "Alert")
+	self:CDBar(args.spellId, 24) -- pull:20.7, 24.3, 25.1
+end
+
+do
+	local prev = 0
+	function mod:ChokingDustDamage(args)
+		local t = GetTime()
+		if self:Me(args.destGUID) and t-prev > 2 then
+			prev = t
+			self:Message(192800, "Personal", "Alarm", CL.underyou:format(args.spellName))
+		end
+	end
 end
 
