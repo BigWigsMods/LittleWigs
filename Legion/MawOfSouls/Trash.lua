3700c151377aad38f47a33d0e7d89b65e6f63580
elvador <reysptools@googlemail.com>
2016-09-09 01:20:41 +0200

    MawOfSouls/Trash: Add another trash mob

--- Legion/MawOfSouls/Trash.lua
+++ Legion/MawOfSouls/Trash.lua
@@ -8,6 +8,7 @@ if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
 	97097, -- Helarjar Champion
+	97182, -- Night Watch Mariner
 	97365, -- Seacursed Mistmender
 	99033, -- Helarjar Mistcaller
 	99307 -- Skjal
@@ -20,6 +21,7 @@ mod:RegisterEnableMob(
 local L = mod:NewLocale("enUS", true)
 if L then
 	L.champion = "Helarjar Champion"
+	L.mariner = "Night Watch Mariner"
 	L.mistmender = "Seacursed Mistmender"
 	L.mistcaller = "Helarjar Mistcaller"
 	L.skjal = "Skjal"
@@ -33,12 +35,14 @@ L = mod:GetLocale()
 function mod:GetOptions()
 	return {
 		198405, -- Bone Chilling Scream
+		192019, -- Lantern of Darkness
 		199514, -- Torrent of Souls
 		199589, -- Whirlpool of Souls
 		216197, -- Surging Waters
 		195293, -- Debilitating Shout
 	}, {
 		[198405] = L.champion,
+		[192019] = L.mariner,
 		[199514] = L.mistmender,
 		[199589] = L.mistcaller,
 		[195293] = L.skjal,
@@ -49,6 +53,7 @@ function mod:OnBossEnable()
 	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
 
 	self:Log("SPELL_CAST_START", "Casts", 198405, 199514, 199589, 216197, 195293)
+	self:Log("SPELL_CAST_START", "LanternOfDarkness", 192019)
 end
 
 --------------------------------------------------------------------------------
@@ -57,4 +62,9 @@ end
 
 function mod:Casts(args)
 	self:Message(args.spellId, "Important", "Alert")
+end
+
+function mod:LanternOfDarkness(args)
+	self:Message(args.spellId, "Attention", "Long")
+	self:Bar(args.spellId, 7, CL.cast:format(args.spellName))
 end
