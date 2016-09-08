c7b11cb07d0546a266ef2d024218bf703fdc8e3f
elvador <reysptools@googlemail.com>
2016-09-09 01:05:28 +0200

    HallsOfValor/Trash: More trash spells

--- Legion/HallsOfValor/Trash.lua
+++ Legion/HallsOfValor/Trash.lua
@@ -1,5 +1,9 @@
 
 --------------------------------------------------------------------------------
+-- TODO List:
+-- - Confirm 198931 (Healing Light), 198934 (Rune of Healing) spell ids
+
+--------------------------------------------------------------------------------
 -- Module Declaration
 --
 
@@ -7,12 +11,14 @@ local mod, CL = BigWigs:NewBoss("Halls of Valor Trash", 1041)
 if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
-	97197, -- Valarjar Purifier
 	97081, -- King Bjorn
 	95843, -- King Haldor
 	97083, -- King Ranulf
 	97084, -- King Tor
 	97202, -- Olmyr the Enlightened
+	95834, -- Valarjar Mystic
+	95842, -- Valarjar Thundercaller
+	97197, -- Valarjar Purifier
 	101637 -- Valarjar Aspirant
 )
 
@@ -22,9 +28,11 @@ mod:RegisterEnableMob(
 
 local L = mod:NewLocale("enUS", true)
 if L then
-	L.purifier = "Valarjar Purifier"
 	L.fourkings = "The Four Kings"
 	L.olmyr = "Olmyr the Enlightened"
+	L.purifier = "Valarjar Purifier"
+	L.thundercaller = "Valarjar Thundercaller"
+	L.mystic = "Valarjar Mystic"
 	L.aspirant = "Valarjar Aspirant"
 end
 L = mod:GetLocale()
@@ -39,11 +47,16 @@ function mod:GetOptions()
 		199726, -- Unruly Yell
 		192158, -- Sanctify
 		191508, -- Blast of Light
+		{215430, "SAY", "FLASH", "PROXIMITY"}, -- Thunderstrike
+		198931, -- Healing Light
+		198934, -- Rune of Healing
 	}, {
 		[192563] = L.purifier,
 		[199726] = L.fourkings,
 		[192158] = L.olmyr,
 		[191508] = L.aspirant,
+		[215430] = L.thundercaller,
+		[198931] = L.mystic,
 	}
 end
 
@@ -51,7 +64,12 @@ function mod:OnBossEnable()
 	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
 
 	--[CLEU] SPELL_CAST_START#Creature-0-2084-1477-6795-99891-00003FDF54#Storm Drake##nil#198892#Crackling Storm#nil#nil
-	self:Log("SPELL_CAST_START", "Casts", 192563, 199726, 192158, 191508) -- Cleansing Flames, Unruly Yell
+
+	-- Cleansing Flames, Unruly Yell, Sanctify, Blast of Light, Healing Light, Rune of Healing
+	self:Log("SPELL_CAST_START", "Casts", 192563, 199726, 192158, 191508, 198931, 198934)
+
+	self:Log("SPELL_AURA_APPLIED", "Thunderstrike", 215430)
+	self:Log("SPELL_AURA_REMOVED", "ThunderstrikeRemoved", 215430)
 
 	--self:Death("Disable", 97197) -- Valarjar Purifier
 end
@@ -62,4 +80,20 @@ end
 
 function mod:Casts(args)
 	self:Message(args.spellId, "Important", "Alert")
+end
+
+function mod:Thunderstrike(args)
+	self:TargetMessage(args.spellId, args.destName, "Urgent", "Alarm")
+	self:TargetBar(args.spellId, 3, args.destName)
+	if self:Me(args.destGUID) then
+		self:Flash(args.spellId)
+		self:Say(args.spellId)
+		self:OpenProximity(args.spellId, 8)
+	end
+end
+
+function mod:ThunderstrikeRemoved(args)
+	if self:Me(args.destGUID) then
+		self:CloseProximity(args.spellId)
+	end
 end
