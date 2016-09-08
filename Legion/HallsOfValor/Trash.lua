9e8cd4b98616a9da3b54aaedfa00379c47fc7b30
elvador <reysptools@googlemail.com>
2016-09-08 18:18:27 +0200

    HallsOfValor/Trash: Adding some trash spells

--- Legion/HallsOfValor/Trash.lua
+++ Legion/HallsOfValor/Trash.lua
@@ -11,7 +11,9 @@ mod:RegisterEnableMob(
 	97081, -- King Bjorn
 	95843, -- King Haldor
 	97083, -- King Ranulf
-	97084 -- King Tor
+	97084, -- King Tor
+	97202, -- Olmyr the Enlightened
+	101637 -- Valarjar Aspirant
 )
 
 --------------------------------------------------------------------------------
@@ -22,6 +24,8 @@ local L = mod:NewLocale("enUS", true)
 if L then
 	L.purifier = "Valarjar Purifier"
 	L.fourkings = "The Four Kings"
+	L.olmyr = "Olmyr the Enlightened"
+	L.aspirant = "Valarjar Aspirant"
 end
 L = mod:GetLocale()
 
@@ -33,9 +37,13 @@ function mod:GetOptions()
 	return {
 		192563, -- Cleansing Flames
 		199726, -- Unruly Yell
+		192158, -- Sanctify
+		191508, -- Blast of Light
 	}, {
 		[192563] = L.purifier,
 		[199726] = L.fourkings,
+		[192158] = L.olmyr,
+		[191508] = L.aspirant,
 	}
 end
 
@@ -43,7 +51,7 @@ function mod:OnBossEnable()
 	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
 
 	--[CLEU] SPELL_CAST_START#Creature-0-2084-1477-6795-99891-00003FDF54#Storm Drake##nil#198892#Crackling Storm#nil#nil
-	self:Log("SPELL_CAST_START", "Casts", 192563, 199726) -- Cleansing Flames, Unruly Yell
+	self:Log("SPELL_CAST_START", "Casts", 192563, 199726, 192158, 191508) -- Cleansing Flames, Unruly Yell
 
 	--self:Death("Disable", 97197) -- Valarjar Purifier
 end
@@ -55,4 +63,3 @@ end
 function mod:Casts(args)
 	self:Message(args.spellId, "Important", "Alert")
 end
-
