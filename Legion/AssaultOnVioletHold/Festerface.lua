d2ace6cb5c5b28cd361c99a89e8be1a82b7b798c
elvador <reysptools@googlemail.com>
2016-09-09 04:27:58 +0200

    AssaultOnVioletHold/Festerface: Add module

--- Legion/AssaultOnVioletHold/Festerface.lua
+++ Legion/AssaultOnVioletHold/Festerface.lua
@@ -6,7 +6,7 @@
 local mod, CL = BigWigs:NewBoss("Festerface", 1066, 1693)
 if not mod then return end
 mod:RegisterEnableMob(101995)
---mod.engageId = 1848
+mod.engageId = 1848
 
 --------------------------------------------------------------------------------
 -- Locals
@@ -19,22 +19,39 @@ mod:RegisterEnableMob(101995)
 
 function mod:GetOptions()
 	return {
-		153764, -- Claws of Argus
-		{153392, "FLASH", "ICON", "PROXIMITY"}, -- Curtain of Flame
+		201598, -- Congealing Vomit
+		201729, -- Uh Oh... (warning for high energy)
 	}
 end
 
 function mod:OnBossEnable()
-	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
-
-	self:Death("Win", 101995)
+	self:Log("SPELL_CAST_START", "CongealingVomit", 201598)
+	self:RegisterUnitEvent("UNIT_POWER_FREQUENT", nil, "boss1")
 end
 
 function mod:OnEngage()
-
+	self:Bar(201598, 11) -- Congealing Vomit
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:CongealingVomit(args)
+	self:Message(args.spellId, "Important", "Alarm")
+	self:Bar(args.spellId, 51)
+end
+
+do
+	local prev = 0
+	function mod:UNIT_POWER_FREQUENT(unit)
+		local power = UnitPower(unit)
+		if power > 70 then
+			local t = GetTime()
+			if t-prev > 5 then
+				prev = t
+				self:Message(201729, "Urgent", "Alert", CL.soon:format(self:SpellName(201729))) -- Uh Oh... soon!
+			end
+		end
+	end
+end
