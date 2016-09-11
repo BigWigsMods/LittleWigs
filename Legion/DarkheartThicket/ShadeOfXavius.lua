def09ef309b133f10e4e610e7c4261d4ad8fd546
funkydude <funkydude@users.noreply.github.com>
2016-09-11 04:25:15 +0100

    DarkheartThicket/ShadeOfXavius: Fix Nightmare Bolt

--- Legion/DarkheartThicket/ShadeOfXavius.lua
+++ Legion/DarkheartThicket/ShadeOfXavius.lua
@@ -15,7 +15,7 @@ mod.engageId = 1839
 function mod:GetOptions()
 	return {
 		{200289, "ICON", "SAY"}, -- Growing Paranoia
-		{212834, "ICON", "SAY"}, -- Nightmare Bolt
+		{200185, "ICON", "SAY"}, -- Nightmare Bolt
 	}
 end
 
@@ -23,12 +23,13 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "GrowingParanoia", 200289)
 	self:Log("SPELL_AURA_APPLIED", "GrowingParanoiaApplied", 200289)
 	self:Log("SPELL_AURA_REMOVED", "GrowingParanoiaRemoved", 200289)
-	self:Log("SPELL_CAST_START", "NightmareBolt", 212834)
+	self:Log("SPELL_CAST_START", "NightmareBolt", 212834, 200185) -- Normal, Heroic+
+	self:Log("SPELL_AURA_REMOVED", "WakingNightmareOver", 200243)
 end
 
 function mod:OnEngage()
-	self:CDBar(200289, 28) -- Growing Paranoia
-	self:CDBar(212834, 8) -- Nightmare Bolt
+	self:CDBar(200289, 27) -- Growing Paranoia
+	self:CDBar(200185, 8) -- Nightmare Bolt
 end
 
 --------------------------------------------------------------------------------
@@ -37,7 +38,7 @@ end
 
 function mod:GrowingParanoia(args)
 	self:TargetMessage(args.spellId, args.destName, "Attention", "Alarm")
-	self:CDBar(args.spellId, 20) -- pull:28.4, 20.6, 21.9
+	self:CDBar(args.spellId, self:Normal() and 20 or 27) -- pull:28.4, 20.6, 21.9 / hc pull:27.1, 32.8, 26.7, 27.9
 end
 
 function mod:GrowingParanoiaApplied(args)
@@ -53,17 +54,25 @@ end
 
 do
 	local function printTarget(self, player, guid)
-		self:TargetMessage(212834, player, "Urgent", "Alert", nil, nil, true)
+		self:TargetMessage(200185, player, "Urgent", "Alert", nil, nil, true)
 
-		if self:Normal() then return end
-		if self:Me(guid) then
-			self:Say(args.spellId)
+		if not self:Normal() then
+			if self:Me(guid) then
+				self:Say(200185)
+			end
+			self:SecondaryIcon(200185, player)
 		end
-		-- XXX icon?
 	end
 	function mod:NightmareBolt(args)
 		self:GetBossTarget(printTarget, 0.4, args.sourceGUID)
-		self:CDBar(args.spellId, 23) -- pull:8.9, 23.0, 23.1
+		if args.spellId == 212834 then -- Normal
+			self:CDBar(200185, 23) -- pull:8.9, 23.0, 23.1
+		else
+			self:CDBar(200185, 23) -- XXX hc pull:9.1, 23.1, 37.6, 21.8 -- hc pull:9.4, 23.5, 21.8
+		end
+	end
+	function mod:WakingNightmareOver()
+		self:SecondaryIcon(200185)
 	end
 end
 
