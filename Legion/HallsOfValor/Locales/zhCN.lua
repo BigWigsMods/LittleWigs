67f3170b99827862348aaba70ac51921437b1d2f
funkydude <funkydude@users.noreply.github.com>
2016-09-14 12:22:27 +0100

    Add locale to Halls of Valor

---
+++ Legion/HallsOfValor/Locales/zhCN.lua
@@ -0,0 +1,19 @@
+local L = BigWigs:NewBossLocale("Odyn", "zhCN")
+if not L then return end
+if L then
+	--L[197963] = "|cFF800080Top Right|r (|T1323037:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top Right"
+	--L[197964] = "|cFFFFA500Bottom Right|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Bottom Right"
+	--L[197965] = "|cFFFFFF00Bottom Left|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Bottom Left"
+	--L[197966] = "|cFF0000FFTop Left|r (|T1323035:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top Left"
+	--L[197967] = "|cFF008000Top|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top"
+end
+
+L = BigWigs:NewBossLocale("Halls of Valor Trash", "zhCN")
+if L then
+	--L.fourkings = "The Four Kings"
+	--L.olmyr = "Olmyr the Enlightened"
+	--L.purifier = "Valarjar Purifier"
+	--L.thundercaller = "Valarjar Thundercaller"
+	--L.mystic = "Valarjar Mystic"
+	--L.aspirant = "Valarjar Aspirant"
+end
