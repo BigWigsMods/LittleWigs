80132d024327accffad21f7fe220925597f629a9
funkydude <funkydude@users.noreply.github.com>
2016-10-22 03:03:40 +0100

    TBC/TheShatteredHalls: Remove some sync events

--- TBC/TheShatteredHalls/Omrogg.lua
+++ TBC/TheShatteredHalls/Omrogg.lua
@@ -18,9 +18,7 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
-	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "target", "focus")
-	self:AddSyncListener("Beatdown")
-
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
 	self:Death("Win", 16809)
 end
 
@@ -30,13 +28,6 @@ end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, _, spellId)
 	if spellId == 30618 then -- Beatdown
-		self:Sync("Beatdown")
-	end
-end
-
-function mod:OnSync(sync)
-	if sync == "Beatdown" then
 		self:Message(-5894, "Attention", "Warning")
 	end
 end
-
