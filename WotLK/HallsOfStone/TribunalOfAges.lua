1151fa340c5330064e434b844ccd38b23cb0ec0a
funkydude <funkydude@users.noreply.github.com>
2016-10-23 03:05:28 +0100

    Remove RegisterEnableYell usage

--- WotLK/HallsOfStone/TribunalOfAges.lua
+++ WotLK/HallsOfStone/TribunalOfAges.lua
@@ -1,5 +1,6 @@
--------------------------------------------------------------------------------
---  Module Declaration
+--------------------------------------------------------------------------------
+-- Module Declaration
+--
 
 local mod = BigWigs:NewBoss("Tribunal of Ages", 526)
 if not mod then return end
@@ -8,44 +9,59 @@ mod.otherMenu = "The Storm Peaks"
 mod:RegisterEnableMob(28070)
 mod.toggleOptions = {"timers"}
 
--------------------------------------------------------------------------------
---  Localization
+--------------------------------------------------------------------------------
+-- Localization
+--
 
 local L = mod:NewLocale("enUS", true)
 if L then
---@do-not-package@
-L["moduleName"] = "Tribunal of Ages"
+	L["moduleName"] = "Tribunal of Ages"
 
-L["enable_trigger"] = "Time to get some answers"
-L["engage_trigger"] = "Now keep an eye out"
-L["defeat_trigger"] = "The old magic fingers"
-L["fail_trigger"] = "Not yet, not"
+	L["enable_trigger"] = "Time to get some answers"
+	L["engage_trigger"] = "Now keep an eye out"
+	L["defeat_trigger"] = "The old magic fingers"
+	L["fail_trigger"] = "Not yet, not"
 
-L["timers"] = "Timers"
-L["timers_desc"] = "Timers for various events that take place."
+	L["timers"] = "Timers"
+	L["timers_desc"] = "Timers for various events that take place."
 
-L["wave"] = "First wave!"--leaving this just incase I revert the warmup
-L["victory"] = "Victory!"--@end-do-not-package@
---@localization(locale="enUS", namespace="Storm_Peaks/Tribunal_of_Ages", format="lua_additive_table", handle-unlocalized="ignore")@
+	L["wave"] = "First wave!"--leaving this just incase I revert the warmup
+	L["victory"] = "Victory!"
 end
 L = mod:GetLocale()
 
 mod.displayName = L["moduleName"]
 
--------------------------------------------------------------------------------
---  Initialization
+--------------------------------------------------------------------------------
+-- Initialization
+--
 
 function mod:OnRegister()
-	self:RegisterEnableYell(L["enable_trigger"])
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 end
+mod.OnBossDisable = mod.OnRegister
 
 function mod:OnBossEnable()
-	self:Yell("Engage", L["engage_trigger"])
-	self:Yell("Reboot", L["fail_trigger"])
-	self:Yell("Win", L["defeat_trigger"])
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 end
 
 function mod:OnEngage()
 	self:Bar("timers", self.displayName, 45, "Achievement_Character_Dwarf_Male")
 	self:Bar("timers", L["victory"], 315, "INV_Misc_PocketWatch_01")
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
+
+function mod:CHAT_MSG_MONSTER_YELL(_, msg)
+	if msg == L.enable_trigger or msg:find(L.enable_trigger, nil, true) then
+		self:Enable()
+	elseif msg == L.fail_trigger or msg:find(L.fail_trigger, nil, true) then
+		self:Reboot()
+	elseif msg == L.engage_trigger or msg:find(L.engage_trigger, nil, true) then
+		self:Engage()
+	elseif msg == L.defeat_trigger or msg:find(L.defeat_trigger, nil, true) then
+		self:Win()
+	end
 end
