5d595d1abc176fbc1e9e392ccae0545808bebf0a
Saccarab <akaya2@binghamton.edu>
2016-11-04 22:55:59 +0300

    Maiden Fixes

--- Legion/Karazhan/MaidenOfVirtue.lua
+++ Legion/Karazhan/MaidenOfVirtue.lua
@@ -9,14 +9,17 @@ local mod, CL = BigWigs:NewBoss("Maiden of Virtue", 1115, 1825)
 if not mod then return end
 mod:RegisterEnableMob(113971)
 mod.engageId = 1954
-
+--------------------------------------------------------------------------------
+-- Locals
+--
+local sacredCount = 1
 --------------------------------------------------------------------------------
 -- Initialization
 --
 
 function mod:GetOptions()
 	return {
-		227817, -- Holy Bulwark
+		227817, -- Holy Bulwark Removed
 		227823, -- Holy Wrath
 		227800, -- Holy Shock
 		227809, -- Holy Bolt
@@ -26,14 +29,16 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
+	self:log("SPELL_AURA_REMOVED", "HolyBulwarkRemoved" 227817)
 	self:Log("SPELL_CAST_START", "HolyShock", 227800)
 	self:Log("SPELL_CAST_START", "HolyWrath", 227823)
 	self:Log("SPELL_CAST_START", "HolyBolt", 227809)
 	self:Log("SPELL_CAST_START", "MassRepentance", 227509)
-	self:Log("SPELL_CAST_START", "Sacred Ground", 227789)
+	self:Log("SPELL_CAST_START", "SacredGround", 227789)
 end
 
 function mod:OnEngage()
+	sacredCount = 1
 	self:Bar(227809, 8) -- Holy Bolt
 	self:Bar(227789, 10.1) -- Sacred Ground
 	self:Bar(227800, 15) -- Holy Shock
@@ -44,8 +49,8 @@ end
 -- Event Handlers
 --
 function mod:SacredGround(args)
-	self:CDBar(args.spellId, 25)
-	self:TargetMessage(args.spellId, args.destName, "Important", "Alarm")
+	self:CDBar(217887, sacredCount % 2 == 1 and 24 or 32)
+	self:Message(args.spellId, args.destName, "Important", "Alarm")
 	if self:Me(args.destGUID) then
 		self:Say(args.spellId)
 		self:Flash(args.spellId)
@@ -55,18 +60,23 @@ end
 function mod:HolyShock(args)
 	self:CDBar(args.spellId, 12)
 	if self:Interrupter(args.sourceGUID) then
-		self:TargetMessage(args.spellId, args.sourceName, "Attention", "Alarm")
+		self:Message(args.spellId, "Attention", "Alarm", CL.incoming:format(args.spellName))
 	end
 end
 
 function mod:HolyWrath(args)
 	self:Message(args.spellId, "Important", "Alarm", CL.incoming:format(args.spellName))
-	self:CDBar(227789, 11)
+	self:CDBar(227789, 51)
 end
 
 function mod:MassRepentance(args)
-	self:Message(args.spellId, "Attention", "Warning", CL.incoming:format(args.spellName))
-	self:StopBar(227809)
+	self:Message(args.spellId, "Attention", "Warning", CL.casting:format(args.spellName))
+	self:Bar(args.spellId, 5, CL.cast:format(args.spellName))
+	self:StopBar(227508)
+end
+
+function mod:HolyBulwarkRemoved(args)
+	self:Message(227823, "Urgent", self:Interrupter(args.sourceGUID) and "Alert", CL.casting:format(227823))
 end
 
 function mod:HolyBolt(args)
