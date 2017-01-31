5fb1a734b4991a3ca9ce66d5c252b63113a625c7
funkydude <funkydude@users.noreply.github.com>
2017-01-31 15:42:32 +0000

    Legion/NeltharionsLair/UlaroggCragshaper: Revert totem marker, closes #18

--- Legion/NeltharionsLair/UlaroggCragshaper.lua
+++ Legion/NeltharionsLair/UlaroggCragshaper.lua
@@ -9,13 +9,6 @@ mod:RegisterEnableMob(91004)
 mod.engageId = 1791
 
 --------------------------------------------------------------------------------
--- Locals
---
-
-local totems = {}
-local bossGUID = nil
-
---------------------------------------------------------------------------------
 -- Localization
 --
 
@@ -32,11 +25,9 @@ L = mod:GetLocale()
 -- Initialization
 --
 
-local totemMarker = mod:AddMarkerOption(true, "npc", 8, 198564, 8) -- Stance of the Mountain
 function mod:GetOptions()
 	return {
 		198564, -- Stance of the Mountain
-		totemMarker,
 		{216290, "ICON"}, -- Strike of the Mountain
 		"bellow",
 	}
@@ -46,11 +37,7 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "StanceOfTheMountain", 198564)
 	self:Log("SPELL_CAST_START", "StrikeOfTheMountain", 216290)
 	self:Log("SPELL_CAST_SUCCESS", "StrikeOfTheMountainOver", 216290)
-	self:Log("SPELL_CAST_SUCCESS", "FallingDebris", 198717)
 	self:Log("SPELL_CAST_START", "BellowOfTheDeeps", 193375)
-	self:Log("SPELL_SUMMON", "StanceOfTheMountainSummon", 216249, 198564, 216250, 198565)
-
-	self:Death("StopScanning", 100818)
 end
 
 function mod:OnEngage()
@@ -62,12 +49,6 @@ end
 --
 
 function mod:StanceOfTheMountain(args)
-	if self:GetOption(totemMarker) then
-		wipe(totems)
-		bossGUID = nil
-		self:RegisterTargetEvents("TotemMark")
-	end
-
 	self:Message(args.spellId, "Attention", "Long")
 	self:CDBar(args.spellId, 97) -- pull:36.6, 97.7
 end
@@ -89,37 +70,8 @@ do
 	end
 end
 
-function mod:FallingDebris(args)
-	-- Falling Debris applies to all totems, but we will only be missing one in the table,
-	-- which will always be the boss, since the boss doesn't summon himself in StanceOfTheMountainSummon().
-	if not totems[args.sourceGUID] then
-		bossGUID = args.sourceGUID
-	end
-end
-
 function mod:BellowOfTheDeeps(args)
 	self:Message("bellow", "Urgent", "Info", CL.incoming:format(L.totems), args.spellId)
 	--self:CDBar(args.spellId, 29) -- pull:20.6, 44.9, 31.5, 31.5
-end
-
-function mod:StanceOfTheMountainSummon(args)
-	totems[args.destGUID] = true
-end
-
-function mod:TotemMark(event, unit)
-	if bossGUID then
-		local guid = UnitGUID(unit)
-		if guid == bossGUID then
-			SetRaidTarget(unit, 8)
-		end
-	end
-end
-
-function mod:StopScanning(args)
-	if args.destGUID == bossGUID then
-		self:UnregisterTargetEvents()
-		wipe(totems)
-		bossGUID = nil
-	end
 end
 
