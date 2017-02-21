12ac9533207b61ea0344d0a3e9d8274befdcb56e
Justwait <Justw8@users.noreply.github.com>
2017-02-21 16:58:46 +0100

    Domtrax: Initial Build (#22)

--- Legion/CathedralOfEternalNight/Domatrax.lua
+++ Legion/CathedralOfEternalNight/Domatrax.lua
@@ -1,6 +1,8 @@
 if not IsTestBuild() then return end -- XXX dont load on live
 --------------------------------------------------------------------------------
 -- TODO List:
+-- - Do we need warnings for the add spells?
+-- - Approaching Doom might be a cast we can track instead
 
 --------------------------------------------------------------------------------
 -- Module Declaration
@@ -8,18 +10,16 @@ if not IsTestBuild() then return end -- XXX dont load on live
 
 local mod, CL = BigWigs:NewBoss("Domatrax", 1146, 1904)
 if not mod then return end
-mod:RegisterEnableMob(119542)
---mod.engageId = 1818
+mod:RegisterEnableMob(118804)
+mod.engageId = 2053
 
 --------------------------------------------------------------------------------
 -- Locals
 --
 
---------------------------------------------------------------------------------
--- Localization
---
-
-local L = mod:GetLocale()
+local cleaveCounter = 1
+local felPortalGuardianCollector = {}
+local felPortalGuardiansCounter = 1
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -27,17 +27,74 @@ local L = mod:GetLocale()
 
 function mod:GetOptions()
 	return {
-		"berserk"
+		236543, -- Felsoul Cleave
+		234107, -- Chaotic Energy
+		-15076, -- Fel Portal Guardian
+		241622, -- Approaching Doom
+	},{
+		[236543] = -15011,
+		[241622] = "mythic",
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "FelsoulCleave", 236543)
+	self:Log("SPELL_CAST_START", "ChaoticEnergy", 234107)
 end
 
 function mod:OnEngage()
-	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
+	cleaveCounter = 1
+	felPortalGuardiansCounter = 1
+	wipe(felPortalGuardianCollector)
+
+	self:CDBar(236543, 8.4)
+	self:CDBar(234107, 31.4)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
+	local felPortalGuardians = {}
+
+	for i = 1, 5 do
+		local guid = UnitGUID(("boss%d"):format(i))
+		if guid then
+			local mobId = self:MobId(guid)
+			if mobId == 118834 then -- Fel Portal Guardian
+				if not felPortalGuardianCollector[guid] then
+					-- New Fel Portal Guardian
+					felPortalGuardianCollector[guid] = felPortalGuardiansCounter
+					if self:Mythic() then
+						self:CDBar(241622, 60, CL.count:format(self:SpellName(241622), felPortalGuardiansCounter))
+					end
+					self:Message(-15076, "Attention", "Alert", CL.count:format(CL.spawned:format(self:SpellName(-15076)), felPortalGuardiansCounter))
+					felPortalGuardiansCounter = felPortalGuardiansCounter + 1
+				end
+				felPortalGuardians[guid] = true
+			end
+		end
+	end
+
+	if self:Mythic() then
+		for guid,_ in pairs(felPortalGuardianCollector) do
+			if not felPortalGuardians[guid] then
+				-- Fel Portal Guardian Died
+				self:StopBar(CL.count:format(self:SpellName(241622), felPortalGuardianCollector[guid]))
+				felPortalGuardianCollector[guid] = nil
+			end
+		end
+	end
+end
+
+function mod:FelsoulCleave(args)
+	cleaveCounter = cleaveCounter + 1
+	self:Message(args.spellId, "Attention", "Alert")
+	self:CDBar(args.spellId, cleaveCounter <= 2 and 18.2 or 17)
+end
+
+function mod:ChaoticEnergy(args)
+	self:Message(args.spellId, "Urgent", "Warning")
+	self:CDBar(args.spellId, 35.3)
+end
