6b82bf2ce2e692e20bfe220c6bf554311a9adf25
WetU <yuk6196@gmail.com>
2017-03-15 23:19:27 +0900

    Update koKR.lua (#37)

---
+++ MoP/TempleOfTheJadeSerpent/Locales/koKR.lua
@@ -0,0 +1,14 @@
+local L = BigWigs:NewBossLocale("Liu Flameheart", "koKR")
+if L then
+  L.engage_yell = "위대한 용의 심장을 너희 손에 넘겨줄 순 없다!"
+end
+
+L = BigWigs:NewBossLocale("Sha of Doubt", "koKR")
+if L then
+  L.engage_yell = "죽거나 항복해라, 너희는 날 이길 수 없어."
+end
+
+L = BigWigs:NewBossLocale("Wise Mari", "koKR")
+if L then
+  L.engage_say = "감히 신성한 물을 휘젓다니! 익사시켜주마!"
+end
