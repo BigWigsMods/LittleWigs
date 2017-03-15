f74f24ca40a851ebab0dbe1ad4527344d9299c40
WetU <yuk6196@gmail.com>
2017-03-15 21:33:45 +0900

    Update cata stonecore koKR.lua

--- Cataclysm/Stonecore/Locales/koKR.lua
+++ Cataclysm/Stonecore/Locales/koKR.lua
@@ -1,4 +1,9 @@
 local L = BigWigs:NewBossLocale("Corborus", "koKR")
 if L then
---@localization(locale="koKR", namespace="Stonecore/Corborus", format="lua_additive_table", handle-unlocalized="ignore")@
+  L.burrow = "잠복/출현"
+	L.burrow_desc = "코보루스가 잠복하거나 지상으로 나타나면 경보합니다."
+	L.burrow_message = "코보루스 잠복!"
+	L.burrow_warning = "5초 후 잠복!"
+	L.emerge_message = "코보루스 출현!"
+  L.emerge_warning = "5초 후 출현!"
 end
