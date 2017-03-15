be5f6b86ebc77b18c86e76d0b1a13a399d72321f
WetU <yuk6196@gmail.com>
2017-03-15 21:27:40 +0900

    Update cata LostCity koKR.lua

--- Cataclysm/LostCity/Locales/koKR.lua
+++ Cataclysm/LostCity/Locales/koKR.lua
@@ -1,4 +1,6 @@
 local L = BigWigs:NewBossLocale("Siamat", "koKR")
 if L then
---@localization(locale="koKR", namespace="LostCity/Siamat", format="lua_additive_table", handle-unlocalized="ignore")@
+	L.engage_trigger = "남국의 바람이여, 불어라! 와서 네 주인을 도와라!"
+	L.servant = "하수인 소환"
+	L.servant_desc = "시아마트의 하수인이 소환되면 경보합니다."
 end
