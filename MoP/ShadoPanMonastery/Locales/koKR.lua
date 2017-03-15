12b4ed73be9ee4710fbb8dc653b280bb0871bd95
WetU <yuk6196@gmail.com>
2017-03-15 23:22:01 +0900

    Update koKR.lua

---
+++ MoP/ShadoPanMonastery/Locales/koKR.lua
@@ -0,0 +1,22 @@
+local L = BigWigs:NewBossLocale("Gu Cloudstrike", "koKR")
+if L then
+  L.engage_say = "내 힘을 보여주마."
+end
+
+L = BigWigs:NewBossLocale("Master Snowdrift", "koKR")
+if L then
+  L.engage_yell = "좋다, 이방인들이여. 이제 너희의 진짜 힘을 보여다오."
+
+	--내가 풋내기였던 시절에는 정권 지르기 한 번도 버거웠다. 하지만 수년간 뼈를 깎는 수련을 거듭한 지금은 달라!
+  L.phase3_yell = "내가 풋내기였던 시절에는"
+end
+
+L = BigWigs:NewBossLocale("Sha of Violence", "koKR")
+if L then
+  L.engage_yell = "또 다시 갇힐 순 없어. 음영파는 날 막을 수 없다. 그건 네 놈도 마찬가지지!"
+end
+
+L = BigWigs:NewBossLocale("Taran Zhu", "koKR")
+if L then
+  L.engage_yell = "증오가 모두를 잠식하고 지배하리라!"
+end
