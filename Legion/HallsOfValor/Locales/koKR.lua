6a54efb15899da219ef6d97dde66a7db2a1f7dee
WetU <yuk6196@gmail.com>
2017-03-15 21:47:07 +0900

    Update legion valor koKR.lua

--- Legion/HallsOfValor/Locales/koKR.lua
+++ Legion/HallsOfValor/Locales/koKR.lua
@@ -1,19 +1,19 @@
 local L = BigWigs:NewBossLocale("Odyn", "koKR")
 if not L then return end
 if L then
-	--L[197963] = "|cFF800080Top Right|r (|T1323037:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top Right"
-	--L[197964] = "|cFFFFA500Bottom Right|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Bottom Right"
-	--L[197965] = "|cFFFFFF00Bottom Left|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Bottom Left"
-	--L[197966] = "|cFF0000FFTop Left|r (|T1323035:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top Left"
-	--L[197967] = "|cFF008000Top|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top"
+  L[197963] = "|cFF800080우측 상단|r (|T1323037:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top Right"
+  L[197964] = "|cFFFFA500우측 하단|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Bottom Right"
+  L[197965] = "|cFFFFFF00좌측 하단|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Bottom Left"
+  L[197966] = "|cFF0000FF좌측 상단|r (|T1323035:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top Left"
+  L[197967] = "|cFF008000상단|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top"
 end
 
 L = BigWigs:NewBossLocale("Halls of Valor Trash", "koKR")
 if L then
-	--L.fourkings = "The Four Kings"
-	--L.olmyr = "Olmyr the Enlightened"
-	--L.purifier = "Valarjar Purifier"
-	--L.thundercaller = "Valarjar Thundercaller"
-	--L.mystic = "Valarjar Mystic"
-	--L.aspirant = "Valarjar Aspirant"
+  L.fourkings = "네명의 왕"
+  L.olmyr = "깨달은 자 올미르"
+  L.purifier = "발라리아르 정화자"
+  L.thundercaller = "발라리아르 천둥술사"
+  L.mystic = "발라리아르 비술사"
+  L.aspirant = "발라리아르 지원자"
 end
