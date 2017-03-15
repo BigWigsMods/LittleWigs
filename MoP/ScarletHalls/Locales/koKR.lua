feb2a10b95df26a24616bcafe94cae7a2016d7d9
WetU <yuk6196@gmail.com>
2017-03-15 22:58:20 +0900

    Update mop scarlethalls koKR.lua

---
+++ MoP/ScarletHalls/Locales/koKR.lua
@@ -0,0 +1,14 @@
+local L = BigWigs:NewBossLocale("Armsmaster Harlan", "koKR")
+if L then
+  L.engage_yell = "하하! 또 내 용맹을 시험할 기회인가."
+end
+
+L = BigWigs:NewBossLocale("Flameweaver Koegler", "koKR")
+if L then
+  L.engage_yell = "너도 잿더미가 되어야 한다!"
+end
+
+L = BigWigs:NewBossLocale("Houndmaster Braun", "koKR")
+if L then
+  L.engage_say = "방금 소리 들었니, 얘들아?"
+end
