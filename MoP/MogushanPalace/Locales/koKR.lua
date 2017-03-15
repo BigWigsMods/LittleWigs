eaf3392feb55c2518e3fae89b0fcceabddc04306
WetU <yuk6196@gmail.com>
2017-03-15 23:18:26 +0900

    Update mop mogushan kokR.lua (#34)
    [skip ci]

---
+++ MoP/MogushanPalace/Locales/koKR.lua
@@ -0,0 +1,14 @@
+local L = BigWigs:NewBossLocale("Gekkan", "koKR")
+if L then
+	L.engage_yell = "잡아라!"
+end
+
+L = BigWigs:NewBossLocale("Trial of the King", "koKR")
+if L then
+  L.scout = "글린트록 정찰병"
+end
+
+L = BigWigs:NewBossLocale("Xin the Weaponmaster", "koKR")
+if L then
+  L.engage_yell = "너희가 내게 도전한 첫 상대는 아니다. 하지만 마지막 상대도 아닐 거다."
+end
