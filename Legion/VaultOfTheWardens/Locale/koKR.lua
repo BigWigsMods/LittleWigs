1ea00b0d229169dbcd8fac68736636fe769e2757
WetU <yuk6196@gmail.com>
2017-03-17 14:17:28 +0900

    Update koKR.lua

--- Legion/VaultOfTheWardens/Locale/koKR.lua
+++ Legion/VaultOfTheWardens/Locale/koKR.lua
@@ -2,10 +2,10 @@ local L = BigWigs:NewBossLocale("Vault of the Wardens Trash", "koKR")
 if not L then return end
 if L then
   L.infester = "지옥서약 감염자"
-	L.illianna = "칼춤꾼 일리아나"
-	L.myrmidon = "지옥서약 미르미돈"
-	L.mendacius = "공포군주 멘다시우스"
-	L.fury = "지옥 마력 격노병"
+  L.illianna = "칼춤꾼 일리아나"
+  L.myrmidon = "지옥서약 미르미돈"
+  L.mendacius = "공포군주 멘다시우스"
+  L.fury = "지옥 마력 격노병"
   L.grimhorn = "험악뿔 구속자"
 end
 
@@ -13,6 +13,6 @@ L = BigWigs:NewBossLocale("Cordana Felsong", "koKR")
 if L then
   L.kick_combo = "발차기 연계"
 
-	L.light_dropped = "%s님이 빛을 떨어뜨렸습니다."
+  L.light_dropped = "%s님이 빛을 떨어뜨렸습니다."
   L.light_picked = "%s님이 빛을 주웠습니다."
 end
