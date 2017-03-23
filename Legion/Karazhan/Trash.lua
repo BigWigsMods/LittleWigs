da26cf1e9f99219f1263d679472b993a1ccfe9e0
elvador <reysptools@googlemail.com>
2017-03-23 18:04:29 +0100

    Legion/Karazhan/Trash: Add module

--- Legion/Karazhan/Trash.lua
+++ Legion/Karazhan/Trash.lua
@@ -7,7 +7,7 @@ local mod, CL = BigWigs:NewBoss("Karazhan Trash", 1115)
 if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
-	
+	114544 -- Skeletal Usher
 )
 
 --------------------------------------------------------------------------------
@@ -16,7 +16,7 @@ mod:RegisterEnableMob(
 
 local L = mod:NewLocale("enUS", true)
 if L then
-	
+	L.skeletalUsher = "Skeletal Usher"
 end
 L = mod:GetLocale()
 
@@ -26,21 +26,29 @@ L = mod:GetLocale()
 
 function mod:GetOptions()
 	return {
-		"berserk"
+		227966, -- Flashlight
+	}, {
+		[227966] = L.skeletalUsher,
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
-
-	
+	self:Log("SPELL_CAST_START", "Flashlight", 227966)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
-function mod:Casts(args)
-	
+do
+	local prev = 0
+	function mod:Flashlight(args)
+		local t = GetTime()
+		if t-prev > 3 then
+			prev = t
+			self:Message(args.spellId, "Attention", "Info")
+		end
+		self:Bar(args.spellId, 3)
+	end
 end
-
