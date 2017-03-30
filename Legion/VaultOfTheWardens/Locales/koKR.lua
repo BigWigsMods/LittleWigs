734e2f86524210830719f12cf3032709444906d6
WetU <yuk6196@gmail.com>
2017-03-31 03:41:12 +0900

    Update warm-up bar

--- Legion/VaultOfTheWardens/Locales/koKR.lua
+++ Legion/VaultOfTheWardens/Locales/koKR.lua
@@ -1,21 +1,21 @@
 local L = BigWigs:NewBossLocale("Cordana Felsong", "koKR")
 if not L then return end
 if L then
-	--L.kick_combo = "Kick Combo"
+        L.kick_combo = "발차기 연계"
 
-	--L.light_dropped = "%s dropped the Light."
-	--L.light_picked = "%s picked up the Light."
+        L.light_dropped = "%s님이 빛을 떨어뜨렸습니다."
+        L.light_picked = "%s님이 빛을 주웠습니다."
 
-	--L.warmup_text = "Cordana Felsong Active"
-	--L.warmup_trigger = "I have what I was after. But I stayed just so that I could put an end to you... once and for all!"
+        L.warmup_text = "콜다나 펠송 활성화"
+        L.warmup_trigger = "난 이미 원하는 걸 손에 넣었다. 그저 기다렸을 뿐... 너희를 확실히 끝장낼 순간을 말이다!"
 end
 
 L = BigWigs:NewBossLocale("Vault of the Wardens Trash", "koKR")
 if L then
-	--L.infester = "Felsworn Infester"
-	--L.illianna = "Blade Dancer Illianna"
-	--L.myrmidon = "Felsworn Myrmidon"
-	--L.mendacius = "Dreadlord Mendacius"
-	--L.fury = "Fel-Infused Fury"
-	--L.grimhorn = "Grimhorn the Enslaver"
+        L.infester = "지옥서약 감염자"
+        L.illianna = "칼춤꾼 일리아나"
+        L.myrmidon = "지옥서약 미르미돈"
+        L.mendacius = "공포군주 멘다시우스"
+        L.fury = "지옥 마력 격노병"
+        L.grimhorn = "험악뿔 구속자"
 end
