d0186d124378e3293a07818c2f8da039c6b8e901
Adavak LEE <adavak@adavak.com>
2017-04-04 11:00:13 +0800

    Update zhCN.lua

--- Legion/CathedralOfEternalNight/Locales/zhCN.lua
+++ Legion/CathedralOfEternalNight/Locales/zhCN.lua
@@ -1,8 +1,8 @@
 local L = BigWigs:NewBossLocale("Mephistroth", "zhCN")
 if not L then return end
 if L then
-	--L.custom_on_time_lost = "Time lost during Shadow Fade"
-	--L.custom_on_time_lost_desc = "Show the time lost during Shadow Fade on the bar in |cffff0000red|r."
+	L.custom_on_time_lost = "暗影消退计时"
+	L.custom_on_time_lost_desc = "显示暗影消退为|cffff0000红|r色计时条。"
 end
 
 L = BigWigs:NewBossLocale("Cathedral of Eternal Night Trash", "zhCN")
