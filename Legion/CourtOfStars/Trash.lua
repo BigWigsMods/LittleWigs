8aeba4b23b9101f44315d5a0ee41967d3d3cce19
Justwait <justwait@method.gg>
2017-05-19 01:21:06 +0200

    Legion/CourtOfStars/Trash: Reduce dispel warnings, cleanup options

--- Legion/CourtOfStars/Trash.lua
+++ Legion/CourtOfStars/Trash.lua
@@ -8,7 +8,6 @@ mod.displayName = CL.trash
 mod:RegisterEnableMob(
 	104246, -- Duskwatch Guard
 	111563, -- Duskwatch Guard
-	104251, -- Duskwatch Sentry
 	104270, -- Guardian Construct
 	104278, -- Felbound Enforcer
 	104277, -- Legion Hound
@@ -19,11 +18,9 @@ mod:RegisterEnableMob(
 	105715, -- Watchful Inquisitor
 	104295, -- Blazing Imp
 	105705, -- Bound Energy
-	104300, -- Shadow Mistress
 	105704, -- Arcane Manifestation
 	105703, -- Mana Wyrm
 	104247, -- Duskwatch Arcanist
-	105699, -- Mana Saber
 	112668, -- Infernal Imp
 	108796, -- Arcanist Malrodi (Court of Stars: The Deceitful Student World Quest)
 	108740  -- Velimar (Court of Stars: Bring Me the Eyes World Quest)
@@ -36,7 +33,6 @@ mod:RegisterEnableMob(
 local L = mod:NewLocale("enUS", true)
 if L then
 	L.Guard = "Duskwatch Guard"
-	L.Sentry = "Duskwatch Sentry"
 	L.Construct = "Guardian Construct"
 	L.Enforcer = "Felbound Enforcer"
 	L.Hound = "Legion Hound"
@@ -47,11 +43,9 @@ if L then
 	L.Inquisitor = "Watchful Inquisitor"
 	L.BlazingImp = "Blazing Imp"
 	L.Energy = "Bound Energy"
-	L.Mistress = "Shadow Mistress"
 	L.Manifestation = "Arcane Manifestation"
 	L.Wyrm = "Mana Wyrm"
 	L.Arcanist = "Duskwatch Arcanist"
-	L.Saber = "Mana Saber"
 	L.InfernalImp = "Infernal Imp"
 	L.Malrodi = "Arcanist Malrodi"
 	L.Velimar = "Velimar"
@@ -66,30 +60,25 @@ function mod:GetOptions()
 	return {
 		209027, -- Quelling Strike (Duskwatch Guard)
 		209033, -- Fortification (Duskwatch Guard)
-		{209036, "SAY", "FLASH"}, -- Throw Torch (Duskwatch Sentry)
 		225100, -- Charging Station (Guardian Construct)
 		209495, -- Charged Smash (Guardian Construct)
 		209512, -- Disrupting Energy (Guardian Construct)
-		{209413, "SAY", "FLASH"}, -- Suppress (Guardian Construct)
+		{209413, "FLASH"}, -- Suppress (Guardian Construct)
 		211464, -- Fel Detonation (Felbound Enforcer)
 		211391, -- Felblaze Puddle (Legion Hound)
 		214692, -- Shadow Bolt Volley (Gerenth the Vile)
 		214688, -- Carrion Swarm (Gerenth the Vile)
-		{214690, "SAY", "FLASH"}, -- Cripple (Gerenth the Vile)
 		207979, -- Shockwave (Jazshariu)
 		209378, -- Whirling Blades (Imacu'tya)
-		{207980, "SAY", "FLASH"}, -- Disintegration Beam (Baalgar the Watchful)
+		{207980, "FLASH"}, -- Disintegration Beam (Baalgar the Watchful)
 		211299, -- Searing Glare (Watchful Inquisitor)
 		212784, -- Eye Storm (Watchful Inquisitor)
 		211401, -- Drifting Embers (Blazing Imp)
 		212031, -- Charged Blast (Bound Energy)
-		{211470, "SAY", "FLASH"}, -- Bewitch (Shadow Mistress)
-		{211473, "SAY", "FLASH"}, -- Shadow Slash (Shadow Mistress)
 		209485, -- Drain Magic (Arcane Manifestation)
 		209477, -- Wild Detonation (Mana Wyrm)
 		209410, -- Nightfall Orb (Duskwatch Arcanist)
 		209404, -- Seal Magic (Duskwatch Arcanist)
-		{209516, "SAY", "FLASH"}, -- Mana Fang (Mana Saber)
 		224377, -- Drifting Embers (Infernal Imp)
 		216110, -- Uncontrolled Blast (Arcanist Malrodi)
 		216096, -- Wild Magic (Arcanist Malrodi)
@@ -97,7 +86,6 @@ function mod:GetOptions()
 		216006, -- Shadowflame Breath (Velimar)
 	}, {
 		[209027] = L.Guard,
-		[209036] = L.Sentry,
 		[225100] = L.Construct,
 		[211464] = L.Enforcer,
 		[211391] = L.Hound,
@@ -108,11 +96,9 @@ function mod:GetOptions()
 		[211299] = L.Inquisitor,
 		[211401] = L.BlazingImp,
 		[212031] = L.Energy,
-		[211470] = L.Mistress,
 		[209485] = L.Manifestation,
 		[209477] = L.Wyrm,
 		[209410] = L.Arcanist,
-		[209516] = L.Saber,
 		[224377] = L.InfernalImp,
 		[216110] = L.Malrodi,
 		[216000] = L.Velimar,
@@ -129,8 +115,8 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "PeriodicDamage", 211391, 209512)
 	self:Log("SPELL_PERIODIC_DAMAGE", "PeriodicDamage", 211391, 209512)
 	self:Log("SPELL_PERIODIC_MISSED", "PeriodicDamage", 211391, 209512)
-	-- Shadow Slash, Bewitch, Suppress, Mana Fang, Cripple, Throw Torch, Shadowflame Breath, Carrion Swarm
-	self:Log("SPELL_AURA_APPLIED", "DispellableDebuffs", 211473, 211470, 209413, 209516, 214690, 209036, 216006, 214688)
+	-- Suppress
+	self:Log("SPELL_AURA_APPLIED", "Supress", 209413)
 	-- Disintegration Beam
 	self:Log("SPELL_AURA_APPLIED", "DisintegrationBeam", 207980)
 	-- Eye Storm
@@ -173,7 +159,7 @@ do
 	end
 end
 
-function mod:DispellableDebuffs(args)
+function mod:Supress(args)
 	self:TargetMessage(args.spellId, args.destName, "Attention", not throttleMessages(args.spellId) and "Alert", nil, nil, self:Dispeller("magic"))
 	if self:Me(args.destGUID) then
 		self:Flash(args.spellId)
@@ -185,7 +171,6 @@ function mod:DisintegrationBeam(args)
 	self:Bar(args.spellId, 5, CL.cast:format(args.spellName, args.destName))
 	if self:Me(args.destGUID) then
 		self:Flash(args.spellId)
-		self:Say(args.spellId)
 	end
 end
 
