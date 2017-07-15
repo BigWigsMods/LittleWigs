7c1524c965973cdd333a8051d6da0974332c2d81
WetU <yuk6196@gmail.com>
2017-07-16 03:28:53 +0900

    Update koKR.lua (#61)

--- Legion/CathedralOfEternalNight/Locales/koKR.lua
+++ Legion/CathedralOfEternalNight/Locales/koKR.lua
@@ -10,7 +10,7 @@ if L then
 	L.felguard = "지옥수호병 파괴자"
 	L.soulmender = "지옥불길 영혼치유사"
 	L.temptress = "지옥불길 요녀"
-	--L.botanist = "Felborne Botanist"
+	L.botanist = "지옥살이 식물학자"
 	L.orbcaster = "지옥길잡이 보주술사"
 	L.waglur = "와글루르"
 	L.gazerax = "가제락스"
