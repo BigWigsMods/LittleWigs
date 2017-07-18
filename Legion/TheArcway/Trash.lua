9a11723415dd2ca1d00fe6f45ff9fa614c4b5aa2
roionsteroids <roionsteroids@gmail.com>
2017-07-18 19:41:51 +0200

    remove arcway changes

--- Legion/TheArcway/Trash.lua
+++ Legion/TheArcway/Trash.lua
@@ -1,3 +1,4 @@
+
 --------------------------------------------------------------------------------
 -- Module Declaration
 --
@@ -6,10 +7,8 @@ local mod, CL = BigWigs:NewBoss("The Arcway Trash", 1079)
 if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
-	98756,  -- Arcane Anomaly
-	106059, -- Warp Shade
-	105952, -- Withered Manawraith
-	98770   -- Wrathguard Felblade
+	98756, -- Arcane Anomaly
+	106059 -- Warp Shade
 )
 
 --------------------------------------------------------------------------------
@@ -20,8 +19,6 @@ local L = mod:NewLocale("enUS", true)
 if L then
 	L.anomaly = "Arcane Anomaly"
 	L.shade = "Warp Shade"
-	L.wraith = "Withered Manawraith"
-	L.blade = "Wrathguard Felblade"
 end
 L = mod:GetLocale()
 
@@ -37,17 +34,9 @@ function mod:GetOptions()
 
 		--[[ Warp Shade ]]--
 		211115, -- Phase Breach
-
-		--[[ Withered Manawraith ]]--
-		210750, -- Collapsing Rift
-
-		--[[ Wrathguard Felblade ]]--
-		211745, -- Fel Strike
 	}, {
 		[211217] = L.anomaly,
-		[211115] = L.shade,
-		[210750] = L.wraith,
-		[211745] = L.blade,
+		[211115] = L.shade
 	}
 end
 
@@ -62,11 +51,6 @@ function mod:OnBossEnable()
 
 	--[[ Warp Shade ]]--
 	self:Log("SPELL_CAST_START", "PhaseBreach", 211115)
-
-	--[[ Withered Manawraith, Wrathguard Felblade ]]--
-	self:Log("SPELL_AURA_APPLIED", "PeriodicDamage", 210750, 211745)
-	self:Log("SPELL_PERIODIC_DAMAGE", "PeriodicDamage", 210750, 211745)
-	self:Log("SPELL_PERIODIC_MISSED", "PeriodicDamage", 210750, 211745)
 end
 
 --------------------------------------------------------------------------------
@@ -86,16 +70,4 @@ end
 -- Warp Shade
 function mod:PhaseBreach(args)
 	self:Message(args.spellId, "Attention", self:Interrupter() and "Alarm", CL.casting:format(args.spellName))
-end
-
--- Withered Manawraith, Wrathguard Felblade
-do
-	local prev = 0
-	function mod:PeriodicDamage(args)
-		local t = GetTime()
-		if self:Me(args.destGUID) and t-prev > 1.5 then
-			prev = t
-			self:Message(args.spellId, "Personal", "Warning", CL.underyou:format(args.spellName))
-		end
-	end
 end
