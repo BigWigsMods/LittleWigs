420ade3712ac36d4e060d649ca5d2b8092ed6921
WetU <yuk6196@gmail.com>
2017-07-19 11:57:37 +0900

    Update koKR.lua for The Arcway (#70)

--- Legion/TheArcway/Locales/koKR.lua
+++ Legion/TheArcway/Locales/koKR.lua
@@ -1,8 +1,8 @@
 local L = BigWigs:NewBossLocale("Odyn", "koKR")
 if not L then return end
 if L then
-	--L.anomaly = "Arcane Anomaly"
-	--L.shade = "Warp Shade"
-	--L.wraith = "Withered Manawraith"
-	--L.blade = "Wrathguard Felblade"
+	L.anomaly = "비전 변형물"
+	L.shade = "뒤틀린 그림자"
+	L.wraith = "메마른 마나 망령"
+	L.blade = "격노수호병 지옥검사"
 end
