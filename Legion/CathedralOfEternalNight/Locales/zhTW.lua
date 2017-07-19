bcd7463815c982b19a7d10dbd471deab4c02a67a
BlueNightSky <afan1128@hotmail.com>
2017-07-19 23:56:23 +0800

    Update zhTW.lua for CathedralOfEternalNight

--- Legion/CathedralOfEternalNight/Locales/zhTW.lua
+++ Legion/CathedralOfEternalNight/Locales/zhTW.lua
@@ -1,17 +1,17 @@
 local L = BigWigs:NewBossLocale("Mephistroth", "zhTW")
 if not L then return end
 if L then
-	--L.custom_on_time_lost = "Time lost during Shadow Fade"
-	--L.custom_on_time_lost_desc = "Show the time lost during Shadow Fade on the bar in |cffff0000red|r."
+	L.custom_on_time_lost = "黑暗漸隱期間的計時"
+	L.custom_on_time_lost_desc = "在黑暗漸隱期間顯示計時的條列為 |cffff0000紅色|r。"
 end
 
 L = BigWigs:NewBossLocale("Cathedral of Eternal Night Trash", "zhTW")
 if L then
-	--L.felguard = "Felguard Destroyer"
-	--L.soulmender = "Helblaze Soulmender"
-	--L.temptress = "Hellblaze Temptress"
-	--L.botanist = "Felborne Botanist"
-	--L.orbcaster = "Felstrider Orbcaster"
+	L.felguard = "惡魔守衛摧毀者"
+	L.soulmender = "獄炎魔能使者"
+	L.temptress = "獄炎妖女"
+	L.botanist = "魔裔植物學家"
+	L.orbcaster = "獄炎補魂者"
 	--L.waglur = "Wa'glur"
-	--L.gazerax = "Gazerax"
+	L.gazerax = "賈澤拉克斯"
 end
