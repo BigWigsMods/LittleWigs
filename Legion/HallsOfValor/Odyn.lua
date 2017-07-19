eb9cd24db98e958ac6a5ad39c7c3e59ba69961f1
elvador <reysptools@googlemail.com>
2017-07-19 13:26:52 +0200

    HallsOfValor/Odyn: Add autotalk and warmup bar

--- Legion/HallsOfValor/Odyn.lua
+++ Legion/HallsOfValor/Odyn.lua
@@ -7,6 +7,7 @@ local mod, CL = BigWigs:NewBoss("Odyn", 1041, 1489)
 if not mod then return end
 mod:RegisterEnableMob(95676)
 mod.engageId = 1809
+mod.respawnTime = 30
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -14,6 +15,9 @@ mod.engageId = 1809
 
 local L = mod:NewLocale("enUS", true)
 if L then
+	L.autotalk = "Autotalk"
+	L.autotalk_desc = "Instantly selects the gossip option to start the fight."
+
 	L[197963] = "|cFF800080Top Right|r (|T1323037:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Purple
 	L[197964] = "|cFFFFA500Bottom Right|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Orange
 	L[197965] = "|cFFFFFF00Bottom Left|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Yellow
@@ -27,6 +31,8 @@ end
 
 function mod:GetOptions()
 	return {
+		"autotalk",
+		"warmup",
 		197961, -- Runic Brand
 		198263, -- Radiant Tempest
 		200988, -- Spear of Light
@@ -41,6 +47,8 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "ShatterSpears", 198077)
 
 	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
+	self:RegisterEvent("GOSSIP_SHOW")
+	self:RegisterMessage("BigWigs_BossComm")
 end
 
 function mod:OnEngage()
@@ -77,5 +85,22 @@ end
 function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, _, spellId)
 	if spellId == 198396 then -- Spear of Light
 		self:Message(200988, "Urgent", "Alert")
+	end
+end
+
+function mod:GOSSIP_SHOW()
+	if self:GetOption("autotalk") > 0 and self:MobId(UnitGUID("npc")) == 95676 then
+		if GetGossipOptions() then
+			SelectGossipOption(1, "", true) -- auto confirm it
+			mod:Sync("odyn")
+		end
+	end
+end
+
+function mod:BigWigs_BossComm(_, msg)
+	if msg == "odyn" then
+		local name = EJ_GetEncounterInfo(1489)
+		self:Message("warmup", "Neutral", "Info", CL.incoming:format(name), false)
+		self:CDBar("warmup", 2.7, name, "achievement_boss_odyn")
 	end
 end
