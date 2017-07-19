2bd427584bf8e23111c694d96ffa47bfb476b0b0
BlueNightSky <afan1128@hotmail.com>
2017-07-19 17:27:31 +0800

    Update zhTW.lua

--- Legion/VaultOfTheWardens/Locales/zhTW.lua
+++ Legion/VaultOfTheWardens/Locales/zhTW.lua
@@ -1,21 +1,21 @@
 local L = BigWigs:NewBossLocale("Cordana Felsong", "zhTW")
 if not L then return end
 if L then
-	--L.kick_combo = "Kick Combo"
+	L.kick_combo = "連環踢"
 
-	--L.light_dropped = "%s dropped the Light."
-	--L.light_picked = "%s picked up the Light."
+	L.light_dropped = "%s 丟掉了光。"
+	L.light_picked = "%s 撿起了光。"
 
-	--L.warmup_text = "Cordana Felsong Active"
-	--L.warmup_trigger = "I have what I was after. But I stayed just so that I could put an end to you... once and for all!"
+	L.warmup_text = "寇達娜．魔歌啟動"
+	L.warmup_trigger = "我已經拿到我要找的東西了。但為了你們，我最好還是留下來…斬草除根！"
 end
 
 L = BigWigs:NewBossLocale("Vault of the Wardens Trash", "zhTW")
 if L then
-	--L.infester = "Felsworn Infester"
-	--L.illianna = "Blade Dancer Illianna"
-	--L.myrmidon = "Felsworn Myrmidon"
-	--L.mendacius = "Dreadlord Mendacius"
-	--L.fury = "Fel-Infused Fury"
-	--L.grimhorn = "Grimhorn the Enslaver"
+	L.infester = "魔誓感染者"
+	L.illianna = "刃舞者伊利安娜"
+	L.myrmidon = "魔誓部屬"
+	L.mendacius = "驚懼領主曼達希斯"
+	L.fury = "魔能怒衛"
+	L.grimhorn = "『奴役者』恐角"
 end
