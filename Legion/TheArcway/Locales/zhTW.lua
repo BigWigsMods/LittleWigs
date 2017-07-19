b49ecda588ec96b8ef146a5c3c96c96b29c94598
BlueNightSky <afan1128@hotmail.com>
2017-07-19 22:00:46 +0800

    Update zhTW for The Arcway

--- Legion/TheArcway/Locales/zhTW.lua
+++ Legion/TheArcway/Locales/zhTW.lua
@@ -1,8 +1,8 @@
 local L = BigWigs:NewBossLocale("The Arcway Trash", "zhTW")
 if not L then return end
 if L then
-	--L.anomaly = "Arcane Anomaly"
-	--L.shade = "Warp Shade"
-	--L.wraith = "Withered Manawraith"
-	--L.blade = "Wrathguard Felblade"
+	L.anomaly = "祕法異常體"
+	L.shade = "扭曲之影"
+	L.wraith = "凋萎者法力怨靈"
+	L.blade = "憤怒守衛魔刃兵"
 end
