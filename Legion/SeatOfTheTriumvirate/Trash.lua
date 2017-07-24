5d63bdedcfcc68264d31a14beac8a74ae1cdbf6e
elvador <reysptools@googlemail.com>
2017-07-25 00:38:59 +0200

    SeatOfTheTriumvirate/Trash: Initial module

--- Legion/SeatOfTheTriumvirate/Trash.lua
+++ Legion/SeatOfTheTriumvirate/Trash.lua
@@ -6,7 +6,28 @@ if not IsTestBuild() then return end -- XXX dont load on live
 local mod, CL = BigWigs:NewBoss("Seat of the Triumvirate Trash", 1178)
 if not mod then return end
 mod.displayName = CL.trash
---mod:RegisterEnableMob()
+mod:RegisterEnableMob(
+	123743, -- Alleria Windrunner
+	124171, -- Shadowguard Subjugator
+	122404 -- Shadowguard Voidbender
+)
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:GetLocale()
+if L then
+	L.custom_on_autotalk = "Autotalk"
+	L.custom_on_autotalk_desc = "Instantly selects Alleria Winrunners gossip option."
+	L.custom_on_autotalk_icon = "inv_bow_1h_artifactwindrunner_d_03"
+	L.gossip_available = "Gossip available"
+	L.alleria_gossip_trigger = "Follow me!" -- Allerias yell after the first boss is defeated
+
+	L.alleria = "Alleria Windrunner"
+	L.subjugator = "Shadowguard Subjugator"
+	L.voidbender = "Shadowguard Voidbender"
+end
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -14,13 +35,48 @@ mod.displayName = CL.trash
 
 function mod:GetOptions()
 	return {
-		"berserk"
+		"custom_on_autotalk",
+		"warmup",
+		{249081, "SAY"}, -- Suppression Field
+		{245510, "SAY"}, -- Corrupting Void
+	}, {
+		["custom_on_autotalk"] = L.alleria,
+		[249081] = L.subjugator,
+		[245510] = L.voidbender,
 	}
 end
 
 function mod:OnBossEnable()
+	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
+
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+
+	self:Log("SPELL_AURA_APPLIED", "PersonalAurasWithSay", 249081, 245510) -- Suppression Field, Corrupting Void
+
+	self:RegisterEvent("GOSSIP_SHOW")
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:CHAT_MSG_MONSTER_YELL(_, msg)
+	if msg == L.alleria_gossip_trigger then
+		self:Bar("warmup", 40, L.gossip_available, L.custom_on_autotalk_icon)
+	end
+end
+
+function mod:PersonalAurasWithSay(args)
+	if self:Me(args.destGUID) then
+		self:TargetMessage(args.spellId, args.destName, "Personal", "Alarm")
+		self:Say(args.spellId)
+	end
+end
+
+function mod:GOSSIP_SHOW()
+	if self:GetOption("custom_on_autotalk") and self:MobId(UnitGUID("npc")) == 123743 then
+		if GetGossipOptions() then
+			SelectGossipOption(1)
+		end
+	end
+end
