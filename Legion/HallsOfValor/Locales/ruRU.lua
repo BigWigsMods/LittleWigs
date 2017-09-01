ea324e0c556ee6a95f5e0cc2c3ab92ca53f5b007
henbe <holypush@yandex.ru>
2017-09-01 23:35:16 +0500

    Update ruRU.lua

--- Legion/HallsOfValor/Locales/ruRU.lua
+++ Legion/HallsOfValor/Locales/ruRU.lua
@@ -1,28 +1,28 @@
 local L = BigWigs:NewBossLocale("Odyn", "ruRU")
 if not L then return end
 if L then
-	--L.custom_on_autotalk = "Autotalk"
-	--L.custom_on_autotalk_desc = "Instantly selects the gossip option to start the fight."
+	L.custom_on_autotalk = "Авторазговор"
+	L.custom_on_autotalk_desc = "Мгновенный выбор опции запуска боя в диалоге."
 
-	--L[197963] = "|cFF800080Top Right|r (|T1323037:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top Right"
-	--L[197964] = "|cFFFFA500Bottom Right|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Bottom Right"
-	--L[197965] = "|cFFFFFF00Bottom Left|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Bottom Left"
-	--L[197966] = "|cFF0000FFTop Left|r (|T1323035:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top Left"
-	--L[197967] = "|cFF008000Top|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top"
+	L[197963] = "|cFF800080Право Верх|r (|T1323037:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top Right"
+	L[197964] = "|cFFFFA500Право Низ|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Bottom Right"
+	L[197965] = "|cFFFFFF00Лево Низ|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Bottom Left"
+	L[197966] = "|cFF0000FFЛево Верх|r (|T1323035:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top Left"
+	L[197967] = "|cFF008000Верх|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Translate "Top"
 end
 
 L = BigWigs:NewBossLocale("God-King Skovald", "ruRU")
 if L then
-	--L.warmup_text = "God-King Skovald Active"
-	--L.warmup_trigger = "The vanquishers have already taken possession of it, Skovald, as was their right. Your protest comes too late."
+	L.warmup_text = "Король-бог Сковальд активен"
+	L.warmup_trigger = "Сковальд, эти герои завладели Эгидой по праву. Уже поздно что-либо оспаривать."
 end
 
 L = BigWigs:NewBossLocale("Halls of Valor Trash", "ruRU")
 if L then
-	--L.custom_on_autotalk = "Autotalk"
-	--L.custom_on_autotalk_desc = "Instantly selects various gossip options around the dungeon."
+	L.custom_on_autotalk = "Авторазговор"
+	L.custom_on_autotalk_desc = "Мгновенный выбор опций в диалогах."
 
-	--L.fourkings = "The Four Kings"
+	L.fourkings = "Четыре Короля"
 	L.olmyr = "Олмир Просвещенный"
 	L.purifier = "Валарьяр-очиститель"
 	L.thundercaller = "Валарьяр - призыватель молний"
