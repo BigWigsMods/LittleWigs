d84d2606049bd145849945110c172b9e66672b92
Justwait <justwait@method.gg>
2017-09-15 18:08:40 +0200

    Legion/EyeOfAzshara/Locales: Add locale files

---
+++ Legion/EyeOfAzshara/Locales/esES.lua
@@ -0,0 +1,9 @@
+local L = BigWigs:NewBossLocale("Eye of Azshara Trash", "esES") or BigWigs:NewBossLocale("Eye of Azshara Trash", "esMX")
+if not L then return end
+if L then
+	--L.stormweaver = "Hatecoil Stormweaver"
+	--L.oracle = "Hatecoil Oracle"
+	--L.siltwalker = "Mak'rana Siltwalker"
+	--L.tides = "Restless Tides"
+	--L.arcanist = "Hatecoil Arcanist"
+end
---
+++ Legion/EyeOfAzshara/Locales/ruRU.lua
@@ -0,0 +1,9 @@
+local L = BigWigs:NewBossLocale("Eye of Azshara Trash", "ruRU")
+if not L then return end
+if L then
+	--L.stormweaver = "Hatecoil Stormweaver"
+	--L.oracle = "Hatecoil Oracle"
+	--L.siltwalker = "Mak'rana Siltwalker"
+	--L.tides = "Restless Tides"
+	--L.arcanist = "Hatecoil Arcanist"
+end
