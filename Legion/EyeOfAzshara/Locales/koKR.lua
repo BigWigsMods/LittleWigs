d6bf00b6cd831ea7c76bf3a7a2d2edae861e3077
WetU <yuk6196@gmail.com>
2017-09-16 03:08:06 +0900

    Update koKR.lua

--- Legion/EyeOfAzshara/Locales/koKR.lua
+++ Legion/EyeOfAzshara/Locales/koKR.lua
@@ -1,9 +1,9 @@
 local L = BigWigs:NewBossLocale("Eye of Azshara Trash", "koKR")
 if not L then return end
 if L then
-	--L.stormweaver = "Hatecoil Stormweaver"
-	--L.oracle = "Hatecoil Oracle"
-	--L.siltwalker = "Mak'rana Siltwalker"
-	--L.tides = "Restless Tides"
-	--L.arcanist = "Hatecoil Arcanist"
+	L.stormweaver = "증오갈퀴 폭풍술사"
+	L.oracle = "증오갈퀴 점쟁이"
+	L.siltwalker = "마크라나 진흙방랑자"
+	L.tides = "안식 없는 조류"
+	L.arcanist = "증오갈퀴 비전술사"
 end
