547e5dab793e8609cd0fce323546957cc7b83dfe
roionsteroids <roionsteroids@gmail.com>
2017-09-21 15:25:07 +0200

    Legion/Karazhan/Trash: add Barnes auto-gossip to start the Opera Hall encounter

--- Legion/Karazhan/Trash.lua
+++ Legion/Karazhan/Trash.lua
@@ -7,7 +7,8 @@ local mod, CL = BigWigs:NewBoss("Karazhan Trash", 1115)
 if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
-	114544 -- Skeletal Usher
+	114544, -- Skeletal Usher
+	114339 -- Barnes
 )
 
 --------------------------------------------------------------------------------
@@ -17,6 +18,8 @@ mod:RegisterEnableMob(
 local L = mod:NewLocale("enUS", true)
 if L then
 	L.skeletalUsher = "Skeletal Usher"
+	L.custom_on_autotalk = "Autotalk"
+	L.custom_on_autotalk_desc = "Instantly selects Barnes' gossip option to start the Opera Hall encounter."
 end
 L = mod:GetLocale()
 
@@ -27,20 +30,25 @@ L = mod:GetLocale()
 function mod:GetOptions()
 	return {
 		227966, -- Flashlight
+		"custom_on_autotalk", -- Barnes
 	}, {
 		[227966] = L.skeletalUsher,
+		["custom_on_autotalk"] = "general",
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
 	self:Log("SPELL_CAST_START", "Flashlight", 227966)
+
+	self:RegisterEvent("GOSSIP_SHOW")
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+-- Skeletal Usher
 do
 	local prev = 0
 	function mod:Flashlight(args)
@@ -50,5 +58,14 @@ do
 			self:Message(args.spellId, "Attention", "Info")
 		end
 		self:Bar(args.spellId, 3)
+	end
+end
+
+-- Barnes
+function mod:GOSSIP_SHOW()
+	if self:GetOption("custom_on_autotalk") and self:MobId(UnitGUID("npc")) == 114339 then
+		if GetGossipOptions() then
+			SelectGossipOption(1)
+		end
 	end
 end
