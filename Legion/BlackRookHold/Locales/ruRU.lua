d8cc77636ead9371cc8f4af1f971e20f1e0f56dd
roionsteroids <roionsteroids@gmail.com>
2017-09-23 17:16:40 +0200

    Legion/BlackRookHold/Locales: add locale files (#116)

---
+++ Legion/BlackRookHold/Locales/ruRU.lua
@@ -0,0 +1,11 @@
+local L = BigWigs:NewBossLocale("Black Rook Hold Trash", "ruRU")
+if not L then return end
+if L then
+	--L.arcanist = "Risen Arcanist"
+	--L.champion = "Soul-torn Champion"
+	--L.swordsman = "Risen Swordsman"
+	--L.archer = "Risen Archer"
+	--L.scout = "Risen Scout"
+	--L.councilor = "Ghostly Councilor"
+	--L.dominator = "Felspite Dominator"
+end
