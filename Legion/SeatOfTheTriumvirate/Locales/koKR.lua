272b2dd7a5d3185a86fae938434fc0b8edfaf8fd
WetU <yuk6196@gmail.com>
2017-09-24 03:52:30 +0900

    Update koKR locales (#119)

--- Legion/SeatOfTheTriumvirate/Locales/koKR.lua
+++ Legion/SeatOfTheTriumvirate/Locales/koKR.lua
@@ -1,12 +1,12 @@
 local L = BigWigs:NewBossLocale("Seat Of The Triumvirate Trash", "koKR")
 if not L then return end
 if L then
-	--L.custom_on_autotalk = "Autotalk"
-	--L.custom_on_autotalk_desc = "Instantly selects Alleria Winrunners gossip option."
-	--L.gossip_available = "Gossip available"
-	--L.alleria_gossip_trigger = "Follow me!" -- Allerias yell after the first boss is defeated
+	L.custom_on_autotalk = "자동 대화"
+	L.custom_on_autotalk_desc = "알레리아 윈드러너의 대화 선택지를 즉시 고릅니다."
+	L.gossip_available = "대화 가능"
+	L.alleria_gossip_trigger = "따라오세요!" -- Allerias yell after the first boss is defeated
 
-	--L.alleria = "Alleria Windrunner"
-	--L.subjugator = "Shadowguard Subjugator"
-	--L.voidbender = "Shadowguard Voidbender"
+	L.alleria = "알레리아 윈드러너"
+	L.subjugator = "어둠수호병 정복자"
+	L.voidbender = "어둠수호병 공허술사"
 end
