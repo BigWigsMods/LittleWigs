b0bfb8be238e374d22cac95e6b49c49c3a70586b
roionsteroids <roionsteroids@gmail.com>
2017-09-27 19:02:04 +0200

    Legion/Karazhan/Locales: add locale files (#120)

---
+++ Legion/Karazhan/Locales/itIT.lua
@@ -0,0 +1,7 @@
+local L = BigWigs:NewBossLocale("Karazhan Trash", "itIT")
+if not L then return end
+if L then
+	--L.skeletalUsher = "Skeletal Usher"
+	--L.custom_on_autotalk = "Autotalk"
+	--L.custom_on_autotalk_desc = "Instantly selects Barnes' gossip option to start the Opera Hall encounter."
+end
