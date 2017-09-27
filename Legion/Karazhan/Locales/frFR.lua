a93f164619bb7e104810b726b0bd31e8a838d7e6
roionsteroids <roionsteroids@gmail.com>
2017-09-27 19:07:33 +0200

    Legion/Karazhan/Trash: add warmup timer for Opera Hall: Westfall Story

--- Legion/Karazhan/Locales/frFR.lua
+++ Legion/Karazhan/Locales/frFR.lua
@@ -4,4 +4,6 @@ if L then
 	--L.skeletalUsher = "Skeletal Usher"
 	--L.custom_on_autotalk = "Autotalk"
 	--L.custom_on_autotalk_desc = "Instantly selects Barnes' gossip option to start the Opera Hall encounter."
+	--L.opera_hall_westfall_story_text = "Opera Hall: Westfall Story"
+	--L.opera_hall_westfall_story_trigger = "we meet two lovers" -- Tonight... we meet two lovers born on opposite sides of Sentinel Hill.
 end
