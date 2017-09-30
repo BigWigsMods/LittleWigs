dcfb5cd8aa52426c03f9b595b4242b4d78c194f3
WetU <yuk6196@gmail.com>
2017-10-01 06:38:39 +0900

    Legion/NeltharionsLair/Update koKR

--- Legion/NeltharionsLair/Locales/koKR.lua
+++ Legion/NeltharionsLair/Locales/koKR.lua
@@ -3,8 +3,8 @@ if not L then return end
 if L then
 	L.breaker = "막돌 파괴자"
 	L.hulk = "악성수정 괴수"
-	--L.gnasher = "Rockback Gnasher"
-	--L.trapper = "Rockbound Trapper"
+	L.gnasher = "돌가죽 뾰족니악어"
+	L.trapper = "돌갑옷 속박투사"
 end
 
 L = BigWigs:NewBossLocale("Ularogg Cragshaper", "koKR")
