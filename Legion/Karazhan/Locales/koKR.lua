dcb28b73ab59b8cd69c7c8d0a892fb3084067ee5
WetU <yuk6196@gmail.com>
2017-09-30 12:27:56 +0900

    Update koKR locales (#125)

--- Legion/Karazhan/Locales/koKR.lua
+++ Legion/Karazhan/Locales/koKR.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("Karazhan Trash", "koKR")
 if not L then return end
 if L then
-	--L.skeletalUsher = "Skeletal Usher"
-	--L.custom_on_autotalk = "Autotalk"
-	--L.custom_on_autotalk_desc = "Instantly selects Barnes' gossip option to start the Opera Hall encounter."
+	L.skeletalUsher = "해골 안내인"
+	L.custom_on_autotalk = "자동 대화"
+	L.custom_on_autotalk_desc = "오페라 극장 우두머리 전투를 시작하는 반즈의 대화 선택지를 즉시 고릅니다."
 end
