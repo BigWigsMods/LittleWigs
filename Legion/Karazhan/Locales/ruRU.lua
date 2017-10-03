ddaf97654d5cce203d7bf539fdaf5f00cfbd94f9
roionsteroids <roionsteroids@gmail.com>
2017-10-03 03:32:57 +0200

    Legion/Karazhan/Trash: add Shadow Rejuvenation and Alluring Aura

--- Legion/Karazhan/Locales/ruRU.lua
+++ Legion/Karazhan/Locales/ruRU.lua
@@ -4,4 +4,6 @@ if L then
 	--L.skeletalUsher = "Skeletal Usher"
 	--L.custom_on_autotalk = "Autotalk"
 	--L.custom_on_autotalk_desc = "Instantly selects Barnes' gossip option to start the Opera Hall encounter."
+	--L.attendant = "Spectral Attendant"
+	--L.hostess = "Wholesome Hostess"
 end
