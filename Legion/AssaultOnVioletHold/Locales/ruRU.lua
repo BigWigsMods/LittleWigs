e6595f05459c8ca0685a79244b7e6e1cb0f83c16
Justwait <justwait@method.gg>
2017-10-12 16:50:52 +0200

    Legion/AssaultOnVioletHold/Locales: Add locale files

---
+++ Legion/AssaultOnVioletHold/Locales/ruRU.lua
@@ -0,0 +1,14 @@
+local L = BigWigs:NewBossLocale("Assault on Violet Hold Trash", "ruRU")
+if not L then return end
+if L then
+	--L.custom_on_autotalk = "Autotalk"
+	--L.custom_on_autotalk_desc = "Instantly selects Lieutenant Sinclaris gossip option to start the Assault on Violet Hold."
+	--L.keeper = "Portal Keeper"
+	--L.guardian = "Portal Guardian"
+	--L.infernal = "Blazing Infernal"
+end
+
+L = BigWigs:NewBossLocale("Thalena", "ruRU")
+if L then
+	--L.essence = "Essence"
+end
---
+++ Legion/AssaultOnVioletHold/Locales/zhTW.lua
@@ -0,0 +1,14 @@
+local L = BigWigs:NewBossLocale("Assault on Violet Hold Trash", "zhTW")
+if not L then return end
+if L then
+	--L.custom_on_autotalk = "Autotalk"
+	--L.custom_on_autotalk_desc = "Instantly selects Lieutenant Sinclaris gossip option to start the Assault on Violet Hold."
+	--L.keeper = "Portal Keeper"
+	--L.guardian = "Portal Guardian"
+	--L.infernal = "Blazing Infernal"
+end
+
+L = BigWigs:NewBossLocale("Thalena", "zhTW")
+if L then
+	--L.essence = "Essence"
+end
