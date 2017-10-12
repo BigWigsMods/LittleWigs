726de271e00d1c2682d1fa71d0090d0b14f0a850
roionsteroids <roionsteroids@gmail.com>
2017-10-12 16:33:27 +0200

    Legion/Karazhan/Trash: add Shadow Rejuvenation and Alluring Aura (#130)

--- Legion/Karazhan/Locales/koKR.lua
+++ Legion/Karazhan/Locales/koKR.lua
@@ -4,4 +4,6 @@ if L then
 	L.skeletalUsher = "해골 안내인"
 	L.custom_on_autotalk = "자동 대화"
 	L.custom_on_autotalk_desc = "오페라 극장 우두머리 전투를 시작하는 반즈의 대화 선택지를 즉시 고릅니다."
+	--L.attendant = "Spectral Attendant"
+	--L.hostess = "Wholesome Hostess"
 end
--- Legion/Karazhan/Trash.lua
+++ Legion/Karazhan/Trash.lua
@@ -8,7 +8,9 @@ if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
 	114544, -- Skeletal Usher
-	114339 -- Barnes
+	114339, -- Barnes
+	114632, -- Spectral Attendant
+	114796 -- Wholesome Hostess
 )
 
 --------------------------------------------------------------------------------
@@ -20,6 +22,8 @@ if L then
 	L.skeletalUsher = "Skeletal Usher"
 	L.custom_on_autotalk = "Autotalk"
 	L.custom_on_autotalk_desc = "Instantly selects Barnes' gossip option to start the Opera Hall encounter."
+	L.attendant = "Spectral Attendant"
+	L.hostess = "Wholesome Hostess"
 end
 L = mod:GetLocale()
 
@@ -31,15 +35,21 @@ function mod:GetOptions()
 	return {
 		227966, -- Flashlight
 		"custom_on_autotalk", -- Barnes
+		228279, -- Shadow Rejuvenation
+		228575, -- Alluring Aura
 	}, {
 		[227966] = L.skeletalUsher,
 		["custom_on_autotalk"] = "general",
+		[228279] = L.attendant,
+		[228575] = L.hostess,
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
 	self:Log("SPELL_CAST_START", "Flashlight", 227966)
+	self:Log("SPELL_CAST_START", "ShadowRejuvenation", 228279)
+	self:Log("SPELL_CAST_START", "AlluringAura", 228575)
 
 	self:RegisterEvent("GOSSIP_SHOW")
 end
@@ -68,4 +78,14 @@ function mod:GOSSIP_SHOW()
 			SelectGossipOption(1)
 		end
 	end
+end
+
+-- Spectral Attendant
+function mod:ShadowRejuvenation(args)
+	self:Message(args.spellId, "Attention", "Warning", CL.casting:format(args.spellName))
+end
+
+-- Wholesome Hostess
+function mod:AlluringAura(args)
+	self:Message(args.spellId, "Important", "Alert", CL.casting:format(args.spellName))
 end
