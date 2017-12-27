fe7cc6440cc1a9381f32bc801db52c03f3a0ad2c
Adavak LEE <adavak@adavak.com>
2017-12-27 21:01:51 +0800

    Update zhCN.lua

--- Legion/CourtOfStars/Locales/zhCN.lua
+++ Legion/CourtOfStars/Locales/zhCN.lua
@@ -33,13 +33,13 @@ if L then
 	L.WoundedNightborneCivilian = "受伤的夜之子平民"
 
 	L.announce_buff_items = "通报增益物品"
-	L.announce_buff_items_desc = "通报此地城所有可用的增益物品，并通报谁可以使用。"
+	L.announce_buff_items_desc = "通报此地下城所有可用的增益物品，并通报谁可以使用。"
 
 	L.available = "%s|cffffffff%s|r可用" -- Context: item is available to use
 	L.usableBy = "使用者：%s" -- Context: item is usable by someone
 
 	L.custom_on_use_buff_items = "立即使用增益物品"
-	L.custom_on_use_buff_items_desc = "启用此选项后，自动确认使用物品前的对话选项并使用物品，这不包含二王前使用会引来守卫的物品。"
+	L.custom_on_use_buff_items_desc = "启用此选项后，自动确认使用物品前的对话选项并使用物品，这不包含二号首领前使用会引来守卫的物品。"
 
 	L.spy_helper = "密探事件助手"
 	L.spy_helper_desc = "在一个信息窗口显示队伍得到密探线索，并通报线索给其他队员。"
