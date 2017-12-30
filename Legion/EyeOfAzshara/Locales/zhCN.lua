a26ba616865d4925c64051195fc9a6f35a0c60eb
Adavak LEE <adavak@adavak.com>
2017-12-31 03:04:49 +0800

    Update zhCN.lua

--- Legion/EyeOfAzshara/Locales/zhCN.lua
+++ Legion/EyeOfAzshara/Locales/zhCN.lua
@@ -1,9 +1,9 @@
 local L = BigWigs:NewBossLocale("Eye of Azshara Trash", "zhCN")
 if not L then return end
 if L then
-	--L.stormweaver = "Hatecoil Stormweaver"
-	--L.oracle = "Hatecoil Oracle"
-	--L.siltwalker = "Mak'rana Siltwalker"
-	--L.tides = "Restless Tides"
-	--L.arcanist = "Hatecoil Arcanist"
+	L.stormweaver = "积怨织雷者"
+	L.oracle = "积怨神谕者"
+	L.siltwalker = "玛拉纳沙地行者"
+	L.tides = "焦躁的海潮元素"
+	L.arcanist = "积怨奥术师"
 end
