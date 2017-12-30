750d7a4f6082e89aa6d2a4ce26108f0c5bb56c1c
Adavak LEE <adavak@adavak.com>
2017-12-31 03:26:14 +0800

    Update zhCN.lua

--- Legion/NeltharionsLair/Locales/zhCN.lua
+++ Legion/NeltharionsLair/Locales/zhCN.lua
@@ -2,7 +2,7 @@ local L = BigWigs:NewBossLocale("Neltharions Lair Trash", "zhCN")
 if not L then return end
 if L then
 	L.breaker = "巨石破坏者"
-	--L.hulk = "Vileshard Hulk"
-	--L.gnasher = "Rockback Gnasher"
-	--L.trapper = "Rockbound Trapper"
+	L.hulk = "邪裂巨人"
+	L.gnasher = "岩背啮咬者"
+	L.trapper = "缚石捕兽者"
 end
