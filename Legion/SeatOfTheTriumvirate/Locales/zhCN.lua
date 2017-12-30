84d4f4229df106b7fd63a48d5a986b6032836673
Adavak LEE <adavak@adavak.com>
2017-12-31 02:08:23 +0800

    Update zhCN.lua

--- Legion/SeatOfTheTriumvirate/Locales/zhCN.lua
+++ Legion/SeatOfTheTriumvirate/Locales/zhCN.lua
@@ -4,7 +4,7 @@ if L then
 	L.custom_on_autotalk = "自动对话"
 	L.custom_on_autotalk_desc = "立即选择奥蕾莉亚·风行者对话选项。"
 	L.gossip_available = "可对话"
-	L.alleria_gossip_trigger = "下令吧，英雄们，我们马上开始突袭。" -- Allerias yell after the first boss is defeated
+	L.alleria_gossip_trigger = "跟我走！" -- Allerias yell after the first boss is defeated
 
 	L.alleria = "奥蕾莉亚·风行者"
 	L.subjugator = "影卫征服者"
