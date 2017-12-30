98967c80069cf240e56010618f8e01adecfd80dc
Adavak LEE <adavak@adavak.com>
2017-12-31 04:19:28 +0800

    add locale

---
+++ Legion/MawOfSouls/Locales/itIT.lua
@@ -0,0 +1,9 @@
+local L = BigWigs:NewBossLocale("Maw of Souls Trash", "itIT")
+if not L then return end
+if L then
+	--L.champion = "Helarjar Champion"
+	--L.mariner = "Night Watch Mariner"
+	--L.mistmender = "Seacursed Mistmender"
+	--L.mistcaller = "Helarjar Mistcaller"
+	--L.skjal = "Skjal"
+end
