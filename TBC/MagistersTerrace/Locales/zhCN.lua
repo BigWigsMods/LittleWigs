4fa1f580b2eef179922bc5aa395353091291d83b
Adavak LEE <adavak@adavak.com>
2017-12-31 10:51:26 +0800

    Update zhCN.lua

--- TBC/MagistersTerrace/Locales/zhCN.lua
+++ TBC/MagistersTerrace/Locales/zhCN.lua
@@ -1,10 +1,8 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("Little Wigs: Kael'thas Sunstrider", "zhCN")
+local L = BigWigs:NewBossLocale("Vexallus", "zhCN")
 if L then
-	--@localization(locale="zhCN", namespace="MagistersTerrace/Kael_thas", format="lua_additive_table", handle-unlocalized="ignore")@
-end
-
-local L = LibStub("AceLocale-3.0"):NewLocale("Little Wigs: Vexallus", "zhCN")
-if L then
-	--@localization(locale="zhCN", namespace="MagistersTerrace/Vexallus", format="lua_additive_table", handle-unlocalized="ignore")@
+	L["adds"] = "Pure Energy"
+	L["adds_desc"] = "Warn when Pure Energy is discharged."
+	L["adds_message"] = "Pure Energy discharged!"
+	L["adds_trigger"] = "discharges pure energy!"
 end
 
