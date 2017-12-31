ce137fb536c3d8330702d9bb5a9647b136fffbb2
Adavak LEE <adavak@adavak.com>
2017-12-31 11:13:33 +0800

    Update koKR.lua

--- MoP/MogushanPalace/Locales/koKR.lua
+++ MoP/MogushanPalace/Locales/koKR.lua
@@ -6,10 +6,10 @@ end
 
 L = BigWigs:NewBossLocale("Trial of the King", "koKR")
 if L then
-  L.scout = "글린트록 정찰병"
+	L.scout = "글린트록 정찰병"
 end
 
 L = BigWigs:NewBossLocale("Xin the Weaponmaster", "koKR")
 if L then
-  L.engage_yell = "너희가 내게 도전한 첫 상대는 아니다. 하지만 마지막 상대도 아닐 거다."
+	L.engage_yell = "너희가 내게 도전한 첫 상대는 아니다. 하지만 마지막 상대도 아닐 거다."
 end
