a491dbffd20f181c0f302837b54c2983981c5180
Adavak LEE <adavak@adavak.com>
2017-12-31 10:33:00 +0800

    Update zhCN.lua

--- Cataclysm/LostCity/Locales/zhCN.lua
+++ Cataclysm/LostCity/Locales/zhCN.lua
@@ -2,5 +2,5 @@ local L = BigWigs:NewBossLocale("Siamat", "zhCN")
 if L then
 	L.engage_trigger = "呼啸的南风，助你的主人一臂之力吧！"
 	L.servant = "召唤仆从"
-  L.servant_desc = "当召唤希亚玛特的仆从时发出警报。"
+	L.servant_desc = "当召唤希亚玛特的仆从时发出警报。"
 end
