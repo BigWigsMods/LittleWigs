cce729e2057a3040763546e9a25e2b46d49a8ace
Adavak LEE <adavak@adavak.com>
2017-12-31 12:04:17 +0800

    add locales

---
+++ TBC/TheBlackMorass/Locales/frFR.lua
@@ -0,0 +1,6 @@
+local L = BigWigs:NewBossLocale("The Black Morass Trash", "frFR")
+if not L then return end
+if L then
+	--L.wave = "Wave Warnings"
+	--L.wave_desc = "Announce approximate warning messages for the waves."
+end
