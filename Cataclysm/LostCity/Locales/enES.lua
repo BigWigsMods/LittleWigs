638895143ea9345ea5c721366131d3e13830fd2d
Adavak LEE <adavak@adavak.com>
2017-12-31 21:13:23 +0800

    Update enES.lua

--- Cataclysm/LostCity/Locales/enES.lua
+++ Cataclysm/LostCity/Locales/enES.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("Augh", "esES")
 if not L then return end
 if L then
-	--L.bossName = "Augh"
+	--L.augh = "Augh"
 end
 
 L = BigWigs:NewBossLocale("Siamat", "esES") or BigWigs:NewBossLocale("Siamat", "esMX")
