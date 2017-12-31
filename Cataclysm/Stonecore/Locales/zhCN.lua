9f42b2d62e6a69aaff09f94c0172994bea1af341
Adavak LEE <adavak@adavak.com>
2017-12-31 10:46:58 +0800

    Update zhCN.lua

--- Cataclysm/Stonecore/Locales/zhCN.lua
+++ Cataclysm/Stonecore/Locales/zhCN.lua
@@ -1,6 +1,6 @@
 local L = BigWigs:NewBossLocale("Corborus", "zhCN")
 if L then
-  L.burrow = "潜地/钻出"
+	L.burrow = "潜地/钻出"
 	L.burrow_desc = "当克伯鲁斯潜地或钻出时发出警报。"
 	L.burrow_message = "克伯鲁斯潜地！"
 	L.burrow_warning = "5秒后潜地！"
