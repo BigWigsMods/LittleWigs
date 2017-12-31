3e25ac24601600cb28ae63215689ba0338364565
Adavak LEE <adavak@adavak.com>
2017-12-31 21:15:12 +0800

    Update esMX.lua

--- Cataclysm/LostCity/Locales/esMX.lua
+++ Cataclysm/LostCity/Locales/esMX.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("Augh", "esMX")
 if not L then return end
 if L then
-	--L.bossName = "Augh"
+	--L.augh = "Augh"
 end
 
 L = BigWigs:NewBossLocale("Siamat", "esES") or BigWigs:NewBossLocale("Siamat", "esMX")
