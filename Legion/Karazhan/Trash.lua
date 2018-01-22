5e997cd1bc3957752585e1a48f4b89524bff9ca0
Zireael <Zireael-N@users.noreply.github.com>
2018-01-22 18:22:31 +0300

    Karazhan/Trash: do not warn about DK-controlled units casting "Shield Smash" and "Banshee Wail", closes #156 (#160)

--- Legion/Karazhan/Trash.lua
+++ Legion/Karazhan/Trash.lua
@@ -105,15 +105,15 @@ function mod:GOSSIP_SHOW()
 end
 
 function mod:Warmup(_, msg)
-	if msg:find(L.opera_hall_westfall_story_trigger, nil, true) then 
+	if msg:find(L.opera_hall_westfall_story_trigger, nil, true) then
 		self:Bar("warmup", 42, L.opera_hall_westfall_story_text, "achievement_raid_karazhan")
 	end
 
-	if msg:find(L.opera_hall_beautiful_beast_story_trigger, nil, true) then 
+	if msg:find(L.opera_hall_beautiful_beast_story_trigger, nil, true) then
 		self:Bar("warmup", 47, L.opera_hall_beautiful_beast_story_text, "achievement_raid_karazhan")
 	end
 
-	if msg:find(L.opera_hall_wikket_story_trigger, nil, true) then 
+	if msg:find(L.opera_hall_wikket_story_trigger, nil, true) then
 		self:Bar("warmup", 70, L.opera_hall_wikket_story_text, "achievement_raid_karazhan")
 	end
 end
@@ -129,7 +129,9 @@ function mod:AlluringAura(args)
 end
 
 function mod:BansheeWail(args)
-	self:Message(args.spellId, "Attention", "Warning", CL.casting:format(args.spellName))
+	if bit.band(args.sourceFlags, COMBATLOG_OBJECT_REACTION_FRIENDLY) == 0 then -- these NPCs can be mind-controlled by DKs
+		self:Message(args.spellId, "Attention", "Warning", CL.casting:format(args.spellName))
+	end
 end
 
 -- Ghostly Philanthropist
@@ -153,6 +155,9 @@ end
 do
 	local prev = 0
 	function mod:ShieldSmash(args)
+		if bit.band(args.sourceFlags, COMBATLOG_OBJECT_REACTION_FRIENDLY) ~= 0 then -- these NPCs can be mind-controlled by DKs
+			return
+		end
 		local t = GetTime()
 		if t-prev > 1.5 then
 			prev = t
