77c73ba300163dc36fa347ec3dec934c8c59139c
Zireael <Zireael-N@users.noreply.github.com>
2018-01-28 21:39:05 +0300

    Add warmup timers for Rokmora and L'ura (#163)

--- Legion/NeltharionsLair/Locales/zhTW.lua
+++ Legion/NeltharionsLair/Locales/zhTW.lua
@@ -7,6 +7,13 @@ if L then
 	--L.trapper = "Rockbound Trapper"
 end
 
+L = BigWigs:NewBossLocale("Rokmora", "zhTW")
+if L then
+	--L.warmup_text = "Rokmora Active"
+	--L.warmup_trigger = "Navarrogg?! Betrayer! You would lead these intruders against us?!"
+	--L.warmup_trigger_2 = "Either way, I will enjoy every moment of it. Rokmora, crush them!"
+end
+
 L = BigWigs:NewBossLocale("Ularogg Cragshaper", "zhTW")
 if L then
 	--L.totems = "Totems"
