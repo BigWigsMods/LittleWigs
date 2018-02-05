2494431cd446b5a16b15968b140348ab989da059
Zireael <Zireael-N@users.noreply.github.com>
2018-02-05 20:54:58 +0300

    EyeOfAzshara/LadyHatecoil: added helper messages for "Static Nova" and "Focused Lightning" (can be turned off in options) (#179)

--- Legion/EyeOfAzshara/Locales/esES.lua
+++ Legion/EyeOfAzshara/Locales/esES.lua
@@ -8,3 +8,12 @@ if L then
 	--L.tides = "Restless Tides"
 	--L.arcanist = "Hatecoil Arcanist"
 end
+
+L = BigWigs:NewBossLocale("Lady Hatecoil", "esES") or BigWigs:NewBossLocale("Lady Hatecoil", "esMX")
+if L then
+	--L.custom_on_show_helper_messages = "Helper messages for Static Nova and Focused Lightning"
+	--L.custom_on_show_helper_messages_desc = "Enable this option to add a helper message telling you whether water or land is safe when the boss starts casting |cff71d5ffStatic Nova|r or |cff71d5ffFocused Lightning|r."
+
+	--L.water_safe = "%s (water is safe)"
+	--L.land_safe = "%s (land is safe)"
+end
