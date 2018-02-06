04192756c16989d3f14bb2ea5b100048b1f16962
Zireael <Zireael-N@users.noreply.github.com>
2018-02-07 01:09:22 +0300

    SeatOfTheTriumvirate/ViceroyNezhar: fixed a typo (#184)

--- Legion/SeatOfTheTriumvirate/ViceroyNezhar.lua
+++ Legion/SeatOfTheTriumvirate/ViceroyNezhar.lua
@@ -94,8 +94,8 @@ function mod:UpdateInfoBox()
 			self:SetInfo("infobox", 2, tentaclesUp)
 		end
 		if guardsUp > 0 then
-			self:SetInfo("infobox", tectaclesUp > 0 and 3 or 1, L.guards)
-			self:SetInfo("infobox", tectaclesUp > 0 and 4 or 2, guardsUp)
+			self:SetInfo("infobox", tentaclesUp > 0 and 3 or 1, L.guards)
+			self:SetInfo("infobox", tentaclesUp > 0 and 4 or 2, guardsUp)
 		end
 	else
 		self:CloseInfo("infobox")
