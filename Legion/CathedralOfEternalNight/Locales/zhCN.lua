b61089c31ccf4ebc4e8428c60572956f1e002e4c
Adavak LEE <adavak@adavak.com>
2018-02-09 21:50:57 +0800

    Update zhCN.lua

--- Legion/CathedralOfEternalNight/Locales/zhCN.lua
+++ Legion/CathedralOfEternalNight/Locales/zhCN.lua
@@ -21,9 +21,9 @@ if L then
 	L.botanist = "邪脉植物学家"
 	L.orbcaster = "邪足晶球法师"
 	L.waglur = "瓦格鲁尔"
-	--L.scavenger = "Wyrmtongue Scavenger"
+	L.scavenger = "虫语清道夫"
 	L.gazerax = "加泽拉克斯"
 	L.vilebark = "邪皮行者"
 
-	--L.throw_tome = "Throw Tome" -- Common part of Throw Arcane/Frost/Silence Tome (242837/242839/242841)
+	L.throw_tome = "投掷宝典" -- Common part of Throw Arcane/Frost/Silence Tome (242837/242839/242841)
 end
