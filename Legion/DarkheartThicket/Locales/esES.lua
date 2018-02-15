5953bc7196cb70dbc0d2e57125c8bbdf02b7512f
Zireael <Zireael-N@users.noreply.github.com>
2018-02-15 20:50:24 +0300

    DarkheartThicket/Trash: added a warning for "Nightmare Toxin", fixed "Blood Bomb" (#192)

--- Legion/DarkheartThicket/Locales/esES.lua
+++ Legion/DarkheartThicket/Locales/esES.lua
@@ -2,6 +2,7 @@ local L = BigWigs:NewBossLocale("Darkheart Thicket Trash", "esES") or BigWigs:Ne
 if not L then return end
 if L then
 	L.ruiner = "Arruinador Almaespanto"
+	L.poisoner = "Envenenador Almaespanto"
 	L.razorbeak = "Picovaja enloquecido"
 	L.grizzly = "Oso pardo con piel supurante"
 	L.fury = "Furia manchada de sangre"
