856a0610ddd616dbc4b88bc099f3f9a8fea2d2e2
Zireael <Zireael-N@users.noreply.github.com>
2018-02-16 21:42:19 +0300

    WotLK/UtgardePinnacle/Skadi: use modern API to detect the encounter, print the target of "Whirlwind" and also warn when taking damage from it

--- WotLK/UtgardePinnacle/Skadi.lua
+++ WotLK/UtgardePinnacle/Skadi.lua
@@ -6,6 +6,7 @@
 local mod, CL = BigWigs:NewBoss("Skadi the Ruthless", 524, 643)
 if not mod then return end
 mod:RegisterEnableMob(26693)
+mob.engageId = 2029
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -19,18 +20,36 @@ end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "Whirlwind", 59322, 50228)
-
-	self:Death("Win", 26693)
+	self:Log("SPELL_DAMAGE", "WhirlwindDamage", 59322, 50228)
+	self:Log("SPELL_MISSED", "WhirlwindDamage", 59322, 50228)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
-function mod:Whirlwind(args)
-	self:Message(59322, "Urgent", "Info")
-	self:DelayedMessage(59322, 23, "Attention", CL.soon:format(args.spellName))
-	self:Bar(59322, 10, CL.cast:format(args.spellName))
-	self:CDBar(59322, 23)
+do
+	local function printTarget(self, player, guid)
+		self:TargetMessage(59322, player, "Urgent", "Info", nil, nil, true)
+		if self:Me(guid) then
+			self:Say(59322)
+		end
+	end
+
+	function mod:Whirlwind(args)
+		self:GetBossTarget(printTarget, 0.4, args.sourceGUID)
+		self:Bar(59322, 10, CL.cast:format(args.spellName))
+		self:CDBar(59322, 23)
+	end
 end
 
+do
+	local prev = 0
+	function mod:WhirlwindDamage(args)
+		local t = GetTime()
+		if self:Me(args.destGUID) and t-prev > 1.5 then
+			prev = t
+			self:Message(59322, "Personal", "Alarm", CL.underyou:format(args.spellName))
+		end
+	end
+end
