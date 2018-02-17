757f20630d5cd86825bc349f6e4c083371a91425
Adavak LEE <adavak@adavak.com>
2018-02-17 12:25:20 +0800

    zhCN update (#196)

--- Legion/CourtOfStars/Locales/zhCN.lua
+++ Legion/CourtOfStars/Locales/zhCN.lua
@@ -147,5 +147,5 @@ end
 
 L = BigWigs:NewBossLocale("Advisor Melandrus", "zhCN")
 if L then
-	--L.warmup_trigger = "Yet another failure, Melandrus. Consider this your chance to correct it. Dispose of these outsiders. I must return to the Nighthold."
+	L.warmup_trigger = "显然你又失败了，麦兰杜斯。我给你一个机会。干掉这些外来者，我得回暗夜要塞了。"
 end
