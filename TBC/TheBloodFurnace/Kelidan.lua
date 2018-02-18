f1ec9a3427d85edb93acae865585003815706ea5
Zireael <Zireael-N@users.noreply.github.com>
2018-02-18 11:46:10 +0300

    TBC/TheBloodFurnace: updates to the latest BigWigs API

--- TBC/TheBloodFurnace/Kelidan.lua
+++ TBC/TheBloodFurnace/Kelidan.lua
@@ -1,27 +1,29 @@
 -------------------------------------------------------------------------------
 --  Module Declaration
 
-local mod = BigWigs:NewBoss("Keli'dan the Breaker", 725, 557)
+local mod, CL = BigWigs:NewBoss("Keli'dan the Breaker", 725, 557)
 if not mod then return end
-mod.partyContent = true
-mod.otherMenu = "Hellfire Citadel"
+--mod.otherMenu = "Hellfire Citadel"
 mod:RegisterEnableMob(17377)
-mod.toggleOptions = {
-	30940,
-}
 
 -------------------------------------------------------------------------------
 --  Initialization
 
+function mod:GetOptions()
+	return {
+		30940, -- Burning Nova
+	}
+end
+
 function mod:OnBossEnable()
-	self:Log("SPELL_CAST_SUCCESS", "Nova", 30940)
+	self:Log("SPELL_CAST_SUCCESS", "BurningNova", 30940)
 	self:Death("Win", 17377)
 end
 
 -------------------------------------------------------------------------------
 --  Event Handlers
 
-function mod:Nova(_, spellId, _, _, spellName)
-	self:Message(30940, spellName, "Important", spellId)
-	self:Bar(30940, spellName, 5, spellId)
+function mod:BurningNova(args)
+	self:Message(args.spellId, "Important")
+	self:Bar(args.spellId, 5)
 end
