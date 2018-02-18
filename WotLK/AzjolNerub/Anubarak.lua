4202ccca4a3855ea9e97b037db01e2210e941231
Zireael <Zireael-N@users.noreply.github.com>
2018-02-18 12:00:38 +0300

    WotLK/AzjolNerub: updates to the latest BigWigs API

--- WotLK/AzjolNerub/Anubarak.lua
+++ WotLK/AzjolNerub/Anubarak.lua
@@ -1,23 +1,20 @@
 -------------------------------------------------------------------------------
 --  Module Declaration
 
-local mod = BigWigs:NewBoss("Anub'arakAN", 533) -- AN (AnzolNerub) is intentional to prevent conflict with Anub'arak from The Coliseum
+local mod, CL = BigWigs:NewBoss("Anub'arakAN", 533, 587) -- AN (AnzolNerub) is intentional to prevent conflict with Anub'arak from The Coliseum
 if not mod then return end
-mod.partycontent = true
-mod.otherMenu = "Dragonblight"
 mod.displayName = "Anub'arak"
+--mod.otherMenu = "Dragonblight"
 mod:RegisterEnableMob(29120)
-mod.toggleOptions = {
-	53472, -- Pound
-}
-
--------------------------------------------------------------------------------
---  Localization
-
-local LCL = LibStub("AceLocale-3.0"):GetLocale("Little Wigs: Common")
 
 -------------------------------------------------------------------------------
 --  Initialization
+
+function mod:GetOptions()
+	return {
+		53472, -- Pound
+	}
+end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "Pound", 53472, 59433)
@@ -27,7 +24,7 @@ end
 -------------------------------------------------------------------------------
 --  Event Handlers
 
-function mod:Pound(_, spellId, _, _, spellName)
-	self:Bar(53472, spellName, 3.2, spellId)
-	self:Message(53472, LCL["casting"]:format(spellName), "Attention", spellId)
+function mod:Pound(args)
+	self:CastBar(53472, 3.2)
+	self:Message(53472, "Attention", nil, CL.casting:format(args.spellName))
 end
--- WotLK/AzjolNerub/Krikthir.lua
+++ WotLK/AzjolNerub/Krikthir.lua
@@ -1,73 +1,58 @@
 -------------------------------------------------------------------------------
 --  Module Declaration
 
-local mod = BigWigs:NewBoss("Krik'thir the Gatewatcher", 533)
+local mod, CL = BigWigs:NewBoss("Krik'thir the Gatewatcher", 533, 585)
 if not mod then return end
-mod.partycontent = true
-mod.otherMenu = "Dragonblight"
+--mod.otherMenu = "Dragonblight"
 mod:RegisterEnableMob(28684)
-mod.defaultToggles = {"MESSAGE"}
-mod.toggleOptions = {
-	{52592, "BAR"}, -- Curse
-	28747, -- Frenzy
-}
-
--------------------------------------------------------------------------------
---  Locals
-
-local frenzyAnnounced = nil
-
--------------------------------------------------------------------------------
---  Localization
-
-local LCL = LibStub("AceLocale-3.0"):GetLocale("Little Wigs: Common")
-
-local L = mod:GetLocale()
-if L then
-	L["frenzysoon_message"] = "Frenzy Soon"
-end
 
 -------------------------------------------------------------------------------
 --  Initialization
 
+function mod:GetOptions()
+	return {
+		52592, -- Curse of Fatigue
+		28747, -- Frenzy
+	}
+end
+
 function mod:OnBossEnable()
-	if bit.band(self.db.profile[(GetSpellInfo(28747))], BigWigs.C.MESSAGE) == BigWigs.C.MESSAGE then
-		self:RegisterEvent("UNIT_HEALTH")
-	end
-	self:Log("SPELL_AURA_APPLIED", "Curse", 52592, 59368)
-	self:Log("SPELL_AURA_APPLIED", "CurseRemoved", 52592, 59368)
+	self:Log("SPELL_AURA_APPLIED", "CurseOfFatigue", 52592, 59368)
+	self:Log("SPELL_AURA_APPLIED", "CurseOfFatigueRemoved", 52592, 59368)
 	self:Log("SPELL_AURA_APPLIED", "Frenzy", 28747)
+
+	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
+	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
 	self:Death("Win", 28684)
 end
 
 function mod:OnEngage()
-	frenzyAnnounced = nil
+	if self:CheckOption(28747, "MESSAGE") then -- Frenzy is %-based
+		self:RegisterEvent("UNIT_HEALTH")
+	end
 end
 
-----------------------------------
---        Event Handlers        --
-----------------------------------
+-------------------------------------------------------------------------------
+--  Event Handlers
 
-function mod:Curse(player, spellId, _, _, spellName)
-	self:Message(52592, spellName..": "..player, "Attention", spellId)
-	self:Bar(52592, player..": "..spellName, 10, spellId)
+function mod:CurseOfFatigue(args)
+	self:TargetMessage(52592, args.destName, "Attention")
+	self:TargetBar(52592, 10, args.destName)
 end
 
-function mod:CurseRemoved(player, _, _, _, spellName)
-	self:SendMessage("BigWigs_StopBar", self, player..": "..spellName)
+function mod:CurseOfFatigueRemoved(args)
+	self:StopBar(52592, args.destName)
 end
 
-function mod:Frenzy(boss, spellId)
-	self:Message(28747, LCL["frenzied"]:format(boss), "Important", spellId)
+function mod:Frenzy(args)
+	self:Message(args.spellId, "Important", nil, CL.onboss:format(args.spellName))
 end
 
-function mod:UNIT_HEALTH(event, msg)
-	if UnitName(msg) ~= mod.displayName then return end
-	local health = UnitHealth(msg)
-	if health > 10 and health <= 15 and not frenzyAnnounced then
-		frenzyAnnounced = true
-		self:Message(28747, L["frenzysoon_message"], "Important", 28747)
-	elseif health > 15 and frenzyAnnounced then
-		frenzyAnnounced = nil
+function mod:UNIT_HEALTH(event, unit)
+	if UnitName(unit) ~= self.displayName then return end
+	local health = UnitHealth(unit) / UnitHealthMax(unit) * 100
+	if health > 10 and health <= 15 then
+		self:UnregisterEvent("UNIT_HEALTH")
+		self:Message(28747, "Important", nil, CL.soon(self:SpellName(28747))) -- Frenzy
 	end
 end
