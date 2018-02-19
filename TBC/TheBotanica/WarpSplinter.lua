6ebb0de9079e111537e3c61f0617b9cf7b31b177
Zireael <Zireael-N@users.noreply.github.com>
2018-02-19 03:19:36 +0300

    mapId -> instanceId

--- TBC/TheBotanica/WarpSplinter.lua
+++ TBC/TheBotanica/WarpSplinter.lua
@@ -2,7 +2,7 @@
 -- Module Declaration
 --
 
-local mod, CL = BigWigs:NewBoss("Warp Splinter", 729, 562)
+local mod, CL = BigWigs:NewBoss("Warp Splinter", 553, 562)
 if not mod then return end
 --mod.otherMenu = "Tempest Keep"
 mod:RegisterEnableMob(17977)
