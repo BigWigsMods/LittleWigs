31f0a324c5bac7abbda9e97ecb249d93337da68f
Zireael <Zireael-N@users.noreply.github.com>
2018-02-20 23:48:24 +0300

    Legion: reduced the amount of GetTime() calls (#231)

--- Legion/MawOfSouls/Harbaron.lua
+++ Legion/MawOfSouls/Harbaron.lua
@@ -73,10 +73,12 @@ end
 do
 	local prev = 0
 	function mod:NetherRipDamage(args)
-		local t = GetTime()
-		if t-prev > 2 and self:Me(args.destGUID) then
-			prev = t
-			self:Message(194668, "Personal", "Alarm", CL.underyou:format(args.spellName))
+		if self:Me(args.destGUID) then
+			local t = GetTime()
+			if t-prev > 2 then
+				prev = t
+				self:Message(194668, "Personal", "Alarm", CL.underyou:format(args.spellName))
+			end
 		end
 	end
 end
