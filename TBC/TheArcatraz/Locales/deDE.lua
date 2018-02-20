dc6d281811b996e5760123f48505ebe863397598
Zireael <Zireael-N@users.noreply.github.com>
2018-02-20 07:21:33 +0300

    TBC/TheArcatraz/Skyriss: extracted locales (#228)

---
+++ TBC/TheArcatraz/Locales/deDE.lua
@@ -0,0 +1,5 @@
+local L = BigWigs:NewBossLocale("Harbinger Skyriss", "deDE")
+if not L then return end
+if L then
+	L.split_trigger = "Das Universum ist unser Zuhause, wir sind zahllos wie die Sterne!"
+end
---
+++ TBC/TheArcatraz/Locales/frFR.lua
@@ -0,0 +1,5 @@
+local L = BigWigs:NewBossLocale("Harbinger Skyriss", "frFR")
+if not L then return end
+if L then
+	L.split_trigger = "Nous nous étendons sur l'univers, aussi innombrables que les étoiles !"
+end
