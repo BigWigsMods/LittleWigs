c0ae71d4537605de44583b9f55ef96ea5c8c1dec
Zireael <Zireael-N@users.noreply.github.com>
2018-02-24 20:35:59 +0300

    Added modules for Gate of the Setting Sun

---
+++ MoP/GateOfTheSettingSun/Rimok.lua
@@ -0,0 +1,97 @@
+
+--------------------------------------------------------------------------------
+-- Module declaration
+--
+
+local mod, CL = BigWigs:NewBoss("Commander Ri'mok", 875, 676)
+if not mod then return end
+mod:RegisterEnableMob(56636)
+mod.engageId = 1406
+mod.respawnTime = 20
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+local checkingTimer = nil
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		-5666, -- Viscous Fluid
+		107120, -- Frenzied Assault
+	}
+end
+
+function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "FrenziedAssault", 107120)
+	self:Log("SPELL_DAMAGE", "FrenziedAssaultDamage", 107121)
+	self:Log("SPELL_MISSED", "FrenziedAssaultDamage", 107121)
+	self:Log("SPELL_AURA_APPLIED", "ViscousFluid", 107122)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "ViscousFluid", 107122)
+end
+
+function mod:OnEngage()
+	self:CDBar(107120, 5.7) -- Frenzied Assault
+end
+
+function mod:OnBossDisable()
+	if checkingTimer then
+		self:CancelTimer(checkingTimer)
+		checkingTimer = nil
+	end
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
+
+local function checkViscousFluidOnBoss(self) -- no SPELL_AURA_ events for the boss's buffs
+	local _, _, _, stacks = UnitBuff("boss1", self:SpellName(107122))
+	if not stacks then return end
+	self:StackMessage(-5666, self.displayName, stacks, "Urgent", "Info")
+end
+
+function mod:FrenziedAssault(args)
+	self:Message(args.spellId, "Attention", "Long", CL.casting:format(args.spellName))
+	self:CastBar(args.spellId, 7) -- 1s cast + 6s channel
+	self:CDBar(args.spellId, 17)
+
+	if self:Tank() and not checkingTimer then
+		checkingTimer = self:ScheduleRepeatingTimer(checkViscousFluidOnBoss, 3, self)
+	end
+end
+
+do
+	local prev = 0
+	function mod:FrenziedAssaultDamage(args)
+		if self:Me(args.destGUID) then
+			local t = GetTime()
+			if (not self:Tank() and t-prev > 1.5) or t-prev > 6 then -- the tank might want to bring adds in front of him
+				prev = t
+				self:Message(107120, "Personal", "Alert", CL.you:format(args.spellName))
+			end
+		end
+	end
+end
+
+function mod:ViscousFluid(args)
+	local amount = args.amount or 1
+	if self:Me(args.destGUID) and amount % 2 == 1 then
+		self:StackMessage(-5666, args.destName, amount, "Important", "Alarm")
+	end
+end
+
+function mod:UNIT_HEALTH_FREQUENT(unit)
+	local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
+	if hp < nextReinforcementsWarning then
+		self:Message(-5946, "Attention", nil, CL.soon:format(self:SpellName(-5946)))
+		nextReinforcementsWarning = nextReinforcementsWarning - 30
+		if nextReinforcementsWarning < 40 then
+			self:UnregisterUnitEvent("UNIT_HEALTH_FREQUENT", unit)
+		end
+	end
+end
