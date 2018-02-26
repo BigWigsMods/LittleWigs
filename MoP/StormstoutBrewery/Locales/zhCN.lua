c6acfe6549dab04df762ce5116fdcd54cc9ef611
Adavak LEE <adavak@adavak.com>
2018-02-26 09:32:20 +0800

    Update zhCN.lua

--- MoP/StormstoutBrewery/Locales/zhCN.lua
+++ MoP/StormstoutBrewery/Locales/zhCN.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Yan-Zhu the Uncasked", "zhCN")
 if not L then return end
 if L then
-	--L.summon_desc = "Warn when Yan-Zhu summons a Yeasty Brew Alemental. They can cast |cff71d5ffFerment|r to heal the boss."
+	L.summon_desc = "当炎诛召唤发酵酒灵时发出警报。他们会施放|cff71d5ff活力发酵|r治疗首领。"
 end
