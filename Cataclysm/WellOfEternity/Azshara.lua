708fd97caef380c78737d8b4fb9e8bc0db350f8a
Zireael <Zireael-N@users.noreply.github.com>
2018-02-27 09:41:13 +0300

    cleanups

--- Cataclysm/WellOfEternity/Azshara.lua
+++ Cataclysm/WellOfEternity/Azshara.lua
@@ -17,13 +17,13 @@ local canEnable = true
 function mod:GetOptions()
 	return {
 		-3968, -- Servant of the Queen
-		-3969, -- Mass Obedience
+		-3969, -- Total Obedience
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
-	self:Log("SPELL_CAST_START", "MassObedience", 103241)
+	self:Log("SPELL_CAST_START", "TotalObedience", 103241)
 	self:Log("SPELL_INTERRUPT", "Interrupt", "*")
 end
 
@@ -39,22 +39,20 @@ end
 -- Event Handlers
 --
 
-function mod:UNIT_SPELLCAST_SUCCEEDED(unit, spellName, _, _, spellId)
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, _, spellId)
 	if spellId == 102334 then -- Servant of the Queen
 		self:Message(-3968, "Attention", "Alert", spellId)
-		--self:TargetMessage(-3969, player, "Urgent", "Long", spellId)
-		--self:PrimaryIcon(-3969, player)
 	end
 end
 
-function mod:MassObedience(args)
+function mod:TotalObedience(args)
 	self:Message(-3969, "Urgent", "Long", args.spellId)
-	self:Bar(-3969, 10, CL.cast:format(args.spellName))
+	self:CastBar(-3969, 10)
 end
 
 function mod:Interrupt(args)
-	if args.extraSpellId == 103241 then -- Mass Obedience
-		self:Message(-3969, "Positive", nil, ("%s (%s)"):format(self:SpellName(134340), self:ColorName(args.sourceName))) -- 134340 = "Interrupted"
-		self:StopBar(CL.cast:format(args.amount)) -- Name of interrupted spell
+	if args.extraSpellId == 103241 then -- Total Obedience
+		self:Message(-3969, "Positive", nil, CL.interrupted_by:format(args.extraSpellName, self:ColorName(args.sourceName)))
+		self:StopBar(CL.cast:format(args.extraSpellName))
 	end
 end
