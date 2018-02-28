fb32d1c598e197fbba20405bdd56696da545aff6
Zireael <Zireael-N@users.noreply.github.com>
2018-02-28 17:57:47 +0300

    Cataclysm/WellOfEternity: updates to the latest BigWigs API, an option to instantly select gossips (#206)

---
+++ Cataclysm/WellOfEternity/Locales/esES.lua
@@ -0,0 +1,6 @@
+local L = BigWigs:NewBossLocale("Well Of Eternity Trash", "esES") or BigWigs:NewBossLocale("Well Of Eternity Trash", "esMX")
+if not L then return end
+if L then
+	--L.custom_on_autotalk = "Autotalk"
+	--L.custom_on_autotalk_desc = "Instantly select Illidan's gossip option."
+end
