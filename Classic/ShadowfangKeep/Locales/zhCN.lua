bcb8d32bf81b841f680f5273009c85528026f2e1
Adavak LEE <adavak@adavak.com>
2018-03-02 00:22:31 +0800

    update zhCN. (#251)

--- Classic/ShadowfangKeep/Locales/zhCN.lua
+++ Classic/ShadowfangKeep/Locales/zhCN.lua
@@ -2,7 +2,7 @@ local L = BigWigs:NewBossLocale("Lord Walden", "zhCN")
 if not L then return end
 if L then
 	-- %s will be either "Toxic Coagulant" or "Toxic Catalyst"
-	--L.coagulant = "%s: Move to dispel"
-	--L.catalyst = "%s: Crit Buff"
-	--L.toxin_healer_message = "%: DoT on everyone"
+	L.coagulant = "%s：移动消除"
+	L.catalyst = "%s：爆击增益"
+	L.toxin_healer_message = "%：全体 DoT"
 end
