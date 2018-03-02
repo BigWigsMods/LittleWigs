1db4dc39cd241618dcae4834f01785b659ede916
Denis M <36163327+iOut1aw@users.noreply.github.com>
2018-03-02 19:46:25 +0300

    Update ruRU.lua

--- Legion/Karazhan/Locales/ruRU.lua
+++ Legion/Karazhan/Locales/ruRU.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("Karazhan Trash", "ruRU")
 if not L then return end
 if L then
-	--L.skeletalUsher = "Skeletal Usher"
+	L.skeletalUsher = "Гниющий билетер"
 	--L.custom_on_autotalk = "Autotalk"
 	--L.custom_on_autotalk_desc = "Instantly selects Barnes' gossip option to start the Opera Hall encounter."
 	--L.attendant = "Spectral Attendant"
