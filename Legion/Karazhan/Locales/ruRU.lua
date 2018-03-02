91ee5bf4e70759991edd2a2a85b2d56e532a7e76
Denis M <36163327+iOut1aw@users.noreply.github.com>
2018-03-02 19:34:06 +0300

    Update ruRU.lua

--- Legion/Karazhan/Locales/ruRU.lua
+++ Legion/Karazhan/Locales/ruRU.lua
@@ -9,7 +9,7 @@ if L then
 	--L.opera_hall_westfall_story_text = "Opera Hall: Westfall Story"
 	L.opera_hall_westfall_story_trigger = "Они родились по разные стороны Сторожевого холма."
 	--L.opera_hall_beautiful_beast_story_text = "Opera Hall: Beautiful Beast"
-	--L.opera_hall_beautiful_beast_story_trigger = "a tale of romance and rage" -- Tonight... a tale of romance and rage, one which will prove once and for all if beaty is more than skin deep.
+	L.opera_hall_beautiful_beast_story_trigger = "Эта история о любви и гневе навсегда поставит точку в вопросе, обманчива ли красота." -- Tonight... a tale of romance and rage, one which will prove once and for all if beaty is more than skin deep.
 	--L.opera_hall_wikket_story_text = "Opera Hall: Wikket"
 	--L.opera_hall_wikket_story_trigger = "Shut your jabber" -- Shut your jabber, drama man! The Monkey King got another plan!
 	--L.barnes = "Barnes"
