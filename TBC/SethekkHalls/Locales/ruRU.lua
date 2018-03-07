220fe1c776281132973d911b92ef8b3a8e0c0306
Zireael <Zireael-N@users.noreply.github.com>
2018-03-07 06:19:21 +0300

    Syth and locale removal

--- TBC/SethekkHalls/Locales/ruRU.lua
+++
@@ -1,7 +0,0 @@
-local L = BigWigs:NewBossLocale("Darkweaver Syth", "ruRU")
-if not L then return end
-if L then
-	--L.summon = "Summon Elementals"
-	--L.summon_desc = "Warn for Summoned Elementals"
-	--L.summon_message = "Elementals Summoned!"
-end
