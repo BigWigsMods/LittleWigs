78c569bc20df9ceb9155fb5147771f84f327c8a8
Zireael <Zireael-N@users.noreply.github.com>
2018-03-09 02:50:23 +0300

    TBC/MagistersTerrace: added a warmup timer for Kael'thas, engageIds, cleanups (#259)

--- TBC/MagistersTerrace/Locales/koKR.lua
+++ TBC/MagistersTerrace/Locales/koKR.lua
@@ -1,8 +1,11 @@
 local L = BigWigs:NewBossLocale("Vexallus", "koKR")
 if not L then return end
 if L then
-	L.adds = "순수한 에너지"
-	L.adds_desc = "순수한 에너지가 소환되면 경보합니다."
-	L.adds_message = "순수한 에너지 소환됨!"
-	L.adds_trigger = "순수한 힘을 방출합니다!"
+	L.energy_discharged = "%s 소환됨" -- %s = Pure Energy (npc ID = 24745)
+end
+
+L = BigWigs:NewBossLocale("Kael'thas Sunstrider ", "koKR")
+if L then
+	-- Don't look so smug! I know what you're thinking, but Tempest Keep was merely a setback. Did you honestly believe I would trust the future to some blind, half-night elf mongrel?
+	-- L.warmup_trigger = "Don't look so smug!"
 end
