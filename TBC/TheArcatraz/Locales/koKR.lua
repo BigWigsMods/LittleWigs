4561e90243f183531bfae372c646de420e070b66
Zireael <Zireael-N@users.noreply.github.com>
2018-03-08 07:15:00 +0300

    TheArcatraz/Skyriss: added a warmup timer

--- TBC/TheArcatraz/Locales/koKR.lua
+++ TBC/TheArcatraz/Locales/koKR.lua
@@ -1,4 +1,6 @@
 local L = BigWigs:NewBossLocale("Harbinger Skyriss", "koKR")
 if not L then return end
 if L then
+	-- It is a small matter to control the mind of the weak... for I bear allegiance to powers untouched by time, unmoved by fate. No force on this world or beyond harbors the strength to bend our knee... not even the mighty Legion!
+	--L.warmup_trigger = "the mighty Legion"
 end
--- TBC/TheArcatraz/Skyriss.lua
+++ TBC/TheArcatraz/Skyriss.lua
@@ -20,6 +20,8 @@ local nextSplitWarning = 71
 
 local L = mod:GetLocale()
 if L then
+	-- It is a small matter to control the mind of the weak... for I bear allegiance to powers untouched by time, unmoved by fate. No force on this world or beyond harbors the strength to bend our knee... not even the mighty Legion!
+	L.warmup_trigger = "the mighty Legion"
 end
 
 --------------------------------------------------------------------------------
@@ -28,6 +30,7 @@ end
 
 function mod:GetOptions()
 	return {
+		"warmup",
 		39415, -- Fear
 		37162, -- Domination
 		36924, -- Mind Rend
@@ -36,6 +39,7 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL", "Warmup")
 	self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", nil, "boss1")
 	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
 
@@ -52,6 +56,13 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:Warmup(event, msg)
+	if msg:find(L.warmup_trigger, nil, true) then
+		self:UnregisterEvent(event)
+		self:Bar("warmup", 30.2, CL.active, "achievement_boss_harbinger_skyriss")
+	end
+end
 
 function mod:Fear(args)
 	self:TargetMessage(args.spellId, args.destName, "Attention")
