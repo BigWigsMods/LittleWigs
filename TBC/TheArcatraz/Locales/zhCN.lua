5b9c1bbcdac5f6f01b47ff056812cfe61a46127d
Adavak LEE <adavak@adavak.com>
2018-03-09 10:04:50 +0800

    Update zhCN.lua

--- TBC/TheArcatraz/Locales/zhCN.lua
+++ TBC/TheArcatraz/Locales/zhCN.lua
@@ -1,14 +1,14 @@
 local L = BigWigs:NewBossLocale("Harbinger Skyriss", "zhCN")
 if not L then return end
 if L then
-	-- I knew the prince would be angry, but I... I have not been myself. I had to let them out! The great one speaks to me, you see. Wait--outsiders. Kael'thas did not send you! Good... I'll just tell the prince you released the prisoners!
-	--L.first_cell_trigger = "I have not been myself"
-	-- Behold, yet another terrifying creature of incomprehensible power!
-	--L.second_and_third_cells_trigger = "of incomprehensible power"
-	-- Anarchy! Bedlam! Oh, you are so wise! Yes, I see it now, of course!
-	--L.fourth_cell_trigger = "Anarchy! Bedlam!"
-	-- It is a small matter to control the mind of the weak... for I bear allegiance to powers untouched by time, unmoved by fate. No force on this world or beyond harbors the strength to bend our knee... not even the mighty Legion!
-	--L.warmup_trigger = "the mighty Legion"
+	-- 我知道王子殿下会非常生气，但是我……我不知道自己怎么回事，居然把它们都放出来了！主人在对我说话，你也看到了……等等，有人来了。你们不是凯尔萨斯派来的！好极了……我会告诉王子殿下，是你们放走了囚犯！
+	L.first_cell_trigger = "我不知道自己怎么回事"
+	-- 看吧！又一个拥有深不可测的力量的恐怖生物！
+	L.second_and_third_cells_trigger = "深不可测的力量的恐怖生物"
+	-- 混乱！疯狂！啊，您真是太睿智了！是的，我明白了，当然！
+	L.fourth_cell_trigger = "混乱！疯狂！"
+	-- 控制弱者的思想是非常容易的事情……因为我所效忠的力量不会为时间所侵蚀，亦不被命运所左右。这个世界上没有什么力量能让我们屈膝……即使是燃烧军团也不行！
+	L.warmup_trigger = "燃烧军团也不行"
 
-	--L.prison_cell = "Prison Cell"
+	L.prison_cell = "鉴于牢笼"
 end
