421e9e96f68884081c981870bdccbff0595d509a
Zireael <Zireael-N@users.noreply.github.com>
2018-03-10 07:26:04 +0300

    WarpSplinter

--- TBC/TheBotanica/WarpSplinter.lua
+++ TBC/TheBotanica/WarpSplinter.lua
@@ -5,6 +5,14 @@
 local mod, CL = BigWigs:NewBoss("Warp Splinter", 729, 562)
 if not mod then return end
 mod:RegisterEnableMob(17977)
+mod.engageId = 1929
+-- mod.respawnTime = 0 -- resets, doesn't respawn
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+local addsAlive = 0
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -12,21 +20,21 @@ mod:RegisterEnableMob(17977)
 
 function mod:GetOptions()
 	return {
-		34727, -- Summon Saplings
+		34716, -- Stomp
+		-5478, -- Summon Saplings
 	}
 end
 
 function mod:OnBossEnable()
-	self:Log("SPELL_SUMMON", "Treants", 34727)
+	self:Log("SPELL_AURA_APPLIED", "Stomp", 34716)
 
-	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
-	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
-	self:Death("Win", 17977)
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
+	self:Death("AddDeath", 19949)
 end
 
 function mod:OnEngage()
-	self:DelayedMessage(34727, 10, "Attention", CL.soon:format(self:SpellName(34727)), nil, "Alarm")
-	self:CDBar(34727, 15)
+	addsAlive = 0
+	self:CDBar(-5478, 15)
 end
 
 --------------------------------------------------------------------------------
@@ -34,15 +42,31 @@ end
 --
 
 do
-	local prev = 0
-	function mod:Treants(args)
-		local t = GetTime()
-		if t - prev > 5 then
-			prev = t
-			self:Message(args.spellId, "Important")
-			-- self:Bar(args.spellId, 20, L.treants) -- L.treants was equal to "Treants" on enUS
-			self:DelayedMessage(34727, 40, "Attention", "Alarm", CL.soon:format(self:SpellName(34727)))
-			self:CDBar(args.spellId, 45)
+	local playerList = mod:NewTargetList()
+	function mod:Stomp(args)
+		playerList[#playerList+1] = args.destName
+		if #playerList == 1 then
+			self:ScheduleTimer("TargetMessage", 0.3, args.spellId, playerList, "Urgent", "Alert", nil, nil, self:Healer())
+		end
+	end
+end
+
+do
+	local barText = CL.onboss:format(mod:SpellName(32130)) -- "Heal on BOSS", the boss gets healed if these don't die within 25 seconds
+	function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, _, spellId)
+		if spellId == 34741 then -- Summon Saplings
+			addsAlive = 6 -- when they despawn to heal him, they don't fire any event; fortunately, no 2 waves can be alive at the same time
+			self:Message(-5478, "Important", "Alarm")
+			self:Bar(-5478, 25, barText, 38658) -- 38658 = "Healing Touch", because it fits the theme
+			self:CDBar(-5478, 45)
+		end
+	end
+
+	function mod:AddDeath()
+		addsAlive = addsAlive - 1
+		self:Message(-5478, "Positive", "Info", CL.add_remaining:format(addsAlive))
+		if addsAlive == 0 then
+			self:StopBar(barText)
 		end
 	end
 end
