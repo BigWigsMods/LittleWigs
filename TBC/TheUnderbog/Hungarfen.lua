4ad7909abf6c842c2d51b43894c403a128980669
Zireael <Zireael-N@users.noreply.github.com>
2018-03-10 10:47:36 +0300

    Hungarfen

--- TBC/TheUnderbog/Hungarfen.lua
+++ TBC/TheUnderbog/Hungarfen.lua
@@ -4,6 +4,8 @@
 local mod, CL = BigWigs:NewBoss("Hungarfen", 726, 576)
 if not mod then return end
 mod:RegisterEnableMob(17770)
+-- mod.engageId = 1946 -- doesn't fire ENCOUNTER_END on a wipe
+-- mod.respawnTime = 0 -- resets, doesn't respawn
 
 -------------------------------------------------------------------------------
 --  Initialization
@@ -15,25 +17,17 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
-	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
-	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
-	self:Death("Win", 17770)
-end
-
-function mod:OnEngage()
-	if self:CheckOption(31673, "MESSAGE") then
-		self:RegisterEvent("UNIT_HEALTH")
-	end
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+	self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", nil, "boss1")
 end
 
 -------------------------------------------------------------------------------
 --  Event Handlers
 
-function mod:UNIT_HEALTH(event, unit)
-	if UnitName(unit) ~= mod.displayName then return end
-	local health = UnitHealth(unit) / UnitHealth(unit) * 100
-	if health > 18 and health <= 24 then
-		self:UnregisterEvent("UNIT_HEALTH")
+function mod:UNIT_HEALTH_FREQUENT(unit)
+	local hp = UnitHealth(unit) / UnitHealth(unit) * 100
+	if hp < 25 then
+		self:UnregisterUnitEvent("UNIT_HEALTH_FREQUENT", unit)
 		self:Message(31673, "Urgent", nil, CL.soon:format(self:SpellName(31673)))
 	end
 end
