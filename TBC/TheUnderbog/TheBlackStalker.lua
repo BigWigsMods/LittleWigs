0d93e2c877115e339d27779d0107081bb35a0419
Zireael <Zireael-N@users.noreply.github.com>
2018-03-10 11:26:26 +0300

    TheBlackStalker

--- TBC/TheUnderbog/TheBlackStalker.lua
+++ TBC/TheUnderbog/TheBlackStalker.lua
@@ -1,19 +1,28 @@
 -------------------------------------------------------------------------------
 --  Module Declaration
+--
 
 local mod, CL = BigWigs:NewBoss("The Black Stalker", 726, 579)
 if not mod then return end
 mod:RegisterEnableMob(17882)
+mod.engageId = 1948
+-- mod.respawnTime = 0 -- resets, doesn't respawn
 
 -------------------------------------------------------------------------------
 --  Initialization
+--
 
 function mod:GetOptions()
 	return {
-		"stages",
+		{31715, "PROXIMITY"}, -- Static Charge
+		31704, -- Levitate
 	}
 end
 
 function mod:OnBossEnable()
-	self:Death("Win", 17882)
 end
+
+-------------------------------------------------------------------------------
+--  Event Handlers
+--
+
