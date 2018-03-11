f49206dc391052aa8df4a60735ad07320a1e54be
Zireael <Zireael-N@users.noreply.github.com>
2018-03-11 07:50:46 +0300

    Jindo: cleanups, engageId and respawnTime

--- Cataclysm/ZulGurub/Jindo.lua
+++ Cataclysm/ZulGurub/Jindo.lua
@@ -1,58 +1,65 @@
 -------------------------------------------------------------------------------
 --  Module Declaration
+--
 
 local mod, CL = BigWigs:NewBoss("Jin'do the Godbreaker", 793, 185)
 if not mod then return end
 mod:RegisterEnableMob(52148)
+mod.engageId = 1182
+mod.respawnTime = 30
 
 --------------------------------------------------------------------------------
 --  Locals
+--
 
-local stage2 = false
-local barrier = 3
+local barriersLeft = 3
 
 -------------------------------------------------------------------------------
 --  Localization
+--
 
 local L = mod:GetLocale()
 if L then
-	L.barrier_down_message = "Barrier %d down!"
+	L.barrier_down_message = "Barrier down, %d remaining"
 end
 
 -------------------------------------------------------------------------------
 --  Initialization
+--
 
 function mod:GetOptions()
 	return {
 		"stages",
 		97172, -- Shadows of Hakkar
-		97417, -- Brittle Barrier
-		{97597, "ICON", "FLASH"}, -- Spirit Warrior's Gaze
 		97170, -- Deadzone
+		{-2910, "ICON", "SAY", "FLASH"}, -- Brittle Barrier
+	}, {
+		[97172] = CL.stage:format(1),
+		[97170] = CL.stage:format(2),
 	}
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "ShadowsOfHakkar", 97172)
 	self:Log("SPELL_AURA_APPLIED", "ShadowsOfHakkarApplied", 97172)
-	self:Log("SPELL_AURA_REMOVED", "BarrierRemoved", 97417)
-	self:Log("SPELL_CAST_START", "Phase2", 97158) -- Shadow Spike
-	self:Log("SPELL_AURA_APPLIED", "WarriorsGaze", 97597)
-	self:Log("SPELL_AURA_REMOVED", "WarriorsGazeRemoved", 97597)
+	self:Log("SPELL_AURA_REMOVED", "ShadowsOfHakkarRemoved", 97172)
 	self:Log("SPELL_CAST_START", "Deadzone", 97170)
 
-	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
-	self:Death("Win", 52148)
+	self:Log("SPELL_AURA_REMOVED", "BrittleBarrierRemoved", 97417)
+	self:Log("SPELL_AURA_APPLIED", "SpiritWarriorsGaze", 97597)
+	self:Log("SPELL_AURA_REMOVED", "SpiritWarriorsGazeRemoved", 97597)
+
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
 end
 
 function mod:OnEngage()
-	stage2 = false
-	barrier = 3
 	self:CDBar(97172, 19) -- Shadows of Hakkar
 end
 
 -------------------------------------------------------------------------------
 --  Event Handlers
+--
+
 
 function mod:ShadowsOfHakkar(args)
 	self:Message(args.spellId, "Attention", "Info", CL.casting:format(args.spellName))
@@ -64,31 +71,37 @@ function mod:ShadowsOfHakkarApplied(args)
 	self:TargetBar(args.spellId, 8, args.destName)
 end
 
-function mod:BarrierRemoved(args)
-	barrier = barrier - 1
-	self:Message(args.spellId, "Important", "Alert", L.barrier_down_message:format(barrier))
-end
-
-function mod:Phase2(args)
-	if not stage then
-		stage2 = true
-		self:Message("stages", "Important", "Long", CL.stage:format(2))
-	end
-end
-
-function mod:WarriorsGaze(args)
-	self:TargetMessage(args.spellId, args.destName, "Important", "Alert")
-	self:SecondaryIcon(args.spellId, args.destName)
-	if self:Me(args.destGUID) then
-		self:Flash(args.spellId)
-	end
-end
-
-function mod:WarriorsGazeRemoved(args)
-	self:SecondaryIcon(args.spellId)
+function mod:ShadowsOfHakkarRemoved(args)
+	self:StopBar(args.spellName, args.destName)
 end
 
 function mod:Deadzone(args)
 	self:Message(args.spellId, "Important", "Long")
 	self:Bar(args.spellId, 21)
+end
+
+function mod:BrittleBarrierRemoved()
+	barriersLeft = barriersLeft - 1
+	self:Message(-2910, "Important", "Alert", L.barrier_down_message:format(barriersLeft))
+end
+
+function mod:SpiritWarriorsGaze(args)
+	if self:Me(args.destGUID) then
+		self:Say(-2910, args.spellId)
+		self:Flash(-2910, args.spellId)
+	end
+	self:TargetMessage(-2910, args.destName, "Important", "Alert", args.spellId)
+	self:SecondaryIcon(-2910, args.destName)
+end
+
+function mod:SpiritWarriorsGazeRemoved()
+	self:SecondaryIcon(-2910)
+end
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, _, spellId)
+	if spellId == 98861 then -- Spirit World
+		barriersLeft = 3
+		self:Message("stages", "Important", "Long", CL.other:format(CL.stage:format(2), self:SpellName(spellId)), false)
+		self:StopBar(97172) -- Shadows of Hakkar
+	end
 end
