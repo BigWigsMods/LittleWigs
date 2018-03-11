7b274f71cce77a330f1cd86e687bf211d59351d3
Zireael <Zireael-N@users.noreply.github.com>
2018-03-11 05:39:00 +0300

    Mandokir: cleanups, engageId and respawnTime
    Chained Spirits' deaths do not fire UNIT_DIED events. Also nothing in the dungeon journal suggests that you have a limited amount of Revives.

--- Cataclysm/ZulGurub/Mandokir.lua
+++ Cataclysm/ZulGurub/Mandokir.lua
@@ -3,30 +3,15 @@
 
 local mod, CL = BigWigs:NewBoss("Bloodlord Mandokir", 793, 176)
 if not mod then return end
-mod:RegisterEnableMob(52151, 52157)
-
---------------------------------------------------------------------------------
---  Locals
-
-local rebirthcount = 8
-
--------------------------------------------------------------------------------
---  Localization
-
-local L = mod:GetLocale()
-if L then
-	L.rebirth = "Ghost rebirth"
-	L.rebirth_desc = "Warn for Ghost rebirth remaining."
-	L.rebirth_message = "Ghost rebirth - %d left"
-	L.ohgan_message = "Ohgan rebirth!"
-end
+mod:RegisterEnableMob(52151, 52157) -- Bloodlord Mandokir, Ohgan
+mod.engageId = 1179
+mod.respawnTime = 30
 
 -------------------------------------------------------------------------------
 --  Initialization
 
 function mod:GetOptions()
 	return {
-		"rebirth",
 		96740, -- Devastating Slam
 		96684, -- Decapitate
 		96776, -- Bloodletting
@@ -36,28 +21,15 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
-	self:Log("SPELL_CAST_START", "Rebirth", 96484)
 	self:Log("SPELL_CAST_START", "DevastatingSlam", 96740)
 	self:Log("SPELL_CAST_SUCCESS", "Decapitate", 96684)
 	self:Log("SPELL_AURA_APPLIED", "Bloodletting", 96776)
 	self:Log("SPELL_AURA_APPLIED", "Frenzy", 96800)
-	self:Log("SPELL_HEAL", "OhganRebirth", 96724)
-
-	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
-	self:Death("Deaths", 52151, 52156)
-end
-
-function mod:OnEngage()
-	rebirthcount = 8
+	self:Log("SPELL_CAST_START", "ReanimateOhgan", 96724)
 end
 
 -------------------------------------------------------------------------------
 --  Event Handlers
-
-function mod:Rebirth()
-	rebirthcount = rebirthcount - 1
-	self:Message("rebirth", "Attention", "Alarm", L.rebirth_message:format(rebirthcount))
-end
 
 function mod:DevastatingSlam(args)
 	self:Message(args.spellId, "Important", "Info")
@@ -78,15 +50,6 @@ function mod:Frenzy(args)
 	self:Message(args.spellId, "Important", "Long")
 end
 
-function mod:OhganRebirth(args)
-	self:Message(args.spellId, "Attention", "Info", L.ohgan_message)
-end
-
-function mod:Deaths(args)
-	if args.mobId == 52156 then -- Chained Spirit
-		rebirthcount = rebirthcount - 1
-		self:Message("rebirth", "Attention", "Alarm", L.rebirth_message:format(rebirthcount))
-	elseif args.mobId == 52151 then
-		self:Win()
-	end
+function mod:ReanimateOhgan(args)
+	self:Message(args.spellId, "Attention", "Info", CL.casting:format(args.spellName))
 end
