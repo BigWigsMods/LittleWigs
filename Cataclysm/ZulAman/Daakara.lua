fbdf09e3b15be76fc820068a267775fa43506e79
Zireael <Zireael-N@users.noreply.github.com>
2018-03-11 04:39:31 +0300

    Daakara: cleanups, engageId and respawnTIme

--- Cataclysm/ZulAman/Daakara.lua
+++ Cataclysm/ZulAman/Daakara.lua
@@ -1,33 +1,40 @@
 -------------------------------------------------------------------------------
 --  Module Declaration
+--
 
 local mod, CL = BigWigs:NewBoss("Daakara", 781, 191)
 if not mod then return end
 mod:RegisterEnableMob(23863)
+mod.engageId = 1194
+mod.respawnTime = 30
 
 -------------------------------------------------------------------------------
 --  Localization
+--
 
 local L = mod:GetLocale()
 if L then
-	L.form = "Phases"
-	L.form_desc = "Warn when Daakara changes form."
-	L[42594] = "Bear Form!" -- short form for "Essence of the Bear"
-	L[42606] = "Eagle Form!"
-	L[42607] = "Lynx Form!"
-	L[42608] = "Dragonhawk Form!"
+	L[42594] = "Bear Form" -- short form for "Essence of the Bear"
+	L[42607] = "Lynx Form"
+	L[42606] = "Eagle Form"
+	L[42608] = "Dragonhawk Form"
 end
 
 -------------------------------------------------------------------------------
 --  Initialization
+--
 
 function mod:GetOptions()
 	return {
-		"form",
+		"stages",
 		{43093, "ICON"}, -- Grievous Throw
 		17207, -- Whirlwind
 		43095, -- Creeping Paralysis
 		43150, -- Claw Rage
+	}, {
+		["stages"] = "general",
+		[43095] = L[42594],
+		[43150] = L[42607],
 	}
 end
 
@@ -38,13 +45,11 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "CreepingParalysis", 43095)
 	self:Log("SPELL_AURA_APPLIED", "ClawRage", 43150)
 	self:Log("SPELL_CAST_START", "Forms", 42594, 42606, 42607, 42608) -- Bear, Eagle, Lynx, Dragonhawk
-
-	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
-	self:Death("Win", 23863)
 end
 
 -------------------------------------------------------------------------------
 --  Event Handlers
+--
 
 do
 	function mod:GrievousThrow(args)
@@ -63,7 +68,7 @@ function mod:Whirlwind(args)
 end
 
 function mod:CreepingParalysis(args)
-	self:Message(args.spellId, "Attention", nil, CL.casting:format(args.spellName))
+	self:Message(args.spellId, "Attention", self:Dispeller("magic") and "Warning", CL.casting:format(args.spellName))
 	self:CastBar(args.spellId, 6)
 	self:CDBar(args.spellId, 27)
 end
@@ -73,5 +78,5 @@ function mod:ClawRage(args)
 end
 
 function mod:Forms(args)
-	self:Message("form", "Important", nil, L[args.spellId], args.spellId)
+	self:Message("stages", "Important", nil, L[args.spellId], args.spellId)
 end
