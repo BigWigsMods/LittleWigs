618808a958e676b62a8c17dd2ded096f934b56e6
Zireael <Zireael-N@users.noreply.github.com>
2018-03-11 03:01:28 +0300

    TBC/TheMechanar: updates to the latest BigWigs API, added a module for Nethermancer Sepethrea (#214)

---
+++ TBC/TheMechanar/Locales/frFR.lua
@@ -0,0 +1,15 @@
+local L = BigWigs:NewBossLocale("Pathaleon the Calculator", "frFR")
+if not L then return end
+if L then
+	L.despawn_message = "Disparition des âmes en peine du Néant imminente"
+end
+
+L = BigWigs:NewBossLocale("Gatewatcher Iron-Hand", "frFR")
+if L then
+	L.name = "Gardien de porte Main-en-Fer"
+end
+
+L = BigWigs:NewBossLocale("Gatewatcher Gyro-Kill", "frFR")
+if L then
+	L.name = "Gardien de porte Gyro-Meurtre"
+end
