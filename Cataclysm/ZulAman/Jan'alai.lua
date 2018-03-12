38850f8172b9fe13db08ce1fa2b990a9a36d50df
Zireael <Zireael-N@users.noreply.github.com>
2018-03-13 01:15:17 +0300

    Cataclysm/ZulAman: updates to the latest BigWigs API (#212)

--- Cataclysm/ZulAman/Jan'alai.lua
+++ Cataclysm/ZulAman/Jan'alai.lua
@@ -1,84 +1,68 @@
 -------------------------------------------------------------------------------
 --  Module Declaration
+--
 
-local mod = BigWigs:NewBoss("Jan'alai", 781)
+local mod, CL = BigWigs:NewBoss("Jan'alai", 781, 188)
 if not mod then return end
-mod.partyContent = true
 mod:RegisterEnableMob(23578)
-mod.toggleOptions = {
-	{97497, "ICON"}, -- Flame Breath (this is the aura debuff spellid)
-	"adds",
-	"bomb",
-	"bosskill",
-}
-
--------------------------------------------------------------------------------
---  Localization
-
-local L = mod:GetLocale()
-if L then
-	L["adds"] = "Amani'shi Hatchers"
-	L["adds_desc"] = "Warn for incoming Amani'shi Hatchers."
-	L["adds_message"] = "Amani'shi Hatchers incoming!"
-	L["adds_all"] = "All remaining Amani'shi Hatchers soon!"
-
-	L["bomb"] = "Fire Bombs"
-	L["bomb_desc"] = "Show timers for Fire Bombs."
-	L["bomb_trigger"] = "I burn ya now!"
-	L["bomb_message"] = "Fire Bombs incoming!"
-end
+mod.engageId = 1191
+mod.respawnTime = 30
 
 -------------------------------------------------------------------------------
 --  Initialization
+--
+
+function mod:GetOptions()
+	return {
+		-2625, -- Amani'shi Hatcher
+		{97497, "ICON"}, -- Flame Breath (this is the aura debuff spellid)
+		-2622, -- Fire Bomb
+	}
+end
 
 function mod:OnBossEnable()
-	self:Log("SPELL_CAST_START", "FlameBreath", 97855) -- This is the actual cast spellid
+	self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", nil, "boss1")
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
 
-	--self:Yell("Adds", L["adds_trigger"])
-	self:Yell("Bomb", L["bomb_trigger"])
-
-	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
-
-	self:Death("Win", 23578)
+	self:Log("SPELL_CAST_START", "FlameBreath", 43140) -- This is the actual cast spellid
+	self:Log("SPELL_CAST_SUCCESS", "FlameBreathSuccess", 43140)
 end
 
 function mod:OnEngage()
-	self:Bar("adds", LW_CL["next"]:format(L["adds"]), 12, 89259)
-	self:RegisterEvent("UNIT_HEALTH")
-	self:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED", "Adds")
+	self:CDBar(-2625, 12, nil, "achievement_character_troll_male") -- Amani'shi Hatchers
 end
 
 -------------------------------------------------------------------------------
 --  Event Handlers
+--
 
 do
-	local function checkTarget(spellName)
-		if not UnitIsPlayer("boss1target") then return end
-		local player = UnitName("boss1target")
-		mod:TargetMessage(97497, spellName, player, "Important", 97497, "Alert")
-		mod:PrimaryIcon(97497, player)
+	local function printTarget(self, player)
+		self:TargetMessage(97497, player, "Important", "Alert")
+		self:PrimaryIcon(97497, player)
 	end
-	function mod:FlameBreath(_, _, _, _, spellName)
-		self:ScheduleTimer(checkTarget, 0.2, spellName)
+	function mod:FlameBreath(args)
+		self:GetBossTarget(printTarget, 0.4, args.sourceGUID)
 	end
-end
-
-function mod:Adds(_, _, _, _, _, spellId)
-	if spellId ~= 43962 then return end
-	self:Message("adds", L["adds_message"], "Attention", 89259, "Long")
-	self:Bar("adds", LW_CL["next"]:format(L["adds"]), 92, 89259)
-end
-
-function mod:Bomb()
-	self:Message("bomb", L["bomb_message"], "Urgent", 42630, "Info")
-	self:Bar("bomb", L["bomb"], 12, 42630)
-end
-
-function mod:UNIT_HEALTH()
-	local hp = UnitHealth("boss1") / UnitHealthMax("boss1") * 100
-	if hp < 37 then
-		self:Message("adds", L["adds_all"], "Attention")
-		self:UnregisterEvent("UNIT_HEALTH")
+	function mod:FlameBreathSuccess()
+		self:PrimaryIcon(97497)
 	end
 end
 
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, _, spellId)
+	if spellId == 43962 then -- Summon Amani'shi Hatcher
+		self:Message(-2625, "Attention", "Long", CL.incoming:format(self:SpellName(-2625)), "achievement_character_troll_male")
+		self:CDBar(-2625, 92, nil, "achievement_character_troll_male")
+	elseif spellId == 43098 then -- Teleport to Center (to cast Fire Bombs)
+		self:Message(-2622, "Urgent", "Info", CL.incoming:format(self:SpellName(-2622)))
+		self:Bar(-2622, 12)
+	end
+end
+
+function mod:UNIT_HEALTH_FREQUENT(unit)
+	local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
+	if hp < 40 then
+		self:UnregisterUnitEvent("UNIT_HEALTH_FREQUENT", unit)
+		self:Message(-2625, "Attention", nil, CL.soon:format(self:SpellName(-2628)), false) -- Hatch All Eggs Soon
+	end
+end
