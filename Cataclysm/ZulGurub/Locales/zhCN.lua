ecff40c9cc91034500eb689aa1a1466832d0d632
Adavak LEE <adavak@adavak.com>
2018-03-14 07:39:30 +0800

    update zhCN. (#269)

--- Cataclysm/ZulGurub/Locales/zhCN.lua
+++ Cataclysm/ZulGurub/Locales/zhCN.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Jin'do the Godbreaker", "zhCN")
 if not L then return end
 if L then
-	--L.barrier_down_message = "Barrier down, %d remaining" -- short name for "Brittle Barrier" (97417)
+	L.barrier_down_message = "壁垒破除，%d剩余" -- short name for "Brittle Barrier" (97417)
 end
