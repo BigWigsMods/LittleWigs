568a452ce71015b4ff3747be0eb2d073bb36df1d
funkydude <funkydude@users.noreply.github.com>
2018-03-14 01:13:13 +0000

    Legion/ArtifactScenarios: Fix load ids.

--- Legion/ArtifactScenarios/Raest.lua
+++ Legion/ArtifactScenarios/Raest.lua
@@ -3,7 +3,7 @@
 -- Module Declaration
 --
 
-local mod, CL = BigWigs:NewBoss("Raest", nil, nil, 1684)
+local mod, CL = BigWigs:NewBoss("Raest", 1684)
 if not mod then return end
 mod:RegisterEnableMob(116409, 116410) -- Raest Magespear, Karam Magespear
 mod.otherMenu = 1716 -- Broken Shore Mage Tower
