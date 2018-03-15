b42193fcac16a87794615d7f117d66a882929463
funkydude <funkydude@users.noreply.github.com>
2018-03-15 01:40:03 +0000

    Fill in Legion options data for repo users.

---
+++ Legion/BlackRookHold/Options/Sounds.lua
@@ -0,0 +1,35 @@
+
+BigWigs:AddSounds("Amalgam of Souls", {
+	[196587] = {"Alert","Warning"},
+	[194956] = "Warning",
+})
+
+BigWigs:AddSounds("Illysanna Ravencrest", {
+	[197797] = "Alarm",
+	[197974] = "Alarm",
+})
+
+BigWigs:AddSounds("Smashspite", {
+	[198073] = "Info",
+	[198446] = "Alert",
+	[198245] = "Alarm",
+	[198079] = "Warning",
+})
+
+BigWigs:AddSounds("Kurtalos Ravencrest", {
+	[198820] = "Warning",
+	[198641] = "Info",
+	[202019] = {"Warning","Info"},
+})
+
+BigWigs:AddSounds("Black Rook Hold Trash", {
+	[200291] = "Alert",
+	[200248] = "Alert",
+	[197974] = "Alarm",
+	[203163] = "Warning",
+	[227913] = "Alarm",
+	[200343] = "Warning",
+	[225573] = "Alarm",
+	[200261] = "Alarm",
+	[214003] = "Alarm",
+})
---
+++ Legion/CourtOfStars/Options/Colors.lua
@@ -0,0 +1,53 @@
+
+BigWigs:AddColors("Patrol Captain Gerdo", {
+	[219488] = "Important",
+	[207806] = "Attention",
+	[207261] = "Urgent",
+	[207815] = "Attention",
+	[207278] = "Attention",
+})
+
+BigWigs:AddColors("Talixae Flamewreath", {
+	[208165] = "Attention",
+	[207906] = "Important",
+	[207881] = "Urgent",
+})
+
+BigWigs:AddColors("Advisor Melandrus", {
+	[209602] = "Important",
+	[209628] = "Urgent",
+	[224333] = "Attention",
+	[209676] = "Attention",
+})
+
+BigWigs:AddColors("Court of Stars Trash", {
+	[216006] = "Attention",
+	["spy_helper"] = {"Neutral","Positive"},
+	[209477] = "Attention",
+	[209512] = "Personal",
+	[209485] = "Attention",
+	[212031] = "Important",
+	[216096] = "Attention",
+	[209404] = "Attention",
+	[211299] = "Important",
+	[214697] = {"Neutral","Personal","Positive"},
+	[211464] = "Important",
+	[216000] = "Attention",
+	[211391] = "Personal",
+	[209033] = "Attention",
+	[209378] = "Attention",
+	[209413] = {"Important","Attention","Personal"},
+	[211401] = "Important",
+	[224377] = "Important",
+	[209495] = "Important",
+	[212784] = {"Important","Attention"},
+	["announce_buff_items"] = "Neutral",
+	[207980] = "Attention",
+	[209410] = "Attention",
+	[216110] = "Attention",
+	[214688] = "Attention",
+	[214692] = "Attention",
+	[207979] = "Attention",
+	[209027] = "Important",
+	[225100] = "Attention",
+})
