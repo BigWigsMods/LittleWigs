25fe193eb0d169834a13a94245c139270c612f13
Zireael <Zireael-N@users.noreply.github.com>
2018-03-17 01:46:23 +0300

    Classic

---
+++ Classic/ShadowfangKeep/Options/Colors.lua
@@ -0,0 +1,31 @@
+
+BigWigs:AddColors("Baron Ashbury", {
+	[93581] = {"Urgent","Personal"},
+	[93423] = "Important",
+	[93757] = "Attention",
+})
+
+BigWigs:AddColors("Baron Silverlaine", {
+	[93857] = {"Important","Attention"},
+})
+
+BigWigs:AddColors("Commander Springvale", {
+	[93687] = "Personal",
+	[93844] = "Urgent",
+	[93852] = {"Important","Personal"},
+})
+
+BigWigs:AddColors("Lord Walden", {
+	[93697] = "Important",
+	[93505] = "Important",
+	[93527] = "Attention",
+	[93689] = "Neutral",
+	[93617] = {"Urgent","Personal","Neutral"},
+})
+
+BigWigs:AddColors("Lord Godfrey", {
+	[93629] = {"Urgent","Personal"},
+	[93675] = {"Attention","Personal"},
+	[93520] = {"Important","Personal"},
+	[93707] = "Neutral",
+})
---
+++ Classic/ShadowfangKeep/Options/Sounds.lua
@@ -0,0 +1,20 @@
+
+BigWigs:AddSounds("Baron Ashbury", {
+	[93757] = "Long",
+})
+
+BigWigs:AddSounds("Commander Springvale", {
+	[93687] = "Alert",
+	[93844] = "Alarm",
+})
+
+BigWigs:AddSounds("Lord Walden", {
+	[93617] = {"Warning","Info"},
+	[93689] = "Info",
+})
+
+BigWigs:AddSounds("Lord Godfrey", {
+	[93707] = "Info",
+	[93520] = {"Long","Alert"},
+	[93629] = "Alarm",
+})
