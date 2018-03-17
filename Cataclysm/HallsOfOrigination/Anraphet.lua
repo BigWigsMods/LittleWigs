1f59836c0d3c51ebd7873d6c88a90c2fa26daee8
Zireael <Zireael-N@users.noreply.github.com>
2018-03-17 22:54:10 +0300

    Anraphet

--- Cataclysm/HallsOfOrigination/Anraphet.lua
+++ Cataclysm/HallsOfOrigination/Anraphet.lua
@@ -1,55 +1,83 @@
 -------------------------------------------------------------------------------
 --  Module Declaration
+--
 
 local mod, CL = BigWigs:NewBoss("Anraphet", 644, 126)
 if not mod then return end
 mod:RegisterEnableMob(39788)
+mod.engageId = 1075
+mod.respawnTime = 39 -- respawns after 30s but is unattackable for a while
 
 -------------------------------------------------------------------------------
 --  Initialization
+--
 
 function mod:GetOptions()
 	return {
-		{76184, "FLASH"}, -- Alpha Beams
+		76184, -- Alpha Beams
 		75622, -- Omega Stance
+		75609, -- Crumbling Ruin
 		75603, -- Nemesis Strike
 	}
 end
 
 function mod:OnBossEnable()
-	self:Log("SPELL_CAST_START", "AlphaBeams", 76184)
-	self:Log("SPELL_AURA_APPLIED", "AlphaBeamsDebuff", 76956)
+	self:Log("SPELL_CAST_START", "AlphaBeamsCast", 76184)
+	self:Log("SPELL_DAMAGE", "AlphaBeamsDamage", 76956)
+	self:Log("SPELL_MISSED", "AlphaBeamsDamage", 76956)
+
 	self:Log("SPELL_CAST_START", "OmegaStance", 75622)
+
+	self:Log("SPELL_AURA_APPLIED", "CrumblingRuin", 75609)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "CrumblingRuin", 75609)
+
 	self:Log("SPELL_AURA_APPLIED", "NemesisStrike", 75603)
+	self:Log("SPELL_AURA_REMOVED", "NemesisStrikeRemoved", 75603)
 
-	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
-
-	self:Death("Win", 39788)
 end
 
 function mod:OnEngage()
-	self:Bar(75622, LW_CL["next"]:format(GetSpellInfo(75622)), 45, 75622)
+	self:CDBar(75622, 30) -- Omega Stance
 end
 
 -------------------------------------------------------------------------------
 --  Event Handlers
+--
 
-function mod:AlphaBeams(args)
+function mod:AlphaBeamsCast(args)
 	self:Message(args.spellId, "Attention", nil, CL.casting:format(args.spellName))
 end
 
-function mod:AlphaBeamsDebuff(args)
-	if self:Me(args.destGUID) then
-		self:TargetMessage(76184, args.destName, "Personal", "Alarm")
-		self:Flash(76184)
+do
+	local prev = 0
+	function mod:AlphaBeamsDamage(args)
+		if not self:Me(args.destGUID) then return end
+		local t = GetTime()
+		if t - prev > 1.5 then
+			prev = t
+			self:Message(76184, "Personal", "Alarm", CL.underyou:format(args.spellName))
+		end
 	end
 end
 
 function mod:OmegaStance(args)
 	self:Message(args.spellId, "Important", "Alert", CL.casting:format(args.spellName))
-	self:CDBar(args.spellId, 37)
+	self:CDBar(args.spellId, 41)
+end
+
+function mod:CrumblingRuin(args)
+	if self:Me(args.destGUID) then
+		self:StackMessage(args.spellId, args.destName, args.amount, "Personal")
+	end
 end
 
 function mod:NemesisStrike(args)
-	self:TargetMessage(args.spellId, args.destName, "Urgent")
+	if self:Me(args.destGUID) or self:Dispeller("magic") then
+		self:TargetMessage(args.spellId, args.destName, "Urgent")
+		self:TargetBar(args.spellId, 10, args.destName)
+	end
+end
+
+function mod:NemesisStrikeRemoved(args)
+	self:StopBar(args.spellName, args.destName)
 end
