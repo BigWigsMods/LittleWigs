c4473afe4b7b149e27df4dc0c1de2b7999007a22
Zireael <Zireael-N@users.noreply.github.com>
2018-03-18 00:32:28 +0300

    Isiset, Construct of Magic

--- Cataclysm/HallsOfOrigination/Isiset.lua
+++ Cataclysm/HallsOfOrigination/Isiset.lua
@@ -1,44 +1,57 @@
 -------------------------------------------------------------------------------
 --  Module Declaration
+--
 
-local mod, CL = BigWigs:NewBoss("Isiset", 644, 127)
+local mod, CL = BigWigs:NewBoss("Isiset, Construct of Magic", 644, 127)
 if not mod then return end
 mod:RegisterEnableMob(39587)
+mod.engageId = 1077
+mod.respawnTime = 30
 
 --------------------------------------------------------------------------------
 -- Locals
 --
 
-local split1 = nil
+local nextSplitWarning = 71
 
 -------------------------------------------------------------------------------
 --  Initialization
+--
 
 function mod:GetOptions()
 	return {
 		74373, -- Veil of Sky
 		74137, -- Supernova
+		74135, -- Astral Rain
+		74045, -- Energy Flux
 		-2556, -- Mirror Images
 	}
 end
 
 function mod:OnBossEnable()
+	-- 3 ids per spell: destroying a mirror image disables 1 ability, empowers others. 2nd & 3rd ids are empowered versions
 	self:Log("SPELL_AURA_APPLIED", "VeilOfSky", 74133, 74372, 74373)
 	self:Log("SPELL_AURA_REMOVED", "VeilOfSkyRemoved", 74133, 74372, 74373)
+
 	self:Log("SPELL_CAST_START", "Supernova", 74136, 74137, 76670)
 
-	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+	self:Log("SPELL_DAMAGE", "AstralRain", 74135, 74366, 74370)
+	self:Log("SPELL_MISSED", "AstralRain", 74135, 74366, 74370)
 
-	self:Death("Win", 39587)
+	self:Log("SPELL_DAMAGE", "EnergyFlux", 74045)
+	self:Log("SPELL_MISSED", "EnergyFlux", 74045)
+
+	self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", nil, "boss1")
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
 end
 
 function mod:OnEngage()
-	split1 = nil
-	self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", nil, "boss1")
+	nextSplitWarning = 71 -- 66% and 33%
 end
 
 -------------------------------------------------------------------------------
 --  Event Handlers
+--
 
 function mod:VeilOfSky(args)
 	self:Message(74373, "Attention")
@@ -50,19 +63,45 @@ function mod:VeilOfSkyRemoved(args)
 end
 
 function mod:Supernova(args)
-	self:Message(74137, "Important", "Alert", CL.casting:format(args.spellName))
+	self:Message(74137, "Important", "Alarm", CL.casting:format(args.spellName))
 	self:CastBar(74137, 3)
 end
 
-function mod:UNIT_HEALTH_FREQUENT(unit)
-	if UnitName(unit) == self.displayName then
-		local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
-		if hp < 72 and not split1 then
-			self:Message(-2556, "Attention", nil, CL.soon:format(self:SpellName(-2556)))
-			split1 = true
-		elseif hp < 39 then
-			self:UnregisterUnitEvent("UNIT_HEALTH_FREQUENT", unit)
-			self:Message(-2556, "Attention", nil, CL.soon:format(self:SpellName(-2556)))
+do
+	local prev = 0
+	function mod:AstralRain(args)
+		if not self:Me(args.destGUID) then return end
+		local t = GetTime()
+		if t - prev > 1.5 then
+			prev = t
+			self:Message(74135, "Personal", "Alert", CL.you:format(args.spellName))
 		end
+	end
+
+	function mod:EnergyFlux(args)
+		if not self:Me(args.destGUID) then return end
+		local t = GetTime()
+		if t - prev > 1.5 then
+			prev = t
+			self:Message(args.spellId, "Personal", "Alert", CL.underyou:format(args.spellName))
+		end
+	end
+end
+
+function mod:UNIT_HEALTH_FREQUENT(unit)
+	local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
+	if hp < nextSplitWarning then
+		self:Message(-2556, "Attention", nil, CL.soon:format(self:SpellName(-2556))) -- Mirror Image
+		nextSplitWarning = nextSplitWarning - 33
+
+		if hp < 33 then
+			self:UnregisterUnitEvent("UNIT_HEALTH_FREQUENT", unit)
+		end
+	end
+end
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, spellName, _, _, spellId)
+	if spellId == 69941 then -- Mirror Image
+		self:Message(-2556, "Neutral", "Info")
 	end
 end
