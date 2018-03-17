1bfe81330bccd2b3394c5646e7eb52c7d1da4b86
Zireael <Zireael-N@users.noreply.github.com>
2018-03-18 00:27:43 +0300

    Cataclysm/BlackrockCaverns: updates to the latest BigWigs API (#216)

--- Cataclysm/BlackrockCaverns/Corla.lua
+++ Cataclysm/BlackrockCaverns/Corla.lua
@@ -1,39 +1,67 @@
 -------------------------------------------------------------------------------
 --  Module Declaration
+--
 
-local mod = BigWigs:NewBoss("Corla, Herald of Twilight", 645)
+local mod, CL = BigWigs:NewBoss("Corla, Herald of Twilight", 645, 106)
 if not mod then return end
-mod.partyContent = true
 mod:RegisterEnableMob(39679)
-mod.toggleOptions = {
-	{75610, "FLASHSHAKE"}, -- Evolution
-	"bosskill",
-}
+mod.engageId = 1038
+mod.respawnTime = 30
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+local warnedAboutEvolution = nil
 
 -------------------------------------------------------------------------------
 --  Initialization
+--
+
+function mod:GetOptions()
+	return {
+		{75697, "FLASH"}, -- Evolution
+		75823, -- Dark Command
+	}
+end
 
 function mod:OnBossEnable()
-	self:Log("SPELL_AURA_APPLIED_DOSE", "Evolution", 75610, 87378)
-	self:Log("SPELL_AURA_REMOVED", "EvolutionRemoved", 75610, 87378)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "Evolution", 75697)
+	self:Log("SPELL_AURA_REMOVED", "EvolutionRemoved", 75697)
 
-	self:Death("Win", 39679)
+	self:Log("SPELL_CAST_START", "DarkCommand", 75823)
+	self:Log("SPELL_AURA_APPLIED", "DarkCommandApplied", 75823)
+	self:Log("SPELL_AURA_APPLIED", "DarkCommandRemoved", 75823)
 end
 
 -------------------------------------------------------------------------------
 --  Event Handlers
+--
 
-function mod:Evolution(player, spellId, _, _, spellName, stack)
-	if UnitIsUnit(player, "player") and stack > 75 and not warned then
-		warned = true
-		self:LocalMessage(75610, CL.stackyou:format(stack), "Personal", spellId, "Alarm")
-		self:FlashShake(75610)
+function mod:Evolution(args)
+	if self:Me(args.destGUID) and args.amount >= 80 and not warnedAboutEvolution then
+		warnedAboutEvolution = true
+		self:StackMessage(args.spellId, args.destName, args.amount, "Personal", "Warning")
+		self:Flash(args.spellId)
 	end
 end
 
-function mod:EvolutionRemoved(player, _, _, _, spellName)
-	if UnitIsUnit(player, "player") and warned then
-		warned = nil
+function mod:EvolutionRemoved(args)
+	if self:Me(args.destGUID) and warnedAboutEvolution then
+		warnedAboutEvolution = nil
+		self:Message(args.spellId, "Positive", nil, CL.removed:format(args.spellName))
 	end
 end
 
+function mod:DarkCommand(args)
+	self:Message(args.spellId, "Urgent", self:Interrupter() and "Alert", CL.casting:format(args.spellName))
+end
+
+function mod:DarkCommandApplied(args)
+	self:TargetMessage(args.spellId, args.destName, "Important", "Alarm", nil, nil, self:Dispeller("magic"))
+	self:TargetBar(args.spellId, 4, args.destName)
+end
+
+function mod:DarkCommandApplied(args)
+	self:StopBar(args.spellName, args.destName)
+end
--- Cataclysm/BlackrockCaverns/Romogg.lua
+++ Cataclysm/BlackrockCaverns/Romogg.lua
@@ -1,42 +1,90 @@
 -------------------------------------------------------------------------------
 --  Module Declaration
+--
 
-local mod = BigWigs:NewBoss("Rom'ogg Bonecrusher", 645)
+local mod, CL = BigWigs:NewBoss("Rom'ogg Bonecrusher", 645, 105)
 if not mod then return end
-mod.partyContent = true
 mod:RegisterEnableMob(39665)
-mod.toggleOptions = {
-	75543, -- The Skullcracker
-	75272, -- Quake
-	75539, -- Chains
-	"bosskill",
-}
+mod.engageId = 1040
+mod.respawnTime = 30
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+local nextChainsWarning = 71
 
 -------------------------------------------------------------------------------
 --  Initialization
+--
+
+function mod:GetOptions()
+	return {
+		75543, -- The Skullcracker
+		75272, -- Quake
+		75539, -- Chains of Woe
+	}
+end
 
 function mod:OnBossEnable()
-	self:Log("SPELL_CAST_START", "Skullcracker", 75543, 93453)
 	self:Log("SPELL_CAST_START", "Quake", 75272)
-	self:Log("SPELL_CAST_START", "Chains", 75539)
-	self:Death("Win", 39665)
+	self:Log("SPELL_AURA_APPLIED", "QuakeDamage", 75272)
+	self:Log("SPELL_MISSED", "QuakeDamage", 75272) -- the application of the debuff can miss but it might apply on the next tick
+	self:Log("SPELL_PERIODIC_DAMAGE", "QuakeDamage", 75272)
+	self:Log("SPELL_PERIODIC_MISSED", "QuakeDamage", 75272)
+
+	self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", nil, "boss1")
+	self:Log("SPELL_CAST_START", "ChainsOfWoe", 75539)
+	self:Log("SPELL_CAST_START", "TheSkullcracker", 75543)
+end
+
+function mod:OnEngage()
+	nextChainsWarning = 71 -- 66% and 33%
 end
 
 -------------------------------------------------------------------------------
 --  Event Handlers
+--
 
-function mod:Skullcracker(_, spellId, _, _, spellName)
-	local time = self:GetInstanceDifficulty() == 2 and 8 or 12 --8sec on heroic, 12 on normal
-	self:Bar(75543, spellName, time, spellId)
-	self:Message(75543, LW_CL["seconds"]:format(spellName, time), "Urgent", spellId)
+function mod:Quake(args)
+	self:Message(args.spellId, "Attention", nil, CL.casting:format(args.spellName))
 end
 
-function mod:Quake(_, spellId, _, _, spellName)
-	--self:Bar(75272, LW_CL["next"]:format(spellName), 19, spellId) --innacurate?
-	self:Message(75272, LW_CL["casting"]:format(spellName), "Attention", spellId)
+do
+	local prev = 0
+	function mod:QuakeDamage(args)
+		if not self:Me(args.destGUID) then return end
+		local t = GetTime()
+		if t - prev > 1.5 then
+			prev = t
+			self:Message(args.spellId, "Personal", "Alert", CL.underyou:format(args.spellName))
+		end
+	end
 end
 
-function mod:Chains(_, spellId, _, _, spellName)
-	self:Message(75539, LW_CL["casting"]:format(spellName), "Important", spellId, "Alert")
+function mod:UNIT_HEALTH_FREQUENT(unit)
+	local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
+	if hp < nextChainsWarning then
+		self:Message(75539, "Attention", nil, CL.soon:format(self:SpellName(75539))) -- Chains of Woe
+		nextChainsWarning = nextChainsWarning - 33
+
+		while nextChainsWarning >= 33 and hp < nextChainsWarning do
+			-- account for high-level characters hitting multiple thresholds
+			nextChainsWarning = nextChainsWarning - 33
+		end
+
+		if nextChainsWarning < 33 then
+			self:UnregisterUnitEvent("UNIT_HEALTH_FREQUENT", unit)
+		end
+	end
 end
 
+function mod:ChainsOfWoe(args)
+	self:Message(args.spellId, "Important", "Alarm", CL.casting:format(args.spellName))
+end
+
+function mod:TheSkullcracker(args)
+	local time = self:Normal() and 12 or 8 -- 12 sec on normal, 8 on heroic
+	self:CastBar(args.spellId, time)
+	self:Message(args.spellId, "Urgent", nil, CL.casting:format(args.spellName))
+end
