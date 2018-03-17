9b48f2d482d5e5b2c8ce854570ebc40bfdc1de18
Zireael <Zireael-N@users.noreply.github.com>
2018-03-18 00:36:20 +0300

    Options/*.lua

---
+++ Cataclysm/HallsOfOrigination/Options/Colors.lua
@@ -0,0 +1,33 @@
+
+BigWigs:AddColors("Temple Guardian Anhuur", {
+	[74938] = {"Attention","Important"},
+	[75592] = {"Attention","Personal"},
+})
+
+BigWigs:AddColors("Anraphet", {
+	[75603] = {"Personal","Urgent"},
+	[75609] = "Personal",
+	[75622] = "Important",
+	[76184] = {"Attention","Personal"},
+})
+
+BigWigs:AddColors("Isiset, Construct of Magic", {
+	[-2556] = {"Attention","Neutral"},
+	[74045] = "Personal",
+	[74135] = "Personal",
+	[74137] = "Important",
+	[74373] = "Attention",
+})
+
+BigWigs:AddColors("Ammunae, Construct of Life", {
+	[75790] = "Attention",
+	[76043] = {"Important","Personal","Urgent"},
+	[80968] = {"Attention","Personal"},
+})
+
+BigWigs:AddColors("Rajh, Construct of Sun", {
+	[-2863] = "Personal",
+	[-2862] = "Urgent",
+	[-2861] = "Attention",
+	[76355] = {"Neutral","Positive"},
+})
