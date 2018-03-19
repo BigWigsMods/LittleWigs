12e069d239a0a7aac623f9515184822f4114d16f
Zireael <Zireael-N@users.noreply.github.com>
2018-03-19 22:42:28 +0300

    Stonecore/Slabhide: added GetOptions()'s categories, swapped around Log()-calls and callbacks to follow the same order

--- Cataclysm/Stonecore/Slabhide.lua
+++ Cataclysm/Stonecore/Slabhide.lua
@@ -17,20 +17,31 @@ function mod:GetOptions()
 	return {
 		{80801, "FLASH"}, -- Lava Pool
 		92265, -- Crystal Storm
+	}, {
+		[80801] = "general",
+		[92265] = "heroic",
 	}
 end
 
 function mod:OnBossEnable()
+	-- Normal
+	self:Log("SPELL_AURA_APPLIED", "LavaPool", 80801)
+
 	-- Heroic
 	self:Log("SPELL_CAST_START", "CrystalStorm", 92265)
 	self:Log("SPELL_AURA_APPLIED", "CrystalStormBegun", 92265)
-	-- Normal
-	self:Log("SPELL_AURA_APPLIED", "LavaPool", 80801)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:LavaPool(args)
+	if self:Me(args.destGUID) then
+		self:TargetMessage(args.spellId, args.destName, "Personal", "Alarm")
+		self:Flash(args.spellId)
+	end
+end
 
 function mod:CrystalStorm(args)
 	self:Message(args.spellId, "Important", "Alert", CL.casting:format(args.spellName))
@@ -40,11 +51,3 @@ end
 function mod:CrystalStormBegun(args)
 	self:Bar(args.spellId, 6)
 end
-
-function mod:LavaPool(args)
-	if self:Me(args.destGUID) then
-		self:TargetMessage(args.spellId, args.destName, "Personal", "Alarm")
-		self:Flash(args.spellId)
-	end
-end
-
