711f0c45d9bc7df93d3acd3ab0225b0dd8e6f9b0
Justwait <justwait@method.gg>
2018-03-20 14:46:34 +0100

    Drycode Undermine

--- BfA/TheUndermine/CrowdPummeler.lua
+++ BfA/TheUndermine/CrowdPummeler.lua
@@ -6,7 +6,7 @@ if not C_ChatInfo then return end -- XXX Don't load outside of 8.0
 
 local mod, CL = BigWigs:NewBoss("Coin-operated Crowd Pummeler", 1594, 2109)
 if not mod then return end
-mod:RegisterEnableMob(17007) -- XXX
+mod:RegisterEnableMob(131224, 129214) -- XXX Won't need both
 mod.engageId = 2105
 
 --------------------------------------------------------------------------------
@@ -15,11 +15,14 @@ mod.engageId = 2105
 
 function mod:GetOptions()
 	return {
-		"berserk",
+		262347, -- Static Pulse
+		257337, -- Chocking Claw
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "StaticPulse", 262347)
+	self:Log("SPELL_CAST_START", "ChockingClaw", 257337)
 end
 
 function mod:OnEngage()
@@ -28,3 +31,13 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:StaticPulse(args)
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "alert", "knockback")
+end
+
+function mod:ChockingClaw(args)
+	self:Message(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm", "watchfront")
+end
