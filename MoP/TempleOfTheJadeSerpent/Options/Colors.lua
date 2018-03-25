feb4ed112fcb43e05bd461d1fad534049f2232a9
Zireael <Zireael-N@users.noreply.github.com>
2018-03-25 19:43:31 +0300

    Options/*.lua

--- MoP/TempleOfTheJadeSerpent/Options/Colors.lua
+++ MoP/TempleOfTheJadeSerpent/Options/Colors.lua
@@ -1,5 +1,23 @@
 
 BigWigs:AddColors("Wise Mari", {
 	[-6327] = {"Attention","Important"},
+	[115167] = "Personal",
 	["stages"] = "Positive",
+})
+
+BigWigs:AddColors("Lorewalker Stonestep", {
+	[-5549] = {"Important","Personal","Positive","Urgent"},
+	["stages"] = "Positive",
+})
+
+BigWigs:AddColors("Liu Flameheart", {
+	[106823] = {"Personal","Urgent"},
+	[106841] = {"Personal","Urgent"},
+	[107110] = "Personal",
+	["stages"] = {"Attention","Positive"},
+})
+
+BigWigs:AddColors("Sha of Doubt", {
+	[106113] = {"Attention","Personal"},
+	[117665] = {"Positive","Urgent"},
 })
--- MoP/TempleOfTheJadeSerpent/Options/Sounds.lua
+++ MoP/TempleOfTheJadeSerpent/Options/Sounds.lua
@@ -1,5 +1,23 @@
 
 BigWigs:AddSounds("Wise Mari", {
-	[-6327] = "Alert",
+	[-6327] = "Alarm",
+	[115167] = "Alert",
 	["stages"] = "Info",
+})
+
+BigWigs:AddSounds("Lorewalker Stonestep", {
+	[-5549] = {"Alert","Info","Warning"},
+	["stages"] = "Info",
+})
+
+BigWigs:AddSounds("Liu Flameheart", {
+	[106823] = "Alarm",
+	[106841] = "Alarm",
+	[107110] = "Alert",
+	["stages"] = "Info",
+})
+
+BigWigs:AddSounds("Sha of Doubt", {
+	[106113] = "Alarm",
+	[117665] = {"Info","Long"},
 })
