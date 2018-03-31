c9deab231860a11c38718a5ea2c32cab1469cf1d
Zireael <Zireael-N@users.noreply.github.com>
2018-03-31 17:52:42 +0300

    Kyrak: added categories to GetOptions()

--- WoD/UpperBlackrockSpire/Kyrak.lua
+++ WoD/UpperBlackrockSpire/Kyrak.lua
@@ -18,6 +18,9 @@ function mod:GetOptions()
 		161203, -- Rejuvenating Serum
 		161288, -- Vileblood Serum
 		{155037, "TANK"}, -- Eruption
+	}, {
+		[161199] = -10260,
+		[155037] = CL.adds,
 	}
 end
 
@@ -26,9 +29,9 @@ function mod:OnBossEnable()
 
 	self:Log("SPELL_CAST_START", "DebilitatingFixation", 161199)
 	self:Log("SPELL_CAST_START", "RejuvenatingSerumIncoming", 161203)
-	self:Log("SPELL_CAST_START", "Eruption", 155037)
 	self:Log("SPELL_CAST_SUCCESS", "RejuvenatingSerum", 161203)
 	self:Log("SPELL_AURA_APPLIED", "VilebloodSerum", 161288)
+	self:Log("SPELL_CAST_START", "Eruption", 155037)
 
 	self:Death("Win", 76021)
 end
@@ -46,11 +49,6 @@ function mod:RejuvenatingSerumIncoming(args)
 	self:Message(args.spellId, "Urgent", "Long", CL.incoming:format(args.spellName))
 end
 
-function mod:Eruption(args)
-	local raidIcon = CombatLog_String_GetIcon(args.sourceRaidFlags)
-	self:Message(args.spellId, "Important", "Info", raidIcon.. args.spellName)
-end
-
 function mod:RejuvenatingSerum(args)
 	self:TargetMessage(args.spellId, args.destName, "Urgent", "Warning", nil, nil, true)
 end
@@ -59,4 +57,9 @@ function mod:VilebloodSerum(args)
 	if self:Me(args.destGUID) then
 		self:Message(args.spellId, "Personal", "Alarm", CL.underyou:format(args.spellName))
 	end
+end
+
+function mod:Eruption(args)
+	local raidIcon = CombatLog_String_GetIcon(args.sourceRaidFlags)
+	self:Message(args.spellId, "Important", "Info", raidIcon.. args.spellName)
 end
