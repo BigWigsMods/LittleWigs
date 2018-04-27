2fcfbe18177d8e1e7920491e58b0d947566f2f73
Adavak LEE <adavak@adavak.com>
2018-04-27 10:45:22 +0800

    Update zhCN.lua

--- Legion/MawOfSouls/Locales/zhCN.lua
+++ Legion/MawOfSouls/Locales/zhCN.lua
@@ -1,10 +1,10 @@
 local L = BigWigs:NewBossLocale("Maw of Souls Trash", "zhCN")
 if not L then return end
 if L then
-	--L.soulguard = "Waterlogged Soul Guard"
+	L.soulguard = "浸水的灵魂卫士"
 	L.champion = "海拉加尔勇士"
 	L.mariner = "守夜水手"
-	--L.swiftblade = "Seacursed Swiftblade"
+	L.swiftblade = "海咒快刀手"
 	L.mistmender = "海咒雾疗师"
 	L.mistcaller = "海拉加尔召雾者"
 	L.skjal = "斯卡加尔"
