d4697ae1c09c4bb1001a617a303397c6bfc1e8c2
Alisson Neri <38860749+kMs182@users.noreply.github.com>
2018-05-10 16:46:46 -0300

    Update ptBR.lua

--- TBC/TheSteamvault/Locales/ptBR.lua
+++ TBC/TheSteamvault/Locales/ptBR.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Mekgineer Steamrigger", "ptBR")
 if not L then return end
 if L then
-	--L.mech_trigger = "Tune 'em up good, boys!"
+	L.mech_trigger = "Ajuste-os bem, rapazes!"
 end
