0abc0eed71aec552f6ad40fb6a45e542baaa83f9
Alisson Neri <38860749+kMs182@users.noreply.github.com>
2018-05-10 17:15:11 -0300

    Update ptBR.lua

--- WoD/UpperBlackrockSpire/Locales/ptBR.lua
+++ WoD/UpperBlackrockSpire/Locales/ptBR.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("Orebender Gor'ashan", "ptBR")
 if not L then return end
 if L then
-	--L.counduitLeft = "%d |4Conduit:Conduits; left"
+	L.counduitLeft = "%d |4Conduit:Conduits; faltando"
 end
 
 L = BigWigs:NewBossLocale("Warlord Zaela", "ptBR")
