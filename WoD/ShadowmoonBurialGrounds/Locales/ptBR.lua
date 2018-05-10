3b720f400c55a68f1e4d4f0e33eb7690ea2ee2b4
Alisson Neri <38860749+kMs182@users.noreply.github.com>
2018-05-10 17:06:54 -0300

    Update ptBR.lua

--- WoD/ShadowmoonBurialGrounds/Locales/ptBR.lua
+++ WoD/ShadowmoonBurialGrounds/Locales/ptBR.lua
@@ -1,6 +1,8 @@
 local L = BigWigs:NewBossLocale("Sadana Bloodfury", "ptBR")
 if not L then return end
 if L then
+	
 	L.custom_on_markadd = "Adicionada marca da Comunhão Sombria"
 	L.custom_on_markadd_desc = "Marca o add gerado pela Comunhão Sombria com uma caveira, requer promovido ou líder."
+	
 end
