f783df9b2903323382bf0b1520780fad8f377304
Alisson Neri <38860749+kMs182@users.noreply.github.com>
2018-05-10 17:12:04 -0300

    Update ptBR.lua

--- WoD/ShadowmoonBurialGrounds/Locales/ptBR.lua
+++ WoD/ShadowmoonBurialGrounds/Locales/ptBR.lua
@@ -3,6 +3,6 @@ if not L then return end
 if L then
 	
 	L.custom_on_markadd = "Adicionada marca da Comunhão Sombria"
-	L.custom_on_markadd_desc = "Marca o add gerado pela Comunhão Sombria com uma caveira, requer promovido ou líder."
+	L.custom_on_markadd_desc = "Marca o add gerado pela Comunhão Sombria com {rt8}, requer promovido ou líder."
 	
 end
