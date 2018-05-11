ed457dbe5cbb159b67e193956dc7ed0588356b14
Alisson Neri <38860749+kMs182@users.noreply.github.com>
2018-05-11 14:44:18 -0300

    Update ptBR.lua

--- Cataclysm/ZulAman/Locales/ptBR.lua
+++ Cataclysm/ZulAman/Locales/ptBR.lua
@@ -1,20 +1,20 @@
 local L = BigWigs:NewBossLocale("Daakara", "ptBR")
 if not L then return end
 if L then
-	--L[42594] = "Bear Form" -- short form for "Essence of the Bear"
-	--L[42607] = "Lynx Form"
-	--L[42606] = "Eagle Form"
-	--L[42608] = "Dragonhawk Form"
+	L[42594] = "Forma de Urso" -- short form for "Essence of the Bear"
+	L[42607] = "Forma de Lince"
+	L[42606] = "Forma de Águia"
+	L[42608] = "Forma de Falcodrago"
 end
 
 L = BigWigs:NewBossLocale("Halazzi", "ptBR")
 if L then
-	--L.spirit_message = "Spirit Phase"
-	--L.normal_message = "Normal Phase"
+	L.spirit_message = "Fase Espiritual"
+	L.normal_message = "Fase Normal"
 end
 
 L = BigWigs:NewBossLocale("Nalorakk", "ptBR")
 if L then
-	--L.troll_message = "Troll Form"
-	--L.troll_trigger = "Make way for da Nalorakk!"
+	L.troll_message = "Forma de Troll"
+	L.troll_trigger = "Abrir caminho para Nalorakk!"
 end
