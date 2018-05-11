bd27aaa02bcde68132ec085fba37518049f85ec3
Alisson Neri <38860749+kMs182@users.noreply.github.com>
2018-05-11 13:18:33 -0300

    Update ptBR.lua

--- MoP/BlackTempleSolo/Locales/ptBR.lua
+++ MoP/BlackTempleSolo/Locales/ptBR.lua
@@ -3,11 +3,11 @@ if not L then return end
 if L then
 	L.name = "Kanrethad Ebanez"
 
-	-- L.summons = "Summons"
-	-- L.debuffs = "Debuffs"
+	 L.summons = "Convocações"
+	 L.debuffs = "Debuffs"
 
-	-- L.start_say = "BEHOLD" -- BEHOLD! I have truly mastered the fel energies of this world! The demonic power I now command... It is indescribable, unlimited, OMNIPOTENT!
-	-- L.win_say = "Jubeka" -- Jubeka?! What are you...?!
+	 L.start_say = "CONTEMPLEM" -- BEHOLD! I have truly mastered the fel energies of this world! The demonic power I now command... It is indescribable, unlimited, OMNIPOTENT!
+	 L.win_say = "Jubeka" -- Jubeka?! What are you...?!
 end
 
 L = BigWigs:NewBossLocale("Essence of Order", "ptBR")
