58df90a7abb166729ff266a1cddcdbf8123cc0e0
Zireael <13710677+Zireael-N@users.noreply.github.com>
2018-05-27 17:51:27 +0300

    Don't include pets in messages related to several AoE-debuffs (#306)

--- TBC/TheArcatraz/Trash.lua
+++ TBC/TheArcatraz/Trash.lua
@@ -91,6 +91,7 @@ do
 	-- One of the randomly chosen debuffs that apply to people being hit by Chaos Breath (36677), so there's a chance of multiple people getting it.
 	local playerList = mod:NewTargetList()
 	function mod:Hex(args)
+		if bit.band(args.destFlags, 0x400) == 0 then return end -- COMBATLOG_OBJECT_TYPE_PLAYER = 0x400, filtering out pets
 		playerList[#playerList + 1] = args.destName
 		if #playerList == 1 then
 			self:ScheduleTimer("TargetMessage", 0.3, args.spellId, playerList, "Urgent", "Alert", nil, nil, self:Dispeller("magic"))
