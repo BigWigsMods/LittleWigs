3c874ad516f61d56d6daf21fa7872f7d18c4acbd
Zireael <13710677+Zireael-N@users.noreply.github.com>
2018-07-30 14:11:13 +0300

    TempleOfSethraliss/Avatar: added warnings for "Rain of Toads" and "Plague" (#311)

--- BfA/TempleOfSethraliss/Avatar.lua
+++ BfA/TempleOfSethraliss/Avatar.lua
@@ -22,14 +22,20 @@ mod.engageId = 2127
 
 function mod:GetOptions()
 	return {
+		269688, -- Rain of Toads
 		273677, -- Taint
 		268024, -- Pulse
+		269686, -- Plague
 	}
 end
 
 function mod:OnBossEnable()
+	self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE")
+
 	self:Log("SPELL_CAST_SUCCESS", "Taint", 273677)
 	self:Log("SPELL_AURA_APPLIED", "Pulse", 268024)
+	self:Log("SPELL_AURA_APPLIED", "Plague", 269686)
+	self:Log("SPELL_AURA_REMOVED", "PlagueRemoved", 269686)
 end
 
 function mod:OnEngage()
@@ -38,6 +44,15 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+-- Rain of Toads
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(_, msg)
+	if msg:find("269688", nil, true) then
+		self:Message(269688, "cyan")
+		self:PlaySound(269688, "info")
+		-- self:CDBar(269688, ???) -- pull:36.76, 36.93, 50.1
+	end
+end
 
 do
 	local prev = 0
@@ -62,4 +77,16 @@ do
 			self:Bar(args.spellId, 40)
 		end
 	end
+end
+
+function mod:Plague(args)
+	if self:Me(args.destGUID) or self:Dispeller("disease") then
+		self:TargetMessage2(args.spellId, "orange", args.destName)
+		self:PlaySound(args.spellId, "alarm")
+		self:TargetBar(args.spellId, 12, args.destName)
+	end
+end
+
+function mod:PlagueRemoved(args)
+	self:StopBar(args.spellName, args.destName)
 end
