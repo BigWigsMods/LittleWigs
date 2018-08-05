a6ed384a353ba62f8be5f6d766c81786ed66cb97
Zireael <Zireael-N@users.noreply.github.com>
2018-08-05 20:12:42 +0300

    Waycrest Manor

---
+++ BfA/WaycrestManor/Locales/itIT.lua
@@ -0,0 +1,19 @@
+local L = BigWigs:NewBossLocale("Waycrest Manor Trash", "itIT")
+if not L then return end
+if L then
+	L.steward = "Attendente del Banchetto"
+	L.gorger = "Divoratore Pallido"
+	L.marksman = "Tiratore Frenetico"
+	L.runeweaver = "Tessirune Frangicuore"
+	L.survivalist = "Survivalista Impazzito"
+	L.bryndle = "Matrona Bryndle"
+	L.thornshaper = "Plasmaspine della Congrega"
+	L.thornguard = "Guardaspine"
+	L.toad = "Rospo Ammorbato"
+	L.raven = "Corvo Alatorva"
+	L.soulcharmer = "Ammalianime Frangicuore"
+	L.captain = "Capitano Stregato"
+	L.disciple = "Discepola Runica"
+	L.sister = "Sorella Marchiata"
+	L.alma = "Matrona Alma"
+end
