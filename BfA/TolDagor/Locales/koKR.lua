8d1d8233a5e56ed6e8ae3251f1efdd6b7caacd0f
Zireael <Zireael-N@users.noreply.github.com>
2018-08-05 20:02:28 +0300

    Tol Dagor

---
+++ BfA/TolDagor/Locales/koKR.lua
@@ -0,0 +1,11 @@
+local L = BigWigs:NewBossLocale("Tol Dagor Trash", "koKR")
+if not L then return end
+if L then
+	-- L.vicejaw = "Sewer Vicejaw"
+	-- L.thug = "Irontide Thug"
+	-- L.seaspeaker = "Bilge Rat Seaspeaker"
+	-- L.flamecaster = "Ashvane Flamecaster"
+	-- L.officer = "Ashvane Officer"
+	-- L.marine = "Ashvane Marine"
+	-- L.priest = "Ashvane Priest"
+end
---
+++ BfA/TolDagor/Locales/ptBR.lua
@@ -0,0 +1,11 @@
+local L = BigWigs:NewBossLocale("Tol Dagor Trash", "ptBR")
+if not L then return end
+if L then
+	L.vicejaw = "Boca-de-alicate de Esgoto"
+	L.thug = "Bandido Maré-férrea"
+	L.seaspeaker = "Falamares Rato de Porão"
+	L.flamecaster = "Lança-chamas Grimpagris"
+	L.officer = "Oficial Grimpagris"
+	L.marine = "Fuzileiro Grimpagris"
+	L.priest = "Sacerdotisa Grimpagris"
+end
