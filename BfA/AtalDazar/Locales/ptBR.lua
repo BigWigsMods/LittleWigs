7939648b1fde4e91a8ceacb180205f0bfed79b9f
Zireael <Zireael-N@users.noreply.github.com>
2018-08-05 19:08:27 +0300

    AtalDazar

---
+++ BfA/AtalDazar/Locales/ptBR.lua
@@ -0,0 +1,11 @@
+local L = BigWigs:NewBossLocale("Atal'Dazar Trash", "ptBR")
+if not L then return end
+if L then
+	L.skyscreamer = "Gritacéu Devorante"
+	L.tlonja = "T'lonja"
+	L.shieldbearer = "Escudeiro de Zul"
+	L.witchdoctor = "Mandingueira Zanchuli"
+	L.kisho = "Dinomante Kish'o"
+	L.priestess = "Sacerdotisa Dourada"
+	L.stalker = "Espreitador Umbralâmina"
+end
