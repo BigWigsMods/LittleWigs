d55ffe27511a663bd51cfe0c0baf74b41f66bb00
Adavak LEE <adavak@adavak.com>
2018-08-16 12:16:44 +0800

    Update zhCN.lua

--- BfA/Underrot/Locales/zhCN.lua
+++ BfA/Underrot/Locales/zhCN.lua
@@ -1,14 +1,14 @@
 local L = BigWigs:NewBossLocale("Underrot Trash", "zhCN")
 if not L then return end
 if L then
-	-- L.spirit = "Befouled Spirit"
-	-- L.priest = "Devout Blood Priest"
-	-- L.maggot = "Fetid Maggot"
-	-- L.matron = "Chosen Blood Matron"
-	-- L.lasher = "Diseased Lasher"
-	-- L.bloodswarmer = "Feral Bloodswarmer"
-	-- L.rot = "Living Rot"
-	-- L.deathspeaker = "Fallen Deathspeaker"
-	-- L.defiler = "Bloodsworn Defiler"
-	-- L.corruptor = "Faceless Corruptor"
+	L.spirit = "亵渎之灵"
+	L.priest = "虔诚鲜血祭司"
+	L.maggot = "恶臭蛆虫"
+	L.matron = "鲜血主母选民"
+	L.lasher = "染病鞭笞者"
+	L.bloodswarmer = "狂野的群居血虱"
+	L.rot = "生命腐质"
+	L.deathspeaker = "堕落的亡语者"
+	L.defiler = "血誓污染者"
+	L.corruptor = "无面腐蚀者"
 end
