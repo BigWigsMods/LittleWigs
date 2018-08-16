b83060beeea550feaf13b6ad93c81e3ae9a88b60
Adavak LEE <adavak@adavak.com>
2018-08-16 12:00:28 +0800

    Update zhCN.lua

--- BfA/AtalDazar/Locales/zhCN.lua
+++ BfA/AtalDazar/Locales/zhCN.lua
@@ -1,11 +1,11 @@
 local L = BigWigs:NewBossLocale("Atal'Dazar Trash", "zhCN")
 if not L then return end
 if L then
-	-- L.skyscreamer = "Feasting Skyscreamer"
-	-- L.tlonja = "T'lonja"
-	-- L.shieldbearer = "Shieldbearer of Zul"
-	-- L.witchdoctor = "Zanchuli Witch-Doctor"
-	-- L.kisho = "Dinomancer Kish'o"
-	-- L.priestess = "Gilded Priestess"
-	-- L.stalker = "Shadowblade Stalker"
+	L.skyscreamer = "飨宴的啸天龙"
+	L.tlonja = "特隆加"
+	L.shieldbearer = "祖尔的持盾卫士"
+	L.witchdoctor = "赞枢利巫医"
+	L.kisho = "恐龙首领吉什奥"
+	L.priestess = "鎏金女祭司"
+	L.stalker = "影刃追猎者"
 end
