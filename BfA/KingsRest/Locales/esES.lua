ce07d4d0f39b4fa012f2ad751aad1b0db22c598c
Zireael <Zireael-N@users.noreply.github.com>
2018-08-19 01:05:32 +0300

    KingsRest: added a module for Dazar

---
+++ BfA/KingsRest/Locales/esES.lua
@@ -0,0 +1,5 @@
+local L = BigWigs:NewBossLocale("Dazar, The First King", "esES") or BigWigs:NewBossLocale("Dazar, The First King", "esMX")
+if not L then return end
+if L then
+	-- L.spears_active = "Spear Launchers Active"
+end
--- BfA/KingsRest/Options/Colors.lua
+++ BfA/KingsRest/Options/Colors.lua
@@ -1,1 +1,31 @@
--- Don't error because I'm empty, please.
+
+BigWigs:AddColors("The Golden Serpent", {
+	[265773] = {"blue","orange"},
+	[265781] = "red",
+	[265910] = "purple",
+	[265923] = "yellow",
+})
+
+BigWigs:AddColors("Mchimba the Embalmer", {
+	[267618] = {"blue","orange"},
+	[267639] = "red",
+	[267702] = {"blue","green","yellow"},
+})
+
+BigWigs:AddColors("The Council of Tribes", {
+	[266206] = "yellow",
+	[266231] = {"blue","orange"},
+	[266237] = "purple",
+	[266951] = {"blue","red"},
+	[267060] = "yellow",
+	[267273] = "orange",
+	["stages"] = "cyan",
+})
+
+BigWigs:AddColors("Dazar, The First King", {
+	[268586] = "purple",
+	[268932] = {"blue","orange"},
+	[269231] = "blue",
+	[269369] = "red",
+	["stages"] = {"cyan","yellow"},
+})
