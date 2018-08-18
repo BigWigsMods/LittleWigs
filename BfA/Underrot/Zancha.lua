c80f317deccbf86ad0098529230a2d4bc332934d
Justwait <justwait@method.gg>
2018-08-18 21:10:16 +0200

    BfA/Underrot/Zancha: Add Heroic+ Abilities

--- BfA/Underrot/Zancha.lua
+++ BfA/Underrot/Zancha.lua
@@ -17,7 +17,8 @@ function mod:GetOptions()
 		259732, -- Festering Harvest
 		259830, -- Boundless Rot
 		272457, -- Shockwave
-		{259718, "SAY"}, -- Upheaval
+		{259718, "SAY", "SAY_COUNTDOWN", "FLASH"}, -- Upheaval
+		273285,
 	}
 end
 
@@ -27,12 +28,15 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "Shockwave", 272457)
 	self:Log("SPELL_AURA_APPLIED", "Upheaval", 259718)
 	self:Log("SPELL_AURA_REMOVED", "UpheavalRemoved", 259718)
+
+	-- Heroic+
+	self:Log("SPELL_CAST_SUCCESS", "VolatilePods", 273285)
 end
 
 function mod:OnEngage()
 	self:Bar(272457, 10) -- Shockwave
-	self:Bar(259718, 18) -- Upheaval
-	self:Bar(259732, 60) -- Festering Harvest
+	self:Bar(259718, 16) -- Upheaval
+	self:Bar(259732, 35) -- Festering Harvest
 end
 
 --------------------------------------------------------------------------------
@@ -41,7 +45,8 @@ end
 
 function mod:FesteringHarvest(args)
 	self:Message(args.spellId, "red")
-	self:PlaySound(args.spellId, "long")
+	self:PlaySound(args.spellId, "alarm")
+	self:Bar(args.spellId, 51)
 end
 
 function mod:BoundlessRot(args)
@@ -50,24 +55,43 @@ function mod:BoundlessRot(args)
 end
 
 function mod:Shockwave(args)
-	self:Message(args.spellId, "yellow")
+	self:Message(args.spellId, "purple")
 	self:PlaySound(args.spellId, "alert")
-	self:Bar(args.spellId, 20)
+	self:CDBar(args.spellId, 14.5)
 end
 
-function mod:Upheaval(args)
-	self:TargetMessage(args.spellId, args.destName, "orange")
-	self:Bar(args.spellId, 20)
-	if self:Me(args.destGUID) then
-		self:PlaySound(args.spellId, "warning", "runout")
-		self:Say(args.spellId)
-		self:SayCountdown(args.spellId, 6)
-		self:Flash(args.spellId)
+do
+	local playerList = mod:NewTargetList()
+	function mod:Upheaval(args)
+		playerList[#playerList+1] = args.destName
+		if #playerList == 1 then
+			self:Bar(args.spellId, 20)
+		end
+		if self:Me(args.destGUID) then
+			self:PlaySound(args.spellId, "warning", "runout")
+			self:Say(args.spellId)
+			self:Flash(args.spellId)
+			self:SayCountdown(args.spellId, 6)
+		end
+		self:TargetsMessage(args.spellId, "orange", playerList, 2)
 	end
 end
 
 function mod:UpheavalRemoved(args)
 	if self:Me(args.destGUID) then
 		self:CancelSayCountdown(args.spellId)
+	end
+end
+
+do
+	local prev = 0
+	function mod:VolatilePods(args)
+		local t = GetTime()
+		if t-prev > 2 then
+			prev = t
+			self:Message(args.spellId, "yellow")
+			self:PlaySound(args.spellId, "long", "interrupt")
+			self:CDBar(args.spellId, 30)
+		end
 	end
 end
