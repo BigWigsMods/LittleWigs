97ec7cb52b3600aca04b9a0db0a92e81177dacdb
Justwait <justwait@method.gg>
2018-08-18 21:09:23 +0200

    BfA/Underrot/Leaxa: Update Blood Mirror and Add Heroic+ Abilities

--- BfA/Underrot/Leaxa.lua
+++ BfA/Underrot/Leaxa.lua
@@ -12,6 +12,7 @@ mod.engageId = 2111
 -- Locals
 --
 
+local effigySpawned = 0
 local effigyKilled = 0
 
 --------------------------------------------------------------------------------
@@ -23,6 +24,7 @@ function mod:GetOptions()
 		260879, -- Blood Bolt
 		260894, -- Creeping Rot
 		264603, -- Blood Mirror
+		264757, -- Sanguine Feast
 	}
 end
 
@@ -31,12 +33,19 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "CreepingRot", 260894)
 	self:Log("SPELL_CAST_START", "BloodMirror", 264603)
 	self:Death("EffigyDeath", 134701)
+
+	-- Heroic+
+	self:Log("SPELL_CAST_START", "SanguineFeast", 264757)
 end
 
 function mod:OnEngage()
-	effigyKilled = 3 -- Set to 3 as check to see what stage it is
+	effigySpawned = 0
+	effigyKilled = 0
 	self:Bar(260894, 12) -- Creeping Rot
-	self:Bar(264603, 20) -- Blood Mirror
+	self:Bar(264603, 15.5) -- Blood Mirror
+	if not self:Normal() then
+		self:Bar(264757, 9) -- Sanguine Feast
+	end
 end
 
 --------------------------------------------------------------------------------
@@ -65,14 +74,16 @@ do
 			prev = t
 			self:Message(args.spellId, "yellow")
 			self:PlaySound(args.spellId, "alarm", "watchstep")
-			if effigyKilled == 3 then -- Don't show bars during Blood Mirror
-				self:Bar(args.spellId, 12) -- XXX Need timers
-			end
+		end
+		local mobID = self:MobId(args.sourceGUID)
+		if mobID == 131318 then -- Actual Boss
+			self:CDBar(args.spellId, 15.8)
 		end
 	end
 end
 
 function mod:BloodMirror(args)
+	effigySpawned = self:Normal() and 1 or effigySpawned + 1 -- XXX Always 1 in normal?
 	effigyKilled = 0
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "long", "intermission")
@@ -80,11 +91,17 @@ end
 
 function mod:EffigyDeath(args)
 	effigyKilled = effigyKilled + 1
-	if effigyKilled == 3 then
+	if effigyKilled == effigySpawned then
 		self:Message(264603, "green", nil, CL.over:format(self:SpellName(264603)))
 		self:PlaySound(264603, "long", "stage")
 	else
-		self:Message(264603, "cyan", nil, CL.mob_killed:format(args.destName, effigyKilled, 3))
+		self:Message(264603, "cyan", nil, CL.mob_killed:format(args.destName, effigyKilled, effigySpawned))
 		self:PlaySound(264603, "info")
 	end
+end
+
+function mod:SanguineFeast(args)
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "alarm", "watchstep")
+	self:Bar(args.spellId, 30.5)
 end
