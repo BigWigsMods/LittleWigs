3f6878f2a572a6f6f7cc014189338acdd1771d9d
Adavak LEE <adavak@adavak.com>
2018-08-19 11:02:01 +0800

    Update zhCN.lua

--- BfA/KingsRest/Locales/zhCN.lua
+++ BfA/KingsRest/Locales/zhCN.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Dazar, The First King", "zhCN")
 if not L then return end
 if L then
-	-- L.spears_active = "Spear Launchers Active"
+	L.spears_active = "长矛发射器已激活"
 end
