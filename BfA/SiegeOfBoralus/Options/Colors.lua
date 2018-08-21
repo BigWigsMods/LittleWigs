88edd256fd1612ab7157856a0c6ac09b1f47ad71
Zireael <Zireael-N@users.noreply.github.com>
2018-08-21 05:41:29 +0300

    SiegeOfBoralus: added Options files

--- BfA/SiegeOfBoralus/Options/Colors.lua
+++ BfA/SiegeOfBoralus/Options/Colors.lua
@@ -1,1 +1,33 @@
--- Don't error because I'm empty, please.
+
+BigWigs:AddColors("Sergeant Bainbridge", {
+	[257585] = "orange",
+	[260924] = "orange",
+	[260954] = {"blue","yellow"},
+	[261428] = {"blue","red"},
+	[277965] = "green",
+	["stages"] = "yellow",
+})
+
+BigWigs:AddColors("Dread Captain Lockwood", {
+	[268230] = "purple",
+	[268260] = "orange",
+	[268752] = {"green","yellow"},
+	[268963] = "cyan",
+	[269029] = "orange",
+	[272471] = "yellow",
+})
+
+BigWigs:AddColors("Hadal Darkfathom", {
+	[257882] = "orange",
+	[261563] = "yellow",
+	[276068] = "red",
+})
+
+BigWigs:AddColors("Viq'Goth", {
+	[269266] = "purple",
+	[269366] = "cyan",
+	[270185] = "orange",
+	[270605] = "yellow",
+	[275014] = {"blue","yellow"},
+	["stages"] = "green",
+})
