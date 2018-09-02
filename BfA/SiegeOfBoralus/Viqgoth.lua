BigWigsMods/LittleWigs

commit 834ee949ead2045d1d6e407b0747b3c9015b4b5e

BfA/SiegeOfBoralus/Viqgoth: Don't announce stage 4 on a win

BfA/SiegeOfBoralus/Viqgoth.lua
@@ -58,10 +58,12 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
 		self:CDBar(270185, timer)
 	elseif spellId == 269984 then -- Damage Boss 35%
 		stage = stage + 1
-		self:Message("stages", "green", nil, CL.stage:format(stage), false)
-		self:PlaySound("stages", "long")
+		if stage > 4 then
+			self:Message("stages", "green", nil, CL.stage:format(stage), false)
+			self:PlaySound("stages", "long")
 
-		self:CDBar(270605, stage == 2 and 39.5 or 55.5) -- Summon Demolisher
+			self:CDBar(270605, stage == 2 and 39.5 or 55.5) -- Summon Demolisher
+		end
 	elseif spellId == 270605 then -- Summon Demolisher
 		self:Message(spellId, "yellow")
 		self:PlaySound(spellId, "alert")

commit 21546a6a0ab9ec3921fe862502733464d0735a3b

SiegeOfBoralus/Viqgoth: Fix a regression caused by 834ee94

BfA/SiegeOfBoralus/Viqgoth.lua
@@ -58,7 +58,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
 		self:CDBar(270185, timer)
 	elseif spellId == 269984 then -- Damage Boss 35%
 		stage = stage + 1
-		if stage > 4 then
+		if stage < 4 then
 			self:Message("stages", "green", nil, CL.stage:format(stage), false)
 			self:PlaySound("stages", "long")
 