87c9b33009e7f49ca1778d5e9ab3dceff34ca7dd
funkydude <funkydude@users.noreply.github.com>
2018-09-05 00:08:05 +0100

    TempleOfSethraliss/Galvazzt: Fix Galvanize warning.

--- BfA/TempleOfSethraliss/Galvazzt.lua
+++ BfA/TempleOfSethraliss/Galvazzt.lua
@@ -14,40 +14,38 @@ mod.engageId = 2126
 
 function mod:GetOptions()
 	return {
-		266923, -- Electroshock
+		266923, -- Galvanize
 		266512, -- Consume Charge
 	}
 end
 
 function mod:OnBossEnable()
-	self:Log("SPELL_AURA_APPLIED", "Electroshock", 266923)
-	self:Log("SPELL_AURA_APPLIED_DOSE", "Electroshock", 266923)
-	self:Log("SPELL_AURA_APPLIED", "ElectroshockOnBoss", 265986) -- Buff for boss is Arc
+	self:Log("SPELL_AURA_APPLIED_DOSE", "Galvanize", 266923)
+	self:Log("SPELL_AURA_APPLIED", "GalvanizeOnBoss", 265986) -- Spell aura on boss is called 'Arc'
 	self:Log("SPELL_CAST_START", "ConsumeCharge", 266512)
-end
-
-function mod:OnEngage()
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
-function mod:Electroshock(args)
+function mod:Galvanize(args)
 	if self:Me(args.destGUID) then
-		local amount = args.amount or 1
-		if amount % 2 == 1 then
-			self:TargetMessage2(args.spellId, "cyan", args.destName)
-			self:PlaySound(args.spellId, "info")
+		if args.amount % 3 == 0 then
+			self:StackMessage(args.spellId, args.destName, args.amount, "blue")
+			if args.amount > 6 then
+				self:PlaySound(args.spellId, "info")
+			end
 		end
 	end
 end
 
-function mod:ElectroshockOnBoss(args)
+function mod:GalvanizeOnBoss(args)
 	self:TargetMessage2(266923, "orange", args.destName)
 	self:PlaySound(266923, "alarm")
 end
 
 function mod:ConsumeCharge(args)
-	self:Message(args.spellId, "red", "Warning")
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "warning")
 end
