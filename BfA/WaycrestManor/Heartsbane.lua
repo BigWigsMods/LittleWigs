1d4f2ec6a7fc37481c8b5a559e49cff5b2df13ff
Zireael <13710677+Zireael-N@users.noreply.github.com>
2018-09-13 18:43:51 +0300

    WaycrestManor/Heartsbane: Use a general message for "Unstable Runic Mark" because it affects the whole group, add a proximity display for it, show warnings for high stacks of "Aura of Dread" (#328)

--- BfA/WaycrestManor/Heartsbane.lua
+++ BfA/WaycrestManor/Heartsbane.lua
@@ -9,30 +9,45 @@ mod:RegisterEnableMob(131825, 131823, 131824) -- Sister Briar, Sister Malady, Si
 mod.engageId = 2113
 
 --------------------------------------------------------------------------------
+-- Locals
+--
+
+local playersWithRunicMark = 0
+
+--------------------------------------------------------------------------------
 -- Initialization
 --
 
 function mod:GetOptions()
 	return {
-		260741, -- Jagged Nettles
-		{260703, "SAY", "FLASH"}, -- Unstable Runic Mark
-		{260926, "ICON"}, -- Soul Manipulation
 		{260805, "ICON"}, -- Focusing Iris
 		260773, -- Dire Ritual
+		260741, -- Jagged Nettles
+		{260703, "SAY", "SAY_COUNTDOWN", "FLASH", "PROXIMITY"}, -- Unstable Runic Mark
+		268086, -- Aura of Dread
+		{260926, "ICON"}, -- Soul Manipulation
+	}, {
+		[260805] = "general",
+		[260741] = -17738, -- Sister Briar,
+		[260703] = -17739, -- Sister Malady
+		[260926] = -17740, -- Sister Solena
 	}
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "JaggedNettles", 260741)
-	-- self:Log("SPELL_CAST_SUCCESS", "UnstableRunicMark", 260703)
+	self:Log("SPELL_CAST_SUCCESS", "UnstableRunicMark", 260703)
 	self:Log("SPELL_AURA_APPLIED", "UnstableRunicMarkApplied", 260703)
+	self:Log("SPELL_AURA_REMOVED", "UnstableRunicMarkRemoved", 260703)
 	self:Log("SPELL_AURA_APPLIED", "SoulManipulation", 260926)
 	self:Log("SPELL_AURA_REMOVED", "SoulManipulationRemovedFromBoss", 260923)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "AuraOfDread", 268086)
 	self:Log("SPELL_AURA_APPLIED", "FocusingIris", 260805)
 	self:Log("SPELL_CAST_START", "DireRitual", 260773)
 end
 
 function mod:OnEngage()
+	playersWithRunicMark = 0
 end
 
 --------------------------------------------------------------------------------
@@ -45,20 +60,31 @@ function mod:JaggedNettles(args)
 	self:Bar(args.spellId, 13.5)
 end
 
--- function mod:UnstableRunicMark()
--- 	self:Bar(args.spellId, 13.5) XXX Need a timer
--- end
+function mod:UnstableRunicMark()
+	self:Message(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+	-- self:Bar(args.spellId, 13.5) XXX Need a timer
+end
 
-do
-	local playerList = mod:NewTargetList()
-	function mod:UnstableRunicMarkApplied(args)
-		playerList[#playerList+1] = args.destName
-		if self:Me(args.destGUID) then
-			self:PlaySound(args.spellId, "alarm", nil, playerList)
-			self:Say(args.spellId)
-			self:Flash(args.spellId)
-		end
-		self:TargetsMessage(args.spellId, "orange", playerList)
+function mod:UnstableRunicMarkApplied(args)
+	playersWithRunicMark = playersWithRunicMark + 1
+	if playersWithRunicMark == 1 then
+		self:OpenProximity(args.spellId, 6)
+	end
+	if self:Me(args.destGUID) then
+		self:Say(args.spellId)
+		self:SayCountdown(args.spellId, 6)
+		self:Flash(args.spellId)
+	end
+end
+
+function mod:UnstableRunicMarkRemoved(args)
+	playersWithRunicMark = playersWithRunicMark - 1
+	if playersWithRunicMark == 0 then
+		self:CloseProximity(args.spellId)
+	end
+	if self:Me(args.destGUID) then
+		self:CancelSayCountdown(args.spellId)
 	end
 end
 
@@ -71,6 +97,15 @@ end
 function mod:SoulManipulationRemovedFromBoss(args)
 	-- Move the icon away from the player and back to the boss
 	self:PrimaryIcon(260805, self:GetUnitIdByGUID(args.destGUID)) -- Focusing Iris
+end
+
+function mod:AuraOfDread(args)
+	if self:Me(args.destGUID) then
+		if args.amount % 3 == 0 or args.amount > 6 then
+			self:StackMessage(args.spellId, args.destName, args.amount, "blue")
+			self:PlaySound(args.spellId, args.amount > 6 and "warning" or "alert")
+		end
+	end
 end
 
 function mod:FocusingIris(args)
