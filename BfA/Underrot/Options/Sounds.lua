2f96940f1b03961ecb8b8ac4eb9fb599703ad261
Zireael <13710677+Zireael-N@users.noreply.github.com>
2018-09-13 20:30:19 +0300

    Underrot/Trash: Add warnings for "Gift of G'huun", "Decaying Mind" and "Sonic Screech", remove abilities that didn't make it to live

--- BfA/Underrot/Options/Sounds.lua
+++ BfA/Underrot/Options/Sounds.lua
@@ -28,20 +28,21 @@ BigWigs:AddSounds("Unbound Abomination", {
 })
 
 BigWigs:AddSounds("Underrot Trash", {
-	[265019] = "info",
+	[265019] = {"info","warning"},
 	[265081] = "long",
 	[265089] = "warning",
+	[265091] = {"alarm","alert"},
 	[265433] = "alert",
 	[265487] = "warning",
 	[265523] = "alarm",
 	[265540] = "alarm",
 	[265568] = {"alarm","alert"},
-	[265668] = "alert",
-	[265687] = "alarm",
-	[266105] = "warning",
+	[265668] = {"alarm","alert"},
+	[266106] = "warning",
 	[266107] = "alarm",
 	[266209] = "info",
 	[272183] = "alert",
 	[272592] = "long",
 	[272609] = "alarm",
+	[278961] = {"alert","info"},
 })
