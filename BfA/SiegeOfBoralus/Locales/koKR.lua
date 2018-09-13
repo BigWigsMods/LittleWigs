f46ed3d1d390c00ad8bf4af4675a93b37115ce7c
Zireael <13710677+Zireael-N@users.noreply.github.com>
2018-09-13 18:26:20 +0300

    SiegeOfBoralus/Viqgoth: Add proximity display and marking functionality for "Putrid Waters" (#327)

---
+++ BfA/SiegeOfBoralus/Locales/koKR.lua
@@ -0,0 +1,5 @@
+local L = BigWigs:NewBossLocale("Viq'Goth", "koKR")
+if not L then return end
+if L then
+	-- L.demolishing_desc = "Warnings and timers for when the Demolishing Terror spawns."
+end
---
+++ BfA/SiegeOfBoralus/Locales/zhTW.lua
@@ -0,0 +1,5 @@
+local L = BigWigs:NewBossLocale("Viq'Goth", "zhTW")
+if not L then return end
+if L then
+	-- L.demolishing_desc = "Warnings and timers for when the Demolishing Terror spawns."
+end
