d406e848c14c7c82afe3eb764c84c42e781391d1
Zireael <13710677+Zireael-N@users.noreply.github.com>
2018-09-16 18:10:05 +0300

    KingsRest/Trash: Import module (#331)

--- BfA/KingsRest/Locales/deDE.lua
+++ BfA/KingsRest/Locales/deDE.lua
@@ -3,3 +3,33 @@ if not L then return end
 if L then
 	-- L.spears_active = "Spear Launchers Active"
 end
+
+L = BigWigs:NewBossLocale("King's Rest Trash", "deDE")
+if L then
+	L.guardian = "Belebter Wächter"
+	L.minion = "Diener von Zul"
+	L.champion = "Schattengeborener Champion"
+	L.shadow_witchdoctor = "Schattengeborener Hexendoktor"
+	L.warrior = "Schattengeborener Krieger"
+	L.timalji = "König Timalji"
+	L.wasi = "Königin Wasi"
+	L.rahuai = "König Rahu'ai"
+	L.atu = "Wachoffizier Atu"
+	L.mbara = "Seneschall M'bara"
+	L.patla = "Königin Patlaa"
+	L.raptor = "Skelettjagdraptor"
+	L.aakul = "König A'akul"
+	L.agent = "Blutverschworener Agent"
+	L.purification_construct = "Läuterungskonstrukt"
+	L.fluid = "Balsamierungsflüssigkeit"
+	L.interment_construct = "Bestattungskonstrukt"
+	L.hex_priest = "Spektraler Hexpriester"
+	L.berserker = "Spektrale Berserkerin"
+	L.spectral_witchdoctor = "Spektrale Hexendoktorin"
+	L.beastmaster = "Spektrale Bestienmeisterin"
+	L.brute = "Spektraler Schläger"
+	L.zul = "Zuls Schatten"
+
+	-- L.casting_on_you = "Casting %s on YOU"
+	-- L.casting_on_other = "Casting %s: %s"
+end
