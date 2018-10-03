df0389df82872718198f5e643f3679c8aec4dec9
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2018-10-03 15:59:08 +0000

    Update deDE.lua

--- BfA/SiegeOfBoralus/Locales/deDE.lua
+++ BfA/SiegeOfBoralus/Locales/deDE.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Viq'Goth", "deDE")
 if not L then return end
 if L then
-	-- L.demolishing_desc = "Warnings and timers for when the Demolishing Terror spawns."
+	L.demolishing_desc = "Warnungen und Timer für das Erscheinen des Verwüstenden Schreckens."
 end
