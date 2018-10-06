04650c27ea9c6b02d60418b7485bfeebbedc12cb
EK <eke00372@gmail.com>
2018-10-07 06:31:04 +0800

    Update zhCN.lua

--- BfA/ShrineOfTheStorm/Locales/zhCN.lua
+++ BfA/ShrineOfTheStorm/Locales/zhCN.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("Aqu'sirr", "zhCN")
 if not L then return end
 if L then
-	-- L.warmup_trigger = "How dare you sully this holy place with your presence!"
+	L.warmup_trigger = "污染了这片圣地"
 end
 
 L = BigWigs:NewBossLocale("Lord Stormsong", "zhCN")
