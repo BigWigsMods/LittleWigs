928a87ea150ec5ee68c2d8b4968fc437ee995b71
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2018-10-08 18:43:45 +0000

    Update deDE.lua

--- WotLK/TrialOfTheChampion/Locales/deDE.lua
+++ WotLK/TrialOfTheChampion/Locales/deDE.lua
@@ -2,5 +2,5 @@ local L = BigWigs:NewBossLocale("Trial of the Champion Trash", "deDE")
 if not L then return end
 if L then
 	L.custom_on_autotalk = "Automatisch ansprechen"
-	L.custom_on_autotalk_desc = "Wählt direkt die Dialogoption zum Start von Begegnungen."
+	L.custom_on_autotalk_desc = "Wählt direkt die Dialogoption zum Starten von Begegnungen."
 end
