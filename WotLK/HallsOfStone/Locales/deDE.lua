f33214227c9a9099b3ed19a6d757396c63829a9d
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2018-10-08 18:26:11 +0000

    Update deDE.lua

--- WotLK/HallsOfStone/Locales/deDE.lua
+++ WotLK/HallsOfStone/Locales/deDE.lua
@@ -1,12 +1,12 @@
 local L = BigWigs:NewBossLocale("Tribunal of Ages", "deDE")
 if not L then return end
 if L then
-	--L.engage_trigger = "Now keep an eye out" -- Now keep an eye out! I'll have this licked in two shakes of a--
-	--L.defeat_trigger = "The old magic fingers" --  Ha! The old magic fingers finally won through! Now let's get down to--
-	--L.fail_trigger = "Not yet... not ye--"
+	L.engage_trigger = "Haltet jetzt die Augen offen" -- Now keep an eye out! I'll have this licked in two shakes of a--
+	L.defeat_trigger = "Die alten, magischen Finger" --  Ha! The old magic fingers finally won through! Now let's get down to--
+	L.fail_trigger = "Noch nicht... noch nich--"
 
-	--L.timers = "Timers"
-	--L.timers_desc = "Timers for various events that take place."
+	L.timers = "Timer"
+	L.timers_desc = "Timer für diverse eintretende Ereignisse."
 
-	--L.victory = "Victory"
+	L.victory = "Sieg"
 end
