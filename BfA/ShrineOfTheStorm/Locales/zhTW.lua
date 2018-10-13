dfce92b8559f2c53e7f1f32f88674a5cedb1dab1
EK <eke00372@gmail.com>
2018-10-13 20:51:54 +0800

    Update zhTW.lua

--- BfA/ShrineOfTheStorm/Locales/zhTW.lua
+++ BfA/ShrineOfTheStorm/Locales/zhTW.lua
@@ -1,11 +1,11 @@
 local L = BigWigs:NewBossLocale("Aqu'sirr", "zhTW")
 if not L then return end
 if L then
-	L.warmup_trigger = "你們這些蠢貨竟敢來弄髒這個聖地"
+	L.warmup_trigger = "你們這些蠢貨竟敢來弄髒這個聖地！"
 end
 
 L = BigWigs:NewBossLocale("Lord Stormsong", "zhTW")
 if L then
-	L.warmup_trigger_horde = "我會把你們丟往黑暗身淵，大海會讓你們粉身碎骨"
+	L.warmup_trigger_horde = "是入侵者？我會把你們丟往黑暗深淵，大海會讓你們粉身碎骨，直到永遠。"
 	-- L.warmup_trigger_alliance = "Master! Stop this madness at once! The Kul Tiran fleet must not fall to darkness!"
 end
