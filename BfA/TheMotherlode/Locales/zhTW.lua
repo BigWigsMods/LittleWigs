a5d60aa2e108ae7ad8bb84544849828d348c131c
roionsteroids <roionsteroids@gmail.com>
2018-11-04 17:50:47 +0100

    BfA/TheMotherlode: add Trash module (#388)

---
+++ BfA/TheMotherlode/Locales/zhTW.lua
@@ -0,0 +1,16 @@
+local L = BigWigs:NewBossLocale("The MOTHERLODE!! Trash", "zhTW")
+if not L then return end
+if L then
+	-- L.tester = "Weapons Tester"
+	-- L.assassin = "Hired Assassin"
+	-- L.jockey = "Mech Jockey"
+	-- L.earthshaper = "Venture Co. Earthshaper"
+	-- l.stonefury = "Stonefury"
+	-- L.assistant = "Feckless Assistant"
+	-- L.technician = "Expert Technician"
+	-- L.mastermind = "Venture Co. Mastermind"
+	-- L.alchemist = "Venture Co. Alchemist"
+	-- L.sapper = "Wanton Sapper"
+	-- L.vendor = "Refreshment Vendor"
+	-- L.extractor = "Azerite Extractor"
+end
