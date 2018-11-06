3315d3dac9204587c28a7d7c360bb35ea4ad6a46
Kyle Scheuing <oppzippy@gmail.com>
2018-11-06 06:37:00 -0500

    BfA/SiegeOfBoralus/Trash: Add more abilities

--- BfA/SiegeOfBoralus/Trash.lua
+++ BfA/SiegeOfBoralus/Trash.lua
@@ -7,6 +7,7 @@ local mod, CL = BigWigs:NewBoss("Siege of Boralus Trash", 1822)
 if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
+	138465, -- Ashvane Cannoneer
 	128969, -- Ashvane Commander
 	135245, -- Bilge Rat Demolisher
 	129369, -- Irontide Raider
@@ -20,6 +21,7 @@ mod:RegisterEnableMob(
 
 local L = mod:GetLocale()
 if L then
+	L.cannoneer = "Ashvane Cannoneer"
 	L.commander = "Ashvane Commander"
 	L.demolisher = "Bilge Rat Demolisher"
 	L.wavetender = "Kul Tiran Wavetender"
@@ -33,6 +35,8 @@ end
 
 function mod:GetOptions()
 	return {
+		-- Ashvane Cannoneer
+		268260, -- Broadside
 		-- Ashvane Commander
 		272874, -- Trample
 		275826, -- Bolstering Shout
@@ -57,26 +61,31 @@ end
 function mod:OnBossEnable()
 	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
 	
+	-- Ashvane Cannoneer
+	self:Log("SPELL_CAST_START", "Broadside", 268260)
 	-- Ashvane Commander
 	self:Log("SPELL_CAST_START", "Trample", 272874)
 	self:Log("SPELL_CAST_START", "BolsteringShout", 275826)
-	
 	-- Bilge Rat Demolisher
 	self:Log("SPELL_CAST_START", "TerrifyingRoar", 257169)
 	self:Log("SPELL_CAST_SUCCESS", "TerrifyingRoarSuccess", 257169)
-	
 	-- Irontide Raider
 	self:Log("SPELL_CAST_START", "SavageTempest", 257170)
 	self:Log("SPELL_CAST_SUCCESS", "SavageTempestSuccess", 257170)
-	
 	-- Kul Tiran Wavetender
 	self:Log("SPELL_CAST_START", "WatertightShell", 256957)
-	
 	-- Kul Tiran Halberd
 	self:Log("SPELL_CAST_START", "SlobberKnocker", 256627)
 	
+	-- Bilge Rat Demolisher's Crushing Slam
 	self:RegisterEvent("UNIT_SPELLCAST_START")
 	self:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+end
+
+function mod:Broadside(args)
+	self:Message2(args.spellId, "red")
+	self:PlaySound(args.spellId, "alarm")
+	self:CastBar(args.spellId, 3)
 end
 
 function mod:Trample(args)
@@ -93,6 +102,7 @@ end
 function mod:TerrifyingRoar(args)
 	self:Message2(args.spellId, "red")
 	self:PlaySound(args.spellId, "alarm")
+	self:CastBar(args.spellId, 3)
 end
 
 function mod:TerrifyingRoarSuccess(args)
@@ -123,6 +133,7 @@ function mod:UNIT_SPELLCAST_START(_, _, _, spellId)
 	if spellId == 272711 then -- Crushing Slam
 		self:Message2(spellId, "orange")
 		self:PlaySound(spellId, "alert")
+		self:CastBar(spellId, 3.5)
 	end
 end
 
