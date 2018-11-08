15bfd869f0a0098a00d9775f0065a93f032fa150
Kyle Scheuing <oppzippy@gmail.com>
2018-11-08 06:04:39 -0500

    BfA/SiegeOfBoralus/Trash: Add locales

--- BfA/SiegeOfBoralus/Locales/frFR.lua
+++ BfA/SiegeOfBoralus/Locales/frFR.lua
@@ -1,5 +1,17 @@
-local L = BigWigs:NewBossLocale("Viq'Goth", "frFR")
+local L = BigWigs:NewBossLocale("Siege of Boralus Trash", "frFR")
 if not L then return end
+if L then
+	L.cannoneer = "Canonnier corsandre"
+	L.commander = "Commandant corsandre"
+	L.spotter = "Guetteur corsandre"
+	L.demolisher = "Démolisseur des Soutaillons"
+	L.wavetender = "Soigne-vagues kultirassien"
+	L.halberd = "Hallebardier kultirassien"
+	L.raider = "Ecumeur des Lamineurs"
+	L.vanguard = "Avant-garde de Kul Tiras"
+end
+
+L = BigWigs:NewBossLocale("Viq'Goth", "frFR")
 if L then
 	-- L.demolishing_desc = "Warnings and timers for when the Demolishing Terror spawns."
 end
--- BfA/SiegeOfBoralus/Locales/zhTW.lua
+++ BfA/SiegeOfBoralus/Locales/zhTW.lua
@@ -1,5 +1,17 @@
-local L = BigWigs:NewBossLocale("Viq'Goth", "zhTW")
+local L = BigWigs:NewBossLocale("Siege of Boralus Trash", "zhTW")
 if not L then return end
+if L then
+	-- L.cannoneer = "Ashvane Cannoneer"
+	-- L.commander = "Ashvane Commander"
+	-- L.spotter = "Ashvane Spotter"
+	-- L.demolisher = "Bilge Rat Demolisher"
+	-- L.wavetender = "Kul Tiran Wavetender"
+	-- L.halberd = "Kul Tiran Halberd"
+	-- L.raider = "Irontide Raider"
+	-- L.vanguard = "Kul Tiran Vanguard"
+end
+
+L = BigWigs:NewBossLocale("Viq'Goth", "zhTW")
 if L then
 	-- L.demolishing_desc = "Warnings and timers for when the Demolishing Terror spawns."
 end
