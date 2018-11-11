162f5e200e2aee7d626f9ee1bd28b22af9cdb52a
EK <eke00372@gmail.com>
2018-11-11 08:16:05 +0800

    Update zhTW.lua

--- BfA/SiegeOfBoralus/Locales/zhTW.lua
+++ BfA/SiegeOfBoralus/Locales/zhTW.lua
@@ -1,14 +1,14 @@
 local L = BigWigs:NewBossLocale("Siege of Boralus Trash", "zhTW")
 if not L then return end
 if L then
-	-- L.cannoneer = "Ashvane Cannoneer"
-	-- L.commander = "Ashvane Commander"
-	-- L.spotter = "Ashvane Spotter"
-	-- L.demolisher = "Bilge Rat Demolisher"
-	-- L.wavetender = "Kul Tiran Wavetender"
-	-- L.halberd = "Kul Tiran Halberd"
-	-- L.raider = "Irontide Raider"
-	-- L.vanguard = "Kul Tiran Vanguard"
+	L.cannoneer = "艾胥凡砲手"
+	L.commander = "艾胥凡指揮官"
+	L.spotter = "艾胥凡偵察兵"
+	L.demolisher = "污鼠會毀滅者"
+	L.wavetender = "庫爾提拉斯平浪者"
+	L.halberd = "庫爾提拉斯長戟兵"
+	L.raider = "鐵潮劫掠者"
+	L.vanguard = "庫爾提拉斯先鋒"
 end
 
 L = BigWigs:NewBossLocale("Viq'Goth", "zhTW")
