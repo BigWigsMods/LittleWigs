910e8669a42043a2339c3337cf4b28bb7a028c19
Kyle Scheuing <oppzippy@gmail.com>
2018-12-22 07:49:08 -0500

    BfA/TempleOfSethraliss/Trash: Add Power Shot

--- BfA/TempleOfSethraliss/Trash.lua
+++ BfA/TempleOfSethraliss/Trash.lua
@@ -7,7 +7,7 @@ local mod, CL = BigWigs:NewBoss("Temple of Sethraliss Trash", 1822)
 if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
-	
+	134600, -- Sandswept Marksman
 )
 
 --------------------------------------------------------------------------------
@@ -16,7 +16,7 @@ mod:RegisterEnableMob(
 
 local L = mod:GetLocale()
 if L then
-	
+	L.marksman = "Sandswept Marksman"
 end
 
 --------------------------------------------------------------------------------
@@ -25,16 +25,22 @@ end
 
 function mod:GetOptions()
 	return {
-	
+		-- Sandswept Marksman
+		264574, -- Power Shot
 	}, {
-	
+		[264574] = L.marksman,
 	}
 end
 
 function mod:OnBossEnable()
-
+	self:Log("SPELL_CAST_START", "PowerShot", 264574)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:PowerShot(args)
+	self:Message2(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alert")
+end
