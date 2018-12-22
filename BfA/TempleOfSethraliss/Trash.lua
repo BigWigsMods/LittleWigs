790e4e82785ff5e4c4b31efbc46d15ef5bf0b170
Kyle Scheuing <oppzippy@gmail.com>
2018-12-22 08:05:13 -0500

    BfA/TempleOfSethraliss/Trash: Add Blade Flurry

--- BfA/TempleOfSethraliss/Trash.lua
+++ BfA/TempleOfSethraliss/Trash.lua
@@ -7,7 +7,9 @@ local mod, CL = BigWigs:NewBoss("Temple of Sethraliss Trash", 1822)
 if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
+	134990, -- Charged Dust Devil
 	134600, -- Sandswept Marksman
+	134602, -- Shrouded Fang
 )
 
 --------------------------------------------------------------------------------
@@ -16,7 +18,9 @@ mod:RegisterEnableMob(
 
 local L = mod:GetLocale()
 if L then
+	L.dustDevil = "Charged Dust Devil"
 	L.marksman = "Sandswept Marksman"
+	L.fang = "Shrouded Fang"
 end
 
 --------------------------------------------------------------------------------
@@ -25,22 +29,46 @@ end
 
 function mod:GetOptions()
 	return {
+		-- Charged Dust Devil
+		265968, -- Healing Surge
 		-- Sandswept Marksman
 		264574, -- Power Shot
+		-- Shrouded Fang
+		258908, -- Blade Flurry
 	}, {
+		[265968] = L.dustDevil,
 		[264574] = L.marksman,
+		[258908] = L.fang,
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "HealingSurge", 265968)
 	self:Log("SPELL_CAST_START", "PowerShot", 264574)
+	self:Log("SPELL_CAST_START", "BladeFlurry", 258908)
+	self:Log("SPELL_AURA_APPLIED", "BladeFlurryApplied", 258908)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:HealingSurge(args)
+	self:Message2(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "alert")
+end
+
 function mod:PowerShot(args)
 	self:Message2(args.spellId, "orange")
 	self:PlaySound(args.spellId, "alert")
+end
+
+function mod:BladeFlurry(args)
+	self:Message2(args.spellId, "orange", L.casting:format(args.spellId))
+	self:PlaySound(args.spellId, "alert")
+end
+
+function mod:BladeFlurryApplied(args)
+	self:Message2(args.spellId, "red")
+	self:PlaySound(args.spellId, "alarm")
 end
