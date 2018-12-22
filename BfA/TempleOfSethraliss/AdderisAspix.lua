556fb16c8b43e4c9c697ffa507875734de6d1129
Kyle Scheuing <oppzippy@gmail.com>
2018-12-22 07:29:07 -0500

    BfA/TempleOfSethralissAdderisAspix: Add timers for Static Shock, Arc Dash, and Lightning Shield

--- BfA/TempleOfSethraliss/AdderisAspix.lua
+++ BfA/TempleOfSethraliss/AdderisAspix.lua
@@ -21,21 +21,20 @@ function mod:GetOptions()
 		263246, -- Lightning Shield
 		{263371, "SAY", "SAY_COUNTDOWN"}, -- Conduction
 		263573, -- Cyclone Strike
-		-- 263257, -- Static Shock
-		-- 263424, -- Arc Dash
-		-- 263758, -- Twister
+		263257, -- Static Shock
+		263424, -- Arc Dash
 	}
 end
 
 function mod:OnBossEnable()
+	self:RegisterUnitEvent("UNIT_POWER_FREQUENT", nil, "boss1", "boss2")
 	self:Log("SPELL_AURA_APPLIED", "LightningShield", 263246)
+	self:Log("SPELL_AURA_REMOVED", "LightningShieldRemoved", 263246)
 	self:Log("SPELL_AURA_APPLIED", "Conduction", 263371)
 	self:Log("SPELL_AURA_REMOVED", "ConductionRemoved", 263371)
 	self:Log("SPELL_CAST_START", "CycloneStrike", 263573)
-	-- self:Log("SPELL_AURA_START", "StaticShock", 263257)
-	-- self:Log("SPELL_AURA_SUCCESS", "ArcDash", 263424)
-	-- self:Log("SPELL_AURA_SUCCESS", "Twister", 263758)
-
+	self:Log("SPELL_CAST_START", "StaticShock", 263257)
+	self:Death("BossDeath", 133379, 133944)
 end
 
 function mod:OnEngage()
@@ -47,9 +46,35 @@ end
 -- Event Handlers
 --
 
+function mod:UNIT_POWER_FREQUENT(event, unit)
+	local guid = UnitGUID(unit)
+	if self:MobId(guid) == 133379 then -- Adderis
+		if UnitPower(unit) == 100 then
+			self:Message2(263424, "orange") -- Arc Dash
+			self:PlaySound(263424, "alert") -- Arc Dash
+		end
+	else -- Aspix
+		if UnitPower(unit) == 0 then
+			self:Bar(263246, 4)
+		end
+	end
+end
+
 function mod:LightningShield(args)
 	self:Message2(args.spellId, "cyan", CL.other:format(args.spellName, args.destName))
 	self:PlaySound(args.spellId, "info")
+	if self:MobId(args.destGUID) == 133944 then -- Adderis
+		self:Bar(263424, 20) -- Arc Dash
+	else -- Aspix
+		self:Bar(263257, 20) -- Static Shock
+	end
+end
+
+function mod:LightningShieldRemoved(args)
+	if UnitIsDead("boss2") then
+		self:Message2(args.spellId, CL.removed:format(args.spellName))
+		self:PlaySound(args.spellId, "info")
+	end
 end
 
 function mod:Conduction(args)
@@ -58,7 +83,6 @@ function mod:Conduction(args)
 		self:PlaySound(args.spellId, "warning")
 		self:Say(args.spellId)
 		self:SayCountdown(args.spellId, 5)
-		-- self:Bar(args.spellId, 13.5)
 	end
 end
 
@@ -74,14 +98,14 @@ function mod:CycloneStrike(args)
 	self:Bar(args.spellId, 13.5)
 end
 
--- function mod:StaticShock(args)
-	-- self:Message2(args.spellId, "orange", "Long")
--- end
+function mod:StaticShock(args)
+	self:Message2(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+	self:CastBar(args.spellId, 2)
+	self:Bar(263246, 6) -- Lightning Shield removal
+end
 
--- function mod:ArcDash(args)
-	-- self:Message2(args.spellId, "yellow", "Alert")
--- end
-
--- function mod:Twister(args)
-	-- self:Message2(args.spellId, "orange", "Alarm")
--- end
+function mod:BossDeath(args)
+	self:StopBar(263424) -- Arc Dash
+	self:StopBar(263257) -- Static Shock
+end
