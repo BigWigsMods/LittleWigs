c7c7934d9d43c8cfe5d35288a59c7181730eaa41
Kyle Scheuing <oppzippy@gmail.com>
2018-12-24 05:24:59 -0500

    BfA/ShrineOfTheStorm/Trash: Added Shrine Templar

--- BfA/ShrineOfTheStorm/Trash.lua
+++ BfA/ShrineOfTheStorm/Trash.lua
@@ -7,7 +7,7 @@ local mod, CL = BigWigs:NewBoss("Siege of Boralus Trash", 1822)
 if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
-
+	134139 -- Shrine Templar
 )
 
 --------------------------------------------------------------------------------
@@ -16,7 +16,7 @@ mod:RegisterEnableMob(
 
 local L = mod:GetLocale()
 if L then
-	
+	L.templar = "Shrine Templar"
 end
 
 --------------------------------------------------------------------------------
@@ -25,17 +25,31 @@ end
 
 function mod:GetOptions()
 	return {
-	
+		-- Shrine Templar
+		276268, -- Heaving Blow
+		267977, -- Tidal Surge
 	}, {
-	
+		[276268] = L.templar,
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
 	
+	self:Log("SPELL_CAST_START", "HeavingBlow", 276268)
+	self:Log("SPELL_CAST_START", "TidalSurge", 267977)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:HeavingBlow(args)
+	self:Message2(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alert")
+end
+
+function mod:TidalSurge(args)
+	self:Message2(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "alert")
+end
