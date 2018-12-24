17beb14ac0c10c62496e2951e4cf4e98d5b56f6d
Kyle Scheuing <oppzippy@gmail.com>
2018-12-24 05:57:18 -0500

    BfA/ShrineOfTheStorm/Trash: Add Windspeaker Heldis

--- BfA/ShrineOfTheStorm/Trash.lua
+++ BfA/ShrineOfTheStorm/Trash.lua
@@ -9,7 +9,8 @@ mod.displayName = CL.trash
 mod:RegisterEnableMob(
 	134139, -- Shrine Templar
 	136186, -- Tidesage Spiritualist
-	139800 -- Galecaller Apprentice
+	139800, -- Galecaller Apprentice
+	136214 -- Windspeaker Heldis
 )
 
 --------------------------------------------------------------------------------
@@ -21,6 +22,7 @@ if L then
 	L.templar = "Shrine Templar"
 	L.spiritualist = "Tidesage Spiritualist"
 	L.apprentice = "Galecaller Apprentice"
+	L.windspeaker = "Windspeaker Heldis"
 end
 
 --------------------------------------------------------------------------------
@@ -38,10 +40,15 @@ function mod:GetOptions()
 		268030, -- Mending Rapids
 		-- Galecaller Apprentice
 		274437, -- Tempest
+		-- Windspeaker Heldis
+		268177, -- Windblast
+		268187, -- Gale Winds
+		268184, -- Minor Swiftness Ward
 	}, {
 		[276268] = L.templar,
 		[268050] = L.spiritualist,
 		[274437] = L.apprentice,
+		[268177] = L.windspeaker,
 	}
 end
 
@@ -54,6 +61,11 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "SpiritsSwiftness", 276266)
 	self:Log("SPELL_CAST_START", "MendingRapids", 268030)
 	self:Log("SPELL_CAST_START", "Tempest", 274437)
+	self:Log("SPELL_CAST_START", "Windblast", 268177)
+	self:Log("SPELL_CAST_START", "GaleWinds", 268187)
+	self:Log("SPELL_CAST_START", "MinorSwiftnessWard", 268187)
+	
+	self:Death("WindspeakerDeath", 136214)
 end
 
 --------------------------------------------------------------------------------
@@ -88,4 +100,24 @@ end
 function mod:Tempest(args)
 	self:Message2(args.spellId, "orange")
 	self:PlaySound(args.spellId, "alert")
+end
+
+function mod:Windblast(args)
+	self:Message2(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alert")
+end
+
+function mod:GaleWinds(args)
+	self:Message2(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, 22)
+end
+
+function mod:MinorSwiftnessWard(args)
+	self:Message2(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "info")
+end
+
+function mod:WindspeakerDeath(args)
+	self:StopBar(268187)
 end
