26afd466645e2f6de3f190e372eefc7ee88920bd
Kyle Scheuing <oppzippy@gmail.com>
2019-03-19 20:06:34 -0400

    BfA/ShrineOfTheStorm/Aqusirr: Set stage to 1 on engage

--- BfA/ShrineOfTheStorm/Aqusirr.lua
+++ BfA/ShrineOfTheStorm/Aqusirr.lua
@@ -53,6 +53,7 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
+	stage = 1
 	self:Bar(264560, 10.2) -- Choking Brine _success
 	self:Bar(264101, 15.5) -- Surging Rush _start
 	self:Bar(264526, 24) -- Grasp from the Depths _success
