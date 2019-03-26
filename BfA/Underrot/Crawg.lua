7c444ec6a8c7717a6f7680ddc1e763cac8f93a68
Kyle Scheuing <oppzippy@gmail.com>
2019-03-26 01:14:40 -0400

    BfA/Underrot/Crawg: Add comments

--- BfA/Underrot/Crawg.lua
+++ BfA/Underrot/Crawg.lua
@@ -52,7 +52,7 @@ function mod:OnEngage()
 	tantrumCount = 0
 	self:CDBar("random_cast", 8, L.random_cast, "inv_misc_questionmark") -- Charge
 	if not self:Normal() then
-		self:Bar(260333, 45) -- Tantrum
+		self:CDBar(260333, 45) -- Tantrum estimate, updated after the first Chrage or Indigestion
 	end
 end
 
@@ -90,7 +90,7 @@ end
 function mod:Tantrum(args)
 	self:Message2(args.spellId, "orange")
 	self:PlaySound(args.spellId, "long", "mobsoon")
-	self:CDBar(args.spellId, 45)
+	self:CDBar(args.spellId, 45) -- Estimate, updated after the next Chrage or Indigestion
 	self:Bar("random_cast", 18, L.random_cast, "inv_misc_questionmark") -- Charge
 	randomCast = true
 	tantrumCount = tantrumCount + 1
