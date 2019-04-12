b3c7bbde6585ffbb0514b42c22ca9aa82260ac73
Kyle Scheuing <oppzippy@gmail.com>
2019-04-12 01:06:12 -0400

    BfA/Freehold/Captains: Add Captain Jolly, add on BOSS messages and personal messages for brews, and improve initial timers

--- BfA/Freehold/Captains.lua
+++ BfA/Freehold/Captains.lua
@@ -1,9 +1,5 @@
 
 --------------------------------------------------------------------------------
--- TODO:
--- -- Lucky Sevens positive buff message?
-
---------------------------------------------------------------------------------
 -- Module Declaration
 --
 
@@ -11,6 +7,7 @@ local mod, CL = BigWigs:NewBoss("Council o' Captains", 1754, 2093)
 if not mod then return end
 mod:RegisterEnableMob(126847, 126848, 126845) -- Captain Raoul, Captain Eudora, Captain Jolly
 mod.engageId = 2094
+mod.respawnTime = 17.5
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -29,10 +26,15 @@ end
 
 function mod:GetOptions()
 	return {
+		-- Captain Raoul
 		258338, -- Blackout Barrel
 		256589, -- Barrel Smash
+		-- Captain Eudora
 		258381, -- Grape Shot
 		256979, -- Powder Shot
+		-- Captain Jolly
+		267533, -- Whirlpool of Blades
+		267522, -- Cutting Surge
 		--[[ Tending Bar ]]--
 		265088, -- Confidence-Boosting Brew (Crit)
 		264608, -- Invigorating Brew (Haste)
@@ -40,6 +42,7 @@ function mod:GetOptions()
 	},{
 		[258338] = -17023, -- Captain Raoul
 		[258381] = -17024, -- Captain Eudora
+		[267533] = -17025, -- Captain Jolly
 		[265088] = -18476, -- Rummy Mancomb
 	}
 end
@@ -49,20 +52,38 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "BarrelSmash", 256589)
 	self:Log("SPELL_CAST_SUCCESS", "GrapeShot", 258381)
 	self:Log("SPELL_CAST_START", "PowderShot", 256979)
+	self:Log("SPELL_CAST_START", "WhirlpoolofBlades", 267533)
+	self:Log("SPELL_CAST_START", "CuttingSurge", 267522)
 
 	self:Log("SPELL_CAST_SUCCESS", "CritBrew", 265088)
+	self:Log("SPELL_AURA_APPLIED", "CritBrewApplied", 265085)
 	self:Log("SPELL_CAST_SUCCESS", "HasteBrew", 264608)
+	self:Log("SPELL_AURA_APPLIED", "HasteBrewApplied", 265056)
 	self:Log("SPELL_CAST_SUCCESS", "CausticBrew", 265168)
-	self:Log("SPELL_AURA_APPLIED", "CausticBrewDamage", 278467)
-	self:Log("SPELL_PERIODIC_DAMAGE", "CausticBrewDamage", 278467)
+	self:Log("SPELL_AURA_APPLIED", "CausticBrewApplied", 278467)
 
 	self:Death("Deaths", 126847, 126848, 126845)
 end
 
-function mod:OnEngage()
-	self:Bar(256589, 6) -- Barrel Smash
-	self:Bar(258381, 8.5) -- Grape Shot
-	self:Bar(258338, 19.4) -- Blackout Barrel
+do
+	local function startTimers()
+		local elapsed = 0.1
+		if UnitCanAttack("player", mod:GetBossId(126847)) then -- Captain Raoul
+			mod:Bar(256589, 6.9 - elapsed) -- Barrel Smash
+			mod:Bar(258338, 19 - elapsed) -- Blackout Barrel
+		end
+		if UnitCanAttack("player", mod:GetBossId(126848)) then -- Captain Eudora
+			mod:Bar(258381, 8.5 - elapsed) -- Grape Shot
+		end
+		if UnitCanAttack("player", mod:GetBossId(126845)) then -- Captain Jolly
+			mod:Bar(267533, 13 - elapsed) -- Whirlpool of Blades
+			mod:Bar(267522, 5.7 - elapsed) -- Cutting Surge
+		end
+	end
+
+	function mod:OnEngage()
+		self:SimpleTimer(startTimers, 0.1)
+	end
 end
 
 function mod:VerifyEnable(unit)
@@ -79,6 +100,9 @@ function mod:Deaths(args)
 		self:StopBar(256589) -- Barrel Smash
 	elseif args.mobId == 126848 then -- Captain Eudora
 		self:StopBar(258381) -- Grape Shot
+	elseif args.mobId == 126845 then -- Captain Jolly
+		self:StopBar(267533) -- Whirlpool of Blades
+		self:StopBar(267522) -- Cutting Surge
 	end
 end
 
@@ -114,14 +138,62 @@ do
 	end
 end
 
+function mod:WhirlpoolofBlades(args)
+	self:Message2(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, 20)
+end
+
+function mod:CuttingSurge(args)
+	self:Message2(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, 23)
+end
+
 function mod:CritBrew(args)
 	self:Message2(args.spellId, "green", L.crit_brew)
 	self:PlaySound(args.spellId, "info")
 end
 
+do
+	local prev = 0
+	function mod:CritBrewApplied(args)
+		local t = args.time
+		if self:Me(args.destGUID) then
+			self:PersonalMessage(265088, nil, L.crit_brew)
+			self:PlaySound(265088, "info")
+		elseif self:Tank() and t-prev > 2 then -- Announce boss crit buff to tanks
+			local bossId = self:GetBossId(args.destGUID)
+			if bossId and UnitCanAttack("player", bossId) then
+				prev = t
+				self:Message2(265088, "purple", CL.onboss:format(L.crit_brew))
+				self:PlaySound(265088, "alarm")
+			end
+		end
+	end
+end
+
 function mod:HasteBrew(args)
 	self:Message2(args.spellId, "green", L.haste_brew)
 	self:PlaySound(args.spellId, "info")
+end
+
+do
+	local prev = 0
+	function mod:HasteBrewApplied(args)
+		local t = args.time
+		if self:Me(args.destGUID) then
+			self:PersonalMessage(264608, nil, L.haste_brew)
+			self:PlaySound(264608, "info")
+		elseif self:Tank() and t-prev > 2 then -- Announce boss haste buff to tanks
+			local bossId = self:GetBossId(args.destGUID)
+			if bossId and UnitCanAttack("player", bossId) then
+				prev = t
+				self:Message2(264608, "purple", CL.onboss:format(L.haste_brew))
+				self:PlaySound(264608, "alarm")
+			end
+		end
+	end
 end
 
 function mod:CausticBrew(args)
@@ -131,13 +203,17 @@ end
 
 do
 	local prev = 0
-	function mod:CausticBrewDamage(args)
+	function mod:CausticBrewApplied(args)
+		local t = args.time
 		if self:Me(args.destGUID) then
-			local t = args.time
-			if t-prev > 2 then
+			self:PersonalMessage(265168, "underyou", L.bad_brew)
+			self:PlaySound(265168, "alarm")
+		elseif self:Tank() and t-prev > 2 then -- Announce DoT on boss to tanks
+			local bossId = self:GetBossId(args.destGUID)
+			if bossId and UnitCanAttack("player", bossId) then
 				prev = t
-				self:PersonalMessage(265168, "underyou", L.bad_brew)
-				self:PlaySound(265168, "alarm")
+				self:Message2(265168, "green", CL.onboss:format(L.bad_brew))
+				self:PlaySound(265168, "info")
 			end
 		end
 	end
