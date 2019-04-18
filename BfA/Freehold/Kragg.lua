766295b6c12fe83527748a9f1d96e067ade29802
Kyle Scheuing <Oppzippy@users.noreply.github.com>
2019-04-18 19:46:22 -0400

    BfA/Freehold: Add Captain Jolly and add messages when brews are applied to bosses (#444)

--- BfA/Freehold/Kragg.lua
+++ BfA/Freehold/Kragg.lua
@@ -53,7 +53,8 @@ end
 --
 
 do
-	local prev = 0
+	local prevBombardment = 0
+	local prevDamage = 0
 	function mod:VileBombardment(args)
 		self:Message2(args.spellId, "yellow")
 		self:PlaySound(args.spellId, "info")
@@ -61,8 +62,20 @@ do
 			self:Bar(args.spellId, 6)
 		else
 			local t = args.time
-			self:Bar(args.spellId, t-prev > 8 and 6 or 10.8)
-			prev = t
+			self:Bar(args.spellId, t-prevBombardment > 8 and 6 or 10.8)
+			prevBombardment = t
+		end
+	end
+
+	function mod:VileCoatingDamage(args)
+		if self:Me(args.destGUID) then
+			local t = args.time
+			-- Don't show message for the first tick after vile bombardment lands
+			if t-prevDamage > 1.5 and t-prevBombardment > 2 then
+				prevDamage = t
+				self:PersonalMessage(args.spellId, "underyou")
+				self:PlaySound(args.spellId, "alarm", "gtfo")
+			end
 		end
 	end
 end
@@ -76,7 +89,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
 		self:CDBar(256106, 7) -- Azerite Powder Shot
 		self:Bar(256005, 6) -- Vile Bombardment
 		if not self:Normal() then
-			-- XXX initial Dive Bomb timer
+			self:Bar(272046, 17) -- Dive Bomb
 		end
 	end
 end
@@ -90,7 +103,7 @@ end
 function mod:DiveBomb(args)
 	self:Message2(args.spellId, "orange")
 	self:PlaySound(args.spellId, "alarm")
-	self:CDBar(args.spellId, 15.5) -- ranges from 12-18
+	self:Bar(args.spellId, 17)
 end
 
 function mod:AzeritePowderShot(args)
@@ -102,18 +115,4 @@ end
 function mod:RevitalizingBrew(args)
 	self:Message2(args.spellId, "red")
 	self:PlaySound(args.spellId, "warning", "interrupt")
-end
-
-do
-	local prev = 0
-	function mod:VileCoatingDamage(args)
-		if self:Me(args.destGUID) then
-			local t = args.time
-			if t-prev > 1.5 then
-				prev = t
-				self:PersonalMessage(args.spellId, "underyou")
-				self:PlaySound(args.spellId, "alarm", "gtfo")
-			end
-		end
-	end
 end
