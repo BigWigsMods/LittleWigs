df8fce6b4de8e4825f626e426c027ed9b325c661
Kyle Scheuing <Oppzippy@users.noreply.github.com>
2019-04-24 17:35:55 -0400

    BfA/WaycrestManor: Improvements (#451)

--- BfA/WaycrestManor/GorakTul.lua
+++ BfA/WaycrestManor/GorakTul.lua
@@ -7,6 +7,16 @@ local mod, CL = BigWigs:NewBoss("Gorak Tul", 1862, 2129)
 if not mod then return end
 mod:RegisterEnableMob(131864)
 mod.engageId = 2117
+mod.respawnTime = 15
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:GetLocale()
+if L then
+	L.add_killed = "Add killed - Ready to burn"
+end
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -17,7 +27,11 @@ function mod:GetOptions()
 		266181, -- Dread Essence
 		266266, -- Summon Deathtouched Slaver
 		266225, -- Darkened Lightning
-		266198, -- Alchemical Fire -- XXX Didn't work in my normal run (no log)
+		266198, -- Alchemical Fire
+		268202, -- Death Lens
+	}, {
+		[266181] = "general",
+		[268202] = "heroic",
 	}
 end
 
@@ -26,12 +40,15 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "SummonDeathtouchedSlaver", 266266)
 	self:Log("SPELL_CAST_START", "DarkenedLightning", 266225)
 	self:Log("SPELL_CAST_SUCCESS", "AlchemicalFire", 266198)
+	self:Log("SPELL_CAST_SUCCESS", "DeathLens", 268202)
+
+	self:Death("DeathtouchedSlaverDeath", 135552)
 end
 
 function mod:OnEngage()
-	self:Bar(266225, 9) -- Darkened Lightning
-	self:Bar(266266, 17) -- Summon Deathtouched Slaver
-	self:Bar(266181, 27.5) -- Dread Essence
+	self:Bar(266266, 4.4) -- Summon Deathtouched Slaver
+	self:Bar(266225, 7.2) -- Darkened Lightning
+	self:CDBar(266181, 25.5) -- Dread Essence
 end
 
 --------------------------------------------------------------------------------
@@ -41,24 +58,34 @@ end
 function mod:DreadEssence(args)
 	self:Message2(args.spellId, "red")
 	self:PlaySound(args.spellId, "warning")
-	self:Bar(args.spellId, 27.5)
+	self:CDBar(args.spellId, 28)
 end
 
 function mod:SummonDeathtouchedSlaver(args)
 	self:Message2(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "info")
-	self:Bar(args.spellId, 27.5)
+	self:CDBar(args.spellId, 17)
 end
 
 function mod:DarkenedLightning(args)
-	self:Message2(args.spellId, "orange")
 	if self:Interrupter() then
+		self:Message2(args.spellId, "orange")
 		self:PlaySound(args.spellId, "alert")
 	end
-	self:Bar(args.spellId, 14.5)
+	self:CDBar(args.spellId, 15.8)
 end
 
 function mod:AlchemicalFire(args)
 	self:Message2(args.spellId, "green")
 	self:PlaySound(args.spellId, "long")
+end
+
+function mod:DeathLens(args)
+	self:TargetMessage2(args.spellId, "red", args.destName)
+	self:PlaySound(args.spellId, "alert", nil, args.destName)
+end
+
+function mod:DeathtouchedSlaverDeath(args)
+	self:Message2(266198, "yellow", L.add_killed) -- Alchemical Fire
+	self:PlaySound(266198, "info") -- Alchemical Fire
 end
--- BfA/WaycrestManor/Heartsbane.lua
+++ BfA/WaycrestManor/Heartsbane.lua
@@ -7,6 +7,7 @@ local mod, CL = BigWigs:NewBoss("Heartsbane Triad", 1862, 2125)
 if not mod then return end
 mod:RegisterEnableMob(131825, 131823, 131824) -- Sister Briar, Sister Malady, Sister Solena
 mod.engageId = 2113
+mod.respawnTime = 20
 
 --------------------------------------------------------------------------------
 -- Locals
