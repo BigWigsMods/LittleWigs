ea48c078161e67c8d3c2bac98fac9509be59421f
Kyle Scheuing <oppzippy@gmail.com>
2019-05-02 18:59:13 -0400

    Add engage ids

--- BfA/Mechagon/TussleTonks.lua
+++ BfA/Mechagon/TussleTonks.lua
@@ -7,7 +7,7 @@ if not IsTestBuild() then return end
 local mod, CL = BigWigs:NewBoss("Tussle Tonks", 2097, 2336)
 if not mod then return end
 mod:RegisterEnableMob(144244, 145185) -- The Platinum Pummeler, Gnomercy 4.U.
---mod.engageId = XXX
+mod.engageId = 2336
 
 --------------------------------------------------------------------------------
 -- Initialization
