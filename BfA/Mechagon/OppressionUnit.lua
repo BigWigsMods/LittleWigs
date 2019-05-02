1890b5740cec7e8b619cbc601c14430761d20421
Kyle Scheuing <oppzippy@gmail.com>
2019-05-02 19:47:16 -0400

    OppressionUnit: Add spells

--- BfA/Mechagon/OppressionUnit.lua
+++ BfA/Mechagon/OppressionUnit.lua
@@ -15,12 +15,20 @@ mod.engageId = 2355
 
 function mod:GetOptions()
 	return {
-
+		295536, -- Cannon Blast
+		302274, -- Fulminating Zap
+		{295445, "TANK_HEALER"}, -- Wreck
+		296464, -- Reinforcement Relay
+		296080, -- Haywire
 	}
 end
 
 function mod:OnBossEnable()
-
+	self:Log("SPELL_CAST_START", "CannonBlast", 295536)
+	self:Log("SPELL_AURA_APPLIED", "FulminatingZapApplied", 302274)
+	self:Log("SPELL_CAST_SUCCESS", "Wreck", 295445)
+	self:Log("SPELL_CAST_SUCCESS", "ReinforcementRelay", 296464)
+	self:Log("SPELL_AURA_APPLIED", "HaywireApplied", 296080)
 end
 
 function mod:OnEngage()
@@ -30,3 +38,30 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:CannonBlast(args)
+	self:Message2(args.spellId, "red")
+	self:PlaySound(args.spellId, "alert")
+	self:CastBar(args.spellId, 3)
+end
+
+function mod:FulminatingZap(args)
+	self:TargetMessage2(args.spellId, "orange", args.destName)
+	self:PlaySound(args.spellId, "info", nil, args.destName)
+end
+
+function mod:Wreck(args)
+	self:Message2(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+end
+
+function mod:ReinforcementRelay(args)
+	self:Message2(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+end
+
+function mod:HaywireApplied(args)
+	self:Message2(args.spellId, "cyan", CL.onboss:format(args.spellName))
+	self:PlaySound(args.spellId, "long")
+	self:TargetBar(args.spellId, 30, args.destName)
+end
