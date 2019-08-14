759356657b794b2f8a3217562f732ea1b48f97c5
Kyle Scheuing <oppzippy@gmail.com>
2019-08-14 01:41:28 -0400

    BfA/Freehold/Captains: Fix spell id and target bar

--- BfA/Freehold/Captains.lua
+++ BfA/Freehold/Captains.lua
@@ -54,7 +54,7 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "GrapeShot", 258381)
 	self:Log("SPELL_CAST_START", "PowderShot", 256979)
 	self:Log("SPELL_CAST_START", "ChainShot", 272902)
-	self:Log("SPELL_AURA_APPLIED", "ChainShotApplied", 272902)
+	self:Log("SPELL_AURA_APPLIED", "ChainShotApplied", 272905)
 	self:Log("SPELL_CAST_START", "WhirlpoolofBlades", 267533)
 	self:Log("SPELL_CAST_START", "CuttingSurge", 267522)
 
@@ -157,7 +157,7 @@ do
 end
 
 function mod:ChainShotApplied(args)
-	self:TargetBar(272902, args.destName)
+	self:TargetBar(272902, 6, args.destName)
 end
 
 function mod:WhirlpoolofBlades(args)
