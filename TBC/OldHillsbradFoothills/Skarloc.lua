cda71d277c328dc60d0ab17026dc9d849642203d
Velithris <13710677+Zireael-N@users.noreply.github.com>
2020-02-02 18:55:18 +0300

    TBC: add modules for The Escape from Durnholde (#502)

---
+++ TBC/OldHillsbradFoothills/Skarloc.lua
@@ -0,0 +1,98 @@
+-------------------------------------------------------------------------------
+-- Module Declaration
+--
+
+local mod, CL = BigWigs:NewBoss("Captain Skarloc", 560, 539)
+if not mod then return end
+mod:RegisterEnableMob(
+	17862, -- Captain Skarloc
+	17860, -- Durnholde Veteran
+	17833 -- Durnholde Warden
+)
+mod.engageId = 1907
+-- mod.respawnTime = 0 -- you have to free Thrall again if you wipe
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:GetLocale()
+if L then
+	-- Thrall! You didn't really think you would escape, did you?  You and your allies shall answer to Blackmoore... after I've had my fun.
+	L.warmup_trigger = "answer to Blackmoore"
+end
+
+
+-------------------------------------------------------------------------------
+--  Initialization
+
+function mod:GetOptions()
+	return {
+		"warmup",
+		38385, -- Consecration
+		13005, -- Hammer of Justice
+		29427, -- Holy Light
+	}
+end
+
+function mod:OnBossEnable()
+	self:RegisterEvent("CHAT_MSG_MONSTER_SAY")
+
+	self:Log("SPELL_DAMAGE", "Consecration", 38385)
+	self:Log("SPELL_MISSED", "Consecration", 38385)
+	self:Log("SPELL_AURA_APPLIED", "HammerOfJustice", 13005)
+	self:Log("SPELL_AURA_REMOVED", "HammerOfJusticeRemoved", 13005)
+	self:Log("SPELL_CAST_START", "HolyLight", 29427)
+	self:Log("SPELL_INTERRUPT", "Interrupt", "*")
+end
+
+function mod:VerifyEnable(_, mobId)
+	if mobId == 17862 then return true end
+
+	-- Durnholde Veteran and Durnholde Warden are trash mobs
+	local _, _, completedFirst = C_Scenario.GetCriteriaInfo(1)
+	local _, _, completedSecond = C_Scenario.GetCriteriaInfo(2)
+	return completedFirst and not completedSecond
+end
+
+-------------------------------------------------------------------------------
+--  Event Handlers
+
+function mod:CHAT_MSG_MONSTER_SAY(event, msg)
+	if msg:find(L.warmup_trigger, nil, true) then
+		self:UnregisterEvent(event)
+		self:Bar("warmup", 7.9, CL.active, "inv_sword_01")
+	end
+end
+
+do
+	local prev = 0
+	function mod:Consecration(args)
+		if self:Me(args.destGUID) and args.time - prev > 2 then
+			prev = args.time
+			self:Message(args.spellId, "blue", "Alarm", CL.underyou:format(args.spellName))
+		end
+	end
+end
+
+function mod:HammerOfJustice(args)
+	if self:Me(args.destGUID) or self:Dispeller("magic") then
+		self:TargetMessage(args.spellId, args.destName, "red", "Warning")
+		self:TargetBar(args.spellId, 5, args.destName)
+	end
+end
+
+function mod:HammerOfJusticeRemoved(args)
+	self:StopBar(args.spellName, args.destName)
+end
+
+function mod:HolyLight(args)
+	self:Message(args.spellId, "orange", "Alert", CL.casting:format(args.spellName))
+	self:CastBar(args.spellId, 2.5)
+end
+
+function mod:Interrupt(args)
+	if args.extraSpellId == 29427 then -- Holy Light
+		self:StopBar(CL.cast:format(args.extraSpellName))
+	end
+end
---
+++ TBC/OldHillsbradFoothills/Trash.lua
@@ -0,0 +1,88 @@
+--------------------------------------------------------------------------------
+-- Module Declaration
+--
+
+local mod, CL = BigWigs:NewBoss("Old Hillsbrad Foothills Trash", 560)
+if not mod then return end
+mod.displayName = CL.trash
+mod:RegisterEnableMob(
+	18723, -- Erozion
+	17876, -- Thrall
+	18887, -- Taretha
+
+	-- To ensure the module is loaded for Incendiary Bombs, mobs from Durnholde Keep:
+	17840, -- Durnholde Tracking Hound
+	18598, -- Orc Prisoner
+	17820, -- Durnholde Rifleman
+	17833 -- Durnholde Warden
+)
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:GetLocale()
+if L then
+	L.custom_on_autotalk = "Autotalk"
+	L.custom_on_autotalk_desc = "Instantly select Erozion's, Thrall's and Taretha's gossip options."
+
+	L.incendiary_bombs = "Incendiary Bombs"
+	L.incendiary_bombs_desc = "Display a message when an Incendiary Bomb is planted."
+	L.incendiary_bombs_icon = "inv_misc_bomb_05"
+end
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		"custom_on_autotalk",
+		"incendiary_bombs"
+	}
+end
+
+function mod:OnBossEnable()
+	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
+	self:RegisterWidgetEvent(522, "BombPlanted")
+	self:RegisterEvent("GOSSIP_SHOW")
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
+
+function mod:GOSSIP_SHOW()
+	if self:GetOption("custom_on_autotalk") then
+		if GetGossipAvailableQuests() or GetGossipActiveQuests() then return end -- let the player take / turn in the quest
+
+		-- If the player is in a group, do not free Thrall automatically,
+		-- because they may deny others a chance to turn in the quest.
+		local mobId = self:MobId(UnitGUID("npc"))
+		if mobId == 17876 and GetNumGroupMembers() > 0 then
+			local _, _, completed = C_Scenario.GetCriteriaInfo(2)
+			if not completed then return end
+		end
+
+		if GetGossipOptions() then
+			SelectGossipOption(1)
+		end
+	end
+end
+
+function mod:BombPlanted(id, text)
+	local bomb = text:match("(%d+).+5")
+	if bomb then
+		bomb = tonumber(bomb)
+		self:Message("incendiary_bombs", "cyan", "Info", CL.count:format(self:SpellName(256798), bomb), L.incendiary_bombs_icon)
+
+		if bomb == 5 then
+			self:UnregisterWidgetEvent(id)
+			local drakeModule = BigWigs:GetBossModule("Lieutenant Drake", true)
+			if drakeModule then
+				drakeModule:Enable()
+				drakeModule:Warmup(20.5)
+			end
+		end
+	end
+end
