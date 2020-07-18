65c85d244d570a6e3ff97686b5956d451b0b7960
Justwait <justwait@method.gg>
2020-07-18 12:23:48 +0200

    Drycode some bosses

--- Shadowlands/Plaguefall/DoctorIckus.lua
+++ Shadowlands/Plaguefall/DoctorIckus.lua
@@ -15,16 +15,59 @@ mod.engageId = 2384
 
 function mod:GetOptions()
 	return {
-		"berserk",
+		322358, -- Burning Strain
+		329217, -- Slime Lunge
+		329110, -- Slime Injection
+		332617, -- Pestilence Surge
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "BurningStrain", 322358)
+	self:Log("SPELL_CAST_SUCCESS", "SlimeLunge", 329217)
+	self:Log("SPELL_CAST_SUCCESS", "SlimeInjection", 329217)
+	self:Log("SPELL_AURA_APPLIED", "SlimeInjectionApplied", 329217)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "SlimeInjectionApplied", 329217)
+	self:Log("SPELL_AURA_REMOVED", "SlimeInjectionRemoved", 329217)
+	self:Log("SPELL_CAST_SUCCESS", "PestilenceSurge", 332617)
 end
 
 function mod:OnEngage()
+	--self:CDBar(329217, 8.6) -- Slime Lunge
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:BurningStrain(args)
+	self:Message2(args.spellId, "red")
+	self:PlaySound(args.spellId, "alarm")
+end
+
+function mod:SlimeLunge(args)
+	self:Message2(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "long")
+	--self:CDBar(args.spellId, 8.6)
+end
+
+function mod:SlimeInjection(args)
+	--self:CDBar(args.spellId, 8.6)
+end
+
+function mod:SlimeInjectionApplied(args)
+	local amount = args.amount or 0
+	self:StackMessage(args.spellId, args.destName, args.amount, "cyan")
+	self:PlaySound(args.spellId, "info")
+end
+
+function mod:SlimeInjectionRemoved(args)
+	self:Message2(args.spellId, "yellow", CL.spawning:format(self:SpellName(-21712))) -- Slithering Ooze
+	self:PlaySound(args.spellId, "alert")
+end
+
+function mod:PestilenceSurge(args)
+	self:Message2(args.spellId, "orange")
+	self:PlaySound(args.spellId, "warning")
+	--self:CDBar(args.spellId, 8.6)
+end
--- Shadowlands/Plaguefall/Globgrog.lua
+++ Shadowlands/Plaguefall/Globgrog.lua
@@ -15,16 +15,42 @@ mod.engageId = 2382
 
 function mod:GetOptions()
 	return {
-		"berserk",
+		324459, -- Beckon Slime
+		324527, -- Plaguestomp
+		324667, -- Slime Wave
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "BeckonSlime", 324459)
+	self:Log("SPELL_CAST_START", "Plaguestomp", 324527)
+	self:Log("SPELL_CAST_START", "SlimeWave", 324667)
 end
 
 function mod:OnEngage()
+	--self:CDBar(324459, 17.1) -- Beckon Slime
+	--self:CDBar(324527, 17.1) -- Plaguestomp
+	--self:CDBar(324667, 17.1) -- Slime Wave
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:BeckonSlime(args)
+	self:Message2(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "long")
+	--self:Bar(args.spellId, 37.7)
+end
+
+function mod:Plaguestomp(args)
+	self:Message2(args.spellId, "red")
+	self:PlaySound(args.spellId, "warning")
+	--self:Bar(args.spellId, 37.7)
+end
+
+function mod:SlimeWave(args)
+	self:Message2(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+	--self:Bar(args.spellId, 37.7)
+end
