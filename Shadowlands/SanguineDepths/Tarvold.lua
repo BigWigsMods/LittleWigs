bad0ae981d4ba14a9bd3196fa23e8b35bbe629d8
Kyle Scheuing <oppzippy@gmail.com>
2020-08-21 20:23:48 -0400

    SanguineDepths/Tarvold: Add boss module

--- Shadowlands/SanguineDepths/Tarvold.lua
+++ Shadowlands/SanguineDepths/Tarvold.lua
@@ -5,7 +5,7 @@
 
 local mod, CL = BigWigs:NewBoss("Executor Tarvold", 2284, 2415)
 if not mod then return end
---mod:RegisterEnableMob(0)
+mod:RegisterEnableMob(162103)
 mod.engageId = 2361
 --mod.respawnTime = 30
 
@@ -15,16 +15,71 @@ mod.engageId = 2361
 
 function mod:GetOptions()
 	return {
-		"berserk",
+		-- General
+		{322554, "SAY"}, -- Castigate
+		322573, -- Coalesce Manifestation
+		{328494, "DISPEL"}, -- Sintouched Anima
+		-- Fleeting Manifestation
+		323551, -- Residue
+	}, {
+		[322554] = "general",
+		[323551] = -21883, -- Fleeting Manifestation
 	}
 end
 
 function mod:OnBossEnable()
+	-- XXX Change to RegisterUnitEvent if boss frames get added to this fight
+	self:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+	self:Log("SPELL_CAST_START", "Castigate", 322554)
+	self:Log("SPELL_AURA_APPLIED", "SintouchedAnimaApplied", 328494)
+	self:Death("FleetingManifestationDeath", 165556)
 end
 
 function mod:OnEngage()
+	self:Bar(322554, 4.7) -- Castigate
+	self:CDBar(322573, 10.8) -- Coalesce Manifestation
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+do
+	local prev = nil
+	function mod:UNIT_SPELLCAST_START(_, _, castGUID, spellId)
+		if castGUID ~= prev then return end
+		prev = castGUID
+		if spellId == 322573 then -- Coalesce Manifestation
+			self:Message2(322573, "yellow")
+			self:PlaySound(322573, "info")
+			self:CDBar(322573, 30)
+		end
+	end
+end
+
+do
+	local function printTarget(self, name, guid)
+		self:Message2(322554, "orange", name)
+		self:PlaySound(322554, "alert", nil, name)
+		if self:Me(guid) then
+			self:Say(322554)
+		end
+	end
+	
+	function mod:Castigate(args)
+		self:GetBossTarget(printTarget, 0.4, args.sourceGUID)
+		self:Bar(args.spellId, 20.7)
+	end
+end
+
+function mod:SintouchedAnimaApplied(args)
+	if self:Me(args.destGUID) or self:Dispeller("curse", nil, args.spellId) then
+		self:TargetMessage2(args.spellId, "yellow", args.destName)
+		self:PlaySound(args.spellId, "info", nil, args.destName)
+	end
+end
+
+function mod:FleetingManifestationDeath(args)
+	self:Message2(323551, "orange") -- Residue
+	self:PlaySound(323551, "alert") -- Residue
+end
