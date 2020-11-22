c434477fb542e2ae90bdff811caf24e67d457386
Kyle Buller <bullerk@gmail.com>
2020-11-22 01:05:57 -0600

    Split modules into addons by expansion (#527)

---
+++ Shadowlands/MistsofTirnaScithe/Options/Colors.lua
@@ -0,0 +1,25 @@
+
+BigWigs:AddColors("Ingra Maloch", {
+	[323057] = "yellow",
+	[323059] = {"cyan","green"},
+	[323137] = "orange",
+	[323149] = "red",
+	[323177] = "cyan",
+	[328756] = "red",
+})
+
+BigWigs:AddColors("Mistcaller", {
+	[321828] = {"blue","purple"},
+	[321834] = "orange",
+	[326180] = "yellow",
+	[336499] = "cyan",
+})
+
+BigWigs:AddColors("Tred'ova", {
+	[322450] = "cyan",
+	[322550] = "cyan",
+	[322563] = {"blue","yellow"},
+	[322614] = {"blue","orange"},
+	[322651] = "yellow",
+	["parasitic"] = {"blue","red"},
+})
---
+++ Shadowlands/MistsofTirnaScithe/Options/Sounds.lua
@@ -0,0 +1,25 @@
+
+BigWigs:AddSounds("Ingra Maloch", {
+	[323057] = "alert",
+	[323059] = {"info","long"},
+	[323137] = "alarm",
+	[323149] = "warning",
+	[323177] = "alert",
+	[328756] = "warning",
+})
+
+BigWigs:AddSounds("Mistcaller", {
+	[321828] = "warning",
+	[321834] = "alarm",
+	[326180] = "alert",
+	[336499] = "long",
+})
+
+BigWigs:AddSounds("Tred'ova", {
+	[322450] = "long",
+	[322550] = "info",
+	[322563] = "alarm",
+	[322614] = "alarm",
+	[322651] = "alert",
+	["parasitic"] = "warning",
+})
