42645a8ec9b81725c315abe844d3664fa8850e74
funkydude <funkydude@users.noreply.github.com>
2020-12-08 15:30:46 +0000

    DeOtherSide/Muehzala: Add soulcrusher warnings

--- Shadowlands/DeOtherSide/Muehzala.lua
+++ Shadowlands/DeOtherSide/Muehzala.lua
@@ -10,11 +10,18 @@ mod.engageId = 2396
 --mod.respawnTime = 30
 
 --------------------------------------------------------------------------------
+-- Locals
+--
+
+local soulcrusherCount = 1
+
+--------------------------------------------------------------------------------
 -- Initialization
 --
 
 function mod:GetOptions()
 	return {
+		{327646, "TANK_HEALER"}, -- Soulcrusher
 		325725, -- Cosmic Artifice
 		325258, -- Master of Death
 		{326171, "EMPHASIZE"}, -- Shatter Reality
@@ -23,6 +30,7 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "Soulcrusher", 327646)
 	self:Log("SPELL_AURA_APPLIED", "CosmicArtificeApplied", 325725)
 	self:Log("SPELL_CAST_START", "MasterOfDeath", 325258)
 	self:Log("SPELL_CAST_START", "ShatterReality", 326171)
@@ -30,6 +38,8 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
+	soulcrusherCount = 1
+	self:CDBar(327646, 6) -- Soulcrusher
 	self:CDBar(325258, 9) -- Master of Death
 	self:CDBar(326171, 60) -- Shatter Reality
 end
@@ -37,6 +47,13 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:Soulcrusher(args)
+	self:Message(args.spellId, "yellow")
+	soulcrusherCount = soulcrusherCount + 1
+	self:CDBar(args.spellId, soulcrusherCount % 2 == 0 and 20 or 10) -- pull:6.4, 19.7, 10.6, 22.4
+	self:PlaySound(args.spellId, "alert")
+end
 
 function mod:CosmicArtificeApplied(args)
 	if self:Me(args.destGUID) then
