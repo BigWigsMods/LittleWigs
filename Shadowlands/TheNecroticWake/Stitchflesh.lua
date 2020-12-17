2e571f35779a25ac13f7384523df2b094000b439
funkydude <funkydude@users.noreply.github.com>
2020-12-17 05:38:18 +0000

    TheNecroticWake/Stitchflesh: Add Morbid Fixation

--- Shadowlands/TheNecroticWake/Stitchflesh.lua
+++ Shadowlands/TheNecroticWake/Stitchflesh.lua
@@ -8,6 +8,7 @@ if not mod then return end
 mod:RegisterEnableMob(162689) -- Surgeon Stitchflesh
 mod.engageId = 2389
 --mod.respawnTime = 30
+
 --------------------------------------------------------------------------------
 -- Locals
 --
@@ -27,21 +28,27 @@ function mod:GetOptions()
 		{320200, "ME_ONLY"}, -- Stitchneedle
 		{334488, "TANK"}, -- Cleave Flesh
 		320359, -- Escape
+		{343556, "SAY"}, -- Morbid Fixation
 	}
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "AwakenCreation", 320358)
 	self:Log("SPELL_AURA_APPLIED", "MeatHookApplied", 322681) -- Aim Debuff
+	self:Log("SPELL_AURA_REMOVED", "MeatHookRemoved", 322681)
 	self:Log("SPELL_AURA_APPLIED", "MeatHookHit", 322548) -- Surgeon Stitchflesh Pull Down
 	self:Log("SPELL_CAST_START", "MutlilateStart", 320376)
 	self:Log("SPELL_CAST_SUCCESS", "EmbalmingIchor", 334476)
 	self:Log("SPELL_AURA_APPLIED", "StitchneedleApplied", 320200)
 	self:Log("SPELL_CAST_START", "CleaveFlesh", 334488)
 	self:Log("SPELL_CAST_SUCCESS", "Escape", 320359)
+	self:Log("SPELL_CAST_START", "MorbidFixation", 343556)
+	self:Log("SPELL_AURA_APPLIED", "MorbidFixationApplied", 343556)
+	self:Log("SPELL_AURA_REMOVED", "MorbidFixationRemoved", 343556)
 end
 
 function mod:OnEngage()
+	hooked = false
 	self:Bar(322681, 10.5) -- Meat Hook
 end
 
@@ -64,6 +71,12 @@ function mod:MeatHookApplied(args)
 		self:Flash(args.spellId)
 	end
 	self:CDBar(args.spellId, 18)
+end
+
+function mod:MeatHookRemoved(args)
+	if self:Me(args.destGUID) then
+		self:CancelSayCountdown(args.spellId)
+	end
 end
 
 function mod:MeatHookHit(args)
@@ -107,4 +120,30 @@ function mod:Escape(args)
 	hooked = false
 
 	self:StopBar(334488) -- Cleave Flesh
+end
+
+do
+	local function printTarget(self, name, guid)
+		if self:Me(guid) then
+			self:Say(343556)
+			self:PlaySound(343556, "warning")
+		end
+		self:TargetMessage(343556, "red", name)
+	end
+
+	function mod:MorbidFixation(args)
+		self:GetBossTarget(printTarget, args.sourceGUID)
+	end
+end
+
+function mod:MorbidFixationApplied(args)
+	if self:Me(args.destGUID) then
+		self:TargetBar(args.spellId, 8, args.destName)
+	end
+end
+
+function mod:MorbidFixationRemoved(args)
+	if self:Me(args.destGUID) then
+		self:StopBar(args.spellId, args.destName)
+	end
 end
