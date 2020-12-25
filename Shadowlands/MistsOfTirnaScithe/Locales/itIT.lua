311d9be1ae9ead09727f6d983dbe9bd672756f5c
Velithris <13710677+Zireael-N@users.noreply.github.com>
2020-12-25 00:27:32 +0000

    Add a full stop to L.parasitic_desc

--- Shadowlands/MistsOfTirnaScithe/Locales/itIT.lua
+++ Shadowlands/MistsOfTirnaScithe/Locales/itIT.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Tred'ova", "itIT")
 if not L then return end
 if L then
-	-- L.parasitic_desc = "Show warnings and timers for Parasitic Pacification, Incapacitation and Domination"
+	-- L.parasitic_desc = "Show warnings and timers for Parasitic Pacification, Incapacitation and Domination."
 end
