608fea72de00690d9f685cbebdcb8757fad7fd63
Nyamp <renanmanieroprado@gmail.com>
2021-01-05 16:23:07 -0300

    Update ptBR (#557)

--- Shadowlands/TheNecroticWake/Locales/ptBR.lua
+++ Shadowlands/TheNecroticWake/Locales/ptBR.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("Nalthor the Rimebinder", "ptBR")
 if not L then return end
 if L then
-	-- L.aegis = "%s removed after %.1f seconds!"
+	L.aegis = "%s removido depois de %.1f segundos!"
 end
 
 L = BigWigs:NewBossLocale("The Necrotic Wake Trash", "ptBR")
