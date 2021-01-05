1db64500d27a72a28b02642624b5a73b406e108e
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2021-01-05 20:58:19 +0100

    Update deDE.lua

--- WotLK/TheVioletHold/Locales/deDE.lua
+++ WotLK/TheVioletHold/Locales/deDE.lua
@@ -1,12 +1,12 @@
 local L = BigWigs:NewBossLocale("Xevozz", "deDE")
 if not L then return end
 if L then
-	-- L.summon_sphere = "Summoning Ethereal Sphere"
+	L.summon_sphere = "Beschwört Sphäre des Astraleums"
 end
 
 L = BigWigs:NewBossLocale("Zuramat the Obliterator", "deDE")
 if L then
-	-- L.short_name = "Zuramat"
+	L.short_name = "Zuramat"
 end
 
 L = BigWigs:NewBossLocale("The Violet Hold Trash", "deDE")
