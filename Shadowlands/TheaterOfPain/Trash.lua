ed414ed35d3c3085dd3eebe863563a537a985ee2
Max Belsky <public.belsky@gmail.com>
2021-01-23 03:37:26 +0300

    Shadowlands/TheaterOfPain/Trash: Add "Demoralizing Shout" alert

--- Shadowlands/TheaterOfPain/Trash.lua
+++ Shadowlands/TheaterOfPain/Trash.lua
@@ -3,24 +3,21 @@
 -- Module Declaration
 --
 
-local mod, CL = BigWigs:NewBoss("Plaguefall Trash", 2289)
+local mod, CL = BigWigs:NewBoss("Theater Of Pain Trash", 2293)
 if not mod then return end
 mod.displayName = CL.trash
--- mod:RegisterEnableMob(
--- 	128434, -- Feasting Skyscreamer
--- 	128455, -- T'lonja
--- 	127879, -- Shieldbearer of Zul
--- 	122969, -- Zanchuli Witch-Doctor
--- 	129553, -- Dinomancer Kish'o
--- 	132126, -- Gilded Priestess
--- 	122970, -- Shadowblade Stalker
--- 	122973, -- Dazar'ai Confessor
--- 	122972  -- Dazar'ai Augur
--- )
+mod:RegisterEnableMob(
+	164506, -- Ancient Captain
+)
 
 --------------------------------------------------------------------------------
 -- Localization
 --
+
+local L = mod:GetLocale()
+if L then
+	L.ancient_captain = "Ancient Captain"
+end
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -28,13 +25,23 @@ mod.displayName = CL.trash
 
 function mod:GetOptions()
 	return {
-		"berserk"
+		-- Ancient Captain
+		330562, -- Demoralizing Shout
+	}, {
+		[330562] = L.ancient_captain,
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "DemoralizingShout", 330562)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+-- Ancient Captain
+function mod:DemoralizingShout(args)
+	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
+	self:PlaySound(args.spellId, self:Interrupter() and "warning" or "alert")
+end
