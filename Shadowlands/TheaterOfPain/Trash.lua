fdf04d4d703a7fd1aa1411f3efc640c67425495c
Max Belsky <public.belsky@gmail.com>
2021-01-23 03:47:48 +0300

    Shadowlands/TheaterOfPain/Trash: Add "Vile Eruption" alert

--- Shadowlands/TheaterOfPain/Trash.lua
+++ Shadowlands/TheaterOfPain/Trash.lua
@@ -9,6 +9,7 @@ mod.displayName = CL.trash
 mod:RegisterEnableMob(
 	164506, -- Ancient Captain
 	174210, -- Blighted Sludge-Spewer
+	163086, -- Rancid Gasbag
 )
 
 --------------------------------------------------------------------------------
@@ -19,6 +20,7 @@ local L = mod:GetLocale()
 if L then
 	L.ancient_captain = "Ancient Captain"
 	L.blighted_sludge_spewer = "Blighted Sludge-Spewer"
+	L.rancid_gasbag = "Rancid Gasbag"
 end
 
 --------------------------------------------------------------------------------
@@ -31,15 +33,19 @@ function mod:GetOptions()
 		330562, -- Demoralizing Shout
 		-- Blighted Sludge-Spewer
 		341969, -- Withering Discharge
+		-- Rancid Gasbag
+		330614, -- Vile Eruption
 	}, {
 		[330562] = L.ancient_captain,
 		[341969] = L.blighted_sludge_spewer,
+		[330614] = L.rancid_gasbag,
 	}
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "DemoralizingShout", 330562)
 	self:Log("SPELL_CAST_START", "WitheringDischarge", 341969)
+	self:Log("SPELL_CAST_START", "VileEruption", 330614)
 end
 
 --------------------------------------------------------------------------------
@@ -57,3 +63,10 @@ function mod:WitheringDischarge(args)
 	self:Message(args.spellId, "orange", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, "alert")
 end
+
+-- Rancid Gasbag
+function mod:VileEruption(args)
+	self:Message(args.spellId, "yellow", CL.casting:format(args.spellName))
+	self:PlaySound(args.spellId, "alert")
+end
+
