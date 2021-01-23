29f5cee866b1b4d11f9f699a042d37538bd6e2e7
Max Belsky <public.belsky@gmail.com>
2021-01-23 03:44:35 +0300

    Shadowlands/TheaterOfPain/Trash: Add "Withering Discharge" alert

--- Shadowlands/TheaterOfPain/Trash.lua
+++ Shadowlands/TheaterOfPain/Trash.lua
@@ -8,6 +8,7 @@ if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
 	164506, -- Ancient Captain
+	174210, -- Blighted Sludge-Spewer
 )
 
 --------------------------------------------------------------------------------
@@ -17,6 +18,7 @@ mod:RegisterEnableMob(
 local L = mod:GetLocale()
 if L then
 	L.ancient_captain = "Ancient Captain"
+	L.blighted_sludge_spewer = "Blighted Sludge-Spewer"
 end
 
 --------------------------------------------------------------------------------
@@ -27,13 +29,17 @@ function mod:GetOptions()
 	return {
 		-- Ancient Captain
 		330562, -- Demoralizing Shout
+		-- Blighted Sludge-Spewer
+		341969, -- Withering Discharge
 	}, {
 		[330562] = L.ancient_captain,
+		[341969] = L.blighted_sludge_spewer,
 	}
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "DemoralizingShout", 330562)
+	self:Log("SPELL_CAST_START", "WitheringDischarge", 341969)
 end
 
 --------------------------------------------------------------------------------
@@ -44,4 +50,10 @@ end
 function mod:DemoralizingShout(args)
 	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, self:Interrupter() and "warning" or "alert")
+end
+
+-- Blighted Sludge-Spewer
+function mod:WitheringDischarge(args)
+	self:Message(args.spellId, "orange", CL.casting:format(args.spellName))
+	self:PlaySound(args.spellId, "alert")
 end
