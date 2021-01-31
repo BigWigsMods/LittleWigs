671ad7f91718a1742415fcc0cb22c41af611c5d7
EK <eke00372@gmail.com>
2021-01-31 22:25:24 +0800

    Update zhTW (#571)

--- Shadowlands/TheNecroticWake/Locales/zhTW.lua
+++ Shadowlands/TheNecroticWake/Locales/zhTW.lua
@@ -1,21 +1,21 @@
 local L = BigWigs:NewBossLocale("Nalthor the Rimebinder", "zhTW")
 if not L then return end
 if L then
-	-- L.aegis = "%s removed after %.1f seconds!"
+	L.aegis = "%s移除，用時 %.1f 秒！"
 end
 
 L = BigWigs:NewBossLocale("The Necrotic Wake Trash", "zhTW")
 if L then
-	-- L.corpse_harvester = "Corpse Harvester"
-	-- L.stitched_vanguard = "Stitched Vanguard"
-	-- L.zolramus_gatekeeper = "Zolramus Gatekeeper"
-	-- L.brittlebone_mage = "Brittlebone Mage"
-	-- L.skeletal_marauder = "Skeletal Marauder"
-	-- L.zolramus_bonemender = "Zolramus Bonemender"
-	-- L.narzudah = "Nar'zudah"
-	-- L.skeletal_monstrosity = "Skeletal Monstrosity"
-	-- L.corpse_collector = "Corpse Collector"
-	-- L.kyrian_stitchwerk = "Kyrian Stitchwerk"
-	-- L.flesh_crafter = "Flesh Crafter"
-	-- L.separation_assistant = "Separation Assistant"
+	L.corpse_harvester = "屍體收割者"
+	L.stitched_vanguard = "縫合前鋒兵"
+	L.zolramus_gatekeeper = "佐拉慕斯守門者"
+	L.brittlebone_mage = "脆骨法師"
+	L.skeletal_marauder = "骷髏掠奪者"
+	L.zolramus_bonemender = "佐拉慕斯修骨者"
+	L.narzudah = "納爾祖達"
+	L.skeletal_monstrosity = "骷髏巨怪"
+	L.corpse_collector = "屍骸收集者"
+	L.kyrian_stitchwerk = "琪瑞安縫補怪"
+	L.flesh_crafter = "血肉工匠"
+	L.separation_assistant = "肢解助理"
 end
