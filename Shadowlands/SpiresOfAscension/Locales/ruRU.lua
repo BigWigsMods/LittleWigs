89f473309127bcfe1f3371e0da66d72b6eef6e55
Wetxius <Wetxius@users.noreply.github.com>
2021-02-06 19:28:46 +0300

    ruRU update

--- Shadowlands/SpiresOfAscension/Locales/ruRU.lua
+++ Shadowlands/SpiresOfAscension/Locales/ruRU.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Oryphrion", "ruRU")
 if not L then return end
 if L then
-	L.early_intermission_over = "Перерыв завершается (%d orbs reached)"
+	L.early_intermission_over = "Перерыв завершается (пропущено сфер: %d)"
 end
