40af2045783b5df2323ddd2bfe007c3f9a6cc3f1
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2021-02-07 19:39:16 +0100

    Update deDE.lua

--- Shadowlands/SpiresOfAscension/Locales/deDE.lua
+++ Shadowlands/SpiresOfAscension/Locales/deDE.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Oryphrion", "deDE")
 if not L then return end
 if L then
-	L.early_intermission_over = "Zwischenphase vorbei (%d |4orb:orbs; reached)"
+	L.early_intermission_over = "Zwischenphase vorbei (%d |4Kugel:Kugeln; durchgekommen)"
 end
