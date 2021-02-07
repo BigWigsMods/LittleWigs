9d03798a21c372c3f62aaf4230cd79ee32fb76ea
EKE00372 <eke00372@gmail.com>
2021-02-08 03:44:04 +0800

    Update zhTW

--- Shadowlands/HallsOfAtonement/Locales/zhTW.lua
+++ Shadowlands/HallsOfAtonement/Locales/zhTW.lua
@@ -9,5 +9,5 @@ if L then
 	L.collector = "墮落的收集者"
 	L.shard = "哈奇厄斯裂片"
 	L.reaver = "石源魔劫奪者"
-	-- L.slasher = "Stoneborn Slasher"
+	L.slasher = "石源魔斬擊者"
 end
--- Shadowlands/SpiresOfAscension/Locales/zhTW.lua
+++ Shadowlands/SpiresOfAscension/Locales/zhTW.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Oryphrion", "zhTW")
 if not L then return end
 if L then
-	L.early_intermission_over = "階段轉換結束（%d orbs reached）"
+	L.early_intermission_over = "階段轉換結束（吸收了 %d 個球）"
 end
