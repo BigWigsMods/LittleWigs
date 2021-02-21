420cdd2cba75c48c94718abab580512649aed219
EK <eke00372@gmail.com>
2021-02-22 07:40:23 +0800

    Update zhTW (#585)

--- Shadowlands/SanguineDepths/Locales/zhTW.lua
+++ Shadowlands/SanguineDepths/Locales/zhTW.lua
@@ -1,15 +1,15 @@
 local L = BigWigs:NewBossLocale("Sanguine Depths Trash", "zhTW")
 if not L then return end
 if L then
-	-- L.anima_collector = "Anima Collector"
-	-- L.chamber_sentinel = "Chamber Sentinel"
-	-- L.depths_warden = "Depths Warden"
-	-- L.dreadful_huntmaster = "Dreadful Huntmaster"
-	-- L.general_kaal = "General Kaal"
-	-- L.grand_overseer = "Grand Overseer"
-	-- L.head_custodian_javlin = "Head Custodian Javlin"
-	-- L.insatiable_brute = "Insatiable Brute"
-	-- L.regal_mistdancer = "Regal Mistdancer"
-	-- L.research_scribe = "Research Scribe"
-	-- L.wicked_oppressor = "Wicked Oppressor"
+	L.anima_collector = "靈魄牢籠"
+	L.chamber_sentinel = "大廳哨兵"
+	L.depths_warden = "深淵看守者"
+	L.dreadful_huntmaster = "恐怖神獵手"
+	L.general_kaal = "凱厄將軍"
+	L.grand_overseer = "大監督者"
+	L.head_custodian_javlin = "監護長賈夫臨"
+	L.insatiable_brute = "貪婪蠻卒"
+	L.regal_mistdancer = "皇家霧舞者"
+	L.research_scribe = "研究紀錄者"
+	L.wicked_oppressor = "邪惡壓迫者"
 end
