4d372f8deee0b8ffb27f8ad35bed32e1142e654b
Kyle Scheuing <kyle@kylescheuing.com>
2021-06-06 01:45:07 -0400

    Update locales

--- Shadowlands/SpiresOfAscension/Locales/frFR.lua
+++ Shadowlands/SpiresOfAscension/Locales/frFR.lua
@@ -3,3 +3,17 @@ if not L then return end
 if L then
 	L.early_intermission_over = "Intervalle terminé (%d |4orb:orbs; reached)"
 end
+
+L = BigWigs:NewBossLocale("Spires of Ascension Trash", "frFR")
+if L then
+	L.etherdiver = "Plonge-éther"
+	L.forsworn_castigator = "Fustigatrice désavouée"
+	L.forsworn_champion = "Champion désavoué"
+	L.forsworn_goliath = "Goliath désavoué"
+	L.forsworn_inquisitor = "Inquisitrice désavouée"
+	L.forsworn_mender = "Soigneur désavoué"
+	L.forsworn_squad_leader = "Chef d'escouade désavoué"
+	L.forsworn_warden = "Veilleur désavoué"
+	L.astronos = "Astronos"
+	L.lakesis = "Lakésis"
+end
--- Shadowlands/SpiresOfAscension/Locales/zhTW.lua
+++ Shadowlands/SpiresOfAscension/Locales/zhTW.lua
@@ -3,3 +3,17 @@ if not L then return end
 if L then
 	L.early_intermission_over = "階段轉換結束（吸收了 %d 個球）"
 end
+
+L = BigWigs:NewBossLocale("Spires of Ascension Trash", "zhTW")
+if L then
+	-- L.etherdiver = "Etherdiver"
+	-- L.forsworn_castigator = "Forsworn Castigator"
+	-- L.forsworn_champion = "Forsworn Champion"
+	-- L.forsworn_goliath = "Forsworn Goliath"
+	-- L.forsworn_inquisitor = "Forsworn Inquisitor"
+	-- L.forsworn_mender = "Forsworn Mender"
+	-- L.forsworn_squad_leader = "Forsworn Squad-Leader"
+	-- L.forsworn_warden = "Forsworn Warden"
+	-- L.astronos = "Astronos"
+	-- L.lakesis = "Lakesis"
+end
