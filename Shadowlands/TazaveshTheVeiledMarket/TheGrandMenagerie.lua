ef04835794ba9028f0a83f74baa5a12461c4e02d
Kyle Scheuing <kyle@kylescheuing.com>
2021-06-26 19:06:10 -0400

    Add The Grand Menagerie timers

--- Shadowlands/TazaveshTheVeiledMarket/TheGrandMenagerie.lua
+++ Shadowlands/TazaveshTheVeiledMarket/TheGrandMenagerie.lua
@@ -42,6 +42,7 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "GrandConsumption", 349797)
 
 	-- Achillite
+	self:Log("SPELL_CAST_SUCCESS", "PurificationProtocol", 349954)
 	self:Log("SPELL_AURA_APPLIED", "PurificationProtocolApplied", 349954)
 	self:Log("SPELL_AURA_REMOVED", "PurificationProtocolRemoved", 349954)
 	self:Log("SPELL_CAST_START", "FlagellationProtocol", 349934)
@@ -53,12 +54,36 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
+	self:SetStage(1)
+	self:Bar(349663, 12) -- Grip of Hunger
+	self:Bar(349797, 25.9) -- Grand Consumption
 
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
+	if self:GetStage() == 1 and self:GetBossId(176555) then -- Achillite
+		self:SetStage(2)
+		self:StopBar(349663) -- Grip of Hunger
+		self:StopBar(349797) -- Grand Consumption
+	
+		self:Bar(349954, 6.7) -- Purification Protocol
+		self:Bar(349934, 15.7) -- Flagellation Protocol
+		self:Bar(349987, 26.7) -- Venting Protocol
+	elseif self:GetStage() == 2 and self:GetBossId(176705) then -- Venza Goldfuse
+		self:SetStage(3)
+		self:StopBar(349954) -- Purification Protocol
+		self:StopBar(349934) -- Flagellation Protocol
+		self:StopBar(349987) -- Venting Protocol
+	
+		self:Bar(350101, 5.2) -- Chains of Damnation
+		self:Bar(350086, 17.4) -- Whirling Annihilation
+	end
+end
 
 function mod:GluttonyApplied(args)
 	self:Message(args.spellId, "yellow")
@@ -77,11 +102,17 @@ end
 function mod:GripOfHunger(args)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "alarm")
+	self:Bar(args.spellId, 23)
 end
 
 function mod:GrandConsumption(args)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "alarm")
+	self:Bar(args.spellId, 30.3)
+end
+
+function mod:PurificationProtocol(args)
+	self:CDBar(args.spellId, 18.2)
 end
 
 do
@@ -105,11 +136,13 @@ end
 function mod:FlagellationProtocol(args)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, 23)
 end
 
 function mod:VentingProtocol(args)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "alarm")
+	self:CDBar(args.spellId, 27)
 end
 
 do
@@ -120,10 +153,12 @@ do
 	
 	function mod:ChainsOfDamnation(args)
 		self:GetBossTarget(printTarget, 0.4, args.sourceGUID)
+		self:Bar(args.spellId, 25.5)
 	end
 end
 
 function mod:WhirlingAnnihilation(args)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "alarm")
+	self:Bar(args.spellId, 25.5)
 end
