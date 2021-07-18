fb33abdc51be0d01644732d47c9f89b37a128321
Nyamp <renanmanieroprado@gmail.com>
2021-07-18 18:48:58 -0300

    Update ptBR (#610)

--- Shadowlands/TazaveshTheVeiledMarket/Locales/ptBR.lua
+++ Shadowlands/TazaveshTheVeiledMarket/Locales/ptBR.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Myza's Oasis", "ptBR")
 if not L then return end
 if L then
-	-- L.add_wave_killed = "Add wave killed (%d/%d)"
+	L.add_wave_killed = "Onda de adds derrotada (%d/%d)"
 end
