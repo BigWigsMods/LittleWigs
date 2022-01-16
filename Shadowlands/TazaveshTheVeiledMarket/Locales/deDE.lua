536a94660009ef46e5c5f116e90b3630a874fad6
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2022-01-16 14:49:42 +0100

    Update deDE.lua

--- Shadowlands/TazaveshTheVeiledMarket/Locales/deDE.lua
+++ Shadowlands/TazaveshTheVeiledMarket/Locales/deDE.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Myza's Oasis", "deDE")
 if not L then return end
 if L then
-	-- L.add_wave_killed = "Add wave killed (%d/%d)"
+	L.add_wave_killed = "Add Welle getötet (%d/%d)"
 end
