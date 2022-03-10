e2252ffdd39eca23b0d61de48a00f1c1864c3e6f
Levanah <7429333+levan4h@users.noreply.github.com>
2022-03-10 23:05:10 +0100

    Update frFR (#627)

--- Shadowlands/TazaveshTheVeiledMarket/Locales/frFR.lua
+++ Shadowlands/TazaveshTheVeiledMarket/Locales/frFR.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Myza's Oasis", "frFR")
 if not L then return end
 if L then
-	-- L.add_wave_killed = "Add wave killed (%d/%d)"
+	 L.add_wave_killed = "Vague d'adds tuée (%d/%d)"
 end
