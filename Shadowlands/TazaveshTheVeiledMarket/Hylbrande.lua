9e977f2988ea76631f4a154a5154a69e1a45d139
Nick <ntowle@gmail.com>
2022-04-23 17:27:22 -0400

    Update Hylbrande.lua

--- Shadowlands/TazaveshTheVeiledMarket/Hylbrande.lua
+++ Shadowlands/TazaveshTheVeiledMarket/Hylbrande.lua
@@ -5,7 +5,10 @@
 
 local mod, CL = BigWigs:NewBoss("Hylbrande", 2441, 2448)
 if not mod then return end
-mod:RegisterEnableMob(175663) -- Hylbrande
+mod:RegisterEnableMob(
+	175663, -- Hylbrande
+	175667  -- Titanic Defense Turret
+)
 mod:SetEncounterID(2426)
 mod:SetRespawnTime(30)
 
@@ -13,18 +16,21 @@ mod:SetRespawnTime(30)
 -- Initialization
 --
 
+local sanitizingCycleTime = 0
+
 function mod:GetOptions()
 	return {
 		346971, -- [DNT] Summon Vault Defender
 		353312, -- Purifying Burst
 		{346116, "TANK"}, -- Shearing Swings
 		347094, -- Titanic Crash
-		346957, -- Purged by Fire
+		{346957, "SAY"}, -- Purged by Fire
 		346766, -- Sanitizing Cycle
 	}
 end
 
 function mod:OnBossEnable()
+	self:RegisterEvent("CHAT_MSG_RAID_BOSS_WHISPER")
 	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
 	self:Log("SPELL_CAST_START", "PurifyingBurst", 353312)
 	self:Log("SPELL_CAST_SUCCESS", "ShearingSwings", 346116)
@@ -42,6 +48,7 @@ function mod:OnEngage()
 	self:Bar(347094, 15.4) -- Titanic Crash
 	self:Bar(346971, 19, CL.adds) -- [DNT] Summon Vault Defender
 	self:Bar(346766, 38.8) -- Sanitizing Cycle
+	sanitizingCycleTime = GetTime() + 38.8
 end
 
 --------------------------------------------------------------------------------
@@ -52,7 +59,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
 	if spellId == 346971 then -- [DNT] Summon Vault Defender
 		self:Message(spellId, "yellow", CL.add_spawned)
 		self:PlaySound(spellId, "info")
-		if self:BarTimeLeft(346766) > 29.1 then -- Sanitizing Cycle
+		if sanitizingCycleTime - GetTime() > 29.1 then -- Sanitizing Cycle
 			self:Bar(spellId, 29.1, CL.adds)
 		end
 	end
@@ -61,7 +68,7 @@ end
 function mod:PurifyingBurst(args)
 	self:Message(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "info")
-	if self:BarTimeLeft(346766) > 21.8 then -- Sanitizing Cycle
+	if sanitizingCycleTime - GetTime() > 21.8 then -- Sanitizing Cycle
 		self:CDBar(args.spellId, 21.8)
 	end
 end
@@ -69,7 +76,7 @@ end
 function mod:ShearingSwings(args)
 	self:Message(args.spellId, "purple")
 	self:PlaySound(args.spellId, "alarm")
-	if self:BarTimeLeft(346766) > 10.9 then -- Sanitizing Cycle
+	if sanitizingCycleTime - GetTime() > 10.9 then -- Sanitizing Cycle
 		self:CDBar(args.spellId, 10.9)
 	end
 end
@@ -77,19 +84,33 @@ end
 function mod:TitanicCrash(args)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "alarm")
-	if self:BarTimeLeft(346766) > 23.1 then -- Sanitizing Cycle
+	if sanitizingCycleTime - GetTime() > 23.1 then -- Sanitizing Cycle
 		self:Bar(args.spellId, 23.1)
 	end
 end
 
-function mod:PurgedByFire(args)
-	self:Message(args.spellId, "orange")
-	self:PlaySound(args.spellId, "alert")
-	if self:BarTimeLeft(346766) > 17 then -- Sanitizing Cycle
-		self:Bar(args.spellId, 17)
+do
+	local prev = 0
+	function mod:CHAT_MSG_RAID_BOSS_WHISPER(_, _, _, _, _, playerName)
+		-- "Titanic Defense Turret acquires a new target."
+		prev = GetTime()
+		self:TargetMessage(346957, "blue", playerName) -- Purged By Fire
+		self:PlaySound(346957, "warning") -- Purged By Fire
+		self:Say(346957) -- Purged By Fire
+	end
+	local function warnPurgedByFire()
+		if GetTime() - prev > 1 then
+			mod:Message(346957, "orange") -- Purged By Fire
+			mod:PlaySound(346957, "alert") -- Purged By Fire
+		end
+	end
+	function mod:PurgedByFire(args)
+		self:SimpleTimer(warnPurgedByFire, 0.1)
+		if sanitizingCycleTime - GetTime() > 17 then -- Sanitizing Cycle
+			self:Bar(args.spellId, 17)
+		end
 	end
 end
-
 do
 	local prev = 0
 	function mod:PurgedByFireDamage(args)
@@ -114,6 +135,7 @@ do
 			self:Message(args.spellId, "cyan")
 			self:PlaySound(args.spellId, "long")
 			self:StopBar(args.spellId)
+			sanitizingCycleTime = 0
 			self:StopBar(353312) -- Purifying Burst
 			self:StopBar(346116) -- Shearing Swings
 			self:StopBar(346957) -- Purged by Fire
@@ -127,6 +149,7 @@ function mod:SanitizingCycleRemoved(args)
 	self:Message(args.spellId, "cyan", CL.over:format(args.spellName))
 	self:PlaySound(args.spellId, "long")
 	self:Bar(args.spellId, 70)
+	sanitizingCycleTime = GetTime() + 70
 	self:Bar(353312, 13.3) -- Purifying Burst
 	self:Bar(346116, 16.6) -- Shearing Swings
 	self:Bar(346957, 19.2) -- Purged by Fire
