9717066c15a63ae4a54ac61792d4fe41a3e1c628
Nick <ntowle@gmail.com>
2022-05-16 15:29:58 -0400

    refactor UNIT_SPELLCAST_SUCCEEDED, UNIT_TARGETABLE_CHANGED handlers

--- Shadowlands/DeOtherSide/TheManastorms.lua
+++ Shadowlands/DeOtherSide/TheManastorms.lua
@@ -37,19 +37,19 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
+	-- Stage Changes
+	self:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+	self:RegisterUnitEvent("UNIT_TARGETABLE_CHANGED", nil, "boss1", "boss2")
+
 	-- Stage 1
 	self:Log("SPELL_CAST_SUCCESS", "SummonPowerCrystal", 320787)
 	self:Log("SPELL_AURA_APPLIED", "EchoFingerLaserXtreme", 323877)
 	self:Log("SPELL_CAST_START", "DiabolicalDooooooom", 320141)
-	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "MillificentCastSucceeded", "boss2")
-	self:RegisterUnitEvent("UNIT_TARGETABLE_CHANGED", "MillhouseDefeated", "boss1")
 
 	-- Stage 2
 	self:Log("SPELL_CAST_SUCCESS", "ExperimentalSquirrelBomb", 320823)
 	self:Log("SPELL_CAST_START", "Shadowfury", 320132)
 	self:Log("SPELL_CAST_START", "AerialRocketChickenBarrage", 321061)
-	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "MillhouseCastSucceeded", "boss1")
-	self:RegisterUnitEvent("UNIT_TARGETABLE_CHANGED", "MillificentDefeated", "boss2")
 end
 
 function mod:OnEngage()
@@ -66,6 +66,67 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+-- Stage Changes
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
+	if spellId == 326920 then -- Teleport (Stage 1 start)
+		-- stop stage 2
+		self:StopBar(320823) -- Experimental Squirrel Bomb
+		if self:Mythic() then
+			self:StopBar(320132) -- Shadowfury
+			self:StopBar(321061) -- Aerial Rocket Chicken Barrage
+		end
+
+		-- stage
+		self:Message("stages", "cyan", CL.stage:format(1), false)
+		self:PlaySound("stages", "long", "stage1")
+
+		-- set up stage 1
+		self:Bar(320787, 10.3) -- Summon Power Crystal
+		if self:Mythic() then
+			if not millificentDefeated then
+				self:Bar(323877, 17) -- Echo Finger Laser X-treme
+			end
+			self:Bar(320141, 45.6) -- Diabolical Dooooooom!
+		end
+	elseif spellId == 326804 then -- Rocket Jump (Stage 2 start)
+		-- stop stage 1
+		self:StopBar(320787) -- Summon Power Crystal
+		if self:Mythic() then
+			self:StopBar(323877) -- Echo Finger Laser X-treme
+			self:StopBar(320141) -- Diabolical Dooooooom!
+		end
+
+		-- stage
+		self:Message("stages", "cyan", CL.stage:format(2), false)
+		self:PlaySound("stages", "long", "stage2")
+
+		-- set up stage 2
+		self:Bar(320823, 8.5) -- Experimental Squirrel Bomb
+		if self:Mythic() then
+			if not millhouseDefeated then
+				shadowfuryCount = 1
+				self:Bar(320132, 18) -- Shadowfury
+			end
+			self:Bar(321061, 45.6) -- Aerial Rocket Chicken Barrage
+		end
+	end
+end
+
+function mod:UNIT_TARGETABLE_CHANGED(_, unit)
+	if self:MobId(self:UnitGUID(unit)) == 164556 and not UnitExists(unit) then -- Millhouse Manastorm
+		millhouseDefeated = true
+		if self:Mythic() then
+			self:StopBar(320132) -- Shadowfury
+		end
+	elseif self:MobId(self:UnitGUID(unit)) == 164555 and not UnitExists(unit) then -- Millificent Manastorm
+		millificentDefeated = true
+		if self:Mythic() then
+			self:StopBar(323877) -- Echo Finger Laser X-treme
+		end
+	end
+end
 
 -- Stage 1
 
@@ -95,40 +156,6 @@ end
 function mod:DiabolicalDooooooom(args)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "long")
-end
-
-function mod:MillificentCastSucceeded(_, _, _, spellId)
-	if spellId == 326804 then -- Rocket Jump (Stage 2 start)
-		-- stop stage 1
-		self:StopBar(320787) -- Summon Power Crystal
-		if self:Mythic() then
-			self:StopBar(323877) -- Echo Finger Laser X-treme
-			self:StopBar(320141) -- Diabolical Dooooooom!
-		end
-
-		-- stage
-		self:Message("stages", "cyan", CL.stage:format(2), false)
-		self:PlaySound("stages", "long", "stage2")
-
-		-- set up stage 2
-		self:Bar(320823, 8.5) -- Experimental Squirrel Bomb
-		if self:Mythic() then
-			shadowfuryCount = 1
-			if not millhouseDefeated then
-				self:Bar(320132, 18) -- Shadowfury
-			end
-			self:Bar(321061, 45.6) -- Aerial Rocket Chicken Barrage
-		end
-	end
-end
-
-function mod:MillhouseDefeated()
-	if not UnitExists("boss1") then
-		millhouseDefeated = true
-		if self:Mythic() then
-			self:StopBar(320132) -- Shadowfury
-		end
-	end
 end
 
 -- Stage 2
@@ -163,37 +190,4 @@ end
 function mod:AerialRocketChickenBarrage(args)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "long")
-end
-
-function mod:MillhouseCastSucceeded(_, _, _, spellId)
-	if spellId == 326920 then -- Teleport (Stage 1 start)
-		-- stop stage 2
-		self:StopBar(320823) -- Experimental Squirrel Bomb
-		if self:Mythic() then
-			self:StopBar(320132) -- Shadowfury
-			self:StopBar(321061) -- Aerial Rocket Chicken Barrage
-		end
-
-		-- stage
-		self:Message("stages", "cyan", CL.stage:format(1), false)
-		self:PlaySound("stages", "long", "stage1")
-
-		-- set up stage 1
-		self:Bar(320787, 10.3) -- Summon Power Crystal
-		if self:Mythic() then
-			if not millificentDefeated then
-				self:Bar(323877, 17) -- Echo Finger Laser X-treme
-			end
-			self:Bar(320141, 45.6) -- Diabolical Dooooooom!
-		end
-	end
-end
-
-function mod:MillificentDefeated()
-	if not UnitExists("boss2") then
-		millificentDefeated = true
-		if self:Mythic() then
-			self:StopBar(323877) -- Echo Finger Laser X-treme
-		end
-	end
 end
