7beaa224a6eda1d37303d241502a7f38fcaced8a
Nick <ntowle@gmail.com>
2022-05-19 12:00:22 -0400

    sort wings

--- Shadowlands/TheaterOfPain/Trash.lua
+++ Shadowlands/TheaterOfPain/Trash.lua
@@ -8,6 +8,8 @@ if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
 	170850, -- Raging Bloodhorn
+	174210, -- Blighted Sludge-Spewer
+	163086, -- Rancid Gasbag
 	167538, -- Dokigg the Brutalizer
 	162744, -- Nekthara the Mangler
 	167532, -- Heavin the Breaker
@@ -15,8 +17,6 @@ mod:RegisterEnableMob(
 	164506, -- Ancient Captain
 	167533, -- Advent Nevermore
 	167534, -- Rek the Hardened
-	174210, -- Blighted Sludge-Spewer
-	163086, -- Rancid Gasbag
 	160495, -- Maniacal Soulbinder
 	170882, -- Bone Magus
 	169893 -- Nefarious Darkspeaker
@@ -29,6 +29,8 @@ mod:RegisterEnableMob(
 local L = mod:GetLocale()
 if L then
 	L.raging_bloodhorn = "Raging Bloodhorn"
+	L.blighted_sludge_spewer = "Blighted Sludge-Spewer"
+	L.rancid_gasbag = "Rancid Gasbag"
 	L.dokigg_the_brutalizer_harugia_the_bloodthirsty = "Dokigg the Brutalizer / Harugia the Bloodthirsty"
 	L.dokigg_the_brutalizer = "Dokigg the Brutalizer"
 	L.nekthara_the_mangler_heavin_the_breaker = "Nekthara the Mangler / Heavin the Breaker"
@@ -40,8 +42,6 @@ if L then
 	L.ancient_captain = "Ancient Captain"
 	L.advent_nevermore = "Advent Nevermore"
 	L.rek_the_hardened = "Rek the Hardened"
-	L.blighted_sludge_spewer = "Blighted Sludge-Spewer"
-	L.rancid_gasbag = "Rancid Gasbag"
 	L.maniacal_soulbinder = "Maniacal Soulbinder"
 	L.bone_magus = "Bone Magus"
 	L.nefarious_darkspeaker = "Nefarious Darkspeaker"
@@ -55,6 +55,10 @@ function mod:GetOptions()
 	return {
 		-- Raging Bloodhorn
 		{333241, "DISPEL"}, -- Raging Tantrum
+		-- Blighted Sludge-Spewer
+		341969, -- Withering Discharge
+		-- Rancid Gasbag
+		330614, -- Vile Eruption
 		-- Dokigg the Brutalizer / Harugia the Bloodthirsty
 		342139, -- Battle Trance
 		-- Dokigg the Brutalizer
@@ -78,10 +82,6 @@ function mod:GetOptions()
 		331275, -- Unbreakable Guard
 		-- Rek the Hardened
 		{333845, "TANK_HEALER"}, -- Unbalancing Blow
-		-- Blighted Sludge-Spewer
-		341969, -- Withering Discharge
-		-- Rancid Gasbag
-		330614, -- Vile Eruption
 		-- Maniacal Soulbinder
 		330868, -- Necrotic Bolt Volley
 		-- Bone Magus
@@ -90,6 +90,8 @@ function mod:GetOptions()
 		333294, -- Death Winds
 	}, {
 		[333241] = L.raging_bloodhorn,
+		[341969] = L.blighted_sludge_spewer,
+		[330614] = L.rancid_gasbag,
 		[342139] = L.dokigg_the_brutalizer_harugia_the_bloodthirsty,
 		[342125] = L.dokigg_the_brutalizer,
 		[342135] = L.nekthara_the_mangler_heavin_the_breaker,
@@ -101,8 +103,6 @@ function mod:GetOptions()
 		[330562] = L.ancient_captain,
 		[333827] = L.advent_nevermore,
 		[333845] = L.rek_the_hardened,
-		[341969] = L.blighted_sludge_spewer,
-		[330614] = L.rancid_gasbag,
 		[330868] = L.maniacal_soulbinder,
 		[342675] = L.bone_magus,
 		[333294] = L.nefarious_darkspeaker
@@ -111,6 +111,8 @@ end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "RagingTantrumApplied", 333241)
+	self:Log("SPELL_CAST_START", "WitheringDischarge", 341969)
+	self:Log("SPELL_CAST_START", "VileEruption", 330614)
 	self:Log("SPELL_CAST_START", "BattleTrance", 342139)
 	self:Log("SPELL_AURA_APPLIED", "BattleTranceApplied", 342139)
 	self:Log("SPELL_CAST_START", "BrutalLeap", 342125)
@@ -124,8 +126,6 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "SeismicStomp", 333827)
 	self:Log("SPELL_CAST_START", "UnbreakableGuard", 331275)
 	self:Log("SPELL_CAST_START", "UnbalancingBlow", 333845)
-	self:Log("SPELL_CAST_START", "WitheringDischarge", 341969)
-	self:Log("SPELL_CAST_START", "VileEruption", 330614)
 	self:Log("SPELL_CAST_START", "NecroticBoltVolley", 330868)
 	self:Log("SPELL_CAST_START", "BoneSpear", 342675)
 	self:Log("SPELL_CAST_START", "DeathWinds", 333294)
@@ -141,6 +141,21 @@ function mod:RagingTantrumApplied(args)
 		self:Message(args.spellId, "orange", CL.buff_other:format(args.destName, args.spellName))
 		self:PlaySound(args.spellId, "warning")
 	end
+end
+
+-- Blighted Sludge-Spewer
+function mod:WitheringDischarge(args)
+	if self:Friendly(args.sourceFlags) then -- these NPCs can be mind-controlled by DKs
+		return
+	end
+	self:Message(args.spellId, "orange", CL.casting:format(args.spellName))
+	self:PlaySound(args.spellId, "alert")
+end
+
+-- Rancid Gasbag
+function mod:VileEruption(args)
+	self:Message(args.spellId, "yellow", CL.casting:format(args.spellName))
+	self:PlaySound(args.spellId, "alert")
 end
 
 -- Dokigg the Brutalizer / Harugia the Bloodthirsty
@@ -273,21 +288,6 @@ function mod:UnbalancingBlow(args)
 			self:PlaySound(args.spellId, "alert")
 		end
 	end
-end
-
--- Blighted Sludge-Spewer
-function mod:WitheringDischarge(args)
-	if self:Friendly(args.sourceFlags) then -- these NPCs can be mind-controlled by DKs
-		return
-	end
-	self:Message(args.spellId, "orange", CL.casting:format(args.spellName))
-	self:PlaySound(args.spellId, "alert")
-end
-
--- Rancid Gasbag
-function mod:VileEruption(args)
-	self:Message(args.spellId, "yellow", CL.casting:format(args.spellName))
-	self:PlaySound(args.spellId, "alert")
 end
 
 -- Maniacal Soulbinder
