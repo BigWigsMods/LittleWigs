a792297fb6f6a72285e5821d72a5cf0fd5dc0134
Nick <ntowle@gmail.com>
2022-05-23 13:43:28 -0400

    respawn timer

--- Shadowlands/Plaguefall/DoctorIckus.lua
+++ Shadowlands/Plaguefall/DoctorIckus.lua
@@ -7,7 +7,7 @@ local mod, CL = BigWigs:NewBoss("Doctor Ickus", 2289, 2403)
 if not mod then return end
 mod:RegisterEnableMob(164967) -- Doctor Ickus
 mod.engageId = 2384
---mod.respawnTime = 30
+mod.respawnTime = 30
 
 --------------------------------------------------------------------------------
 -- Initialization
