92234609177f152d7c6b9ab28ba83aebbf3a4915
Nick <ntowle@gmail.com>
2022-05-23 14:03:19 -0400

    30s respawn

--- Shadowlands/Plaguefall/DominaVenomblade.lua
+++ Shadowlands/Plaguefall/DominaVenomblade.lua
@@ -7,7 +7,7 @@ local mod, CL = BigWigs:NewBoss("Domina Venomblade", 2289, 2423)
 if not mod then return end
 mod:RegisterEnableMob(164266) -- Domina Venomblade
 mod.engageId = 2385
---mod.respawnTime = 30
+mod.respawnTime = 30
 
 --------------------------------------------------------------------------------
 -- Initialization
