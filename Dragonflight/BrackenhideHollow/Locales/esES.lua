72081f4545718f8816830f2c06033beabcc60cbf
Nick Towle <ntowle@gmail.com>
2022-07-23 19:43:46 -0400

    stubs for Brackenhide Hollow

---
+++ Dragonflight/BrackenhideHollow/Locales/esES.lua
@@ -0,0 +1,5 @@
+local L = BigWigs:NewBossLocale("Brackenhide Hollow Trash", "esES")
+if not L then return end
+if L then
+
+end
---
+++ Dragonflight/BrackenhideHollow/Options/Sounds.lua
@@ -0,0 +1,15 @@
+
+BigWigs:AddSounds("Hackclaw's War Band", {
+})
+
+BigWigs:AddSounds("Treemouth", {
+})
+
+BigWigs:AddSounds("Gutshot", {
+})
+
+BigWigs:AddSounds("Decatriarch Wratheye", {
+})
+
+BigWigs:AddSounds("Brackenhide Hollow Trash", {
+})
