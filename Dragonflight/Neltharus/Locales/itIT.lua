0329ff56ceb9edddd80878ea7b085fd8dd5ed1db
Nick Towle <ntowle@gmail.com>
2022-07-23 23:24:07 -0400

    stubs for Neltharus

---
+++ Dragonflight/Neltharus/Locales/itIT.lua
@@ -0,0 +1,5 @@
+local L = BigWigs:NewBossLocale("Neltharus Trash", "itIT")
+if not L then return end
+if L then
+
+end
---
+++ Dragonflight/Neltharus/Trash.lua
@@ -0,0 +1,40 @@
+--------------------------------------------------------------------------------
+-- Module Declaration
+--
+
+local mod, CL = BigWigs:NewBoss("Neltharus Trash", 2519)
+if not mod then return end
+mod.displayName = CL.trash
+mod:RegisterEnableMob(
+
+)
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:GetLocale()
+if L then
+
+end
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		
+	}, {
+		
+	}
+end
+
+function mod:OnBossEnable()
+	
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
+
