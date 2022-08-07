f3b1715835b6207b09eb43017e4538a3113e6d2e
Nick <ntowle@gmail.com>
2022-08-07 17:20:13 -0400

    Grom'kar Gunner

--- WoD/GrimrailDepot/Trash.lua
+++ WoD/GrimrailDepot/Trash.lua
@@ -7,7 +7,8 @@ if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
 	81236, -- Grimrail Technician
-	164168 -- Grimrail Overseer
+	164168, -- Grimrail Overseer
+	80937, -- Grom'kar Gunner
 )
 
 --------------------------------------------------------------------------------
@@ -18,6 +19,7 @@ local L = mod:GetLocale()
 if L then
 	L.grimrail_technician = "Grimrail Technician"
 	L.grimrail_overseer = "Grimrail Overseer"
+	L.gromkar_gunner = "Grom'kar Gunner"
 end
 
 --------------------------------------------------------------------------------
@@ -31,16 +33,23 @@ function mod:GetOptions()
 		164192, -- 50,000 Volts
 		-- Grimrail Overseer
 		164168, -- Dash
+		-- Grom'kar Gunner
+		166675, -- Shrapnel Blast
 	}, {
 		[163966] = L.grimrail_technician,
 		[164168] = L.grimrail_overseer,
+		[166675] = L.gromkar_gunner,
 	}
 end
 
 function mod:OnBossEnable()
+	-- Grimrail Technician
 	self:Log("SPELL_CAST_START", "Activating", 163966)
 	self:Log("SPELL_CAST_START", "FiftyThousandVolts", 164192)
+	-- Grimrail Overseer
 	self:Log("SPELL_CAST_START", "Dash", 164168)
+	-- Grom'kar Gunner
+	self:Log("SPELL_CAST_START", "ShrapnelBlast", 166675)
 end
 
 --------------------------------------------------------------------------------
@@ -71,4 +80,11 @@ do
 			self:PlaySound(args.spellId, "alert")
 		end
 	end
+end
+
+-- Grom'kar Gunner
+
+function mod:ShrapnelBlast(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "alert")
 end
