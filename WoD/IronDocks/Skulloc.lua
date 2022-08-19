df6f33049520382051c02910560dcedec1638ba9
Nick <ntowle@gmail.com>
2022-08-19 17:49:07 -0400

    cannon barrage over

--- WoD/IronDocks/Skulloc.lua
+++ WoD/IronDocks/Skulloc.lua
@@ -9,6 +9,12 @@ mod:SetEncounterID(1754)
 mod:SetRespawnTime(33)
 
 --------------------------------------------------------------------------------
+-- Locals
+--
+
+local cannonBarrageActive = false
+
+--------------------------------------------------------------------------------
 -- Initialization
 --
 
@@ -28,6 +34,9 @@ end
 function mod:OnBossEnable()
 	-- Skulloc
 	self:Log("SPELL_CAST_START", "GronnSmash", 168227)
+	self:RegisterUnitEvent("UNIT_SPELLCAST_INTERRUPTED", nil, "boss1")
+	self:Log("SWING_DAMAGE", "BossSwing", "*")
+	self:Log("SWING_MISSED", "BossSwing", "*")
 	self:Log("SPELL_CAST_START", "CannonBarrage", 168929)
 	self:Death("SkullocDeath", 83612)
 
@@ -42,6 +51,7 @@ end
 function mod:OnEngage()
 	self:CDBar(168227, 30) -- Gronn Smash
 	self:Bar(168965, 10.9) -- Berserker Leap
+	cannonBarrageActive = false
 end
 
 --------------------------------------------------------------------------------
@@ -53,8 +63,31 @@ end
 function mod:GronnSmash(args)
 	self:Message(args.spellId, "orange")
 	self:PlaySound(args.spellId, "long")
-	self:CDBar(args.spellId, 54.7)
 	self:StopBar(168348) -- Rapid Fire
+	cannonBarrageActive = true
+end
+
+do
+	local function cannonBarrageOver()
+		cannonBarrageActive = false
+		mod:Message(168929, "green", CL.over:format(mod:SpellName(168929)))
+		mod:PlaySound(168929, "info")
+		mod:Bar(168227, 40) -- Gronn Smash
+	end
+
+	function mod:UNIT_SPELLCAST_INTERRUPTED(_, _, _, spellId)
+		if cannonBarrageActive and spellId == 168929 then -- Cannon Barrage
+			cannonBarrageOver()
+		end
+	end
+
+	-- if someone runs into the trigger area between casts there's no UNIT_SPELLCAST_INTERRUPTED event,
+	-- in that case we must rely on swings from Skulloc to detect the phase has ended.
+	function mod:BossSwing(args)
+		if cannonBarrageActive and self:MobId(args.sourceGUID) == 83612 then -- Skulloc
+			cannonBarrageOver()
+		end
+	end
 end
 
 function mod:CannonBarrage(args)
