585d0f7a5a3e522cca339ec34a144503721da0af
Nick <ntowle@gmail.com>
2022-09-11 17:22:08 -0400

    wipe detection

--- Legion/Karazhan/OperaWikket.lua
+++ Legion/Karazhan/OperaWikket.lua
@@ -24,12 +24,12 @@ end
 
 function mod:OnBossEnable()
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+	self:RegisterEvent("ENCOUNTER_END")
+
 	self:Log("SPELL_CAST_START", "DefyGravity", 227447)
 	self:Log("SPELL_CAST_SUCCESS", "WondrousRadiance", 227410)
 	self:Log("SPELL_CAST_START", "MagicMagnificent", 227776)
 	self:Log("SPELL_CAST_SUCCESS", "SummonAssistants", 227477)
-
-	self:RegisterEvent("BOSS_KILL")
 end
 
 function mod:OnEngage()
@@ -42,6 +42,17 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:ENCOUNTER_END(_, engageId, _, _, _, status)
+	if engageId == 1957 then
+		if status == 0 then
+			-- force a respawn timer
+			self:SendMessage("BigWigs_EncounterEnd", self, engageId, self.displayName, self:Difficulty(), 5, status)
+		else
+			self:Win()
+		end
+	end
+end
 
 function mod:DefyGravity(args)
 	self:MessageOld(args.spellId, "yellow", "info")
@@ -61,10 +72,4 @@ end
 function mod:SummonAssistants(args)
 	self:MessageOld(args.spellId, "orange", "alert")
 	self:CDBar(args.spellId, 32.5)
-end
-
-function mod:BOSS_KILL(_, id)
-	if id == 1957 then
-		self:Win()
-	end
 end
