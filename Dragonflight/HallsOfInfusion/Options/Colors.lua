634fa635f2c957969d83ea524923890064b026b8
Nick Towle <ntowle@gmail.com>
2022-09-19 14:14:50 -0400

    Dragonflight/HallsOfInfusion/WatcherIrideus: Add boss module (#752)

--- Dragonflight/HallsOfInfusion/Options/Colors.lua
+++ Dragonflight/HallsOfInfusion/Options/Colors.lua
@@ -1,5 +1,11 @@
 
 BigWigs:AddColors("Watcher Irideus", {
+	[383840] = {"green","yellow"},
+	[384014] = "orange",
+	[384351] = "yellow",
+	[384524] = "purple",
+	[389179] = "red",
+	[389446] = "red",
 })
 
 BigWigs:AddColors("Gulping Goliath", {
--- Dragonflight/HallsOfInfusion/Options/Sounds.lua
+++ Dragonflight/HallsOfInfusion/Options/Sounds.lua
@@ -1,5 +1,11 @@
 
 BigWigs:AddSounds("Watcher Irideus", {
+	[383840] = {"info","long"},
+	[384014] = {"alert","warning"},
+	[384351] = "alert",
+	[384524] = "alarm",
+	[389179] = "alert",
+	[389446] = "alarm",
 })
 
 BigWigs:AddSounds("Gulping Goliath", {
