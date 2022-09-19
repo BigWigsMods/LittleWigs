e2518e0c3fc7ab33b4d57a1524242ed0a4aad6bd
Nick Towle <ntowle@gmail.com>
2022-09-19 16:29:13 -0400

    small refactor to primal chill

--- Dragonflight/RubyLifePools/MelidrussaChillworn.lua
+++ Dragonflight/RubyLifePools/MelidrussaChillworn.lua
@@ -64,15 +64,16 @@ end
 do
 	local prev = 0
 	function mod:PrimalChillApplied(args)
+		local amount = args.amount
 		-- TODO build 45632 this ability is presumably bugged and cannot be dispelled by movement-dispelling effects
-		if args.amount > 2 and (self:Dispeller("magic", nil, args.spellId) --[[or self:Dispeller("movement", nil, args.spellId)]] or self:Me(args.destGUID)) then
+		if amount >= 3 and amount < 5 and (self:Dispeller("magic", nil, args.spellId) --[[or self:Dispeller("movement", nil, args.spellId)]] or self:Me(args.destGUID)) then
 			-- this can sometimes apply rapidly or to more than one person, so add a short throttle.
 			local t = args.time
 			if t - prev > 1 then
 				prev = t
 				-- Stuns at 5 stacks
-				self:StackMessage(args.spellId, "red", args.destName, args.amount, 4)
-				if args.amount == 4 then
+				self:StackMessage(args.spellId, "red", args.destName, amount, 4)
+				if amount == 4 then
 					self:PlaySound(args.spellId, "warning", nil, args.destName)
 				else
 					self:PlaySound(args.spellId, "alert", nil, args.destName)
