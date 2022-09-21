eab6f6f1ec61be6be66679803d5188be607c3096
Nick <ntowle@gmail.com>
2022-09-21 17:19:17 -0400

    leymor

--- Dragonflight/TheAzureVault/Options/Colors.lua
+++ Dragonflight/TheAzureVault/Options/Colors.lua
@@ -1,5 +1,10 @@
 
 BigWigs:AddColors("Leymor", {
+	[374364] = "yellow",
+	[374567] = "orange",
+	[374720] = "red",
+	[374789] = "purple",
+	[386660] = "orange",
 })
 
 BigWigs:AddColors("Azureblade", {
--- Dragonflight/TheAzureVault/Options/Sounds.lua
+++ Dragonflight/TheAzureVault/Options/Sounds.lua
@@ -1,5 +1,10 @@
 
 BigWigs:AddSounds("Leymor", {
+	[374364] = "info",
+	[374567] = "alarm",
+	[374720] = "alert",
+	[374789] = "alert",
+	[386660] = "alarm",
 })
 
 BigWigs:AddSounds("Azureblade", {
