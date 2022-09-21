a2f15d382f35fc81d53a90eff3b6e50e7584d9ca
Nick <ntowle@gmail.com>
2022-09-21 17:49:18 -0400

    azureblade

--- Dragonflight/TheAzureVault/Options/Colors.lua
+++ Dragonflight/TheAzureVault/Options/Colors.lua
@@ -3,6 +3,10 @@ BigWigs:AddColors("Leymor", {
 })
 
 BigWigs:AddColors("Azureblade", {
+	[372222] = "purple",
+	[384132] = {"cyan","green"},
+	[384223] = "yellow",
+	[385578] = "red",
 })
 
 BigWigs:AddColors("Telash Greywing", {
--- Dragonflight/TheAzureVault/Options/Sounds.lua
+++ Dragonflight/TheAzureVault/Options/Sounds.lua
@@ -3,6 +3,10 @@ BigWigs:AddSounds("Leymor", {
 })
 
 BigWigs:AddSounds("Azureblade", {
+	[372222] = "alarm",
+	[384132] = {"info","long"},
+	[384223] = "info",
+	[385578] = "alarm",
 })
 
 BigWigs:AddSounds("Telash Greywing", {
