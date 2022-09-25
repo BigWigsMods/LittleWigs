d4d2311cbc3dc27d8986106bae5b25a3e94225f8
Nick <ntowle@gmail.com>
2022-09-25 04:09:33 -0400

    decatriarch wratheye

--- Dragonflight/BrackenhideHollow/Options/Colors.lua
+++ Dragonflight/BrackenhideHollow/Options/Colors.lua
@@ -9,6 +9,10 @@ BigWigs:AddColors("Gutshot", {
 })
 
 BigWigs:AddColors("Decatriarch Wratheye", {
+	[373917] = "purple",
+	[373944] = "yellow",
+	[373960] = "orange",
+	[376170] = "red",
 })
 
 BigWigs:AddColors("Brackenhide Hollow Trash", {
