5199e4fb56649d60bc0c7b92dcf3642888d283a9
Nick <ntowle@gmail.com>
2022-09-25 16:40:48 -0400

    gorek

--- Dragonflight/Neltharus/ForgemasterGorek.lua
+++ Dragonflight/Neltharus/ForgemasterGorek.lua
@@ -15,19 +15,55 @@ mod:SetRespawnTime(30)
 
 function mod:GetOptions()
 	return {
-
+		374635, -- Might of the Forge
+		{374839, "SAY", "SAY_COUNTDOWN"}, -- Blazing Aegis
+		374969, -- Forgestorm
+		374533, -- Heated Swings
 	}
 end
 
 function mod:OnBossEnable()
-
+	self:Log("SPELL_CAST_SUCCESS", "MightOfTheForge", 374635)
+	self:Log("SPELL_AURA_APPLIED", "BlazingAegis", 374842)
+	self:Log("SPELL_CAST_START", "Forgestorm", 374969) -- TODO on SUCCESS better?
+	self:Log("SPELL_CAST_START", "HeatedSwings", 374533)
 end
 
 function mod:OnEngage()
-
+	-- TODO these timers are all wrong
+	self:Bar(374635, 2.3) -- Might of the Forge
+	self:Bar(374969, 25.5) -- Forgestorm
+	self:Bar(374533, 18.9) -- Heated Swings
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:MightOfTheForge(args)
+	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
+	self:PlaySound(args.spellId, "long")
+	self:Bar(374839, 8.3) -- Blazing Aegis
+	-- TODO might of the forge bar
+end
+
+function mod:BlazingAegis(args)
+	self:TargetMessage(374839, "yellow", args.destName)
+	self:PlaySound(374839, "alert", nil, args.destName)
+	if self:Me(args.destGUID) then
+		self:Say(374839)
+		self:SayCountdown(374839, 3.5)
+	end
+end
+
+function mod:Forgestorm(args)
+	self:Message(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+	-- TODO bar
+end
+
+function mod:HeatedSwings(args)
+	self:Message(args.spellId, "purple")
+	self:PlaySound(args.spellId, "alarm")
+	-- TODO bar
+end
--- Dragonflight/Neltharus/Options/Sounds.lua
+++ Dragonflight/Neltharus/Options/Sounds.lua
@@ -3,6 +3,10 @@ BigWigs:AddSounds("Chargath, Bane of Scales", {
 })
 
 BigWigs:AddSounds("Forgemaster Gorek", {
+	[374533] = "alarm",
+	[374635] = "long",
+	[374839] = "alert",
+	[374969] = "alarm",
 })
 
 BigWigs:AddSounds("Magmatusk", {
