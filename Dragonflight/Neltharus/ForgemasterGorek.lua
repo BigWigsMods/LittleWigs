d1c69a3f37459da95de76283339fa41726bfa432
Nick <ntowle@gmail.com>
2022-09-25 18:29:45 -0400

    timer updates

--- Dragonflight/Neltharus/ForgemasterGorek.lua
+++ Dragonflight/Neltharus/ForgemasterGorek.lua
@@ -43,8 +43,8 @@ end
 function mod:MightOfTheForge(args)
 	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, "long")
+	self:Bar(args.spellId, 30.4)
 	self:Bar(374839, 8.3) -- Blazing Aegis
-	-- TODO might of the forge bar
 end
 
 function mod:BlazingAegis(args)
@@ -66,5 +66,5 @@ end
 function mod:HeatedSwings(args)
 	self:Message(args.spellId, "purple")
 	self:PlaySound(args.spellId, "alarm")
-	-- TODO bar
+	self:Bar(args.spellId, 30.4)
 end
