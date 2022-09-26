577afd835671b8d83a2f7aecdee2b45b822d2d0d
funkydude <funkydude@users.noreply.github.com>
2022-09-26 04:49:08 +0100

    TBC/TheSteamvault: Update option files

--- TBC/TheSteamvault/Options/Colors.lua
+++ TBC/TheSteamvault/Options/Colors.lua
@@ -6,8 +6,8 @@ BigWigs:AddColors("Hydromancer Thespia", {
 })
 
 BigWigs:AddColors("Mekgineer Steamrigger", {
-	[-5999] = {"red","yellow"},
 	[31485] = {"blue","orange"},
+	["mechanics"] = {"red","yellow"},
 })
 
 BigWigs:AddColors("Warlord Kalithresh", {
