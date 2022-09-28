fb455396e9ea10e742c0732ed0fb048d279c53ed
Nick Towle <ntowle@gmail.com>
2022-09-28 13:15:23 -0400

    WoD/GrimrailDepot/Rocketspark: Missile Barrage stops at 20%, VX18-B Target Eliminator alert timing, timer updates (#770)

--- WoD/GrimrailDepot/Rocketspark.lua
+++ WoD/GrimrailDepot/Rocketspark.lua
@@ -16,6 +16,9 @@ mod:SetRespawnTime(8)
 --
 
 local borkaDefeated = false
+local rocketsparkBelow20 = false
+local firstBetterPosition = true
+local firstVX18BTargetEliminator = true
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -39,10 +42,12 @@ end
 
 function mod:OnBossEnable()
 	-- Railmaster Rocketspark
+	self:RegisterUnitEvent("UNIT_HEALTH", nil, "boss1", "boss2")
 	self:Log("SPELL_CAST_START", "X2101AMissileBarrage", 162407)
 	self:Log("SPELL_AURA_APPLIED", "RecoveringApplied", 163947)
 	self:Log("SPELL_CAST_START", "BetterPosition", 162171)
 	self:Log("SPELL_CAST_START", "VX18BTargetEliminator", 162500)
+	self:Log("SPELL_CAST_SUCCESS", "VX18BTargetEliminatorSuccess", 162500)
 	self:Log("SPELL_AURA_APPLIED", "NewPlan", 161091)
 
 	-- Borka the Brute
@@ -53,9 +58,12 @@ end
 
 function mod:OnEngage()
 	borkaDefeated = false
-	self:CDBar(161090, 28) -- Mad Dash
+	rocketsparkBelow20 = false
+	firstBetterPosition = true
+	firstVX18BTargetEliminator = true
+	self:Bar(161090, 28) -- Mad Dash
 	self:CDBar(162617, 7) -- Slam
-	self:Bar(162500, 3) -- VX18-B Target Eliminator
+	self:CDBar(162500, 3) -- VX18-B Target Eliminator
 	self:Bar(162407, 19) -- X21-01A Missile Barrage
 end
 
@@ -65,51 +73,72 @@ end
 
 -- Railmaster Rocketspark
 
+function mod:UNIT_HEALTH(event, unit)
+	-- have to register both boss1 and boss2 because boss1 is whichever is aggro'd first
+	if self:MobId(self:UnitGUID(unit)) == 77803 then -- Railmaster Rocketspark
+		-- Rocketspark stops casting Missile Barrage below 20% HP
+		if self:GetHealth(unit) < 20 then
+			rocketsparkBelow20 = true
+			self:StopBar(162407) -- X21-01A Missile Barrage
+			self:StopBar(161090) -- Mad Dash
+			self:UnregisterUnitEvent(event, unit)
+		end
+	else
+		-- don't need to watch Borka's health
+		self:UnregisterUnitEvent(event, unit)
+	end
+end
+
 function mod:X2101AMissileBarrage(args)
 	self:Message(args.spellId, "orange")
 	self:PlaySound(args.spellId, "long")
-	self:Bar(args.spellId, 40.4)
 end
 
-do
-	local firstBetterPosition = true
-	local firstVX18BTargetEliminator = true
-
-	function mod:RecoveringApplied(args)
-		self:Message(args.spellId, "green")
-		self:PlaySound(args.spellId, "info")
-		self:Bar(args.spellId, 8)
-		self:Bar(162171, 8) -- Better Position
-		self:Bar(162500, 12) -- VX18-B Target Eliminator
-		firstBetterPosition = true
-		firstVX18BTargetEliminator = true
+function mod:RecoveringApplied(args)
+	self:Message(args.spellId, "green")
+	self:PlaySound(args.spellId, "info")
+	self:Bar(args.spellId, 8)
+	self:Bar(162171, 8) -- Better Position
+	self:CDBar(162500, 10.9) -- VX18-B Target Eliminator
+	if not rocketsparkBelow20 then
+		self:CDBar(161090, 35.2) -- Mad Dash
+		self:Bar(162407, 26.2) -- X21-01A Missile Barrage
 	end
+	firstBetterPosition = true
+	firstVX18BTargetEliminator = true
+end
 
-	function mod:BetterPosition(args)
-		self:Message(args.spellId, "yellow")
-		self:PlaySound(args.spellId, "alert")
-		if borkaDefeated then
-			self:Bar(args.spellId, 13.4)
-		elseif firstBetterPosition then
-			self:Bar(args.spellId, 9)
-			firstBetterPosition = false
-		end
+function mod:BetterPosition(args)
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "alert")
+	if borkaDefeated then
+		self:Bar(args.spellId, 13.4)
+	elseif rocketsparkBelow20 then
+		self:Bar(args.spellId, 8.5)
+	elseif firstBetterPosition then
+		self:Bar(args.spellId, 9)
+		firstBetterPosition = false
 	end
+end
 
-	function mod:VX18BTargetEliminator(args)
-		self:Message(args.spellId, "red")
-		self:PlaySound(args.spellId, "alarm")
-		if firstVX18BTargetEliminator then
-			self:Bar(args.spellId, 7.5)
-			firstVX18BTargetEliminator = false
-		end
+function mod:VX18BTargetEliminator(args)
+	self:StopBar(args.spellId)
+	self:CastBar(args.spellId, 3)
+end
+
+function mod:VX18BTargetEliminatorSuccess(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "alarm")
+	if rocketsparkBelow20 or firstVX18BTargetEliminator then
+		self:CDBar(args.spellId, 5.3)
+		firstVX18BTargetEliminator = false
 	end
 end
 
 function mod:NewPlan(args)
 	borkaDefeated = true
 	self:StopBar(161090) -- Mad Dash
-	self:StopBar(CL.cast:format(args.spellName)) -- Casting: Mad Dash
+	self:StopBar(CL.cast:format(self:SpellName(161090))) -- Casting: Mad Dash
 	self:StopBar(162617) -- Slam
 	self:StopBar(162407) -- X21-01A Missile Barrage
 	self:StopBar(162500) -- VX18-B Target Eliminator
@@ -122,8 +151,7 @@ end
 function mod:MadDash(args)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "warning")
-	self:CDBar(args.spellId, 41.2)
-	self:Bar(args.spellId, 3, CL.cast:format(args.spellName))
+	self:CastBar(args.spellId, 2.5)
 end
 
 function mod:Slam(args)
