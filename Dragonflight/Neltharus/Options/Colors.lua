a43e6a04176275cef60b7c76a68bfc07d658023f
Nick <ntowle@gmail.com>
2022-09-29 22:04:25 -0400

    options

--- Dragonflight/Neltharus/Options/Colors.lua
+++ Dragonflight/Neltharus/Options/Colors.lua
@@ -8,7 +8,7 @@ BigWigs:AddColors("Forgemaster Gorek", {
 BigWigs:AddColors("Magmatusk", {
 	[374365] = "orange",
 	[375068] = "orange",
-	[375251] = {"blue","yellow"},
+	[375251] = "yellow",
 	[375439] = "red",
 	[375890] = "red",
 })
