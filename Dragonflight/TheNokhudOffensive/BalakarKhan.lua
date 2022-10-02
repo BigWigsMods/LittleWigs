1d14581f60f6fcbd42a58465f278c318c651289f
Nick <ntowle@gmail.com>
2022-10-02 11:02:45 -0400

    options

--- Dragonflight/TheNokhudOffensive/BalakarKhan.lua
+++ Dragonflight/TheNokhudOffensive/BalakarKhan.lua
@@ -15,7 +15,21 @@ mod:SetRespawnTime(30)
 
 function mod:GetOptions()
 	return {
-
+		-- Stage One: Balakar's Might
+		376644, -- Iron Spear
+		-- TODO Upheaval
+		375937, -- Rending Strike
+		-- Intermission: Stormwinds
+		376730, -- Stormwinds
+		-- TODO Lightning?
+		-- Stage Two: The Storm Unleashed
+		376865, -- Static Spear
+		376892, -- Crackling Upheaval
+		376827, -- Conductive Strike
+	}, {
+		[376644] = -25185, -- Stage One: Balakar's Might
+		[376730] = -25192, -- Intermission: Stormwinds
+		[376865] = -25187, -- Stage Two: The Storm Unleashed
 	}
 end
 
