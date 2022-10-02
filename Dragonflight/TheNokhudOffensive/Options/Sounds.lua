e2b974305a86b80420f309c6c8f4e4439e984823
Nick <ntowle@gmail.com>
2022-10-02 13:24:05 -0400

    teera and maruuk

--- Dragonflight/TheNokhudOffensive/Options/Sounds.lua
+++ Dragonflight/TheNokhudOffensive/Options/Sounds.lua
@@ -6,6 +6,14 @@ BigWigs:AddSounds("The Raging Tempest", {
 })
 
 BigWigs:AddSounds("Teera And Maruuk", {
+	[382670] = "alarm",
+	[382836] = "alert",
+	[384808] = "alert",
+	[385339] = "long",
+	[385434] = "info",
+	[386063] = "alarm",
+	[386547] = "alert",
+	[392198] = {"info","warning"},
 })
 
 BigWigs:AddSounds("Balakar Khan", {
--- Dragonflight/TheNokhudOffensive/TeeraAndMaruuk.lua
+++ Dragonflight/TheNokhudOffensive/TeeraAndMaruuk.lua
@@ -18,10 +18,12 @@ mod:SetRespawnTime(30)
 
 function mod:GetOptions()
 	return {
+		-- General
 		392198, -- Ancestral Bond
 		-- Teera
 		382670, -- Gale Arrow
-		384808, -- Guardian Wind (or Repel 386547?)
+		386547, -- Repel
+		384808, -- Guardian Wind
 		385434, -- Spirit Leap
 		-- Maruuk
 		385339, -- Earthsplitter
@@ -35,14 +37,102 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
+	-- General
+	self:Log("SPELL_AURA_APPLIED", "AncestralBondApplied", 392198)
+	self:Log("SPELL_AURA_REMOVED", "AncestralBondRemoved", 392198)
 
+	-- Teera
+	self:Log("SPELL_CAST_START", "GaleArrow", 382670)
+	self:Log("SPELL_CAST_START", "Repel", 386547)
+	self:Log("SPELL_AURA_APPLIED", "GuardianWind", 384808)
+	self:Log("SPELL_CAST_START", "SpiritLeap", 385434)
+
+	-- Maruuk
+	self:Log("SPELL_CAST_START", "Earthsplitter", 385339)
+	self:Log("SPELL_CAST_START", "FrightfulRoar", 386063)
+	self:Log("SPELL_CAST_START", "Brutalize", 382836)
 end
 
 function mod:OnEngage()
-
+	self:Bar(382836, 8.7) -- Brutalize
+	self:Bar(385434, 11.1) -- Spirit Leap
+	self:Bar(386063, 12.2) -- Frightful Roar
+	self:Bar(382670, 14.8) -- Gale Arrow
+	self:Bar(386547, 20.4) -- Repel
+	self:Bar(385339, 21.6) -- Earthsplitter
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+-- General
+
+do
+	local prev = 0
+	function mod:AncestralBondApplied(args)
+		local t = args.time
+		if t-prev > 1 then
+			prev = t
+			self:Message(args.spellId, "red", CL.onboss:format(args.spellName))
+			self:PlaySound(args.spellId, "warning")
+		end
+	end
+end
+
+do
+	local prev = 0
+	function mod:AncestralBondRemoved(args)
+		local t = args.time
+		if t-prev > 1 then
+			prev = t
+			self:Message(args.spellId, "green", CL.removed:format(args.spellName))
+			self:PlaySound(args.spellId, "info")
+		end
+	end
+end
+
+-- Teera
+
+function mod:GaleArrow(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "alarm")
+	-- TODO CD, cast at 100 energy, ~20 seconds
+end
+
+function mod:Repel(args)
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "alert")
+	-- TODO CD
+end
+
+function mod:GuardianWind(args)
+	self:Message(args.spellId, "orange", CL.casting:format(args.spellName))
+	self:PlaySound(args.spellId, "alert")
+end
+
+function mod:SpiritLeap(args)
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "info")
+	self:Bar(args.spellId, 27.5)
+end
+
+-- Maruuk
+
+function mod:Earthsplitter(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "long")
+	-- TODO CD, cast at 100 energy, ~20 seconds
+end
+
+function mod:FrightfulRoar(args)
+	self:Message(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+	-- TODO CD
+end
+
+function mod:Brutalize(args)
+	self:Message(args.spellId, "purple")
+	self:PlaySound(args.spellId, "alert")
+	self:Bar(args.spellId, 30)
+end
