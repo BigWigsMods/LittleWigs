be1ebd68389b4baf31380e9b46140e731472bf63
Nick Towle <ntowle@gmail.com>
2022-10-02 22:57:35 -0400

    Dragonflight/TheNokhudOffensive/TeeraAndMaruuk: Add boss module (#788)

--- Dragonflight/TheNokhudOffensive/Options/Colors.lua
+++ Dragonflight/TheNokhudOffensive/Options/Colors.lua
@@ -11,6 +11,14 @@ BigWigs:AddColors("The Raging Tempest", {
 })
 
 BigWigs:AddColors("Teera And Maruuk", {
+	[382670] = "red",
+	[382836] = "purple",
+	[384808] = "orange",
+	[385339] = "red",
+	[385434] = "yellow",
+	[386063] = "orange",
+	[386547] = "yellow",
+	[392198] = {"green","red"},
 })
 
 BigWigs:AddColors("Balakar Khan", {
