4daba555b4d1a78d866eeb087da5bbb6194af9ae
Nick <ntowle@gmail.com>
2022-10-04 15:03:00 -0400

    doragosa

--- Dragonflight/AlgetharAcademy/Options/Colors.lua
+++ Dragonflight/AlgetharAcademy/Options/Colors.lua
@@ -9,6 +9,11 @@ BigWigs:AddColors("Crawth", {
 })
 
 BigWigs:AddColors("Echo of Doragosa", {
+	[374352] = {"blue","red"},
+	[374361] = "orange",
+	[388822] = "yellow",
+	[388901] = "blue",
+	[389011] = "blue",
 })
 
 BigWigs:AddColors("Algeth'ar Academy Trash", {
--- Dragonflight/AlgetharAcademy/Options/Sounds.lua
+++ Dragonflight/AlgetharAcademy/Options/Sounds.lua
@@ -9,6 +9,11 @@ BigWigs:AddSounds("Crawth", {
 })
 
 BigWigs:AddSounds("Echo of Doragosa", {
+	[374352] = "alert",
+	[374361] = "alarm",
+	[388822] = "alarm",
+	[388901] = "underyou",
+	[389011] = "alert",
 })
 
 BigWigs:AddSounds("Algeth'ar Academy Trash", {
