164344b75b1404f5b41c40c6f4a2c7c343ccd021
Nick <ntowle@gmail.com>
2022-10-09 17:06:57 -0400

    bromach

--- Dragonflight/UldamanLegacyOfTyr/Options/Sounds.lua
+++ Dragonflight/UldamanLegacyOfTyr/Options/Sounds.lua
@@ -3,6 +3,12 @@ BigWigs:AddSounds("The Lost Dwarves", {
 })
 
 BigWigs:AddSounds("Bromach", {
+	[369605] = "long",
+	[369660] = "info",
+	[369675] = "warning",
+	[369700] = "alert",
+	[369703] = "alarm",
+	[369754] = "alert",
 })
 
 BigWigs:AddSounds("Sentinel Talondras", {
