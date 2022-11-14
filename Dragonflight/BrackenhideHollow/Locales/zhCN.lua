48a4a313f89c3edd930151f2b5a5540e8c42b4aa
枫聖 <2303601+nanjuekaien1@users.noreply.github.com>
2022-11-14 15:02:15 +0800

    Update zhCN.lua

--- Dragonflight/BrackenhideHollow/Locales/zhCN.lua
+++ Dragonflight/BrackenhideHollow/Locales/zhCN.lua
@@ -1,6 +1,6 @@
 local L = BigWigs:NewBossLocale("Brackenhide Hollow Trash", "zhCN")
 if not L then return end
 if L then
-	--L.bracken_warscourge = "蕨斑战灾者"
-	--L.stinkbreath = "臭息"
+	L.bracken_warscourge = "蕨斑战灾者"
+	L.stinkbreath = "臭息"
 end
