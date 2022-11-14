2775b476f25e1ec082b00f7b2ef237e0beabc4d3
枫聖 <2303601+nanjuekaien1@users.noreply.github.com>
2022-11-14 17:48:38 +0800

    Update zhCN.lua

--- Dragonflight/AlgetharAcademy/Locales/zhCN.lua
+++ Dragonflight/AlgetharAcademy/Locales/zhCN.lua
@@ -2,12 +2,12 @@ local L = BigWigs:NewBossLocale("Algeth'ar Academy Trash", "zhCN")
 if not L then return end
 if L then
 	L.recruiter_autotalk = "自动对话"
-	--L.recruiter_autotalk_desc = "Instantly pledge to the Dragonflight Recruiters for a buff."
+	L.recruiter_autotalk_desc = "立即找巨龙招募人员获取增益"
 	L.critical_strike = "+5% 爆击"
 	L.haste = "+5% 急速"
-	L.mastery = "+精通"
+	L.mastery = "+97精通"
 	L.versatility = "+5% 全能"
-	--L.healing_taken = "+10% Healing Taken"
+	L.healing_taken = "+10% 治疗效果"
 
 	L.corrupted_manafiend = "被腐化的嗜魔者"
 	L.spellbound_scepter = "缚法节杖"
