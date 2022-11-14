cb905c503f3cdcb89146d8ec0dd6f26497664493
枫聖 <2303601+nanjuekaien1@users.noreply.github.com>
2022-11-14 17:29:05 +0800

    Update zhCN.lua

--- Dragonflight/TheNokhudOffensive/Locales/zhCN.lua
+++ Dragonflight/TheNokhudOffensive/Locales/zhCN.lua
@@ -6,11 +6,11 @@ end
 
 L = BigWigs:NewBossLocale("Granyth", "zhCN")
 if L then
-	--L.lance_ready = "Lance Ready"
+	L.lance_ready = "屠龙者之矛就绪"
 end
 
 L = BigWigs:NewBossLocale("The Raging Tempest", "zhCN")
 if L then
-	--L.on_you = "On you"
-	--L.on_boss = "On the boss"
+	L.on_you = "在你身上"
+	L.on_boss = "在首领身上"
 end
