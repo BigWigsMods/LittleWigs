9fabb43da88a3373a8b52fffff70ba4e2b510f74
枫聖 <2303601+nanjuekaien1@users.noreply.github.com>
2022-11-15 22:00:22 +0800

    Update zhCN.lua

--- Dragonflight/UldamanLegacyOfTyr/Locales/zhCN.lua
+++ Dragonflight/UldamanLegacyOfTyr/Locales/zhCN.lua
@@ -1,9 +1,9 @@
 local L = BigWigs:NewBossLocale("Uldaman: Legacy of Tyr Trash", "zhCN")
 if not L then return end
 if L then
-	--L.hulking_berserker = "Hulking Berserker"
-	--L.vicious_basilisk = "Vicious Basilisk"
-	--L.earthen_warder = "Earthen Warder"
-	--L.cavern_seeker = "Cavern Seeker"
-	--L.runic_protector = "Runic Protector"
+	L.hulking_berserker = "魁梧的狂战士"
+	L.vicious_basilisk = "恶毒的蜥蜴"
+	L.earthen_warder = "土灵看守"
+	L.cavern_seeker = "洞穴探寻者"
+	L.runic_protector = "符文保护者"
 end
