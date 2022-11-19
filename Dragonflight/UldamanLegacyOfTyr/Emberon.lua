9e17b6171941efd203919b77207dcb4b5625ba7c
Nick Towle <ntowle@gmail.com>
2022-11-19 14:44:52 -0500

    Dragonflight/UldamanLegacyOfTyr/Emberon: Purging Flames updates

--- Dragonflight/UldamanLegacyOfTyr/Emberon.lua
+++ Dragonflight/UldamanLegacyOfTyr/Emberon.lua
@@ -10,12 +10,6 @@ mod:SetRespawnTime(30)
 mod:SetStage(1)
 
 --------------------------------------------------------------------------------
--- Locals
---
-
-local purgingFlameCount = 0
-
---------------------------------------------------------------------------------
 -- Initialization
 --
 
@@ -37,10 +31,11 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
-	purgingFlameCount = 0
 	self:SetStage(1)
 	self:CDBar(369061, 4.6) -- Searing Clap
 	self:CDBar(369110, 13.1) -- Unstable Embers
+	-- 35s energy gain + ~5s delay
+	self:CDBar(368990, 40.2) -- Purging Flames
 end
 
 --------------------------------------------------------------------------------
@@ -48,11 +43,8 @@ end
 --
 
 function mod:PurgingFlames(args)
-	-- cast at 70% and then 30% health
-	local percent = purgingFlameCount == 0 and 70 or 30
-	purgingFlameCount = purgingFlameCount + 1
 	self:SetStage(2)
-	self:Message(args.spellId, "cyan", CL.percent:format(percent, args.spellName))
+	self:Message(args.spellId, "cyan")
 	self:PlaySound(args.spellId, "long")
 	self:StopBar(369110) -- Unstable Embers
 	self:StopBar(369061) -- Searing Clap
@@ -69,9 +61,11 @@ do
 		local addsNeeded = self:Normal() and 3 or 4
 		addsKilled = addsKilled + 1
 		if addsKilled == addsNeeded then
+			self:SetStage(1)
 			self:Message(368990, "cyan", CL.over:format(self:SpellName(368990))) -- Purging Flames Over
 			self:PlaySound(368990, "long")
-			self:SetStage(1)
+			-- 35s energy gain + ~5s delay
+			self:CDBar(368990, 40.2) -- Purging Flames
 		else
 			self:Message(args.spellId, "green", CL.add_killed:format(addsKilled, addsNeeded))
 			self:PlaySound(args.spellId, "info")
