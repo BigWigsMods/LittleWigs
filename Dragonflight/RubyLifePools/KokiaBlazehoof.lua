0486e934d64791ff17d5a49c25928a32b1f80442
Nick Towle <ntowle@gmail.com>
2022-11-20 13:23:45 -0500

    Dragonflight/RubyLifePools/KokiaBlazehoof: Searing Blows update, timer update

--- Dragonflight/RubyLifePools/KokiaBlazehoof.lua
+++ Dragonflight/RubyLifePools/KokiaBlazehoof.lua
@@ -14,26 +14,32 @@ mod:SetRespawnTime(30)
 
 function mod:GetOptions()
 	return {
+		-- Kokia Blazehoof
 		372863, -- Ritual of Blazebinding
 		372107, -- Molten Boulder
 		{372858, "TANK_HEALER"}, -- Searing Blows
-		373087, -- Burnout
+		-- Blazebound Firestorm
 		373017, -- Roaring Blaze
+		373087, -- Burnout
 	}, {
-		[373087] = -24945, -- Blazebound Firestorm
+		[372863] = self.displayName,
+		[373017] = -24945, -- Blazebound Firestorm
 	}
 end
 
 function mod:OnBossEnable()
+	-- Kokia Blazehoof
 	self:Log("SPELL_CAST_START", "RitualOfBlazebinding", 372863)
 	self:Log("SPELL_CAST_START", "MoltenBoulder", 372107)
-	self:Log("SPELL_CAST_START", "SearingBlows", 372858)
-	self:Log("SPELL_CAST_START", "Burnout", 373087)
+	self:Log("SPELL_CAST_SUCCESS", "SearingBlows", 372858)
+
+	-- Blazebound Firestorm
 	self:Log("SPELL_CAST_START", "RoaringBlaze", 373017)
+	self:Log("SPELL_CAST_START", "Burnout", 373087)
 end
 
 function mod:OnEngage()
-	self:Bar(372863, 7.6) -- Ritual of Blazebinding
+	self:Bar(372863, 7.3) -- Ritual of Blazebinding
 	self:Bar(372107, 14.5) -- Molten Boulder
 	self:Bar(372858, 21.6) -- Searing Blows
 end
@@ -42,10 +48,12 @@ end
 -- Event Handlers
 --
 
+-- Kokia Blazehoof
+
 function mod:RitualOfBlazebinding(args)
 	self:Message(args.spellId, "orange")
 	self:PlaySound(args.spellId, "long")
-	self:Bar(args.spellId, 33.6)
+	self:CDBar(args.spellId, 33.9)
 end
 
 function mod:MoltenBoulder(args)
@@ -57,17 +65,19 @@ end
 function mod:SearingBlows(args)
 	self:Message(args.spellId, "purple")
 	self:PlaySound(args.spellId, "alert")
-	self:CDBar(args.spellId, 32.9)
+	self:Bar(args.spellId, 32.7)
+end
+
+-- Blazebound Firestorm
+
+function mod:RoaringBlaze(args)
+	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
+	if self:Interrupter() then
+		self:PlaySound(args.spellId, "warning")
+	end
 end
 
 function mod:Burnout(args)
 	self:Message(args.spellId, "orange")
 	self:PlaySound(args.spellId, "alarm")
-end
-
-function mod:RoaringBlaze(args)
-	self:Message(args.spellId, "red")
-	if self:Interrupter() then
-		self:PlaySound(args.spellId, "warning")
-	end
 end
