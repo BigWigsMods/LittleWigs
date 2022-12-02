26d9b32eb58c15bbee83ea626f27c9278e1b64bb
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2022-12-02 02:44:31 +0100

    Update deDE.lua

--- Dragonflight/RubyLifePools/Locales/deDE.lua
+++ Dragonflight/RubyLifePools/Locales/deDE.lua
@@ -1,11 +1,11 @@
 local L = BigWigs:NewBossLocale("Ruby Life Pools Trash", "deDE")
 if not L then return end
 if L then
-	--L.primal_juggernaut = "Primal Juggernaut"
-	--L.flashfrost_chillweaver = "Flashfrost Chillweaver"
-	--L.defier_draghar = "Defier Draghar"
-	--L.blazebound_destroyer = "Blazebound Destroyer"
-	--L.primalist_shockcaster = "Primalist Shockcaster"
+	L.primal_juggernaut = "Urzeitlicher Koloss"
+	L.flashfrost_chillweaver = "Blitzfrostkühlweber"
+	L.defier_draghar = "Trotzer Draghar"
+	L.blazebound_destroyer = "Lohengebundener Zerstörer"
+	L.primalist_shockcaster = "Schockwirker der Primalisten"
 	--L.thunderhead = "Thunderhead"
 	--L.flamegullet = "Flamegullet"
 end
