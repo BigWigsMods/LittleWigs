4b9e3475cb01125f603e84b693fd345d434c73e6
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2022-12-02 02:16:54 +0100

    Update deDE.lua

--- Dragonflight/BrackenhideHollow/Locales/deDE.lua
+++ Dragonflight/BrackenhideHollow/Locales/deDE.lua
@@ -1,6 +1,6 @@
 local L = BigWigs:NewBossLocale("Brackenhide Hollow Trash", "deDE")
 if not L then return end
 if L then
-	--L.bracken_warscourge = "Bracken Warscourge"
-	--L.stinkbreath = "Stinkbreath"
+	L.bracken_warscourge = "Brackenkriegsgeißel"
+	L.stinkbreath = "Stinkatem"
 end
