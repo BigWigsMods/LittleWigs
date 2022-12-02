38c1bcdd85cde3e247142c0b274b9a412e4c48a5
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2022-12-02 02:06:36 +0100

    Update deDE.lua

--- MoP/TempleOfTheJadeSerpent/Locales/deDE.lua
+++ MoP/TempleOfTheJadeSerpent/Locales/deDE.lua
@@ -10,14 +10,14 @@ end
 
 L = BigWigs:NewBossLocale("Temple of the Jade Serpent Trash", "deDE")
 if L then
-	--L.corrupt_living_water = "Corrupt Living Water"
+	L.corrupt_living_water = "Verderbtes lebendiges Wasser"
 	--L.fallen_waterspeaker = "Fallen Waterspeaker"
-	--L.haunting_sha = "Haunting Sha"
-	--L.the_talking_fish = "The Talking Fish"
-	--L.the_songbird_queen = "The Songbird Queen"
-	--L.the_crybaby_hozen = "The Crybaby Hozen"
-	--L.the_nodding_tiger = "The Nodding Tiger"
-	--L.the_golden_beetle = "The Golden Beetle"
+	L.haunting_sha = "Geisterhaftes Sha"
+	L.the_talking_fish = "Der sprechende Fisch"
+	L.the_songbird_queen = "Die Singvogelkönigin"
+	L.the_crybaby_hozen = "Der Heulsusen-Ho-zen"
+	L.the_nodding_tiger = "Der Nickende Tiger"
+	L.the_golden_beetle = "Der Goldkäfer"
 	--L.sha_touched_guardian = "Sha-Touched Guardian"
 	--L.depraved_mistweaver = "Depraved Mistweaver"
 	--L.shambling_infester = "Shambling Infester"
