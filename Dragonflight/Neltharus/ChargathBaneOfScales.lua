65ac43f8bbcacd0565c7ea84baca65f7e1a5335c
Nick Towle <ntowle@gmail.com>
2022-12-16 19:31:39 -0500

    Dragonflight/Neltharus/ChargathBaneOfScales: Fiery Focus updates

--- Dragonflight/Neltharus/ChargathBaneOfScales.lua
+++ Dragonflight/Neltharus/ChargathBaneOfScales.lua
@@ -12,7 +12,7 @@ mod:SetRespawnTime(30)
 -- Locals
 --
 
-local recalculateBladeLock = false
+local recalculateFieryFocus = false
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -22,7 +22,7 @@ function mod:GetOptions()
 	return {
 		373424, -- Grounding Spear
 		388523, -- Fetter
-		375056, -- Blade Lock
+		375056, -- Fiery Focus
 		373733, -- Dragon Strike
 		373742, -- Magma Wave
 	}
@@ -34,8 +34,8 @@ function mod:OnBossEnable()
 	-- 3 different Fetter debuffs: 388523=long, 374655=short, 374638=player
 	self:Log("SPELL_AURA_APPLIED", "FetterApplied", 388523, 374655)
 	self:Log("SPELL_AURA_REMOVED", "FetterRemoved", 388523, 374655)
-	self:Log("SPELL_CAST_START", "BladeLock", 375056)
-	self:Log("SPELL_CAST_SUCCESS", "BladeLockOver", 375056)
+	self:Log("SPELL_CAST_START", "FieryFocus", 375056)
+	self:Log("SPELL_CAST_SUCCESS", "FieryFocusOver", 375056)
 	self:Log("SPELL_CAST_START", "DragonStrike", 373733)
 	self:Log("SPELL_CAST_START", "MagmaWave", 373742)
 end
@@ -44,7 +44,7 @@ function mod:OnEngage()
 	self:CDBar(373733, 3.4) -- Dragon Strike
 	self:CDBar(373424, 10.7) -- Grounding Spear
 	self:CDBar(373742, 15.5) -- Magma Wave
-	self:CDBar(375056, 29.3) -- Blade Lock
+	self:CDBar(375056, 29.3) -- Fiery Focus
 end
 
 --------------------------------------------------------------------------------
@@ -52,12 +52,12 @@ end
 --
 
 function mod:UNIT_POWER_UPDATE(_, unit)
-	if recalculateBladeLock then
-		-- ~29 seconds between Blade Lock casts, cast at max Energy
-		local nextBladeLock = ceil(29 * (1 - UnitPower(unit) / 100))
-		if nextBladeLock > 0 then
-			recalculateBladeLock = false
-			self:Bar(375056, {nextBladeLock + .2, 29.2}) -- Blade Lock, ~.2s delay at max energy
+	if recalculateFieryFocus then
+		-- ~29 seconds between Fiery Focus casts, cast at max Energy
+		local nextFieryFocus = ceil(29 * (1 - UnitPower(unit) / 100))
+		if nextFieryFocus > 0 then
+			recalculateFieryFocus = false
+			self:Bar(375056, {nextFieryFocus + .2, 29.2}) -- Fiery Focus, ~.2s delay at max energy
 		end
 	end
 end
@@ -73,29 +73,29 @@ end
 -- Normal/Heroic: 1 stack of Fetter always stuns
 -- Mythic: 1 or 2 hits of Fetter just slows, 3 hits stuns
 function mod:FetterApplied(args)
-	recalculateBladeLock = true
+	recalculateFieryFocus = true
 	if args.spellId == 388523 then -- 14s long Fetter on boss
 		self:Message(388523, "green", CL.onboss:format(args.spellName))
 		self:PlaySound(388523, "info")
 		self:Bar(388523, 14, CL.onboss:format(args.spellName))
 	else -- 5s Short Fetter on boss
 		self:Bar(388523, 5, CL.onboss:format(args.spellName))
-		self:PauseBar(375056) -- Blade Lock, Chargath doesn't gain energy during Fetter
+		self:PauseBar(375056) -- Fiery Focus, Chargath doesn't gain energy during Fetter
 	end
 end
 
 function mod:FetterRemoved(args)
-	recalculateBladeLock = true
+	recalculateFieryFocus = true
 end
 
-function mod:BladeLock(args)
+function mod:FieryFocus(args)
 	self:StopBar(args.spellName)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "long")
 end
 
-function mod:BladeLockOver(args)
-	recalculateBladeLock = true
+function mod:FieryFocusOver(args)
+	recalculateFieryFocus = true
 end
 
 do
