3a079130f3a66b6fa338d33c45d33bb68c87a4f8
Nick Towle <ntowle@gmail.com>
2022-12-21 18:04:10 -0500

    Dragonflight/TheNokhudOffensive/Granyth: Auto-mark Saboteur, timer updates

--- Dragonflight/TheNokhudOffensive/Granyth.lua
+++ Dragonflight/TheNokhudOffensive/Granyth.lua
@@ -24,6 +24,7 @@ end
 -- Initialization
 --
 
+local saboteurMarker = mod:AddMarkerOption(true, "npc", 8, -25612, 8) -- Nokhud Saboteur
 function mod:GetOptions()
 	return {
 		-- Granyth
@@ -34,11 +35,12 @@ function mod:GetOptions()
 		-- Dragonkiller Lance
 		386530, -- Dragonkiller Lance
 		-- Nokhud Saboteur
+		saboteurMarker,
 		386490, -- Dismantle
 	}, {
 		[388283] = -25455, -- Granyth
 		[386530] = -25614, -- Dragonkiller Lance
-		[386490] = -25612, -- Nokhud Saboteur
+		[saboteurMarker] = -25612, -- Nokhud Saboteur
 	}
 end
 
@@ -47,7 +49,7 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "Eruption", 388283)
 	self:Log("SPELL_CAST_START", "ShardsOfStone", 388817)
 	self:Log("SPELL_CAST_START", "TectonicStomp", 385916)
-	self:Log("SPELL_CAST_SUCCESS", "SummonSaboteur", 386320, 386747, 386748)
+	self:Log("SPELL_SUMMON", "SummonSaboteur", 386320, 386747, 386748)
 
 	-- Dragonkiller Lance
 	self:Log("SPELL_CAST_START", "Reload", 386921)
@@ -55,15 +57,16 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "Lanced", 387155)
 	
 	-- Nokhud Saboteur (Mythic-only)
-	self:Log("SPELL_AURA_APPLIED", "Dismantle", 386490)
+	self:Log("SPELL_CAST_START", "Dismantle", 386490)
+	self:Log("SPELL_AURA_APPLIED", "DismantleApplied", 386490)
 end
 
 function mod:OnEngage()
-	self:Bar(388283, 28.8) -- Eruption
-	self:Bar(388817, 10.6) -- Shards of Stone
+	self:CDBar(388283, 28.8) -- Eruption
+	self:CDBar(388817, 10.6) -- Shards of Stone
 	self:CDBar(385916, 15.5) -- Tectonic Stomp
 	if self:Mythic() then
-		self:Bar(386320, 5.5) -- Summon Saboteur
+		self:CDBar(386320, 5.1) -- Summon Saboteur
 	end
 end
 
@@ -74,6 +77,7 @@ end
 -- Granyth
 
 function mod:Eruption(args)
+	self:StopBar(388817) -- Shards of Stone
 	self:StopBar(args.spellId)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "long")
@@ -83,27 +87,43 @@ end
 function mod:ShardsOfStone(args)
 	self:Message(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "alert")
-	self:CDBar(args.spellId, 13.4)
+	self:CDBar(args.spellId, 13.3)
 end
 
 function mod:TectonicStomp(args)
 	self:Message(args.spellId, "orange")
 	self:PlaySound(args.spellId, "alarm")
-	self:CDBar(args.spellId, 19.4)
 end
 
-function mod:SummonSaboteur(args)
-	local direction -- 386320 = W Lance, 386747 = NE Lance, 386748 = SE Lance
-	if args.spellId == 386320 then
-		direction = L.west
-	elseif args.spellId == 386747 then
-		direction = L.northeast
-	else -- 386748
-		direction = L.southeast
+do
+	local saboteurGUID = nil
+
+	function mod:SummonSaboteur(args)
+		local direction -- 386320 = W Lance, 386747 = NE Lance, 386748 = SE Lance
+		if args.spellId == 386320 then
+			direction = L.west
+		elseif args.spellId == 386747 then
+			direction = L.northeast
+		else -- 386748
+			direction = L.southeast
+		end
+		self:Message(386320, "red", CL.other:format(args.spellName, direction))
+		self:PlaySound(386320, "alert")
+
+		-- register events to auto-mark saboteur
+		if self:GetOption(saboteurMarker) then
+			saboteurGUID = args.destGUID
+			self:RegisterTargetEvents("MarkSaboteur")
+		end
 	end
-	self:Message(386320, "red", CL.other:format(args.spellName, direction))
-	self:PlaySound(386320, "alert")
-	self:CDBar(386320, 29.1)
+
+	function mod:MarkSaboteur(_, unit, guid)
+		if saboteurGUID == guid then
+			saboteurGUID = nil
+			self:CustomIcon(saboteurMarker, unit, 8)
+			self:UnregisterTargetEvents()
+		end
+	end
 end
 
 -- Dragonkiller Lance
@@ -121,6 +141,9 @@ function mod:Lanced(args)
 	self:Message(386530, "green", args.spellName) -- Dragonkiller Lance
 	self:PlaySound(386530, "info") -- Dragonkiller Lance
 	self:StopBar(CL.cast:format(self:SpellName(388283))) -- Eruption
+	self:CDBar(386320, 5.3) -- Summon Saboteur
+	self:CDBar(388817, 15.4) -- Shards of Stone
+	self:CDBar(385916, 20.1) -- Tectonic Stomp
 	self:CDBar(388283, 33.1) -- Eruption 5s stun, 27s energy gain, ~1s delay
 end
 
@@ -129,4 +152,8 @@ end
 function mod:Dismantle(args)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "warning")
+end
+
+function mod:DismantleApplied(args)
+	self:StopBar(386921) -- Reload
 end
