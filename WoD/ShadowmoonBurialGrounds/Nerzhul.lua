e4820e1f19516627c1f76be04a6d0bf0d070b8a2
Nick Towle <ntowle@gmail.com>
2022-12-27 13:38:11 -0500

    WoD/ShadowmoonBurialGrounds/Nerzhul: Play Malevolence sound for all roles

--- WoD/ShadowmoonBurialGrounds/Nerzhul.lua
+++ WoD/ShadowmoonBurialGrounds/Nerzhul.lua
@@ -1,4 +1,3 @@
-
 --------------------------------------------------------------------------------
 -- Module Declaration
 --
@@ -6,8 +5,8 @@
 local mod, CL = BigWigs:NewBoss("Ner'zhul", 1176, 1160)
 if not mod then return end
 mod:RegisterEnableMob(76407)
-mod.engageId = 1682
-mod.respawnTime = 33
+mod:SetEncounterID(1682)
+mod:SetRespawnTime(33)
 
 --------------------------------------------------------------------------------
 -- Locals
@@ -44,22 +43,23 @@ end
 --
 
 function mod:Malevolence(args)
-	self:MessageOld(args.spellId, "yellow", self:Tank() and "alarm")
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "alarm")
 	self:CDBar(args.spellId, 9) -- 8.4-10.8, remove?
 end
 
-do
-	-- BETA: 36.8, 13.7, 35.8, 17.2, 37, 14.8, 35.8
-	function mod:OmenOfDeath(args)
-		self:MessageOld(args.spellId, "red", "alert")
-		self:CDBar(args.spellId, omenCounter % 2 == 0 and 14 or 35.8)
-		omenCounter = omenCounter + 1
-	end
+-- BETA: 36.8, 13.7, 35.8, 17.2, 37, 14.8, 35.8
+function mod:OmenOfDeath(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, omenCounter % 2 == 0 and 14 or 35.8)
+	omenCounter = omenCounter + 1
 end
 
 function mod:RitualOfBones(_, _, _, spellId)
 	if spellId == 154671 then -- Ritual of Bones
-		self:MessageOld(-9680, "orange", "warning")
+		self:Message(-9680, "orange")
+		self:PlaySound(-9680, "warning")
 		self:CDBar(-9680, 50.5) -- 50.5-53.0
 	end
 end
