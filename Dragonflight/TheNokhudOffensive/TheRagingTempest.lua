be1f29c9b09448efc351ec32df4411d8af3d1de1
Nick Towle <ntowle@gmail.com>
2023-01-03 23:40:59 -0500

    Dragonflight/TheNokhudOffensive/TheRagingTempest: Surge of Power update

--- Dragonflight/TheNokhudOffensive/TheRagingTempest.lua
+++ Dragonflight/TheNokhudOffensive/TheRagingTempest.lua
@@ -65,7 +65,7 @@ function mod:SurgeOfPowerAppliedToPlayer(args)
 	if self:Me(args.destGUID) then
 		self:StackMessage(args.spellId, "blue", args.destName, 1, 1)
 		self:PlaySound(args.spellId, "info")
-		self:TargetBar(args.spellId, 15, args.destName)
+		self:TargetBar(args.spellId, 18, args.destName)
 	end
 end
 
