71f9b649c1c3a6fab103d93348f62447c8cea65e
Nick Towle <ntowle@gmail.com>
2023-01-11 18:27:39 -0500

    Dragonflight/TheNokhudOffensive/Granyth: Shards of Stone bar update

--- Dragonflight/TheNokhudOffensive/Granyth.lua
+++ Dragonflight/TheNokhudOffensive/Granyth.lua
@@ -21,6 +21,12 @@ if L then
 end
 
 --------------------------------------------------------------------------------
+-- Locals
+--
+
+local shardsOfStoneRemaining = 2
+
+--------------------------------------------------------------------------------
 -- Initialization
 --
 
@@ -62,6 +68,7 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
+	shardsOfStoneRemaining = 2
 	self:CDBar(388283, 28.8) -- Eruption
 	self:CDBar(388817, 10.6) -- Shards of Stone
 	self:CDBar(385916, 15.5) -- Tectonic Stomp
@@ -85,9 +92,12 @@ function mod:Eruption(args)
 end
 
 function mod:ShardsOfStone(args)
+	shardsOfStoneRemaining = shardsOfStoneRemaining - 1
 	self:Message(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "alert")
-	self:CDBar(args.spellId, 13.3)
+	if shardsOfStoneRemaining > 0 then
+		self:CDBar(args.spellId, 13.3)
+	end
 end
 
 function mod:TectonicStomp(args)
@@ -138,6 +148,7 @@ function mod:ReloadSuccess(args)
 end
 
 function mod:Lanced(args)
+	shardsOfStoneRemaining = 2
 	self:Message(386530, "green", args.spellName) -- Dragonkiller Lance
 	self:PlaySound(386530, "info") -- Dragonkiller Lance
 	self:StopBar(CL.cast:format(self:SpellName(388283))) -- Eruption
