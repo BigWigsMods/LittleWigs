bef25a804e9d488eea7e89960ceeaa2410857c57
Nick Towle <ntowle@gmail.com>
2023-03-18 12:24:14 -0400

    Legion/NeltharionsLair/Dargrul: Molten Crash timer improvement

--- Legion/NeltharionsLair/Dargrul.lua
+++ Legion/NeltharionsLair/Dargrul.lua
@@ -60,6 +60,7 @@ function mod:Landslide(args)
 	-- TODO pattern? pull:15.8, 17.1, 19.5, 21.8, 16.9, 17.0, 25.5, 17.0, 17.0, 26.7, 19.4, 21.8, 21.8
 	--               pull:15.8, 17.1, 19.4, 21.9, 17.0, 17.1, 25.6, 17.1, 17.0, 25.6, 19.5, 21.9, 18.3, 17.1, 17.1, 20.7, 17.1, 17.1, 17.0, 24.4, 17.1, 18.3, 18.3, 18.3, 17.1, 25.6, 17.1, 17.0, 26.8, 19.5, 21.9
 	self:CDBar(args.spellId, 17.0) -- pull:15.9, 17.0, 17.0, 23.1, 19.4, 17.0
+	-- Molten Crash is always 3.2s after Landslide
 	self:Bar(200732, {3.2, 17.0}) -- Molten Crash
 end
 
@@ -80,11 +81,11 @@ end
 function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
 	-- faster than combat log event for Magma Wave (200404)
 	if spellId == 201661 or spellId == 201663 then -- Dargrul Ability Callout 02, Dargrul Ability Callout 03
-		self:Message(200404, "red")
-		self:PlaySound(200404, "long")
+		self:Message(200404, "red") -- Magma Wave
+		self:PlaySound(200404, "long") -- Magma Wave
 		-- cast at 100 energy, 60s energy gain + .6s delay
-		self:CDBar(200404, 60.6)
-		self:CastBar(200404, 7)
+		self:CDBar(200404, 60.6) -- Magma Wave
+		self:CastBar(200404, 7) -- Magma Wave
 	end
 end
 
@@ -104,10 +105,11 @@ function mod:CrystalSpikes(args)
 	self:Message(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "alarm")
 	self:CDBar(args.spellId, 21.8) -- pull:21.9, 21.8, 24.3, 21.8, 21.8
-	-- soonest Landslide can be after this is 2.43s
+	-- soonest Landslide (+Molten Crash) can be after this is 2.43s
 	local landslideTimeLeft = self:BarTimeLeft(200700)
 	if landslideTimeLeft < 2.43 then
-		self:Bar(200700, {2.43, 17.0})
+		self:Bar(200700, {2.43, 17.0}) -- Landslide
+		self:Bar(200732, {5.63, 17.0}) -- Molten Crash
 	end
 end
 
