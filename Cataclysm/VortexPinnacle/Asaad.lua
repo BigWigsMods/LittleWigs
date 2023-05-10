bfd12a8709249825e0d7778b2232bf8fc2427812
Nick Towle <ntowle@gmail.com>
2023-05-09 23:16:17 -0400

    Cataclysm/VortexPinnacle/Asaad: Timer update

--- Cataclysm/VortexPinnacle/Asaad.lua
+++ Cataclysm/VortexPinnacle/Asaad.lua
@@ -40,13 +40,13 @@ end
 
 function mod:OnEngage()
 	skyfallNovaCount = 1
-	self:CDBar(87622, 10.6) -- Chain Lightning
-	self:Bar(-2434, 15.5, nil, 413263) -- Skyfall Nova
+	self:CDBar(87622, 12.1) -- Chain Lightning
+	self:CDBar(-2434, 18.0, nil, 413263) -- Skyfall Nova
 	if not self:Normal() then
-		self:Bar(87618, 25.5) -- Static Cling
+		self:CDBar(87618, 25.2) -- Static Cling
 	end
-	self:Bar(86911, 30.5) -- Unstable Grounding Field
-	self:CDBar(86930, 40.5) -- Supremacy of the Storm
+	self:CDBar(86911, 30.1) -- Unstable Grounding Field
+	self:CDBar(86930, 40.2) -- Supremacy of the Storm
 end
 
 --------------------------------------------------------------------------------
@@ -62,7 +62,7 @@ do
 			prev = t
 			self:Message(args.spellId, "green")
 			self:PlaySound(args.spellId, "info")
-			self:Bar(args.spellId, 64.4)
+			self:CDBar(args.spellId, 63.2)
 			self:StopBar(87622) -- Chain Lightning
 			self:CDBar(86930, 10) -- Supremacy of the Storm
 		end
@@ -95,25 +95,24 @@ end
 
 function mod:ChainLightningSuccess(args)
 	-- have to start the timer on success because the boss can interrupt his own cast
-	-- TODO pattern?
-	-- pull:13.7, 37.7, 19.5, 23.1, 37.7, 19.4, 36.5, 19.4
-	-- pull:13.7, 38.9, 19.4, 23.1, 37.6, 19.9, 37.3, 19.5
-	-- pull:13.3, 38.1, 19.0, 23.5, 37.6, 19.5, 37.7, 19.5
-	-- pull:13.8, 36.5, 19.4, 23.1, 38.9, 19.4, 36.4, 19.5, 37.7, 19.0, 19.4, 38.9, 22.3, 37.7, 19.4, 37.3, 19.4, 19.4, 39.3
-	-- 19.0s CD - 2.75s cast
-	self:CDBar(args.spellId, 16.3)
+	-- pull:13.1, 35.3, 19.5, 18.2, 29.1, 19.4, 18.2, 25.7, 19.5
+	-- 18.2s CD - 2.75s cast
+	self:CDBar(args.spellId, 15.5)
 end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
 	if spellId == 96260 then -- Summon Skyfall Star
 		self:Message(-2434, "cyan", CL.spawned:format(self:SpellName(413263)), 413263) -- Skyfall Nova
 		self:PlaySound(-2434, "alert")
-		-- pull:16.0, 47.4, 30.4, 49.8, 47.4, 30.4, 48.2, 49.8, 30.4, 49.0, 49.8, 31.1
+		-- pull:18.0, 38.9, 25.5, 41.2, 25.6, 37.8
+		-- pull:18.2, 39.0, 25.6, 37.7, 25.5, 41.3, 25.5, 41.3, 26.0, 41.3
 		skyfallNovaCount = skyfallNovaCount + 1
-		if skyfallNovaCount % 3 == 0 then
-			self:CDBar(-2434, 30.4, nil, 413263)
+		if skyfallNovaCount == 2 then
+			self:CDBar(-2434, 38.9, nil, 413263)
+		elseif skyfallNovaCount % 2 == 0 then
+			self:CDBar(-2434, 37.7, nil, 413263)
 		else
-			self:CDBar(-2434, 47.4, nil, 413263)
+			self:CDBar(-2434, 25.5, nil, 413263)
 		end
 	end
 end
@@ -126,11 +125,9 @@ do
 		self:Message(args.spellId, "yellow", CL.casting:format(args.spellName))
 		self:PlaySound(args.spellId, "warning")
 		-- TODO pattern?
-		-- pull:25.5, 63.2, 64.8, 61.6
-		-- pull:25.6, 61.9, 64.4, 62.0
-		-- pull:28.5, 62.0, 64.4, 60.7
-		-- pull:25.3, 61.2, 65.6, 60.8, 61.6, 63.6, 64.4, 61.5
-		self:CDBar(args.spellId, 60.7)
+		-- pull:25.3, 38.9, 29.2, 37.6, 63.4
+		-- pull:25.5, 39.0, 63.2, 29.2, 37.7, 28.3, 37.7, 29.2, 38.9
+		self:CDBar(args.spellId, 28.3)
 	end
 
 	function mod:StaticClingApplied(args)
