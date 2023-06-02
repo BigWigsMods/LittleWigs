aa7436bfc73ff8fe05a332e5728f6931062532f2
EK <eke00372@gmail.com>
2023-06-02 13:15:09 +0800

    Update zhTW

--- Dragonflight/Neltharus/Locales/zhTW.lua
+++ Dragonflight/Neltharus/Locales/zhTW.lua
@@ -1,29 +1,29 @@
 local L = BigWigs:NewBossLocale("Neltharus Trash", "zhTW")
 if not L then return end
 if L then
-	L.custom_on_autotalk = "自动对话"
-	--L.custom_on_autotalk_desc = "Instantly selects the gossip options to get profession buffs."
+	L.custom_on_autotalk = "自動對話"
+	L.custom_on_autotalk_desc = "自動對話獲得專業增益。"
 
-	--L.qalashi_warden = "Qalashi Warden"
-	--L.qalashi_hunter = "Qalashi Hunter"
-	--L.overseer_lahar = "Overseer Lahar"
-	--L.qalashi_trainee = "Qalashi Trainee"
-	--L.qalashi_bonetender = "Qalashi Bonetender"
-	--L.qalashi_irontorch = "Qalashi Irontorch"
-	--L.qalashi_bonesplitter = "Qalashi Bonesplitter"
-	--L.qalashi_lavabearer = "Qalashi Lavabearer"
-	--L.irontorch_commander = "Irontorch Commander"
-	--L.qalashi_blacksmith = "Qalashi Blacksmith"
-	--L.forgewrought_monstrosity = "Forgewrought Monstrosity"
-	--L.qalashi_plunderer = "Qalashi Plunderer"
-	--L.qalashi_thaumaturge = "Qalashi Thaumaturge"
-	--L.apex_blazewing = "Apex Blazewing"
-	--L.qalashi_lavamancer = "Qalahsi Lavamancer"
+	L.qalashi_warden = "喀拉希守望者"
+	L.qalashi_hunter = "喀拉希獵人"
+	L.overseer_lahar = "監督者來哈爾"
+	L.qalashi_trainee = "喀拉希新兵"
+	L.qalashi_bonetender = "喀拉希癒骨者"
+	L.qalashi_irontorch = "喀拉希鐵炬"
+	L.qalashi_bonesplitter = "喀拉希碎骨者"
+	L.qalashi_lavabearer = "喀拉希熔岩纏繞者"
+	L.irontorch_commander = "鐵炬指揮官"
+	L.qalashi_blacksmith = "喀拉希鐵匠"
+	L.forgewrought_monstrosity = "爐鑄巨怪"
+	L.qalashi_plunderer = "喀拉希盜掠者"
+	L.qalashi_thaumaturge = "喀拉希奇術師"
+	L.apex_blazewing = "至尊焰翼"
+	L.qalashi_lavamancer = "喀拉希熔岩法師"
 end
 
 L = BigWigs:NewBossLocale("Chargath, Bane of Scales", "zhTW")
 if L then
-	--L.slow = "Slow"
+	L.slow = "鎖鏈"
 	L.boss = "首領"
 end
 
