184bb7e8f4c3542dc86b5265fd98ddc433f2848b
EK <eke00372@gmail.com>
2023-06-18 09:01:50 +0800

    Update zhTW.lua

--- Legion/NeltharionsLair/Locales/zhTW.lua
+++ Legion/NeltharionsLair/Locales/zhTW.lua
@@ -1,31 +1,31 @@
 local L = BigWigs:NewBossLocale("Neltharions Lair Trash", "zhTW")
 if not L then return end
 if L then
-	--L.rokmora_first_warmup_trigger = "Navarrogg?! Betrayer! You would lead these intruders against us?!"
-	--L.rokmora_second_warmup_trigger = "Either way, I will enjoy every moment of it. Rokmora, crush them!"
+	L.rokmora_first_warmup_trigger = "納瓦羅格？！叛徒！你竟然帶領入侵者對抗我們？"
+	L.rokmora_second_warmup_trigger = "就算這樣，我也樂見其成。羅克摩拉，碾碎他們！"
 
-	--L.vileshard_crawler = "Vileshard Crawler"
-	--L.tarspitter_lurker = "Tarspitter Lurker"
-	--L.rockback_gnasher = "Rockback Gnasher"
-	--L.vileshard_hulk = "Vileshard Hulk"
+	L.vileshard_crawler = "邪裂爬行蛛"
+	L.tarspitter_lurker = "噴油潛伏者"
+	L.rockback_gnasher = "石背銳齒蜥"
+	L.vileshard_hulk = "邪裂巨石怪"
 	--L.vileshard_chunk = "Vileshard Chunk"
-	--L.understone_drummer = "Understone Drummer"
-	--L.mightstone_breaker = "Mightstone Breaker"
-	--L.blightshard_shaper = "Blightshard Shaper"
-	--L.stoneclaw_grubmaster = "Stoneclaw Grubmaster"
-	--L.tarspitter_grub = "Tarspitter Grub"
+	L.understone_drummer = "底石擊鼓兵"
+	L.mightstone_breaker = "力石破壞者"
+	L.blightshard_shaper = "荒碎塑形者"
+	L.stoneclaw_grubmaster = "石爪蟲王"
+	L.tarspitter_grub = "噴油幼蟲"
 	--L.rotdrool_grabber = "Rotdrool Grabber"
-	--L.understone_demolisher = "Understone Demolisher"
-	--L.rockbound_trapper = "Rockbound Trapper"
-	--L.emberhusk_dominator = "Emberhusk Dominator"
+	L.understone_demolisher = "底石毀滅者"
+	L.rockbound_trapper = "岩縛陷補者"
+	L.emberhusk_dominator = "燼殼支配者"
 end
 
 L = BigWigs:NewBossLocale("Rokmora", "zhTW")
 if L then
-	--L.warmup_text = "Rokmora Active"
+	L.warmup_text = "對話結束"
 end
 
 L = BigWigs:NewBossLocale("Ularogg Cragshaper", "zhTW")
 if L then
-	--L.totems = "Totems"
+	L.totems = "圖騰"
 end
