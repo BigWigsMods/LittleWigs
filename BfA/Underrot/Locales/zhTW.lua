2cfb6caf67ed4d29db9b224603ab58cbf194386a
EK <eke00372@gmail.com>
2023-06-19 03:47:37 +0800

    Update zhTW (#915)

--- BfA/Underrot/Locales/zhTW.lua
+++ BfA/Underrot/Locales/zhTW.lua
@@ -1,19 +1,19 @@
 local L = BigWigs:NewBossLocale("Underrot Trash", "zhTW")
 if not L then return end
 if L then
-	--L.custom_on_fixate_plates = "Thirst For Blood icon on Enemy Nameplate"
-	--L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is fixating on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
+	L.custom_on_fixate_plates = "在名條上顯示追擊圖示"
+	L.custom_on_fixate_plates_desc = "當你被鮮血渴求鎖定時，在名條上顯示追擊圖示。\n需要開啟敵方名條，並使用支援此功能的名條插件（如KuiNameplates、Plater）。"
 
 	L.spirit = "被玷污的靈魂"
 	L.priest = "虔誠的鮮血祭司"
 	L.maggot = "惡臭蛆蟲"
 	L.matron = "受選之血族母"
-	--L.fanatical_headhunter = "Fanatical Headhunter"
+	L.fanatical_headhunter = "狂熱獵頭者"
 	L.lasher = "染病的鞭笞者"
 	L.bloodswarmer = "野生鮮血蟲"
 	L.rot = "活體腐質"
 	L.deathspeaker = "死亡的亡頌者"
-	--L.grotesque_horror = "Grotesque Horror"
+	L.grotesque_horror = "怪誕之懼"
 	L.defiler = "血誓污染者"
 	L.corruptor = "無面墮落者"
 end
