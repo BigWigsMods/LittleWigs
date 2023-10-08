431d771453a7303ae1d03c80d787a47af0bd0c4f
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2023-10-08 09:05:18 +0200

    Update deDE.lua

--- Cataclysm/ThroneTides/Locales/deDE.lua
+++ Cataclysm/ThroneTides/Locales/deDE.lua
@@ -1,13 +1,13 @@
 local L = BigWigs:NewBossLocale("Throne of the Tides Trash", "deDE")
 if not L then return end
 if L then
-	--L.nazjar_oracle = "Naz'jar Oracle"
+	L.nazjar_oracle = "Orakel der Naz'jar"
 	--L.vicious_snap_dragon = "Vicious Snap Dragon"
-	--L.nazjar_sentinel = "Naz'jar Sentinel"
-	--L.nazjar_ravager = "Naz'jar Ravager"
-	--L.nazjar_tempest_witch = "Naz'jar Tempest Witch"
-	--L.faceless_seer = "Faceless Seer"
-	--L.faceless_watcher = "Faceless Watcher"
+	L.nazjar_sentinel = "Schildwache der Naz'jar"
+	L.nazjar_ravager = "Verheerer der Naz'jar"
+	L.nazjar_tempest_witch = "Sturmhexe der Naz'jar"
+	L.faceless_seer = "Gesichtsloser Seher"
+	L.faceless_watcher = "Gesichtsloser Beobachter"
 	--L.tainted_sentry = "Tainted Sentry"
 end
 
