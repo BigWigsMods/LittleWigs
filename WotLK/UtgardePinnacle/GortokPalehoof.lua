ff7d35286420a17b3ca5e4f6347b18a359c9ea88
Nick Towle <ntowle@gmail.com>
2023-10-14 15:53:05 -0400

    WotLK/UtgardePinnacle/GortokPalehoof: Add encounter IDs

--- WotLK/UtgardePinnacle/GortokPalehoof.lua
+++ WotLK/UtgardePinnacle/GortokPalehoof.lua
@@ -1,11 +1,11 @@
-
 --------------------------------------------------------------------------------
--- Module declaration
+-- Module Declaration
 --
 
 local mod, CL = BigWigs:NewBoss("Gortok Palehoof", 575, 642)
 if not mod then return end
 mod:RegisterEnableMob(26687)
+mod:SetEncounterID(mod:Classic() and 579 or 2027)
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -21,8 +21,6 @@ end
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "WitheringRoar", 48256, 59267) -- normal, heroic
 	self:Log("SPELL_AURA_APPLIED", "Impale", 48261, 59268) -- normal, heroic
-
-	self:Death("Win", 26687)
 end
 
 --------------------------------------------------------------------------------
@@ -30,11 +28,10 @@ end
 --
 
 function mod:WitheringRoar()
-	self:MessageOld(59267, "orange")
+	self:Message(59267, "orange")
 	self:CDBar(59267, 10)
 end
 
 function mod:Impale(args)
-	self:TargetMessageOld(59268, args.destName, "yellow")
+	self:TargetMessage(59268, "yellow", args.destName)
 end
-
