cd52d08565a90de450568e64cb88ae008f5ae3c5
BlueNightSky <afan1128@hotmail.com>
2023-10-17 22:46:29 +0800

    Update zhTW (#942)

--- Legion/DarkheartThicket/Locales/zhTW.lua
+++ Legion/DarkheartThicket/Locales/zhTW.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("Darkheart Thicket Trash", "zhTW")
 if not L then return end
 if L then
-	--L.archdruid_glaidalis_warmup_trigger = "Defilers... I can smell the Nightmare in your blood. Be gone from these woods or suffer nature's wrath!"
+	L.archdruid_glaidalis_warmup_trigger = "污染者…我能聞到你血液裡的夢魘。離開這片林地，否則就面對大自然的憤怒吧！"
 
 	L.mindshattered_screecher = "碎心尖嘯鴞"
 	L.dreadsoul_ruiner = "懼魂毀滅者"
@@ -13,7 +13,7 @@ if L then
 	L.rotheart_keeper = "腐心看守者"
 	L.nightmare_dweller = "夢魘居者"
 	L.bloodtainted_fury = "污血怒靈"
-	--L.bloodtainted_burster = "Bloodtainted Burster"
+	L.bloodtainted_burster = "污血爆靈"
 	L.taintheart_summoner = "腐心召喚師"
 	L.dreadfire_imp = "懼火小鬼"
 	L.tormented_bloodseeker = "痛苦的覓血蝙蝠"
