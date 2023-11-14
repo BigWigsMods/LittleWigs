2b081adf77d4e46d769f8b8067b2decb72865626
Nick Towle <ntowle@gmail.com>
2023-11-14 11:28:03 -0500

    Cataclysm/ThroneTides/Mindbender: Support Cataclysm Classic (#958)

--- Cataclysm/ThroneTides/Mindbender.lua
+++ Cataclysm/ThroneTides/Mindbender.lua
@@ -1,4 +1,3 @@
-local isTenDotTwo = select(4, GetBuildInfo()) >= 100200 --- XXX delete when 10.2 is live everywhere
 --------------------------------------------------------------------------------
 -- Module Declaration
 --
@@ -17,8 +16,7 @@ mod:SetStage(1)
 -- Initialization
 --
 
--- XXX remove isTenDotTwo check once 10.2 is live everywhere
-local stormflurryTotemMarker = isTenDotTwo and mod:AddMarkerOption(true, "npc", 8, 429037, 8) -- Stormflurry Totem
+local stormflurryTotemMarker = mod:Retail() and mod:AddMarkerOption(true, "npc", 8, 429037, 8) or nil -- Stormflurry Totem
 function mod:GetOptions()
 	return {
 		"stages",
@@ -29,10 +27,6 @@ function mod:GetOptions()
 		{429048, "DISPEL", "OFF"}, -- Flame Shock
 		-- Mindbender Ghur'sha
 		{429172, "CASTBAR", "CASTBAR_COUNTDOWN"}, -- Terrifying Vision
-		-- XXX delete these option keys below when 10.2 is live everywhere
-		not isTenDotTwo and {76207, "ICON"} or nil, -- Enslave
-		not isTenDotTwo and 76307 or nil, -- Absorb Magic
-		not isTenDotTwo and 76230 or nil, -- Mind Fog
 	}, {
 		[429051] = -2194, -- Erunak Stonespeaker
 		[429172] = -2199, -- Mindbender Ghur'sha
@@ -61,8 +55,11 @@ function mod:OnEngage()
 	self:CDBar(429051, 22.9) -- Earthfury
 end
 
--- XXX delete this entire block below when 10.2 is live everywhere
-if not isTenDotTwo then
+--------------------------------------------------------------------------------
+-- Classic Initialization
+--
+
+if mod:Classic() then
 	function mod:GetOptions()
 		return {
 			{76207, "ICON"}, -- Enslave
@@ -71,6 +68,7 @@ if not isTenDotTwo then
 			"stages",
 		}
 	end
+
 	function mod:OnBossEnable()
 		self:Log("SPELL_CAST_SUCCESS", "Enslave", 76207)
 		self:Log("SPELL_AURA_REMOVED", "EnslaveRemoved", 76207)
@@ -78,6 +76,7 @@ if not isTenDotTwo then
 		self:Log("SPELL_AURA_APPLIED", "MindFog", 76230)
 		self:RegisterUnitEvent("UNIT_HEALTH", nil, "boss1")
 	end
+
 	function mod:OnEngage()
 	end
 end
@@ -152,18 +151,21 @@ function mod:TerrifyingVision(args)
 	self:CDBar(args.spellId, 23.0)
 end
 
--- XXX delete everything below this comment when 10.2 is live everywhere
+--------------------------------------------------------------------------------
+-- Classic Event Handlers
+--
 
 function mod:UNIT_HEALTH(event, unit)
 	local hp = self:GetHealth(unit)
 	if hp < 55 then
 		self:UnregisterUnitEvent(event, unit)
-		self:MessageOld("stages", "cyan", nil, CL.soon:format(CL.stage:format(2)), false)
+		self:Message("stages", "cyan", CL.soon:format(CL.stage:format(2)), false)
 	end
 end
 
 function mod:Enslave(args)
-	self:TargetMessageOld(args.spellId, args.destName, "red", "alert")
+	self:TargetMessage(args.spellId, "red", args.destName)
+	self:PlaySound(args.spellId, "alert", nil, args.destName)
 	self:PrimaryIcon(args.spellId, args.destName)
 end
 
@@ -172,11 +174,12 @@ function mod:EnslaveRemoved(args)
 end
 
 function mod:AbsorbMagic(args)
-	self:MessageOld(args.spellId, "orange", nil, CL.casting:format(args.spellName))
+	self:Message(args.spellId, "orange", CL.casting:format(args.spellName))
 end
 
 function mod:MindFog(args)
 	if self:Me(args.destGUID) then
-		self:TargetMessageOld(args.spellId, args.destName, "blue", "alarm")
+		self:PersonalMessage(args.spellId)
+		self:PlaySound(args.spellId, "alarm", nil, args.destName)
 	end
 end
