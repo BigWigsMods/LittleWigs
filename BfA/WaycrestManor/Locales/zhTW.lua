9d2561aebf27e02cd01033f64c0c1af9ce256274
BlueNightSky <afan1128@hotmail.com>
2023-11-19 16:07:37 +0800

    Update zhTW.lua

--- BfA/WaycrestManor/Locales/zhTW.lua
+++ BfA/WaycrestManor/Locales/zhTW.lua
@@ -3,7 +3,7 @@ if not L then return end
 if L then
 	L.heartsbane_runeweaver = "禍心織符者"
 	L.bewitched_captain = "被蠱惑的隊長"
-	--L.thistle_acolyte = "Thistle Acolyte"
+	L.thistle_acolyte = "棘刺助祭"
 	L.blight_toad = "荒疫蟾蜍"
 	L.dreadwing_raven = "懼翼烏鴉"
 	L.heartsbane_vinetwister = "禍心盤藤者"
@@ -11,12 +11,12 @@ if L then
 	L.coven_thornshaper = "塑棘女巫"
 	L.thornguard = "棘衛"
 	L.matron_bryndle = "高階女巫布恩朵"
-	--L.devouring_maggot = "Devouring Maggot"
+	L.devouring_maggot = "貪吃的蛆蟲"
 	L.pallid_gorger = "蒼白吞食者"
 	L.banquet_steward = "宴會服務員"
 	L.crazed_marksman = "瘋狂的神射手"
 	L.maddened_survivalist = "發狂的倖存者"
-	--L.faceless_maiden = "Faceless Maiden"
+	L.faceless_maiden = "無面女僕"
 	L.marked_sister = "符印女巫"
 	L.matron_alma = "高階女巫艾爾瑪"
 	L.heartsbane_soulcharmer = "禍心誘魂者"
@@ -24,5 +24,5 @@ end
 
 L = BigWigs:NewBossLocale("Gorak Tul", "zhTW")
 if L then
-	-- L.add_killed = "Add killed - Ready to burn"
+	L.add_killed = "小怪已擊殺 - 準備焚燒"
 end
