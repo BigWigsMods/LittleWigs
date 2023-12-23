47acabba5029092412af890f0a0bdf438003bddc
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2023-12-23 07:14:20 +0100

    Update deDE.lua

--- Dragonflight/BrackenhideHollow/Locales/deDE.lua
+++ Dragonflight/BrackenhideHollow/Locales/deDE.lua
@@ -3,8 +3,8 @@ if not L then return end
 if L then
 	L.custom_on_cauldron_autotalk = "Automatisch ansprechen"
 	L.custom_on_cauldron_autotalk_desc = "[Alchemie] Verfallende Kessel sofort entgiften für einen Krankheit Dispel Buff."
-	--L.captive_tuskarr_freed = "Captive Tuskarr Freed"
-	--L.captive_tuskarr_freed_desc = "Show an alert when a Captive Tuskarr has been freed."
+	L.captive_tuskarr_freed = "Gefangener Tuskarr befreit"
+	L.captive_tuskarr_freed_desc = "Eine Benachrichtigung anzeigen wenn ein Gefangener Tuskarr befreit wurde."
 
 	L.decaying_cauldron = "Verfallender Kessel"
 	L.captive_tuskarr = "Gefangener Tuskarr"
