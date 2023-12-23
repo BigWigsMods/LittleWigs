0bbe699356384caff7c7e297e8e2ebb74715803e
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2023-12-23 14:58:43 +0800

    Update zhCN.lua

--- Dragonflight/BrackenhideHollow/Locales/zhCN.lua
+++ Dragonflight/BrackenhideHollow/Locales/zhCN.lua
@@ -2,7 +2,7 @@ local L = BigWigs:NewBossLocale("Brackenhide Hollow Trash", "zhCN")
 if not L then return end
 if L then
 	L.custom_on_cauldron_autotalk = "自动对话"
-	L.custom_on_cauldron_autotalk_desc = "[炼金术] 使用腐蚀坩埚来移除疾病。"
+	L.custom_on_cauldron_autotalk_desc = "[炼金术] 使用腐蚀坩埚获得可以移除疾病的特殊技能。"
 	L.captive_tuskarr_freed = "解救海象人俘虏"
 	L.captive_tuskarr_freed_desc = "当海象人俘虏被解救时显示警报。"
 
