e40265c2ff0f465eb68bb0e7bb75808e9fc3c869
Nick Towle <ntowle@gmail.com>
2024-01-04 22:26:35 -0500

    Dragonflight/DawnOfTheInfinite/Trash: Localization

--- Dragonflight/DawnOfTheInfinite/Locales/koKR.lua
+++ Dragonflight/DawnOfTheInfinite/Locales/koKR.lua
@@ -35,7 +35,7 @@ if L then
 	L.custom_on_rift_autotalk = "자동 대화"
 	--L.custom_on_rift_autotalk_desc = "Instantly start channeling to open the Temporal Rift."
 	--L.rift_opened = "Temporal Rift Opened"
-	--L.rift_stability = "Rift Stability"
+	L.rift_stability = "균열 안정성"
 	--L.rift_stability_desc = "Show an alert when the Temporal Rift has been opened."
 
 	--L.manifested_timeways_warmup_trigger = "Even the Aspect of Time cannot be allowed to disrupt the timeways!"
