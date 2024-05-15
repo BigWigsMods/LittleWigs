cfd421eb9982922e00292c1bb34ddde15b288fb7
BlueNightSky <afan1128@hotmail.com>
2024-05-16 03:00:25 +0800

    Update zhTW (#1002)

--- Dragonflight/RubyLifePools/Locales/zhTW.lua
+++ Dragonflight/RubyLifePools/Locales/zhTW.lua
@@ -2,22 +2,22 @@ local L = BigWigs:NewBossLocale("Ruby Life Pools Trash", "zhTW")
 if not L then return end
 if L then
 	L.primal_juggernaut = "原始重戰士"
-	--L.flashfrost_earthshaper = "Flashfrost Earthshaper"
+	L.flashfrost_earthshaper = "閃霜塑地者"
 	L.flashfrost_chillweaver = "閃霜織寒者"
 	L.defier_draghar = "挑戰者德拉哈"
 	L.primalist_flamedancer = "洪荒使者舞焰者"
 	L.blazebound_destroyer = "火縛毀滅者"
-	--L.primalist_shockcaster = "Primalist Shockcaster"
+	L.primalist_shockcaster = "洪荒使者地震法師"
 	L.thunderhead = "雷首蜥蜴"
-	--L.flamegullet = "Flamegullet"
-	--L.tempest_channeler = "Tempest Channeler"
+	L.flamegullet = "火喉"
+	L.tempest_channeler = "暴風導魔師"
 	L.flame_channeler = "烈焰傳導者"
 	--L.high_channeler_ryvati = "High Channeler Ryvati"
 
-	--L.kyrakka_and_erkhart_warmup_trigger = "Your false queen cannot stop us. We are the truth."
+	L.kyrakka_and_erkhart_warmup_trigger = "那個假龍后阻止不了我們，因為我們才是真理。"
 end
 
 L = BigWigs:NewBossLocale("Kyrakka and Erkhart Stormvein", "zhTW")
 if L then
-	--L.winds = "Winds"
+	L.winds = "風"
 end
