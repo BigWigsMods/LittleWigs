083378ee9b82c0b1a98c2cf266f6a36f0e69a729
Nick Towle <ntowle@gmail.com>
2024-05-24 13:19:57 -0400

    split esES and esMX

--- MoP/Scholomance/Locales/esMX.lua
+++ MoP/Scholomance/Locales/esMX.lua
@@ -1,4 +1,4 @@
-local L = BigWigs:NewBossLocale("Lilian Voss", "esES")
+local L = BigWigs:NewBossLocale("Lilian Voss", "esMX")
 if not L then return end
 if L then
 	--L.stage_2_trigger = "Now, Lilian, it is time for your transformation."
