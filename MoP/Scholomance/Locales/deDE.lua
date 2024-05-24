568b07631f69f6a28f5e54c7f097be2b477fef04
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2024-05-24 23:47:09 +0200

    Update deDE (#1007)

--- MoP/Scholomance/Locales/deDE.lua
+++ MoP/Scholomance/Locales/deDE.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Lilian Voss", "deDE")
 if not L then return end
 if L then
-	--L.stage_2_trigger = "Now, Lilian, it is time for your transformation."
+	L.stage_2_trigger = "Jetzt ist es an der Zeit für Eure Transformation, Lilian."
 end
