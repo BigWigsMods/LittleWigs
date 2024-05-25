ee4466c4579fab693ccfc06c66bdec26971d511b
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-05-25 09:59:16 +0800

    Update zhCN (#1009)

--- MoP/Scholomance/Locales/zhCN.lua
+++ MoP/Scholomance/Locales/zhCN.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Lilian Voss", "zhCN")
 if not L then return end
 if L then
-	--L.stage_2_trigger = "Now, Lilian, it is time for your transformation."
+	L.stage_2_trigger = "现在，莉莉安，你变身的时刻到了。"
 end
