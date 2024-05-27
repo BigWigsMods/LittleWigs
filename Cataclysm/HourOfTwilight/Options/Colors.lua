4956a0c6c7a55b652ef1d027984aef5956a227f2
Nick Towle <ntowle@gmail.com>
2024-05-27 17:06:10 -0400

    Cataclysm/HourOfTwilight/Benedictus: Add boss module

--- Cataclysm/HourOfTwilight/Options/Colors.lua
+++ Cataclysm/HourOfTwilight/Options/Colors.lua
@@ -5,7 +5,14 @@ BigWigs:AddColors("Arcurion", {
 BigWigs:AddColors("Asira Dawnslayer", {
 })
 
-BigWigs:AddColors("Benedictus", {
+BigWigs:AddColors("Archbishop Benedictus", {
+	[103151] = "red",
+	[103363] = "red",
+	[103565] = "yellow",
+	[103678] = "orange",
+	[103767] = "yellow",
+	[103780] = "orange",
+	["stages"] = "cyan",
 })
 
 BigWigs:AddColors("The Hour of Twilight Trash", {
--- Cataclysm/HourOfTwilight/Options/Sounds.lua
+++ Cataclysm/HourOfTwilight/Options/Sounds.lua
@@ -5,7 +5,14 @@ BigWigs:AddSounds("Arcurion", {
 BigWigs:AddSounds("Asira Dawnslayer", {
 })
 
-BigWigs:AddSounds("Benedictus", {
+BigWigs:AddSounds("Archbishop Benedictus", {
+	[103151] = "alarm",
+	[103363] = "alarm",
+	[103565] = "alert",
+	[103678] = "alarm",
+	[103767] = "alert",
+	[103780] = "alarm",
+	["stages"] = "long",
 })
 
 BigWigs:AddSounds("The Hour of Twilight Trash", {
