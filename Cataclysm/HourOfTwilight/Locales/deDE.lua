f49c2e5faabf421fb9d6d563b3e28cd98fce455b
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2024-05-28 17:46:08 +0200

    Update deDE (#1016)

--- Cataclysm/HourOfTwilight/Locales/deDE.lua
+++ Cataclysm/HourOfTwilight/Locales/deDE.lua
@@ -2,5 +2,5 @@ local L = BigWigs:NewBossLocale("The Hour of Twilight Trash", "deDE")
 if not L then return end
 if L then
 	L.custom_on_autotalk = "Automatisch ansprechen"
-	--L.custom_on_autotalk_desc = "Instantly select Thrall's gossip options."
+	L.custom_on_autotalk_desc = "Direkt Thralls Dialogoptionen auswählen."
 end
