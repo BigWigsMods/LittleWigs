150afd46714ede340ff8a3c9c0508c8e51b606d8
Nick Towle <ntowle@gmail.com>
2024-06-08 18:11:22 -0400

    Delves/TheDreadPit/UnderLordViktis: Add boss module

--- Delves/TheDreadPit/UnderLordViktis.lua
+++ Delves/TheDreadPit/UnderLordViktis.lua
@@ -29,14 +29,23 @@ end
 
 function mod:GetOptions()
 	return {
+		448634, -- Impale
+		448644, -- Burrowing Tremors
+		448663, -- Stinging Swarm
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterEvent("ENCOUNTER_START")
+	self:Log("SPELL_CAST_START", "Impale", 448634)
+	self:Log("SPELL_CAST_START", "BurrowingTremors", 448644)
+	self:Log("SPELL_CAST_START", "StingingSwarm", 448663)
 end
 
 function mod:OnEngage()
+	self:CDBar(448634, 6.0) -- Impale
+	self:CDBar(448644, 12.1) -- Burrowing Tremors
+	self:CDBar(448663, 26.7) -- Stinging Swarm
 end
 
 --------------------------------------------------------------------------------
@@ -48,4 +57,22 @@ function mod:ENCOUNTER_START(_, id)
 	if id == self.engageId then
 		self:Engage()
 	end
+end
+
+function mod:Impale(args)
+	self:Message(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+	self:CDBar(args.spellId, 14.6)
+end
+
+function mod:BurrowingTremors(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, 31.6)
+end
+
+function mod:StingingSwarm(args)
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, 34.0)
 end
