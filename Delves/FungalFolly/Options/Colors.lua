ee9bb5d0a09d1ee7b90cc56d717b85d8b28f9c39
Nick Towle <ntowle@gmail.com>
2024-06-08 18:08:08 -0400

    Delves/FungalFolly/Spinshroom: Add boss module

--- Delves/FungalFolly/Options/Colors.lua
+++ Delves/FungalFolly/Options/Colors.lua
@@ -1,3 +1,7 @@
 
 BigWigs:AddColors("Spinshroom", {
+	[415406] = "yellow",
+	[415492] = "red",
+	[415499] = "green",
+	[425315] = "orange",
 })
--- Delves/FungalFolly/Spinshroom.lua
+++ Delves/FungalFolly/Spinshroom.lua
@@ -29,14 +29,25 @@ end
 
 function mod:GetOptions()
 	return {
+		415406, -- Fungalstorm
+		415499, -- Dizzy
+		415492, -- Fungal Charge
+		425315, -- Fungsplosion
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterEvent("ENCOUNTER_START")
+	self:Log("SPELL_CAST_START", "Fungalstorm", 415406)
+	self:Log("SPELL_AURA_APPLIED", "Dizzy", 415499)
+	self:Log("SPELL_CAST_START", "FungalCharge", 415492)
+	self:Log("SPELL_CAST_START", "Fungsplosion", 425315)
 end
 
 function mod:OnEngage()
+	self:CDBar(415406, 5.7) -- Fungalstorm
+	self:CDBar(415492, 20.0) -- Fungal Charge
+	self:CDBar(425315, 26.4) -- Fungsplosion
 end
 
 --------------------------------------------------------------------------------
@@ -48,4 +59,27 @@ function mod:ENCOUNTER_START(_, id)
 	if id == self.engageId then
 		self:Engage()
 	end
+end
+
+function mod:Fungalstorm(args)
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "long")
+	self:CDBar(args.spellId, 30.2)
+end
+
+function mod:Dizzy(args)
+	self:Message(args.spellId, "green")
+	self:PlaySound(args.spellId, "info")
+end
+
+function mod:FungalCharge(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "alarm")
+	self:CDBar(args.spellId, 30.2)
+end
+
+function mod:Fungsplosion(args)
+	self:Message(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+	self:CDBar(args.spellId, 30.2)
 end
