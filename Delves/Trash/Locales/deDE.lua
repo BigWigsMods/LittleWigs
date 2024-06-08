a24f80ce80358d1e9edfd5a2981e9a8a9cdf4acb
Nick Towle <ntowle@gmail.com>
2024-06-08 18:51:55 -0400

    Delves/Trash/Nerubian: Add trash module (#1039)

--- Delves/Trash/Locales/deDE.lua
+++ Delves/Trash/Locales/deDE.lua
@@ -21,6 +21,11 @@ end
 
 L = BigWigs:NewBossLocale("Nerubian Delve Trash", "deDE")
 if L then
+	--L.nerubian_lord = "Nerubian Lord"
+	--L.nerubian_darkcaster = "Nerubian Darkcaster"
+	--L.nerubian_captain = "Nerubian Captain"
+	--L.chittering_fearmonger = "Chittering Fearmonger"
+	--L.skittering_swarmer = "Skittering Swarmer"
 end
 
 L = BigWigs:NewBossLocale("Nightfall Delve Trash", "deDE")
