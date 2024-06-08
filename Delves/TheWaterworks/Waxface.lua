4877a7a3eabdfda61faf3c679ebd7b528e95f3b3
Nick Towle <ntowle@gmail.com>
2024-06-08 18:44:13 -0400

    Delves/TheWaterworks/Waxface: Add boss module (#1035)

--- Delves/TheWaterworks/Waxface.lua
+++ Delves/TheWaterworks/Waxface.lua
@@ -29,14 +29,20 @@ end
 
 function mod:GetOptions()
 	return {
+		450142, -- Burn Away
+		450128, -- Noxious Gas
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterEvent("ENCOUNTER_START")
+	self:Log("SPELL_CAST_START", "NoxiousGas", 450128)
+	self:Log("SPELL_CAST_START", "BurnAway", 450142)
 end
 
 function mod:OnEngage()
+	self:CDBar(450128, 3.6) -- Noxious Gas
+	self:CDBar(450142, 18.2) -- Burn Away
 end
 
 --------------------------------------------------------------------------------
@@ -48,4 +54,16 @@ function mod:ENCOUNTER_START(_, id)
 	if id == self.engageId then
 		self:Engage()
 	end
+end
+
+function mod:NoxiousGas(args)
+	self:Message(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+	self:CDBar(args.spellId, 20.6)
+end
+
+function mod:BurnAway(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, 21.9)
 end
