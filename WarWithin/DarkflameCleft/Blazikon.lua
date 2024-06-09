7e2d41eb4798672449ac1a64beba4555b7b08298
Nick Towle <ntowle@gmail.com>
2024-06-09 17:20:38 -0400

    WarWithin/DarkflameCleft/Blazikon: Add boss module

--- WarWithin/DarkflameCleft/Blazikon.lua
+++ WarWithin/DarkflameCleft/Blazikon.lua
@@ -15,19 +15,56 @@ mod:SetRespawnTime(30)
 
 function mod:GetOptions()
 	return {
-
+		421817, -- Wicklighter Barrage
+		424212, -- Incite Flames
+		423109, -- Enkindling Inferno
+		-- TODO Blazing Storms no one in melee range
+		425394, -- Dousing Breath (Normal / Heroic)
+		-- TODO Extinguishing Gust (Mythic)
+	}, {
+		[425394] = CL.normal.." / "..CL.heroic,
+		--[] = CL.mythic,
 	}
 end
 
 function mod:OnBossEnable()
-
+	self:Log("SPELL_CAST_START", "WicklighterBarrage", 421817)
+	self:Log("SPELL_CAST_START", "InciteFlames", 424212)
+	self:Log("SPELL_CAST_START", "EnkindlingInferno", 423109)
+	self:Log("SPELL_CAST_START", "DousingBreath", 425394)
 end
 
 function mod:OnEngage()
-
+	self:CDBar(425394, 3.4) -- Dousing Breath
+	self:CDBar(421817, 9.5) -- Wicklighter Barrage
+	self:CDBar(424212, 21.6) -- Incite Flames
+	self:CDBar(423109, 30.1) -- Enkindling Inferno
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:WicklighterBarrage(args)
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "long")
+	self:CDBar(args.spellId, 31.6)
+end
+
+function mod:InciteFlames(args)
+	self:Message(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+	self:CDBar(args.spellId, 31.6)
+end
+
+function mod:EnkindlingInferno(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "warning")
+	self:CDBar(args.spellId, 31.6)
+end
+
+function mod:DousingBreath(args)
+	self:Message(args.spellId, "cyan")
+	self:PlaySound(args.spellId, "info")
+	self:CDBar(args.spellId, 31.6)
+end
--- WarWithin/DarkflameCleft/Options/Colors.lua
+++ WarWithin/DarkflameCleft/Options/Colors.lua
@@ -3,6 +3,10 @@ BigWigs:AddColors("Ol' Waxbeard", {
 })
 
 BigWigs:AddColors("Blazikon", {
+	[421817] = "yellow",
+	[423109] = "red",
+	[424212] = "orange",
+	[425394] = "cyan",
 })
 
 BigWigs:AddColors("The Candle King", {
