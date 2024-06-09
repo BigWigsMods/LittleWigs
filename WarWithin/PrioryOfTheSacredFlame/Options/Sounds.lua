91a9893587637b99ba49bb2bdc69bb0c54dfbb0e
Nick Towle <ntowle@gmail.com>
2024-06-09 17:43:30 -0400

    WarWithin/PrioryOfTheSacredFlame/PrioressMurrpray: Add boss module (#1044)

--- WarWithin/PrioryOfTheSacredFlame/Options/Sounds.lua
+++ WarWithin/PrioryOfTheSacredFlame/Options/Sounds.lua
@@ -13,6 +13,10 @@ BigWigs:AddSounds("Baron Braunpyke", {
 })
 
 BigWigs:AddSounds("Prioress Murrpray", {
+	[423588] = {"info","long"},
+	[444546] = "alarm",
+	[444608] = "info",
+	[451605] = "alert",
 })
 
 BigWigs:AddSounds("Priory of the Sacred Flame Trash", {
