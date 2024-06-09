677fa0d275b019ae9468f3946f151fc93bc95bfd
Nick Towle <ntowle@gmail.com>
2024-06-09 17:54:09 -0400

    WarWithin/TheStonevault/HighSpeakerEirich: Add boss module (#1049)

--- WarWithin/TheStonevault/Options/Colors.lua
+++ WarWithin/TheStonevault/Options/Colors.lua
@@ -21,6 +21,9 @@ BigWigs:AddColors("Forge Speakers", {
 })
 
 BigWigs:AddColors("High Speaker Eirich", {
+	[427461] = {"green","yellow"},
+	[427852] = "red",
+	[427869] = "orange",
 })
 
 BigWigs:AddColors("The Stonevault Trash", {
--- WarWithin/TheStonevault/Options/Sounds.lua
+++ WarWithin/TheStonevault/Options/Sounds.lua
@@ -21,6 +21,9 @@ BigWigs:AddSounds("Forge Speakers", {
 })
 
 BigWigs:AddSounds("High Speaker Eirich", {
+	[427461] = "info",
+	[427852] = "alert",
+	[427869] = "alarm",
 })
 
 BigWigs:AddSounds("The Stonevault Trash", {
