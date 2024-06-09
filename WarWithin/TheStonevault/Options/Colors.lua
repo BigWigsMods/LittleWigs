5b00bf8edf3b70961374e4fec4f7826a41469be1
Nick Towle <ntowle@gmail.com>
2024-06-09 16:37:47 -0400

    WarWithin/TheStonevault/Trash: Add trash module

--- WarWithin/TheStonevault/Options/Colors.lua
+++ WarWithin/TheStonevault/Options/Colors.lua
@@ -12,4 +12,18 @@ BigWigs:AddColors("High Speaker Eirich", {
 })
 
 BigWigs:AddColors("The Stonevault Trash", {
+	[425027] = "yellow",
+	[426308] = "yellow",
+	[426345] = "purple",
+	[426771] = "yellow",
+	[428703] = "orange",
+	[428879] = "purple",
+	[429109] = "red",
+	[429545] = "yellow",
+	[445207] = "red",
+	[447141] = "orange",
+	[448640] = "red",
+	[449130] = "orange",
+	[449154] = "yellow",
+	[449455] = "red",
 })
