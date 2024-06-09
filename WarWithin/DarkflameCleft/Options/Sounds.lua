1328334a377449a66c7791187468b3af25ba6398
Nick Towle <ntowle@gmail.com>
2024-06-09 17:57:06 -0400

    WarWithin/DarkflameCleft/Blazikon: Add boss module (#1051)

--- WarWithin/DarkflameCleft/Options/Sounds.lua
+++ WarWithin/DarkflameCleft/Options/Sounds.lua
@@ -3,6 +3,10 @@ BigWigs:AddSounds("Ol' Waxbeard", {
 })
 
 BigWigs:AddSounds("Blazikon", {
+	[421817] = "long",
+	[423109] = "warning",
+	[424212] = "alarm",
+	[425394] = "info",
 })
 
 BigWigs:AddSounds("The Candle King", {
