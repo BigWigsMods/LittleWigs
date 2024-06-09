e5d89274dd888fc5ee631db4b08412bf1b83723b
Nick Towle <ntowle@gmail.com>
2024-06-09 16:44:22 -0400

    WarWithin/TheStonevault/HighSpeakerEirich: Add boss module

--- WarWithin/TheStonevault/HighSpeakerEirich.lua
+++ WarWithin/TheStonevault/HighSpeakerEirich.lua
@@ -15,19 +15,50 @@ mod:SetRespawnTime(30)
 
 function mod:GetOptions()
 	return {
-
+		427461, -- Void Corruption
+		427852, -- Entropic Reckoning
+		427869, -- Unbridled Void
 	}
 end
 
 function mod:OnBossEnable()
-
+	self:Log("SPELL_CAST_START", "VoidCorruption", 427461)
+	self:Log("SPELL_AURA_REMOVED", "VoidCorruptionRemoved", 427329)
+	self:Log("SPELL_CAST_START", "EntropicReckoning", 427852)
+	self:Log("SPELL_CAST_START", "UnbridledVoid", 427869)
 end
 
 function mod:OnEngage()
-
+	self:CDBar(427869, 7.6) -- Unbridled Void
+	self:CDBar(427461, 16.1) -- Void Corruption
+	self:CDBar(427852, 22.2) -- Entropic Reckoning
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:VoidCorruption(args)
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "info")
+	self:CDBar(args.spellId, 27.9)
+end
+
+function mod:VoidCorruptionRemoved(args)
+	if self:Me(args.destGUID) then
+		self:Message(427461, "green", CL.removed:format(args.spellName))
+		self:PlaySound(427461, "info", nil, args.destName)
+	end
+end
+
+function mod:EntropicReckoning(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, 17.0)
+end
+
+function mod:UnbridledVoid(args)
+	self:Message(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+	self:CDBar(args.spellId, 20.6)
+end
--- WarWithin/TheStonevault/Options/Colors.lua
+++ WarWithin/TheStonevault/Options/Colors.lua
@@ -9,6 +9,9 @@ BigWigs:AddColors("Forge Speakers", {
 })
 
 BigWigs:AddColors("High Speaker Eirich", {
+	[427461] = {"green","yellow"},
+	[427852] = "red",
+	[427869] = "orange",
 })
 
 BigWigs:AddColors("The Stonevault Trash", {
