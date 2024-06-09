3c696093f48fd3f6abf21ace40d6e881486c0855
Nick Towle <ntowle@gmail.com>
2024-06-09 16:42:07 -0400

    WarWithin/TheStonevault/ForgeSpeakers: Add boss module

--- WarWithin/TheStonevault/Options/Colors.lua
+++ WarWithin/TheStonevault/Options/Colors.lua
@@ -6,6 +6,13 @@ BigWigs:AddColors("Skarmorak", {
 })
 
 BigWigs:AddColors("Forge Speakers", {
+	[428202] = "yellow",
+	[428508] = "red",
+	[428711] = "purple",
+	[430097] = "red",
+	[439577] = "cyan",
+	[445541] = "orange",
+	[449167] = "orange",
 })
 
 BigWigs:AddColors("High Speaker Eirich", {
--- WarWithin/TheStonevault/Options/Sounds.lua
+++ WarWithin/TheStonevault/Options/Sounds.lua
@@ -6,6 +6,13 @@ BigWigs:AddSounds("Skarmorak", {
 })
 
 BigWigs:AddSounds("Forge Speakers", {
+	[428202] = "long",
+	[428508] = "alarm",
+	[428711] = "alert",
+	[430097] = "alert",
+	[439577] = "long",
+	[445541] = "alarm",
+	[449167] = "alarm",
 })
 
 BigWigs:AddSounds("High Speaker Eirich", {
