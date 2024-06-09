a02c1ff6c62229e40667b53dbf33c00f882f5503
Nick Towle <ntowle@gmail.com>
2024-06-09 15:27:26 -0400

    WarWithin/PrioryOfTheSacredFlame/Trash: Add trash module

--- WarWithin/PrioryOfTheSacredFlame/Options/Colors.lua
+++ WarWithin/PrioryOfTheSacredFlame/Options/Colors.lua
@@ -9,4 +9,25 @@ BigWigs:AddColors("Prioress Murrpray", {
 })
 
 BigWigs:AddColors("Priory of the Sacred Flame Trash", {
+	[424420] = {"blue","orange","red"},
+	[424423] = "red",
+	[424431] = "yellow",
+	[424462] = "yellow",
+	[424621] = "orange",
+	[427342] = "yellow",
+	[427346] = "orange",
+	[427356] = "red",
+	[427484] = "orange",
+	[427583] = "red",
+	[427601] = "yellow",
+	[427609] = "red",
+	[427897] = "orange",
+	[427950] = "red",
+	[428150] = "red",
+	[429091] = "yellow",
+	[435088] = "green",
+	[448485] = "orange",
+	[448492] = "red",
+	[448515] = "purple",
+	[451818] = "orange",
 })
