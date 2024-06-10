d9c1591bc55413ed6de6030ef3a14dbd88e4ce64
Nick Towle <ntowle@gmail.com>
2024-06-10 18:27:14 -0400

    WarWithin/CinderbrewMeadery/GoldieBaronbottom: Add boss module

--- WarWithin/CinderbrewMeadery/Options/Colors.lua
+++ WarWithin/CinderbrewMeadery/Options/Colors.lua
@@ -9,6 +9,10 @@ BigWigs:AddColors("Benk Buzzbee", {
 })
 
 BigWigs:AddColors("Goldie Baronbottom", {
+	[435560] = "cyan",
+	[435622] = "yellow",
+	[436592] = "purple",
+	[436644] = {"blue","orange"},
 })
 
 BigWigs:AddColors("Cinderbrew Meadery Trash", {
--- WarWithin/CinderbrewMeadery/Options/Sounds.lua
+++ WarWithin/CinderbrewMeadery/Options/Sounds.lua
@@ -9,6 +9,10 @@ BigWigs:AddSounds("Benk Buzzbee", {
 })
 
 BigWigs:AddSounds("Goldie Baronbottom", {
+	[435560] = "info",
+	[435622] = "long",
+	[436592] = "alert",
+	[436644] = "alarm",
 })
 
 BigWigs:AddSounds("Cinderbrew Meadery Trash", {
