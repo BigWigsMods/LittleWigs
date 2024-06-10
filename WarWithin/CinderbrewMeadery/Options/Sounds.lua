ad0977096822b64ecae50af017ee2a3c0125b9c3
Nick Towle <ntowle@gmail.com>
2024-06-10 19:59:45 -0400

    WarWithin/CinderbrewMeadery/Ipa: Add boss module (#1053)

--- WarWithin/CinderbrewMeadery/Options/Sounds.lua
+++ WarWithin/CinderbrewMeadery/Options/Sounds.lua
@@ -8,6 +8,10 @@ BigWigs:AddSounds("Brew Master Aldryr", {
 })
 
 BigWigs:AddSounds("I'pa", {
+	[439031] = "alert",
+	[439202] = "long",
+	[439365] = "long",
+	[440147] = "alarm",
 })
 
 BigWigs:AddSounds("Benk Buzzbee", {
