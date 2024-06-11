eb04d9a3668788e98aaa9815bd116d6801a1ca1d
Nick Towle <ntowle@gmail.com>
2024-06-10 20:08:17 -0400

    WarWithin/CinderbrewMeadery/Trash: Add trash module (#1056)

--- WarWithin/CinderbrewMeadery/Locales/ptBR.lua
+++ WarWithin/CinderbrewMeadery/Locales/ptBR.lua
@@ -1,7 +1,16 @@
 local L = BigWigs:NewBossLocale("Cinderbrew Meadery Trash", "ptBR")
 if not L then return end
 if L then
-
+	--L.venture_co_pyromaniac = "Venture Co. Pyromaniac"
+	--L.hired_muscle = "Hired Muscle"
+	--L.tasting_room_attendant = "Tasting Room Attendant"
+	--L.chef_chewie = "Chef Chewie"
+	--L.flavor_scientist = "Flavor Scientist"
+	--L.careless_hopgoblin = "Careless Hopgoblin"
+	--L.bee_wrangler = "Bee Wrangler"
+	--L.venture_co_honey_harvester = "Venture Co. Honey Harvester"
+	--L.royal_jelly_purveyor = "Royal Jelly Purveyor"
+	--L.yes_man = "Yes Man"
 end
 
 L = BigWigs:NewBossLocale("Brew Master Aldryr", "ptBR")
--- WarWithin/CinderbrewMeadery/Options/Sounds.lua
+++ WarWithin/CinderbrewMeadery/Options/Sounds.lua
@@ -25,4 +25,18 @@ BigWigs:AddSounds("Goldie Baronbottom", {
 })
 
 BigWigs:AddSounds("Cinderbrew Meadery Trash", {
+	[434706] = "alarm",
+	[434761] = "alarm",
+	[434998] = "long",
+	[437721] = "alert",
+	[437956] = "alarm",
+	[439467] = "alarm",
+	[440687] = "alert",
+	[440876] = "alarm",
+	[441119] = "alarm",
+	[441434] = "info",
+	[441627] = "alert",
+	[442589] = "alarm",
+	[442995] = "alert",
+	[448619] = "alarm",
 })
