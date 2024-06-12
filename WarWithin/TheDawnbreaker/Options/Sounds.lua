b36698cd0497ccca72d0d6535ee82be46d4ab9c0
Nick Towle <ntowle@gmail.com>
2024-06-12 00:34:41 -0400

    WarWithin/TheDawnbreaker/Trash: Add trash module

--- WarWithin/TheDawnbreaker/Options/Sounds.lua
+++ WarWithin/TheDawnbreaker/Options/Sounds.lua
@@ -9,4 +9,19 @@ BigWigs:AddSounds("Rasha'nan Dawnbreaker", {
 })
 
 BigWigs:AddSounds("The Dawnbreaker Trash", {
+	[431304] = "alert",
+	[431309] = {"alert","info"},
+	[431349] = "alarm",
+	[431494] = "alarm",
+	[432448] = "alarm",
+	[432565] = "info",
+	[446615] = "info",
+	[450756] = "warning",
+	[450854] = "alarm",
+	[451091] = "info",
+	[451097] = "alert",
+	[451098] = "alarm",
+	[451102] = "alert",
+	[451107] = "alarm",
+	[451117] = "alarm",
 })
