6ac90a830db9cfa0b6f102dd7f41ee3fc68c9dae
Nick Towle <ntowle@gmail.com>
2024-06-12 02:10:12 -0400

    WarWithin/AraKara/KikatalTheHarvester: Add boss module (#1063)

--- WarWithin/AraKara/Options/Sounds.lua
+++ WarWithin/AraKara/Options/Sounds.lua
@@ -15,6 +15,10 @@ BigWigs:AddSounds("Anub'zekt", {
 })
 
 BigWigs:AddSounds("Ki'katal the Harvester", {
+	[432031] = "info",
+	[432117] = "alarm",
+	[432130] = "alarm",
+	[432227] = "alert",
 })
 
 BigWigs:AddSounds("Ara-Kara, City of Echoes Trash", {
