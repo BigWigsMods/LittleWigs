f730ce9720531b5dc7b1d36dcc9487ef32f7902d
Nick Towle <ntowle@gmail.com>
2024-06-12 02:04:50 -0400

    WarWithin/AraKara/KikatalTheHarvester: Add boss module

--- WarWithin/AraKara/KikatalTheHarvester.lua
+++ WarWithin/AraKara/KikatalTheHarvester.lua
@@ -15,19 +15,51 @@ mod:SetRespawnTime(30)
 
 function mod:GetOptions()
 	return {
-
+		-- Ki'katal the Harvester
+		432117, -- Cosmic Singularity
+		432227, -- Venom Volley
+		432130, -- Erupting Webs
+		-- Bloodworkers
+		{432031, "ME_ONLY"}, -- Grasping Blood
 	}
 end
 
 function mod:OnBossEnable()
-
+	self:Log("SPELL_CAST_START", "CosmicSingularity", 432117)
+	self:Log("SPELL_CAST_START", "VenomVolley", 432227)
+	self:Log("SPELL_CAST_START", "EruptingWebs", 432130)
+	self:Log("SPELL_AURA_APPLIED", "GraspingBloodApplied", 432031)
 end
 
 function mod:OnEngage()
-
+	self:CDBar(432130, 6.5) -- Erupting Webs
+	self:CDBar(432227, 16.2) -- Venom Volley
+	self:CDBar(432117, 26.5) -- Cosmic Singularity
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:CosmicSingularity(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "alarm")
+	self:CDBar(args.spellId, 46.1)
+end
+
+function mod:VenomVolley(args)
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, 23.0)
+end
+
+function mod:EruptingWebs(args)
+	self:Message(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+	self:CDBar(args.spellId, 18.2)
+end
+
+function mod:GraspingBloodApplied(args)
+	self:TargetMessage(args.spellId, "yellow", args.destName)
+	self:PlaySound(args.spellId, "info", nil, args.destName)
+end
--- WarWithin/AraKara/Options/Colors.lua
+++ WarWithin/AraKara/Options/Colors.lua
@@ -6,6 +6,10 @@ BigWigs:AddColors("Anub'zekt", {
 })
 
 BigWigs:AddColors("Ki'katal the Harvester", {
+	[432031] = {"blue","yellow"},
+	[432117] = "red",
+	[432130] = "orange",
+	[432227] = "yellow",
 })
 
 BigWigs:AddColors("Ara-Kara, City of Echoes Trash", {
