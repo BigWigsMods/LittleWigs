88698fedfae594169734cd04cdc64f9c6c1c6d39
Nick Towle <ntowle@gmail.com>
2024-06-12 00:24:34 -0400

    WarWithin/TheDawnbreaker/Shadowcrown: Add boss module

--- WarWithin/TheDawnbreaker/Options/Colors.lua
+++ WarWithin/TheDawnbreaker/Options/Colors.lua
@@ -1,5 +1,11 @@
 
-BigWigs:AddColors("Speaker Shadowcrown", {
+BigWigs:AddColors("Shadowcrown", {
+	[425264] = "purple",
+	[426735] = {"blue","yellow"},
+	[445996] = "orange",
+	[451026] = "cyan",
+	[453140] = "orange",
+	[453212] = "purple",
 })
 
 BigWigs:AddColors("Anub'ikkaj", {
