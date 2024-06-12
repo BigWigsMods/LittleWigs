2286b6f7e6d65de1067795c93a4ce6cef2a36057
Nick Towle <ntowle@gmail.com>
2024-06-12 02:18:23 -0400

    WarWithin/CityOfThreads/Trash: Add trash module (#1066)

--- WarWithin/CityOfThreads/Options/Colors.lua
+++ WarWithin/CityOfThreads/Options/Colors.lua
@@ -12,4 +12,13 @@ BigWigs:AddColors("Izo, the Grand Splicer", {
 })
 
 BigWigs:AddColors("City of Threads Trash", {
+	[442653] = "cyan",
+	[443430] = "red",
+	[443437] = {"blue","yellow"},
+	[443500] = "orange",
+	[448047] = {"blue","cyan"},
+	[450784] = "orange",
+	[451423] = "red",
+	[451543] = "orange",
+	[452162] = "red",
 })
--- WarWithin/CityOfThreads/Options/Sounds.lua
+++ WarWithin/CityOfThreads/Options/Sounds.lua
@@ -12,4 +12,13 @@ BigWigs:AddSounds("Izo, the Grand Splicer", {
 })
 
 BigWigs:AddSounds("City of Threads Trash", {
+	[442653] = "info",
+	[443430] = "alert",
+	[443437] = "alarm",
+	[443500] = "alarm",
+	[448047] = "info",
+	[450784] = "alarm",
+	[451423] = "long",
+	[451543] = "alarm",
+	[452162] = "alert",
 })
