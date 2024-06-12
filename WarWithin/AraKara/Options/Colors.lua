cbfde21a984b7002bda8e265721c2e2c27bc027e
Nick Towle <ntowle@gmail.com>
2024-06-12 02:07:27 -0400

    WarWithin/AraKara/Anubzekt: Add boss module (#1061)

--- WarWithin/AraKara/Options/Colors.lua
+++ WarWithin/AraKara/Options/Colors.lua
@@ -3,6 +3,11 @@ BigWigs:AddColors("Avanoxx", {
 })
 
 BigWigs:AddColors("Anub'zekt", {
+	[433740] = {"blue","yellow"},
+	[433766] = {"cyan","green"},
+	[435012] = "purple",
+	[439506] = {"blue","orange"},
+	[442210] = "red",
 })
 
 BigWigs:AddColors("Ki'katal the Harvester", {
