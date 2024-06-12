937bb2e6840ca5351ee2ca87212fb7c5368115f2
Nick Towle <ntowle@gmail.com>
2024-06-12 00:31:55 -0400

    WarWithin/TheDawnbreaker/Rashanan: Add boss module

--- WarWithin/TheDawnbreaker/Options/Colors.lua
+++ WarWithin/TheDawnbreaker/Options/Colors.lua
@@ -6,6 +6,15 @@ BigWigs:AddColors("Anub'ikkaj", {
 })
 
 BigWigs:AddColors("Rasha'nan Dawnbreaker", {
+	[434089] = "yellow",
+	[434407] = "red",
+	[434655] = {"blue","cyan","green"},
+	[435793] = "purple",
+	[448213] = "orange",
+	[448888] = "yellow",
+	[449528] = "green",
+	[449734] = {"green","yellow"},
+	["stages"] = "cyan",
 })
 
 BigWigs:AddColors("The Dawnbreaker Trash", {
