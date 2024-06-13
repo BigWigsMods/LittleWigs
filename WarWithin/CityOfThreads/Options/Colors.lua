3d572b4bdab7289cd7552bd1079df39c7800c899
Nick Towle <ntowle@gmail.com>
2024-06-13 01:29:32 -0400

    WarWithin/CityOfThreads/FangsOfTheQueen: Add boss module

--- WarWithin/CityOfThreads/Options/Colors.lua
+++ WarWithin/CityOfThreads/Options/Colors.lua
@@ -7,19 +7,41 @@ BigWigs:AddColors("Orator Krix'vizk", {
 })
 
 BigWigs:AddColors("Fangs of the Queen", {
+	[439522] = "cyan",
+	[439621] = "purple",
+	[439692] = "orange",
+	[440238] = {"blue","yellow"},
+	[440468] = "purple",
+	[441286] = {"blue","red"},
+	[441298] = {"blue","yellow"},
+	[458741] = {"blue","purple"},
 })
 
 BigWigs:AddColors("The Coaglamation", {
+	[438658] = "orange",
+	[441289] = "red",
+	[441395] = "yellow",
+	[448185] = "cyan",
 })
 
 BigWigs:AddColors("Izo, the Grand Splicer", {
+	[437700] = "orange",
+	[438860] = "yellow",
+	[439341] = "red",
+	[439401] = "yellow",
+	[439646] = "purple",
 })
 
 BigWigs:AddColors("City of Threads Trash", {
+	[434137] = "yellow",
 	[442653] = "cyan",
 	[443430] = "red",
 	[443437] = {"blue","yellow"},
 	[443500] = "orange",
+	[445813] = "orange",
+	[446086] = "red",
+	[446717] = "yellow",
+	[447271] = "orange",
 	[448047] = {"blue","cyan"},
 	[450784] = "orange",
 	[451423] = "red",
--- WarWithin/CityOfThreads/Options/Sounds.lua
+++ WarWithin/CityOfThreads/Options/Sounds.lua
@@ -7,19 +7,41 @@ BigWigs:AddSounds("Orator Krix'vizk", {
 })
 
 BigWigs:AddSounds("Fangs of the Queen", {
+	[439522] = "info",
+	[439621] = "alarm",
+	[439692] = "alarm",
+	[440238] = "alert",
+	[440468] = "alarm",
+	[441286] = "alert",
+	[441298] = "info",
+	[458741] = "warning",
 })
 
 BigWigs:AddSounds("The Coaglamation", {
+	[438658] = "alarm",
+	[441289] = "long",
+	[441395] = "alert",
+	[448185] = "info",
 })
 
 BigWigs:AddSounds("Izo, the Grand Splicer", {
+	[437700] = "alarm",
+	[438860] = "long",
+	[439341] = "alert",
+	[439401] = "info",
+	[439646] = "alarm",
 })
 
 BigWigs:AddSounds("City of Threads Trash", {
+	[434137] = "long",
 	[442653] = "info",
 	[443430] = "alert",
 	[443437] = "alarm",
 	[443500] = "alarm",
+	[445813] = "alert",
+	[446086] = "alert",
+	[446717] = "alert",
+	[447271] = "alarm",
 	[448047] = "info",
 	[450784] = "alarm",
 	[451423] = "long",
