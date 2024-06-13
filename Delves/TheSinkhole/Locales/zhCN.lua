97067acf3d1c15409924d17369df34613483a397
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-06-13 10:18:59 +0800

    Update zhCN.lua

--- Delves/TheSinkhole/Locales/zhCN.lua
+++ Delves/TheSinkhole/Locales/zhCN.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Mirror Master Murkna", "zhCN")
 if not L then return end
 if L then
-	--L.mirror_master_murkna = "Mirror Master Murkna"
+	L.mirror_master_murkna = "镜子大师默克纳"
 end
