c5fc0773d7c0b0eaaedbe1820a045d2833cb7bb9
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-06-13 10:17:12 +0800

    Update zhCN.lua

--- Delves/SkitteringBreach/Locales/zhCN.lua
+++ Delves/SkitteringBreach/Locales/zhCN.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Nerl'athekk the Skulking", "zhCN")
 if not L then return end
 if L then
-	--L.nerlathekk_the_skulking = "Nerl'athekk the Skulking"
+	L.nerlathekk_the_skulking = "潜伏者奈拉瑟克"
 end
