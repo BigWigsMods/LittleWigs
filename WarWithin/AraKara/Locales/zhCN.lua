3623e0eba51ec8d391adee01c9c1d43aa5334ed5
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-06-15 08:17:06 +0800

    Update zhCN.lua

--- WarWithin/AraKara/Locales/zhCN.lua
+++ WarWithin/AraKara/Locales/zhCN.lua
@@ -1,10 +1,10 @@
 local L = BigWigs:NewBossLocale("Ara-Kara, City of Echoes Trash", "zhCN")
 if not L then return end
 if L then
-	--L.ixin = "Ixin"
-	--L.nakt = "Nakt"
-	--L.atik = "Atik"
-	--L.hulking_bloodguard = "Hulking Bloodguard"
-	--L.bloodstained_webmage = "Bloodstained Webmage"
-	--L.blood_overseer = "Blood Overseer"
+	L.ixin = "伊克辛"
+	L.nakt = "纳克特"
+	L.atik = "阿提克"
+	L.hulking_bloodguard = "魁梧的血卫"
+	L.bloodstained_webmage = "沾血的网法师"
+	L.blood_overseer = "鲜血监督者"
 end
