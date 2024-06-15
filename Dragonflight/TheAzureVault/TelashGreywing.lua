5fcd2e9b9ad2ea728626ad5aa3a7b6ab2439e544
Nick Towle <ntowle@gmail.com>
2024-06-14 23:30:21 -0400

    Dragonflight/TheAzureVault/TelashGreywing: Ability counts

--- Dragonflight/TheAzureVault/TelashGreywing.lua
+++ Dragonflight/TheAzureVault/TelashGreywing.lua
@@ -12,7 +12,8 @@ mod:SetRespawnTime(30)
 -- Locals
 --
 
-local absoluteZeroCount = 0
+local absoluteZeroCount = 1
+local frostBombCount = 1
 local frostBombRemaining = 3
 local icyDevastatorRemaining = 2
 
@@ -34,17 +35,19 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "AbsoluteZeroApplied", 396722)
 	self:Log("SPELL_CAST_START", "FrostBomb", 386781)
 	self:Log("SPELL_AURA_APPLIED", "FrostBombApplied", 386881)
+	self:Log("SPELL_AURA_REMOVED", "FrostBombRemoved", 386881)
 	self:Log("SPELL_CAST_START", "IcyDevastator", 387151)
 end
 
 function mod:OnEngage()
-	absoluteZeroCount = 0
+	absoluteZeroCount = 1
+	frostBombCount = 1
 	frostBombRemaining = 1
 	icyDevastatorRemaining = 1
-	self:Bar(386781, 3.5) -- Frost Bomb
+	self:Bar(386781, 3.5, CL.count:format(self:SpellName(386781), frostBombCount)) -- Frost Bomb
 	self:Bar(387151, 14.5) -- Icy Devastator
 	-- cast at 100 energy, 20s energy gain + 1.5s delay
-	self:Bar(388008, 21.5, CL.count:format(self:SpellName(388008), 1)) -- Absolute Zero (1)
+	self:Bar(388008, 21.5, CL.count:format(self:SpellName(388008), absoluteZeroCount)) -- Absolute Zero
 end
 
 --------------------------------------------------------------------------------
@@ -56,11 +59,10 @@ do
 
 	function mod:AbsoluteZero(args)
 		playerList = {}
-		absoluteZeroCount = absoluteZeroCount + 1
-		self:StopBar(386781) -- Frost Bomb
+		self:StopBar(CL.count:format(self:SpellName(386781), frostBombCount)) -- Frost Bomb
 		self:StopBar(387151) -- Icy Devastator
-		self:StopBar(CL.count:format(args.spellName, absoluteZeroCount)) -- Absolute Zero (n)
-		self:Message(args.spellId, "red", CL.count:format(args.spellName, absoluteZeroCount)) -- Absolute Zero (n)
+		self:StopBar(CL.count:format(args.spellName, absoluteZeroCount)) -- Absolute Zero
+		self:Message(args.spellId, "red", CL.count:format(args.spellName, absoluteZeroCount)) -- Absolute Zero
 		self:PlaySound(args.spellId, "long")
 		self:CastBar(args.spellId, 8)
 	end
@@ -68,10 +70,11 @@ do
 	function mod:AbsoluteZeroSuccess(args)
 		frostBombRemaining = 3
 		icyDevastatorRemaining = 2
-		self:Bar(386781, 4.3) -- Frost Bomb
+		self:Bar(386781, 4.3, CL.count:format(self:SpellName(386781), frostBombCount)) -- Frost Bomb
 		self:Bar(387151, 15.2) -- Icy Devastator
 		-- 8s cast at 100 energy: 50s energy gain + 2.8s or 6.4s delay
-		self:Bar(args.spellId, 52.8, CL.count:format(args.spellName, absoluteZeroCount + 1)) -- or 56.4
+		absoluteZeroCount = absoluteZeroCount + 1
+		self:CDBar(args.spellId, 52.8, CL.count:format(args.spellName, absoluteZeroCount)) -- or 56.4
 	end
 
 	function mod:AbsoluteZeroApplied(args)
@@ -84,22 +87,23 @@ do
 end
 
 function mod:FrostBomb(args)
-	self:Message(args.spellId, "orange")
+	self:StopBar(CL.count:format(args.spellName, frostBombCount))
+	self:Message(args.spellId, "orange", CL.count:format(args.spellName, frostBombCount))
 	self:PlaySound(args.spellId, "alert")
 	-- two possibilities:
 	-- 1. AZ cycle is 64.4s and FB is [(AZ 8) 4.3, (ID) 18.2, 15.8 (ID)]
 	-- 2. AZ cycle is 60.8s and FB is [(AZ 8) 4.3, (ID) 18.2, (ID) 23.1]
+	frostBombCount = frostBombCount + 1
 	frostBombRemaining = frostBombRemaining - 1
 	if frostBombRemaining == 2 then
-		self:Bar(args.spellId, 18.2)
+		self:Bar(args.spellId, 18.2, CL.count:format(args.spellName, frostBombCount))
 	elseif frostBombRemaining == 1 then
-		self:CDBar(args.spellId, 15.8) -- or 23.1
+		self:CDBar(args.spellId, 15.8, CL.count:format(args.spellName, frostBombCount)) -- or 23.1
 	elseif frostBombRemaining == 0 then
-		self:StopBar(args.spellId)
-		if icyDevastatorRemaining == 1 and absoluteZeroCount > 0 then
+		if icyDevastatorRemaining == 1 and absoluteZeroCount > 1 then
 			-- fix final timers for the FB -> ID -> FB -> FB -> ID -> AZ variation
 			self:Bar(387151, 10.8) -- Icy Devastator
-			self:Bar(388008, {18.1, 52.8}, CL.count:format(self:SpellName(388008), absoluteZeroCount + 1)) -- Absolute Zero (n)
+			self:Bar(388008, {18.1, 52.8}, CL.count:format(self:SpellName(388008), absoluteZeroCount)) -- Absolute Zero
 		end
 	end
 end
@@ -107,6 +111,12 @@ end
 function mod:FrostBombApplied(args)
 	if self:Me(args.destGUID) then
 		self:TargetBar(386781, 5, args.destName)
+	end
+end
+
+function mod:FrostBombRemoved(args)
+	if self:Me(args.destGUID) then
+		self:StopBar(386781, args.destName)
 	end
 end
 
@@ -133,7 +143,7 @@ do
 			self:StopBar(args.spellId)
 			if frostBombRemaining == 1 then
 				-- fix final timers for the FB -> ID -> FB -> ID -> FB -> AZ variation
-				self:Bar(386781, 7.3) -- Frost Bomb
+				self:Bar(386781, 7.3, CL.count:format(self:SpellName(386781), frostBombCount)) -- Frost Bomb
 			end
 		end
 	end
