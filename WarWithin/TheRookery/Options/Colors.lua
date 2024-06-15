17f3edaa2f7d5ca9edd92e7f71c9c5493206406d
Nick Towle <ntowle@gmail.com>
2024-06-15 11:42:03 -0400

    WarWithin/TheRookery/Kyrioss: Add boss module

--- WarWithin/TheRookery/Options/Colors.lua
+++ WarWithin/TheRookery/Options/Colors.lua
@@ -1,5 +1,9 @@
 
 BigWigs:AddColors("Kyrioss", {
+	[419870] = "orange",
+	[420739] = {"blue","yellow"},
+	[424148] = {"blue","red"},
+	[444123] = "cyan",
 })
 
 BigWigs:AddColors("Stormguard Gorren", {
