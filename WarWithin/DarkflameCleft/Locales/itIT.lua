6ab675e29f6afb47ba58c5a2b5b1b0d386217090
Nick Towle <ntowle@gmail.com>
2024-06-17 18:02:04 -0400

    WarWithin/DarkflameCleft/Trash: Add trash module

--- WarWithin/DarkflameCleft/Locales/itIT.lua
+++ WarWithin/DarkflameCleft/Locales/itIT.lua
@@ -1,7 +1,13 @@
 local L = BigWigs:NewBossLocale("Darkflame Cleft Trash", "itIT")
 if not L then return end
 if L then
-
+	--L.rank_overseer = "Rank Overseer"
+	--L.lowly_moleherd = "Lowly Moleherd"
+	--L.wandering_candle = "Wandering Candle"
+	--L.blazing_fiend = "Blazing Fiend"
+	--L.sootsnout = "Sootsnout"
+	--L.torchsnarl = "Torchsnarl"
+	--L.shuffling_horror = "Shuffling Horror"
 end
 
 L = BigWigs:NewBossLocale("The Darkness", "itIT")
