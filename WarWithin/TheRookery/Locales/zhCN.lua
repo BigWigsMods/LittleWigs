0d30ad02814a792fa52aa271bf9ec70f50540589
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-06-18 09:35:20 +0800

    Update zhCN (#1088)

--- WarWithin/TheRookery/Locales/zhCN.lua
+++ WarWithin/TheRookery/Locales/zhCN.lua
@@ -1,14 +1,14 @@
 local L = BigWigs:NewBossLocale("The Rookery Trash", "zhCN")
 if not L then return end
 if L then
-	--L.quartermaster_koratite = "Quartermaster Koratite"
-	--L.cursed_stormrider = "Cursed Stormrider"
-	--L.cursed_rooktender = "Cursed Rooktender"
-	--L.unruly_stormrook = "Unruly Stormrook"
-	--L.corrupted_rookguard = "Corrupted Rookguard"
-	--L.corrupted_oracle = "Corrupted Oracle"
-	--L.corrupted_thunderer = "Corrupted Thunderer"
-	--L.inflicted_civilian = "Inflicted Civilian"
-	--L.void_ascendant = "Void Ascendant"
-	--L.radiating_voidstone = "Radiating Voidstone"
+	L.quartermaster_koratite = "军需官科拉泰特"
+	L.cursed_stormrider = "被诅咒的驭雷者"
+	L.cursed_rooktender = "被诅咒的雷鸫看护者"
+	L.unruly_stormrook = "任性的雷鸫"
+	L.corrupted_rookguard = "被腐化的雷鸫守卫"
+	L.corrupted_oracle = "被腐化的先知"
+	L.corrupted_thunderer = "被腐化的雷霆使"
+	L.inflicted_civilian = "痛苦的平民"
+	L.void_ascendant = "虚空晋升者"
+	L.radiating_voidstone = "辐射虚空石"
 end
