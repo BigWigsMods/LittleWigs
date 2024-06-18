05a5e670f6be412c5dea7e25fedde8177b209b3d
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-06-18 08:13:04 +0800

    Update zhCN.lua

--- WarWithin/DarkflameCleft/Locales/zhCN.lua
+++ WarWithin/DarkflameCleft/Locales/zhCN.lua
@@ -1,13 +1,13 @@
 local L = BigWigs:NewBossLocale("Darkflame Cleft Trash", "zhCN")
 if not L then return end
 if L then
-	--L.rank_overseer = "Rank Overseer"
-	--L.lowly_moleherd = "Lowly Moleherd"
-	--L.wandering_candle = "Wandering Candle"
-	--L.blazing_fiend = "Blazing Fiend"
-	--L.sootsnout = "Sootsnout"
-	--L.torchsnarl = "Torchsnarl"
-	--L.shuffling_horror = "Shuffling Horror"
+	L.rank_overseer = "职阶监督者"
+	L.lowly_moleherd = "低贱的鼹鼠倌"
+	L.wandering_candle = "漫游蜡烛"
+	L.blazing_fiend = "炽焰魔"
+	L.sootsnout = "灰鼻"
+	L.torchsnarl = "炬龇"
+	L.shuffling_horror = "残步恐魔"
 end
 
 L = BigWigs:NewBossLocale("The Darkness", "zhCN")
