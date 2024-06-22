05e1cdfe13970e0b0015167896123e189384bf58
Nick Towle <ntowle@gmail.com>
2024-06-22 17:07:15 -0400

    Delves/TheUnderkeep/ResearcherXikvik: Add boss module (#1092)

--- Delves/TheUnderkeep/Locales/zhTW.lua
+++ Delves/TheUnderkeep/Locales/zhTW.lua
@@ -3,3 +3,8 @@ if not L then return end
 if L then
 	--L.researcher_venkex = "Researcher Ven'kex"
 end
+
+L = BigWigs:NewBossLocale("Researcher Xik'vik", "zhTW")
+if L then
+	--L.researcher_xikvik = "Researcher Xik'vik"
+end
