0234534d90a18b0428ae65e73a6b829fa10f7287
Nick Towle <ntowle@gmail.com>
2024-06-22 17:07:53 -0400

    Delves/TheSinkhole/LeviathanCaller: Add boss module (#1093)

--- Delves/TheSinkhole/Locales/ruRU.lua
+++ Delves/TheSinkhole/Locales/ruRU.lua
@@ -3,3 +3,8 @@ if not L then return end
 if L then
 	--L.mirror_master_murkna = "Mirror Master Murkna"
 end
+
+L = BigWigs:NewBossLocale("Leviathan Caller", "ruRU")
+if L then
+	--L.leviathan_caller = "Leviathan Caller"
+end
--- Delves/TheSinkhole/Locales/zhCN.lua
+++ Delves/TheSinkhole/Locales/zhCN.lua
@@ -3,3 +3,8 @@ if not L then return end
 if L then
 	L.mirror_master_murkna = "镜子大师默克纳"
 end
+
+L = BigWigs:NewBossLocale("Leviathan Caller", "zhCN")
+if L then
+	--L.leviathan_caller = "Leviathan Caller"
+end
