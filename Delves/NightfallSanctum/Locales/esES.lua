7e0b0ff38b26297f57ca06e60afb5f75de10dc00
Nick Towle <ntowle@gmail.com>
2024-06-30 14:32:39 -0400

    Delves/NightfallSanctum/ReformedFury: Add boss module (#1099)

--- Delves/NightfallSanctum/Locales/esES.lua
+++ Delves/NightfallSanctum/Locales/esES.lua
@@ -3,3 +3,9 @@ if not L then return end
 if L then
 	--L.speaker_halven = "Speaker Halven"
 end
+
+L = BigWigs:NewBossLocale("Reformed Fury", "esES") or BigWigs:NewBossLocale("Reformed Fury", "esMX")
+if L then
+	--L.speaker_davenruth = "Speaker Davenruth"
+	--L.reformed_fury = "Reformed Fury"
+end
--- Delves/NightfallSanctum/Locales/itIT.lua
+++ Delves/NightfallSanctum/Locales/itIT.lua
@@ -3,3 +3,9 @@ if not L then return end
 if L then
 	--L.speaker_halven = "Speaker Halven"
 end
+
+L = BigWigs:NewBossLocale("Reformed Fury", "itIT")
+if L then
+	--L.speaker_davenruth = "Speaker Davenruth"
+	--L.reformed_fury = "Reformed Fury"
+end
