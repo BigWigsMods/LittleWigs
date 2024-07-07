a4614bacb665055487f41c464826444189c07ef6
Nick Towle <ntowle@gmail.com>
2024-07-07 14:28:35 -0400

    BfA/SiegeOfBoralus/Trash: Azerite Charge, cleanups

--- BfA/SiegeOfBoralus/Trash.lua
+++ BfA/SiegeOfBoralus/Trash.lua
@@ -63,7 +63,7 @@ function mod:GetOptions()
 		-- Ashvane Cannoneer
 		268260, -- Broadside
 		-- Ashvane Commander
-		272874, -- Trample
+		{454437, "SAY"}, -- Azerite Charge
 		275826, -- Bolstering Shout
 		-- Ashvane Invader
 		275835, -- Stinging Venom Coating
@@ -86,8 +86,6 @@ function mod:GetOptions()
 		256627, -- Slobber Knocker
 		-- Kul Tiran Vanguard
 		257288, -- Heavy Slash
-		-- Kul Tiran Marksman
-		257641, -- Molten Slug
 		-- Snarling Dockhound
 		256897, -- Clamping Jaws
 		-- Riptide Shredder
@@ -99,7 +97,7 @@ function mod:GetOptions()
 		256673, -- Immolation
 	}, {
 		[268260] = L.cannoneer,
-		[272874] = L.commander,
+		[454437] = L.commander,
 		[275835] = L.invader,
 		[272421] = L.spotter,
 		[257169] = L.demolisher,
@@ -110,7 +108,6 @@ function mod:GetOptions()
 		[256957] = L.wavetender,
 		[256627] = L.halberd,
 		[257288] = L.vanguard,
-		[257641] = L.marksman,
 		[256897] = L.dockhound,
 		[256866] = L.shredder,
 		[257036] = L.packmaster,
@@ -118,8 +115,70 @@ function mod:GetOptions()
 	}
 end
 
+-- XXX remove the block below when TWW is live
+if not BigWigsLoader.isBeta then
+	function mod:GetOptions()
+		return {
+			-- Ashvane Cannoneer
+			268260, -- Broadside
+			-- Ashvane Commander
+			275826, -- Bolstering Shout
+			-- Ashvane Invader
+			275835, -- Stinging Venom Coating
+			-- Ashvane Spotter
+			{272421, "SAY"}, -- Sighted Artillery
+			-- Bilge Rat Demolisher
+			257169, -- Terrifying Roar
+			272711, -- Crushing Slam
+			-- Bilge Rat Pillager
+			272827, -- Viscous Slobber
+			-- Bilge Rat Tempest
+			274569, -- Revitalizing Mist
+			-- Bilge Rat Buccaneer
+			272546, -- Banana Rampage
+			-- Irontide Raider
+			257170, -- Savage Tempest
+			-- Kul Tiran Wavetender
+			256957, -- Watertight Shell
+			-- Kul Tiran Halberd
+			256627, -- Slobber Knocker
+			-- Kul Tiran Vanguard
+			257288, -- Heavy Slash
+			-- Snarling Dockhound
+			256897, -- Clamping Jaws
+			-- Riptide Shredder
+			256866, -- Iron Ambush
+			-- Dockhound Packmaster
+			{257036, "SAY"}, -- Feral Charge
+			-- Blacktar Bomber
+			256640, -- Burning Tar
+			256673, -- Immolation
+		}, {
+			[268260] = L.cannoneer,
+			[275826] = L.commander,
+			[275835] = L.invader,
+			[272421] = L.spotter,
+			[257169] = L.demolisher,
+			[272827] = L.pillager,
+			[274569] = L.tempest,
+			[272546] = L.buccaneer,
+			[257170] = L.raider,
+			[256957] = L.wavetender,
+			[256627] = L.halberd,
+			[257288] = L.vanguard,
+			[256897] = L.dockhound,
+			[256866] = L.shredder,
+			[257036] = L.packmaster,
+			[256640] = L.bomber,
+		}
+	end
+end
+
 function mod:OnBossEnable()
 	-- Ashvane Commander
+	if BigWigsLoader.isBeta then -- XXX remove check when TWW is live
+		self:Log("SPELL_AURA_APPLIED", "AzeriteCharge", 454437)
+	end
 	self:Log("SPELL_CAST_START", "BolsteringShout", 275826)
 	self:Log("SPELL_CAST_SUCCESS", "BolsteringShoutSuccess", 275826)
 	-- Ashvane Invader
@@ -137,14 +196,11 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "BananaRampage", 272546)
 	-- Irontide Raider
 	self:Log("SPELL_CAST_START", "SavageTempest", 257170)
-	self:Death("IrontideRaiderDeath", 129369)
 	-- Kul Tiran Wavetender
 	self:Log("SPELL_CAST_START", "WatertightShell", 256957)
 	self:Log("SPELL_AURA_APPLIED", "WatertightShellApplied", 256957)
 	-- Kul Tiran Halberd
 	self:Log("SPELL_CAST_START", "SlobberKnocker", 256627)
-	-- Kul Tiran Marksman
-	self:Log("SPELL_CAST_START", "MoltenSlug", 257641)
 	-- Snarling Dockhound
 	self:Log("SPELL_CAST_SUCCESS", "ClampingJaws", 256897)
 	-- Riptide Shredder
@@ -156,7 +212,6 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "Immolation", 256673)
 
 	-- Ashvane Cannoneer's Broadside
-	-- Ashvane Commander's Trample
 	-- Kul Tiran Vanguard's Heavy Slash
 	self:RegisterEvent("UNIT_SPELLCAST_START")
 end
@@ -165,14 +220,24 @@ end
 -- Event Handlers
 --
 
+function mod:AzeriteCharge(args)
+	self:TargetMessage(args.spellId, "orange", args.destName)
+	self:PlaySound(args.spellId, "alarm", nil, args.destName)
+	if self:Me(args.destGUID) then
+		self:Say(args.spellId, nil, nil, "Azerite Charge")
+	end
+end
+
 function mod:BolsteringShout(args)
-	self:Message(args.spellId, "orange", CL.casting:format(args.spellName))
-	self:PlaySound(args.spellId, "info")
+	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
+	self:PlaySound(args.spellId, "alert")
 end
 
 function mod:BolsteringShoutSuccess(args)
-	self:Message(args.spellId, "red")
-	self:PlaySound(args.spellId, "alarm")
+	if self:Dispeller("magic", true) then
+		self:Message(args.spellId, "yellow")
+		self:PlaySound(args.spellId, "alarm")
+	end
 end
 
 do
@@ -212,7 +277,7 @@ function mod:ViscousSlobber(args)
 end
 
 function mod:RevitalizingMist(args)
-	self:Message(args.spellId, "orange")
+	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "alert")
 end
 
@@ -224,10 +289,6 @@ end
 function mod:SavageTempest(args)
 	self:Message(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "long")
-end
-
-function mod:IrontideRaiderDeath(args)
-	self:StopBar(257170) -- Savage Tempest
 end
 
 function mod:WatertightShell(args)
@@ -245,24 +306,6 @@ end
 function mod:SlobberKnocker(args)
 	self:Message(args.spellId, "purple")
 	self:PlaySound(args.spellId, "alarm")
-end
-
-do
-	local prev = 0
-	local function printTarget(self, name, guid)
-		if self:Me(guid) then
-			local t = GetTime()
-			if t - prev > 2 then
-				prev = t
-				self:PersonalMessage(257641) -- Molten Slug
-				self:PlaySound(257641, "info") -- Molten Slug
-			end
-		end
-	end
-
-	function mod:MoltenSlug(args)
-		self:GetUnitTarget(printTarget, 0.4, args.sourceGUID)
-	end
 end
 
 function mod:ClampingJaws(args)
@@ -315,10 +358,6 @@ do
 				self:Message(spellId, "orange")
 				self:PlaySound(spellId, "alarm")
 			end
-		elseif spellId == 272874 and castGUID ~= prev then -- Trample
-			prev = castGUID
-			self:Message(spellId, "orange")
-			self:PlaySound(spellId, "info")
 		elseif spellId == 257288 and castGUID ~= prev and self:MobId(self:UnitGUID(unit)) == 138019 then -- Heavy Slash, Kul Tiran Vanguard
 			prev = castGUID
 			self:Message(spellId, "orange")
