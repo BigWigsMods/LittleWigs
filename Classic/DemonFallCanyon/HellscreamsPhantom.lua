530e8a1a59a55ab4ffae0e063259b0c20a2d7a79
funkydude <funkydude@users.noreply.github.com>
2024-07-13 21:54:09 +0100

    Classic/DemonFallCanyon/HellscreamsPhantom: Add Mortal Combo

--- Classic/DemonFallCanyon/HellscreamsPhantom.lua
+++ Classic/DemonFallCanyon/HellscreamsPhantom.lua
@@ -30,15 +30,18 @@ function mod:GetOptions()
 	return {
 		460254, -- Spiritstorm
 		460084, -- Spectral Shout
+		460119, -- Mortal Combo
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "MortalCombo", 460119)
 	self:Log("SPELL_CAST_START", "Spiritstorm", 460254)
 	self:Log("SPELL_CAST_SUCCESS", "SpectralShout", 460084)
 end
 
 function mod:OnEngage()
+	self:CDBar(460119, 14.6) -- Mortal Combo
 	self:CDBar(460254, 21.3) -- Spiritstorm
 	self:CDBar(460084, 27.5) -- Spectral Shout
 end
@@ -47,14 +50,20 @@ end
 -- Event Handlers
 --
 
+function mod:MortalCombo(args)
+	self:Message(args.spellId, "purple")
+	self:CDBar(args.spellId, 14.5)
+	self:PlaySound(args.spellId, "info")
+end
+
 function mod:Spiritstorm(args)
 	self:Message(args.spellId, "orange")
-	self:PlaySound(args.spellId, "alarm")
 	self:CDBar(args.spellId, 21.0)
+	self:PlaySound(args.spellId, "alarm")
 end
 
 function mod:SpectralShout(args)
 	self:TargetMessage(args.spellId, "purple", args.destName)
-	self:PlaySound(args.spellId, "alert", nil, args.destName)
 	self:CDBar(args.spellId, 25.9)
+	self:PlaySound(args.spellId, "alert", nil, args.destName)
 end
