e3b847315dbbef877db4f772ad491ae9634107bb
funkydude <funkydude@users.noreply.github.com>
2024-07-14 02:49:48 +0100

    Classic/DemonFallCanyon/Zilbagob: Chaos Chopper targets

--- Classic/DemonFallCanyon/Zilbagob.lua
+++ Classic/DemonFallCanyon/Zilbagob.lua
@@ -10,6 +10,13 @@ mod:SetEncounterID(3029)
 mod:SetAllowWin(true)
 
 --------------------------------------------------------------------------------
+-- Locals
+--
+
+local bossGUID = nil
+local loopCount = 0
+
+--------------------------------------------------------------------------------
 -- Localization
 --
 
@@ -29,16 +36,19 @@ end
 function mod:GetOptions()
 	return {
 		460403, -- Kerosene Kick
-		460408, -- Chaos Chopper
+		{460408, "SAY"}, -- Chaos Chopper
 	}
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "KeroseneKick", 460403)
-	self:Log("SPELL_CAST_START", "ChaosChopper", 460408)
+	self:Log("SPELL_CAST_START", "ChaosChopperStart", 460408)
+	self:Log("SPELL_CAST_SUCCESS", "ChaosChopper", 460408)
 end
 
 function mod:OnEngage()
+	bossGUID = nil
+	loopCount = 0
 	self:CDBar(460403, 11.3) -- Kerosene Kick
 	self:CDBar(460408, 27.5) -- Chaos Chopper
 end
@@ -49,12 +59,41 @@ end
 
 function mod:KeroseneKick(args)
 	self:Message(args.spellId, "orange")
-	self:CDBar(args.spellId, 30.8)
+	self:CDBar(args.spellId, 30.4)
 	self:PlaySound(args.spellId, "alarm")
 end
 
-function mod:ChaosChopper(args)
-	self:Message(args.spellId, "red")
-	self:CDBar(args.spellId, 25.9)
-	self:PlaySound(args.spellId, "alarm")
+function mod:ChaosChopperStart(args)
+	self:Message(args.spellId, "red", CL.incoming:format(args.spellName))
+	self:CDBar(args.spellId, 23.9)
+	self:PlaySound(args.spellId, "alert")
+end
+
+do
+	local function LoopThreat()
+		loopCount = loopCount + 1
+		if not mod:IsEngaged() or loopCount > 14 then return end
+		mod:SimpleTimer(LoopThreat, 1)
+
+		local bossUnit = bossGUID and mod:GetUnitIdByGUID(bossGUID)
+		if bossUnit then
+			for unit in mod:IterateGroup() do
+				-- Unit is a threat target of something that isn't the boss
+				if mod:ThreatTarget(unit) and not mod:ThreatTarget(unit, bossUnit) then
+					loopCount = 100
+					local unitName = mod:UnitName(unit)
+					mod:TargetMessage(460408, "red", mod:UnitName(unitName))
+					if mod:Me(mod:UnitGUID(unit)) then
+						mod:Say(460408, nil, nil, "Chaos Chopper")
+						mod:PlaySound(460408, "warning", nil, unitName)
+					end
+				end
+			end
+		end
+	end
+	function mod:ChaosChopper(args)
+		loopCount = 0
+		bossGUID = args.sourceGUID
+		self:SimpleTimer(LoopThreat, 1)
+	end
 end
