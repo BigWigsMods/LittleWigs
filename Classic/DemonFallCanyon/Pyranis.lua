c1df59d869ad67cd11c1147d48e96b3d02464248
funkydude <funkydude@users.noreply.github.com>
2024-07-14 02:51:49 +0100

    Classic/DemonFallCanyon/Pyranis: Add Wrath, cleanups

--- Classic/DemonFallCanyon/Pyranis.lua
+++ Classic/DemonFallCanyon/Pyranis.lua
@@ -32,6 +32,9 @@ function mod:GetOptions()
 		460666, -- Summon Dread Seed
 		460676, -- Summon Dread Lasher
 		460545, -- Enshrouding Fog
+		460671, -- Wrath
+	},nil,{
+		[460671] = CL.interruptible, -- Wrath (Interruptible)
 	}
 end
 
@@ -41,7 +44,9 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "SummonDreadLasher", 460676)
 	self:Log("SPELL_CAST_START", "EnshroudingFog", 460545)
 	self:Log("SPELL_CAST_SUCCESS", "EnshroudingFogSuccess", 460545)
+	self:Log("SPELL_AURA_APPLIED", "EnshroudingFogApplied", 460545)
 	self:Log("SPELL_AURA_REMOVED", "EnshroudingFogRemoved", 460545)
+	self:Log("SPELL_CAST_START", "Wrath", 460671)
 end
 
 function mod:OnEngage()
@@ -74,6 +79,7 @@ end
 
 do
 	local enshroudingFogStart = 0
+	local enshroudingFogCount = 0
 
 	function mod:EnshroudingFog(args)
 		self:StopBar(460577) -- Summon Dread Fungus
@@ -84,16 +90,21 @@ do
 	end
 
 	function mod:EnshroudingFogSuccess(args)
+		enshroudingFogCount = 0
 		enshroudingFogStart = args.time
 	end
 
-	do
-		local prev = 0
-		function mod:EnshroudingFogRemoved(args)
-			-- this debuff is on the whole party during Enshrouding Fog
-			local t = args.time
-			if t - prev > 5 then
-				prev = t
+	function mod:EnshroudingFogApplied(args)
+		if self:Player(args.destFlags) then -- Players, not pets
+			enshroudingFogCount = enshroudingFogCount + 1
+		end
+	end
+
+	function mod:EnshroudingFogRemoved(args)
+		-- this debuff is on the whole party during Enshrouding Fog
+		if self:Player(args.destFlags) then -- Players, not pets
+			enshroudingFogCount = enshroudingFogCount - 1
+			if enshroudingFogCount == 0 then
 				local enshroudingFogDuration = args.time - enshroudingFogStart
 				self:Message(args.spellId, "green", CL.removed_after:format(args.spellName, enshroudingFogDuration))
 				self:PlaySound(args.spellId, "info")
@@ -101,4 +112,9 @@ do
 			end
 		end
 	end
+end
+
+function mod:Wrath(args)
+	self:Message(args.spellId, "yellow", CL.extra:format(args.spellName, CL.interruptible))
+	self:PlaySound(args.spellId, "alarm")
 end
