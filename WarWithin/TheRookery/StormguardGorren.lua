2ca13e49790a667d5bf056693515c20a6dee782e
Nick Towle <ntowle@gmail.com>
2024-07-19 14:19:48 -0400

    WarWithin/TheRookery/StormguardGorren: Engage update, timer updates

--- WarWithin/TheRookery/StormguardGorren.lua
+++ WarWithin/TheRookery/StormguardGorren.lua
@@ -26,7 +26,6 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
-	self:RegisterEvent("ENCOUNTER_START") -- XXX no boss frames
 	self:Log("SPELL_CAST_START", "ChaoticCorruption", 424737)
 	self:Log("SPELL_CAST_START", "DarkGravity", 425048)
 	self:Log("SPELL_CAST_START", "CrushReality", 424958)
@@ -38,20 +37,13 @@ end
 
 function mod:OnEngage()
 	self:CDBar(424958, 8.1) -- Crush Reality
-	self:CDBar(425048, 22.7) -- Dark Gravity
-	self:CDBar(424737, 33.7) -- Chaotic Corruption
+	self:CDBar(425048, 15.8) -- Dark Gravity
+	self:CDBar(424737, 31.6) -- Chaotic Corruption
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
-
--- XXX no boss frames
-function mod:ENCOUNTER_START(_, id)
-	if id == self.engageId then
-		self:Engage()
-	end
-end
 
 function mod:ChaoticCorruption(args)
 	self:Message(args.spellId, "red")
@@ -68,7 +60,7 @@ end
 function mod:CrushReality(args)
 	self:Message(args.spellId, "orange")
 	self:PlaySound(args.spellId, "alarm")
-	self:CDBar(args.spellId, 23.0)
+	self:CDBar(args.spellId, 15.8)
 end
 
 -- Mythic
