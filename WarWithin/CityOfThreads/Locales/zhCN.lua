041b24f5d27fdf714fe3e78ffd4b804b4840c79c
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-07-20 10:09:50 +0800

    Update zhCN (#1114)

--- WarWithin/CityOfThreads/Locales/zhCN.lua
+++ WarWithin/CityOfThreads/Locales/zhCN.lua
@@ -6,7 +6,7 @@ if L then
 	L.royal_swarmguard = "皇家虫群卫士"
 	L.xephitik = "谢非提克"
 	L.pale_priest = "苍白牧师"
-	L.eye_of_the_queen = "女王之眼"
+	L.eye_of_the_queen = "高耸虚居居民"
 	L.covert_webmancer = "隐秘网士"
 	L.royal_venomshell = "皇家毒壳者"
 	--L.unstable_test_subject = "Unstable Test Subject"
@@ -14,7 +14,7 @@ if L then
 	L.elder_shadeweaver = "长者织影"
 	L.hulking_warshell = "魁梧的战甲尉"
 
-	--L.xephitik_defeated_trigger = "Enough!"
-	--L.fangs_of_the_queen_warmup_trigger = "The Transformatory was once the home of our sacred evolution."
+	L.xephitik_defeated_trigger = "够了！"
+	L.fangs_of_the_queen_warmup_trigger = "蜕躯工厂曾经是我们神圣的进化之家。"
 	L.izo_warmup_trigger = "够了！你们赢得了成为我藏品的机会，让我送你们一程。"
 end
