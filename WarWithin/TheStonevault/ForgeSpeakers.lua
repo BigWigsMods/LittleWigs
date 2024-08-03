a83c45eb5c8a0ade66a88f80e41f6a7a8dd6bbb7
Nick Towle <ntowle@gmail.com>
2024-08-03 01:44:22 -0400

    WarWithin/TheStonevault/ForgeSpeakers: Timer updates

--- WarWithin/TheStonevault/ForgeSpeakers.lua
+++ WarWithin/TheStonevault/ForgeSpeakers.lua
@@ -21,13 +21,13 @@ function mod:GetOptions()
 	return {
 		439577, -- Silenced Speaker
 		-- Speaker Brokk
-		445541, -- Activate Ventilation
+		445541, -- Exhaust Vents
 		430097, -- Molten Metal
 		428202, -- Scrap Song
 		-- Speaker Dorlita
-		428508, -- Deconstruction
-		{428711, "TANK"}, -- Molten Hammer
-		449167, -- Lava Expulsion
+		428508, -- Blazing Crescendo
+		{428711, "TANK"}, -- Igneous Hammer
+		449167, -- Lava Cannon
 	}, {
 		[445541] = -28459, -- Speaker Brokk
 		[428508] = -28461, -- Speaker Dorlita
@@ -39,24 +39,24 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "SilencedSpeaker", 439577)
 
 	-- Speaker Brokk
-	self:Log("SPELL_CAST_START", "ActivateVentilation", 445541)
+	self:Log("SPELL_CAST_START", "ExhaustVents", 445541)
 	self:Log("SPELL_CAST_START", "MoltenMetal", 430097)
 	self:Log("SPELL_CAST_START", "ScrapSong", 428202)
 
 	-- Speaker Dorlita
-	self:Log("SPELL_CAST_SUCCESS", "Deconstruction", 428508)
-	self:Log("SPELL_CAST_START", "MoltenHammer", 428711)
-	self:Log("SPELL_CAST_START", "LavaExpulsion", 449167)
+	self:Log("SPELL_CAST_SUCCESS", "BlazingCrescendo", 428508)
+	self:Log("SPELL_CAST_START", "IgneousHammer", 428711)
+	self:Log("SPELL_CAST_START", "LavaCannon", 449167)
 end
 
 function mod:OnEngage()
 	self:SetStage(1)
 	self:CDBar(430097, 4.5) -- Molten Metal
-	self:CDBar(428711, 6.9) -- Molten Hammer
-	self:CDBar(445541, 9.3) -- Activate Ventilation
-	self:CDBar(449167, 13.0) -- Lava Expulsion
+	self:CDBar(428711, 6.9) -- Igneous Hammer
+	self:CDBar(445541, 9.3) -- Exhaust Vents
+	self:CDBar(449167, 13.0) -- Lava Cannon
 	self:CDBar(428202, 19.1) -- Scrap Song
-	self:CDBar(428508, 45.7) -- Deconstruction
+	self:CDBar(428508, 45.7) -- Blazing Crescendo
 end
 
 --------------------------------------------------------------------------------
@@ -68,13 +68,13 @@ end
 function mod:SilencedSpeaker(args)
 	-- this is cast by the defeated boss on the remaining boss
 	if self:MobId(args.sourceGUID) == 213217 then -- Speaker Brokk defeated
-		self:StopBar(445541) -- Activate Ventilation
+		self:StopBar(445541) -- Exhaust Vents
 		self:StopBar(430097) -- Molten Metal
 		self:StopBar(428202) -- Scrap Song
 	else -- Speaker Dorlita defeated
-		self:StopBar(428508) -- Deconstruction
-		self:StopBar(428711) -- Molten Hammer
-		self:StopBar(449167) -- Lava Expulsion
+		self:StopBar(428508) -- Blazing Crescendo
+		self:StopBar(428711) -- Igneous Hammer
+		self:StopBar(449167) -- Lava Cannon
 	end
 	self:SetStage(2)
 	self:Message(args.spellId, "cyan", CL.onboss:format(args.spellName))
@@ -83,39 +83,39 @@ end
 
 -- Speaker Brokk
 
-function mod:ActivateVentilation(args)
+function mod:ExhaustVents(args)
 	self:Message(args.spellId, "orange")
 	self:PlaySound(args.spellId, "alarm")
-	self:CDBar(args.spellId, 34.0)
+	self:CDBar(args.spellId, 15.8) -- TODO often delayed
 end
 
 function mod:MoltenMetal(args)
 	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, "alert")
-	self:CDBar(args.spellId, 9.7)
+	self:CDBar(args.spellId, 8.5) -- TODO often delayed
 end
 
 function mod:ScrapSong(args)
 	self:Message(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "long")
-	self:CDBar(args.spellId, 49.6)
+	self:CDBar(args.spellId, 52.1)
 end
 
 -- Speaker Dorlita
 
-function mod:Deconstruction(args)
+function mod:BlazingCrescendo(args)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "alarm")
-	self:CDBar(args.spellId, 46.1)
+	self:CDBar(args.spellId, 52.1)
 end
 
-function mod:MoltenHammer(args)
+function mod:IgneousHammer(args)
 	self:Message(args.spellId, "purple")
 	self:PlaySound(args.spellId, "alert")
 	self:CDBar(args.spellId, 12.1)
 end
 
-function mod:LavaExpulsion(args)
+function mod:LavaCannon(args)
 	-- doesn't seem possible to determine the target
 	self:Message(args.spellId, "orange")
 	self:PlaySound(args.spellId, "alarm")
