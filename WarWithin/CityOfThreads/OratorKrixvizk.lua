f8b77afea9361dca71a839f0fe71e3d55ae954b5
Nick Towle <ntowle@gmail.com>
2024-08-09 18:11:44 -0400

    WarWithin/CityOfThreads/OratorKrixvizk: Shadows of Doubt say countdown

--- WarWithin/CityOfThreads/OratorKrixvizk.lua
+++ WarWithin/CityOfThreads/OratorKrixvizk.lua
@@ -27,7 +27,7 @@ function mod:GetOptions()
 		434779, -- Terrorize
 		434829, -- Vociferous Indoctrination
 		434926, -- Lingering Influence
-		{448561, "SAY"}, -- Shadows of Doubt (Mythic)
+		{448561, "SAY", "SAY_COUNTDOWN"}, -- Shadows of Doubt (Mythic)
 	}, {
 		[448561] = CL.mythic, -- Shadows of Doubt
 	}
@@ -43,6 +43,7 @@ function mod:OnBossEnable()
 	-- Mythic
 	self:Log("SPELL_CAST_SUCCESS", "ShadowsOfDoubt", 448560)
 	self:Log("SPELL_AURA_APPLIED", "ShadowsOfDoubtApplied", 448561)
+	self:Log("SPELL_AURA_REMOVED", "ShadowsOfDoubtRemoved", 448561)
 end
 
 function mod:OnEngage()
@@ -128,6 +129,13 @@ do
 		self:PlaySound(args.spellId, "alarm", nil, playerList)
 		if self:Me(args.destGUID) then
 			self:Say(args.spellId, nil, nil, "Shadows of Doubt")
+			self:SayCountdown(args.spellId, 6)
+		end
+	end
+
+	function mod:ShadowsOfDoubtRemoved(args)
+		if self:Me(args.destGUID) then
+			self:CancelSayCountdown(args.spellId)
 		end
 	end
 end
