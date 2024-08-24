09c51afbea8380c7ff4bb206f00849190e4b8c79
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2024-08-24 11:02:19 +0200

    Update deDE.lua

--- WarWithin/TheRookery/Locales/deDE.lua
+++ WarWithin/TheRookery/Locales/deDE.lua
@@ -1,15 +1,15 @@
 local L = BigWigs:NewBossLocale("The Rookery Trash", "deDE")
 if not L then return end
 if L then
-	--L.quartermaster_koratite = "Quartermaster Koratite"
+	L.quartermaster_koratite = "Rüstmeister Koratit"
 	--L.cursed_stormrider = "Cursed Stormrider"
-	--L.cursed_rooktender = "Cursed Rooktender"
-	--L.unruly_stormrook = "Unruly Stormrook"
-	--L.corrupted_rookguard = "Corrupted Rookguard"
-	--L.corrupted_oracle = "Corrupted Oracle"
-	--L.corrupted_thunderer = "Corrupted Thunderer"
-	--L.inflicted_civilian = "Inflicted Civilian"
-	--L.void_ascendant = "Void Ascendant"
-	--L.radiating_voidstone = "Radiating Voidstone"
-	--L.void_fragment = "Void Fragment"
+	L.cursed_rooktender = "Verfluchter Krähenhüter"
+	L.unruly_stormrook = "Widerspenstige Sturmkrähe"
+	L.corrupted_rookguard = "Verfluchter Krähenwächter"
+	L.corrupted_oracle = "Verderbtes Orakel"
+	L.corrupted_thunderer = "Verfluchte Donnerin"
+	L.inflicted_civilian = "Betroffener Bürger"
+	L.void_ascendant = "Leerenaszendentin"
+	L.radiating_voidstone = "Strahlender Leerenstein"
+	L.void_fragment = "Leerenfragment"
 end
