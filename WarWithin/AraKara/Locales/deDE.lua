58d0f9fe99d02bb631a88dbdee30f3f1656d91d8
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2024-08-24 09:35:08 +0200

    Update deDE.lua

--- WarWithin/AraKara/Locales/deDE.lua
+++ WarWithin/AraKara/Locales/deDE.lua
@@ -1,15 +1,15 @@
 local L = BigWigs:NewBossLocale("Ara-Kara, City of Echoes Trash", "deDE")
 if not L then return end
 if L then
-	--L.engorged_crawler = "Engorged Crawler"
-	--L.trilling_attendant = "Trilling Attendant"
-	--L.ixin = "Ixin"
-	--L.nakt = "Nakt"
-	--L.atik = "Atik"
-	--L.hulking_bloodguard = "Hulking Bloodguard"
-	--L.bloodstained_webmage = "Bloodstained Webmage"
-	--L.blood_overseer = "Blood Overseer"
-	--L.nerubian_hauler = "Nerubian Hauler"
+	L.engorged_crawler = "Vollgefressener Krabbler"
+	L.trilling_attendant = "Trillernder Bediensteter"
+	L.ixin = "Ixin"
+	L.nakt = "Nakt"
+	L.atik = "Atik"
+	L.hulking_bloodguard = "Bullige Blutwache"
+	L.bloodstained_webmage = "Blutbefleckter Netzmagier"
+	L.blood_overseer = "Blutaufseher"
+	L.nerubian_hauler = "Schlepperin der Neruber"
 
-	--L.custom_on_autotalk_desc = "|cFFFF0000Requires 25 skill in Khaz Algar Tailoring.|r Automatically select the NPC dialog option that grants you 'Silk Wrap' which you can use by clicking your extra action button."
+	L.custom_on_autotalk_desc = "|cFFFF0000Benötigt 25 Fertigkeitspunkte der Schneiderei von Khaz Algar.|r Wählt automatisch die NPC Dialogoption, welche Euch die Fähigkeit 'Fangnetz' gewährt. Diese kann mit dem Extra Aktionsbutton genutzt werden."
 end
