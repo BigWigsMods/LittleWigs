764930646a292c7873505f33bd596ccb0e575bcc
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2024-08-24 09:26:48 +0200

    Update deDE.lua

--- Delves/Trash/Locales/deDE.lua
+++ Delves/Trash/Locales/deDE.lua
@@ -1,81 +1,81 @@
 local L = BigWigs:NewBossLocale("Fungarian Delve Trash", "deDE")
 if not L then return end
 if L then
-	--L.fungarian_trash = "Fungarian Trash"
+	L.fungarian_trash = "Pilz Trash"
 
-	--L.sporbit = "Sporbit"
-	--L.fungal_speartender = "Fungal Speartender"
-	--L.gnarled_reviver = "Gnarled Reviver"
-	--L.infected_beast = "Infected Beast"
-	--L.fungal_gutter = "Fungal Gutter"
-	--L.fungarian_flinger = "Fungarian Flinger"
+	L.sporbit = "Sporenbiss"
+	L.fungal_speartender = "Pilzspeerhüter"
+	L.gnarled_reviver = "Knorriger Wiederbeleber"
+	L.infected_beast = "Infiziertes Wildtier"
+	L.fungal_gutter = "Pilzschlitzer"
+	L.fungarian_flinger = "Fungianischer Werfer"
 	--L.particularly_bad_guy = "Particularly Bad Guy"
 end
 
 L = BigWigs:NewBossLocale("Kobold Delve Trash", "deDE")
 if L then
-	--L.kobold_trash = "Kobold Trash"
+	L.kobold_trash = "Kobold Trash"
 
-	--L.kobold_taskfinder = "Kobold Taskfinder"
-	--L.spitfire_charger = "Spitfire Charger"
-	--L.spitfire_fusetender = "Spitfire Fusetender"
+	L.kobold_taskfinder = "Koboldauftragssucher"
+	L.spitfire_charger = "Feuerspeiende Stürmerin"
+	L.spitfire_fusetender = "Luntenhüter des Feuerspeiers"
 end
 
 L = BigWigs:NewBossLocale("Kobyss Delve Trash", "deDE")
 if L then
-	--L.kobyss_trash = "Kobyss Trash"
+	L.kobyss_trash = "Kobyss Trash"
 
-	--L.kobyss_necromancer = "Kobyss Necromancer"
-	--L.kobyss_spearfisher = "Kobyss Spearfisher"
+	L.kobyss_necromancer = "Totenbeschwörer der Kobyss"
+	L.kobyss_spearfisher = "Speerfischer der Kobyss"
 	--L.kobyss_witherer = "Kobyss Witherer"
-	--L.wandering_gutter = "Wandering Gutter"
+	L.wandering_gutter = "Wandernder Ausweider"
 	--L.crazed_predator = "Crazed Predator"
 end
 
 L = BigWigs:NewBossLocale("Nerubian Delve Trash", "deDE")
 if L then
-	--L.nerubian_trash = "Nerubian Trash"
+	L.nerubian_trash = "Neruber Trash"
 
-	--L.nerubian_lord = "Nerubian Lord"
-	--L.nerubian_darkcaster = "Nerubian Darkcaster"
-	--L.nerubian_captain = "Nerubian Captain"
-	--L.chittering_fearmonger = "Chittering Fearmonger"
-	--L.skittering_swarmer = "Skittering Swarmer"
-	--L.nerubian_webspinner = "Nerubian Webspinner"
+	L.nerubian_lord = "Lady der Neruber"
+	L.nerubian_darkcaster = "Dunkelzauberer der Neruber"
+	L.nerubian_captain = "Hauptmann der Neruber"
+	L.chittering_fearmonger = "Zirpender Furchtschürer"
+	L.skittering_swarmer = "Huschender Schwärmer"
+	L.nerubian_webspinner = "Netzspinner der Neruber"
 end
 
 L = BigWigs:NewBossLocale("Nightfall Delve Trash", "deDE")
 if L then
-	--L.nightfall_trash = "Nightfall Trash"
+	L.nightfall_trash = "Nachtsturz Trash"
 
-	--L.dark_bombardier = "Dark Bombardier"
-	--L.nightfall_inquisitor = "Nightfall Inquisitor"
-	--L.devouring_shade = "Devouring Shade"
-	--L.weeping_shade = "Weeping Shade"
-	--L.nightfall_shadeguard = "Nightfall Shadeguard"
-	--L.shadow_elemental = "Shadow Elemental"
+	L.dark_bombardier = "Dunkle Bombardierin"
+	L.nightfall_inquisitor = "Inquisitor von Nachtsturz"
+	L.devouring_shade = "Verschlingender Schemen"
+	L.weeping_shade = "Weinender Schemen"
+	L.nightfall_shadeguard = "Schattenwache von Nachtsturz"
+	L.shadow_elemental = "Schattenelementar"
 end
 
 L = BigWigs:NewBossLocale("The Underkeep Trash", "deDE")
 if L then
-	--L.ascended_webfriar = "Ascended Webfriar"
-	--L.deepwalker_guardian = "Deepwalker Guardian"
-	--L.crazed_abomination = "Crazed Abomination"
-	--L.web_marauder = "Web Marauder"
+	L.ascended_webfriar = "Aufgestiegener Netzmönch"
+	L.deepwalker_guardian = "Tiefenläuferwächter"
+	L.crazed_abomination = "Wahnsinnige Monstrosität"
+	L.web_marauder = "Netzmarodeur"
 end
 
 L = BigWigs:NewBossLocale("Delve Rares", "deDE")
 if L then
-	--L.rares = "Rares"
+	L.rares = "Seltene Gegner"
 
-	--L.stolen_loader = "Stolen Loader"
-	--L.invasive_sporecap = "Invasive Sporecap"
-	--L.reno_jackson = "Reno Jackson"
-	--L.sir_finley_mrgglton = "Sir Finley Mrgglton"
-	--L.treasure_wraith = "Treasure Wraith"
+	L.stolen_loader = "Gestohlener Lader"
+	L.invasive_sporecap = "Invasive Sporenkappe"
+	L.reno_jackson = "Reno Jackson"
+	L.sir_finley_mrgglton = "Sir Finley Mrrgglton"
+	L.treasure_wraith = "Schatzgespenst"
 	--L.venombite = "Venombite"
-	--L.kasdru = "Kas'dru"
-	--L.tala = "Tala"
-	--L.velo = "Velo"
-	--L.anubvir = "Anub'vir"
+	L.kasdru = "Kas'dru"
+	L.tala = "Tala"
+	L.velo = "Velo"
+	L.anubvir = "Anub'vir"
 end
