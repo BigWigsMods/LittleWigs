82e24aea4b975db3ab8ef081473934a7a49ecea6
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2024-08-27 19:41:07 +0200

    Update deDE.lua

--- Delves/TheSinkhole/Locales/deDE.lua
+++ Delves/TheSinkhole/Locales/deDE.lua
@@ -1,12 +1,12 @@
 local L = BigWigs:NewBossLocale("Mirror Master Murkna", "deDE")
 if not L then return end
 if L then
-	--L.mirror_master_murkna = "Mirror Master Murkna"
+	L.mirror_master_murkna = "Spiegelmeister Murkna"
 end
 
 L = BigWigs:NewBossLocale("Leviathan Caller", "deDE")
 if L then
-	--L.leviathan_caller = "Leviathan Caller"
+	--L.leviathan_caller = "Leviathanrufer"
 	--L.guardian_tentacle = "Guardian Tentacle"
 	--L.slamming_tentacles = "Slamming Tentacles" -- plural
 end
