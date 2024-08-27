76de2ddd7286a9d8c3455e0f33da5536508295dd
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2024-08-27 19:48:55 +0200

    Update deDE.lua

--- WarWithin/CinderbrewMeadery/Locales/deDE.lua
+++ WarWithin/CinderbrewMeadery/Locales/deDE.lua
@@ -1,18 +1,18 @@
 local L = BigWigs:NewBossLocale("Cinderbrew Meadery Trash", "deDE")
 if not L then return end
 if L then
-	--L.venture_co_pyromaniac = "Venture Co. Pyromaniac"
-	--L.hired_muscle = "Hired Muscle"
-	--L.tasting_room_attendant = "Tasting Room Attendant"
+	L.venture_co_pyromaniac = "Pyromane der Venture Co."
+	L.hired_muscle = "Angeheuerter Kraftprotz"
+	L.tasting_room_attendant = "Bedienstete des Verkostungsraums"
 	L.chef_chewie = "Chefkoch Nager"
-	--L.cooking_pot = "Cooking Pot"
+	L.cooking_pot = "Kochtopf"
 	L.flamethrower = "Flammenwerfer"
-	--L.flavor_scientist = "Flavor Scientist"
-	--L.careless_hopgoblin = "Careless Hopgoblin"
-	--L.bee_wrangler = "Bee Wrangler"
-	--L.venture_co_honey_harvester = "Venture Co. Honey Harvester"
-	--L.royal_jelly_purveyor = "Royal Jelly Purveyor"
-	--L.yes_man = "Yes Man"
+	L.flavor_scientist = "Aromawissenschaftlerin"
+	L.careless_hopgoblin = "Unvorsichtiger Hobgoblin"
+	L.bee_wrangler = "Bienenbändiger"
+	L.venture_co_honey_harvester = "Honigernter der Venture Co."
+	L.royal_jelly_purveyor = "Lieferant von Gelée Royale"
+	L.yes_man = "Ja-Sager"
 
 	L.custom_on_cooking_autotalk_desc = "|cFFFF0000Benötigt 25 Fertigkeitspunkte der Alchemie oder Kochkunt von Khaz Algar.|r Wählt automatisch die NPC Dialogoption, welche Euch die Fähigkeit 'Klebriger Honig' gewährt. Diese kann mit dem Extra Aktionsbutton genutzt werden.\n\n|T451169:16|tKlebriger Honig\n{438997}"
 	L.custom_on_flamethrower_autotalk_desc = "|cFFFF0000Benötigt Gnom, Goblin, Mechagnom, oder 25 Fertigkeitspunkte der Ingenieurskunst von Khaz Algar.|r Wählt automatisch die NPC Dialogoption, welche Euch die Fähigkeit 'Flammeninferno' gewährt. Diese kann mit dem Extra Aktionsbutton genutzt werden.\n\n|T135789:16|tFlammeninferno\n{439616}"
