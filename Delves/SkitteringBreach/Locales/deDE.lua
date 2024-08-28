5dd6b0905c87aa8fae95231edbc666e0f00eba76
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2024-08-28 07:02:06 +0200

    Update deDE.lua

--- Delves/SkitteringBreach/Locales/deDE.lua
+++ Delves/SkitteringBreach/Locales/deDE.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("Nerl'athekk the Skulking", "deDE")
 if not L then return end
 if L then
-	--L.nerlathekk_the_skulking = "Nerl'athekk the Skulking"
+	L.nerlathekk_the_skulking = "Nerl'athekk der Lauernde"
 end
 
 L = BigWigs:NewBossLocale("Faceless One", "deDE")
