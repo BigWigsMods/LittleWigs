189ef785b1d7bab8d449e7b2382e1bb3201a18d6
Nick Towle <ntowle@gmail.com>
2024-08-29 12:57:35 -0400

    WarWithin/Scenarios/AwakeningTheMachine: Add scenario module

---
+++ WarWithin/Scenarios/Locales/frFR.lua
@@ -0,0 +1,15 @@
+local L = BigWigs:NewBossLocale("Awakening the Machine", "frFR")
+if not L then return end
+if L then
+	L.awakening_the_machine = "L’éveil de la machine"
+
+	--L.stages_desc = "Show an alert when a new wave of enemies spawns."
+
+	--L.corrupted_machinist = "Corrupted Machinist"
+	--L.malfunctioning_pylon = "Malfunctioning Pylon"
+	--L.explosive_bomberbot = "Explosive Bomberbot"
+	--L.automatic_ironstrider = "Automatic Ironstrider"
+	--L.medbot = "Medbot"
+	--L.nullbot = "Nullbot"
+	L.awakened_phalanx = "Phalange éveillée"
+end
---
+++ WarWithin/Scenarios/Locales/koKR.lua
@@ -0,0 +1,15 @@
+local L = BigWigs:NewBossLocale("Awakening the Machine", "koKR")
+if not L then return end
+if L then
+	L.awakening_the_machine = "기계의 각성"
+
+	--L.stages_desc = "Show an alert when a new wave of enemies spawns."
+
+	--L.corrupted_machinist = "Corrupted Machinist"
+	--L.malfunctioning_pylon = "Malfunctioning Pylon"
+	--L.explosive_bomberbot = "Explosive Bomberbot"
+	--L.automatic_ironstrider = "Automatic Ironstrider"
+	--L.medbot = "Medbot"
+	--L.nullbot = "Nullbot"
+	--L.awakened_phalanx = "Awakened Phalanx"
+end
