d77c6b4e194de794e56b5a94a472a62fe9fde72d
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-08-30 08:44:42 +0800

    Update zhCN.lua

--- WarWithin/Scenarios/Locales/zhCN.lua
+++ WarWithin/Scenarios/Locales/zhCN.lua
@@ -3,13 +3,13 @@ if not L then return end
 if L then
 	L.awakening_the_machine = "主机觉醒"
 
-	--L.stages_desc = "Show an alert when a new wave of enemies spawns."
+	L.stages_desc = "新一波敌人出现时显示警告。"
 
-	--L.corrupted_machinist = "Corrupted Machinist"
-	--L.malfunctioning_pylon = "Malfunctioning Pylon"
-	--L.explosive_bomberbot = "Explosive Bomberbot"
-	--L.automatic_ironstrider = "Automatic Ironstrider"
-	--L.medbot = "Medbot"
-	--L.nullbot = "Nullbot"
-	--L.awakened_phalanx = "Awakened Phalanx"
+	L.corrupted_machinist = "被腐蚀的机械师"
+	L.malfunctioning_pylon = "失控的晶塔"
+	L.explosive_bomberbot = "爆破炸弹机器人"
+	L.automatic_ironstrider = "自动踏铁者"
+	L.medbot = "医疗机器人"
+	L.nullbot = "空值机器人"
+	L.awakened_phalanx = "觉醒的方阵兵"
 end
