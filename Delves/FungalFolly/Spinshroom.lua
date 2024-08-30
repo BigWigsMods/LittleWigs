fd112484f49aceb7a8ea2d962001f80dd8e9fa6a
funkydude <funkydude@users.noreply.github.com>
2024-08-30 19:41:37 +0100

    Delves/FungalFolly/Spinshroom: Renames, tweaks

--- Delves/FungalFolly/Spinshroom.lua
+++ Delves/FungalFolly/Spinshroom.lua
@@ -24,6 +24,8 @@ end
 
 function mod:OnRegister()
 	self.displayName = L.spinshroom
+	self:SetSpellRename(415499, CL.weakened) -- Dizzy (Weakened)
+	self:SetSpellRename(415492, CL.charge) -- Fungal Charge (Charge)
 end
 
 function mod:GetOptions()
@@ -32,6 +34,9 @@ function mod:GetOptions()
 		415499, -- Dizzy
 		415492, -- Fungal Charge
 		425315, -- Fungsplosion
+	},nil,{
+		[415499] = CL.weakened, -- Dizzy (Weakened)
+		[415492] = CL.charge, -- Fungal Charge (Charge)
 	}
 end
 
@@ -44,7 +49,7 @@ end
 
 function mod:OnEngage()
 	self:CDBar(415406, 5.7) -- Fungalstorm
-	self:CDBar(415492, 19.7) -- Fungal Charge
+	self:CDBar(415492, 19.7, CL.charge) -- Fungal Charge
 	self:CDBar(425315, 26.3) -- Fungsplosion
 end
 
@@ -54,23 +59,24 @@ end
 
 function mod:Fungalstorm(args)
 	self:Message(args.spellId, "yellow")
-	self:PlaySound(args.spellId, "long")
 	self:CDBar(args.spellId, 30.2)
+	self:PlaySound(args.spellId, "long")
 end
 
 function mod:Dizzy(args)
-	self:Message(args.spellId, "green")
+	self:Message(args.spellId, "green", CL.weakened)
+	self:Bar(args.spellId, 8, CL.weakened)
 	self:PlaySound(args.spellId, "info")
 end
 
 function mod:FungalCharge(args)
-	self:Message(args.spellId, "red")
+	self:Message(args.spellId, "red", CL.charge)
+	self:CDBar(args.spellId, 30.2, CL.charge)
 	self:PlaySound(args.spellId, "alarm")
-	self:CDBar(args.spellId, 30.2)
 end
 
 function mod:Fungsplosion(args)
 	self:Message(args.spellId, "orange")
-	self:PlaySound(args.spellId, "alarm")
 	self:CDBar(args.spellId, 30.2)
+	self:PlaySound(args.spellId, "alarm")
 end
