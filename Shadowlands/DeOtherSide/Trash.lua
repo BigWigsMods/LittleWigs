44751fd97947f0265296a79606189f1330e4a238
Nick Towle <ntowle@gmail.com>
2024-09-02 23:58:58 -0400

    Shadowlands/DeOtherSide/Trash: Undying Rage and Haywire timers

--- Shadowlands/DeOtherSide/Trash.lua
+++ Shadowlands/DeOtherSide/Trash.lua
@@ -126,41 +126,70 @@ end
 
 function mod:OnBossEnable()
 	-- [[ The ring ]] --
+
+	-- Risen Cultist
 	self:Log("SPELL_CAST_SUCCESS", "DarkLotus", 328740)
+
+	-- Risen Warlord
+	self:Log("SPELL_CAST_SUCCESS", "UndyingRage", 333227)
 	self:Log("SPELL_AURA_APPLIED", "UndyingRageApplied", 333227)
 	self:Log("SPELL_AURA_REMOVED", "UndyingRageRemoved", 333227)
 	self:Log("SPELL_AURA_APPLIED", "ReaverApplied", 333250)
 	self:Log("SPELL_PERIODIC_DAMAGE", "ReaverApplied", 333250)
 	self:Log("SPELL_PERIODIC_MISSED", "ReaverApplied", 333250)
+	self:Death("RisenWarlordDeath", 169905)
+
+	-- Enraged Spirit
 	self:Log("SPELL_CAST_SUCCESS", "Rage", 333787)
+
+	-- Death Speaker
 	self:Log("SPELL_CAST_START", "EruptingDarkness", 334051)
 
 	-- [[ Path to Hakkar ]] --
+
+	-- Atal'ai Deathwalker
 	self:Log("SPELL_AURA_APPLIED_DOSE", "GushingWound", 332678)
 	self:Log("SPELL_CAST_START", "Bladestorm", 332671)
+
+	-- Atal'ai High Priest
 	self:Log("SPELL_CAST_START", "Heal", 332706)
+
+	-- Atal'ai Hoodoo Hexxer
 	self:Log("SPELL_CAST_START", "HealingWave", 332612)
 	self:Log("SPELL_CAST_START", "Hex", 332605)
 	self:Log("SPELL_CAST_START", "LightningDischarge", 332608)
 
 	-- [[ Path to the Manastorms ]] --
-	self:Log("SPELL_CAST_START", "HaywirePreCast", 331927)
-	self:Log("SPELL_CAST_SUCCESS", "Haywire", 331927)
+
+	-- Defunct Dental Drill
+	self:Log("SPELL_CAST_START", "Haywire", 331927)
+	self:Log("SPELL_CAST_SUCCESS", "HaywireSuccess", 331927)
 	self:Log("SPELL_AURA_REMOVED", "HaywireOver", 331927)
+	self:Death("DefunctDentalDrillDeath", 167962)
+
+	-- 4.RF-4.RF
 	self:Log("SPELL_CAST_START", "MetallicJaws", 331548)
 	self:Log("SPELL_CAST_SUCCESS", "MetallicJawsSuccess", 331548)
 	self:Log("SPELL_CAST_START", "W00F", 331846)
 	self:Log("SPELL_CAST_SUCCESS", "W00FSuccess", 331846)
-	self:Log("SPELL_CAST_START", "SelfCleaningCycle", 332084)
 	self:Death("ArfArfDeath", 167964)
 
+	-- Lubricator
+	self:Log("SPELL_CAST_START", "SelfCleaningCycle", 332084)
+
 	-- [[ Path to Xyexa ]] --
+
+	-- Weald Shimmermoth
 	self:Log("SPELL_AURA_APPLIED_DOSE", "SoporificShimmerdust", 334496)
+
+	-- Mythresh, Sky's Talons
 	self:Log("SPELL_CAST_START", "WailingGrief", 340026)
 	self:Log("SPELL_CAST_SUCCESS", "WailingGriefSuccess", 340026)
 	self:Death("MythreshDeath", 171184)
 
 	-- [[ Common ]] --
+
+	-- Atal'ai Deathwalker, Headless Client
 	self:Log("SPELL_DAMAGE", "PeriodicDamage", 332672, 332157) -- Bladestorm, Spinning Up
 	self:Log("SPELL_MISSED", "PeriodicDamage", 332672, 332157)
 end
@@ -170,7 +199,9 @@ end
 --
 
 -- [[ The ring ]] --
+
 -- Risen Cultist
+
 do
 	local prev = 0
 	function mod:DarkLotus(args)
@@ -186,10 +217,14 @@ do
 end
 
 -- Risen Warlord
+
+function mod:UndyingRage(args)
+	self:Nameplate(args.spellId, 32.8, args.sourceGUID)
+end
+
 function mod:UndyingRageApplied(args)
 	self:Message(args.spellId, "red", CL.buff_other:format(args.destName, args.spellName))
 	self:PlaySound(args.spellId, (self:Tank() or self:Dispeller("enrage", true)) and "warning" or "alert")
-	--self:Nameplate(args.spellId, 33, args.sourceGUID)
 end
 
 function mod:UndyingRageRemoved(args)
@@ -211,7 +246,12 @@ do
 	end
 end
 
+function mod:RisenWarlordDeath(args)
+	self:ClearNameplate(args.destGUID)
+end
+
 -- Death Speaker
+
 function mod:EruptingDarkness(args)
 	self:Message(args.spellId, "orange", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, "alert")
@@ -219,6 +259,7 @@ function mod:EruptingDarkness(args)
 end
 
 -- Enraged Spirit
+
 function mod:Rage(args)
 	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, "warning")
@@ -226,7 +267,9 @@ function mod:Rage(args)
 end
 
 -- [[ Path to Hakkar ]] --
+
 -- Atal'ai Deathwalker
+
 function mod:GushingWound(args)
 	local stacks = args.amount
 	if stacks % 3 == 0 then
@@ -239,7 +282,7 @@ do
 	local prev = 0
 	function mod:Bladestorm(args)
 		local t = args.time
-		if t - prev > 1 then
+		if t - prev > 1.5 then
 			prev = t
 			self:Message(332672, "yellow", CL.casting:format(args.spellName))
 			self:PlaySound(332672, "info")
@@ -248,12 +291,14 @@ do
 end
 
 -- Atal'ai High Priest
+
 function mod:Heal(args)
 	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, "warning")
 end
 
 -- Atal'ai Hoodoo Hexxer
+
 function mod:HealingWave(args)
 	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, "warning")
@@ -268,7 +313,6 @@ do
 	local function printTarget(self, name, guid)
 		self:TargetMessage(332608, "orange", name)
 		self:PlaySound(332608, "alarm", nil, name)
-
 		if self:Me(guid) then
 			self:Say(332608, nil, nil, "Lightning Discharge")
 		end
@@ -280,22 +324,29 @@ do
 end
 
 -- [[ Path to the Manastorms ]] --
+
 -- Defunct Dental Drill
-function mod:HaywirePreCast(args)
+
+function mod:Haywire(args)
 	self:Message(args.spellId, "yellow", CL.casting:format(args.spellName))
+	self:Nameplate(args.spellId, 22.7, args.sourceGUID)
 	self:PlaySound(args.spellId, "warning")
 end
 
-function mod:Haywire(args)
+function mod:HaywireSuccess(args)
 	self:CastBar(args.spellId, 4)
-	--self:Nameplate(args.spellId, 19, args.sourceGUID)
 end
 
 function mod:HaywireOver(args)
 	self:StopBar(CL.cast:format(args.spellName))
 end
 
+function mod:DefunctDentalDrillDeath(args)
+	self:ClearNameplate(args.destGUID)
+end
+
 -- 4.RF-4.RF
+
 function mod:MetallicJaws(args)
 	self:Message(args.spellId, "purple", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, "alert")
@@ -329,21 +380,22 @@ end
 
 function mod:ArfArfDeath()
 	self:StopBar(331548) -- Metallic Jaws
-
-	-- W-00F
-	self:StopBar(331846)
-	self:StopBar(CL.cast:format(self:SpellName(331846)))
-	self:CancelSayCountdown(331846)
+	self:StopBar(331846) -- W-00F
+	self:StopBar(CL.cast:format(self:SpellName(331846))) -- W-00F
+	self:CancelSayCountdown(331846) -- W-00F
 end
 
 -- Lubricator
+
 function mod:SelfCleaningCycle(args)
 	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, "warning")
 end
 
 -- [[ Path to Xyexa ]] --
+
 -- Weald Shimmermoth
+
 function mod:SoporificShimmerdust(args)
 	if self:Me(args.destGUID) then
 		local stacks = args.amount
@@ -355,6 +407,7 @@ function mod:SoporificShimmerdust(args)
 end
 
 -- Mythresh, Sky's Talons
+
 function mod:WailingGrief(args)
 	self:Message(args.spellId, "orange", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, "alert")
@@ -372,6 +425,7 @@ function mod:MythreshDeath()
 end
 
 -- [[ Common ]] --
+
 do
 	local prev = 0
 	function mod:PeriodicDamage(args)
@@ -379,7 +433,6 @@ do
 			local t = args.time
 			if t - prev > (self:Melee() and 6 or 1.5) then
 				prev = t
-
 				self:PersonalMessage(args.spellId, "near")
 				self:PlaySound(args.spellId, "alarm")
 			end
