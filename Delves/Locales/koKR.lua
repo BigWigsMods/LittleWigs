b27ffbdffc36cb171788d1fae34f3eb063272ad9
Nick Towle <ntowle@gmail.com>
2024-09-10 18:01:18 -0400

    Delves/ZekvirsLair/Zekvir: Add boss module

---
+++ Delves/Locales/koKR.lua
@@ -0,0 +1,5 @@
+local L = BigWigs:NewBossLocale("Zekvir", "koKR")
+if not L then return end
+if L then
+	L.zekvir = "제크비르"
+end
---
+++ Delves/Locales/zhCN.lua
@@ -0,0 +1,5 @@
+local L = BigWigs:NewBossLocale("Zekvir", "zhCN")
+if not L then return end
+if L then
+	L.zekvir = "泽克维尔"
+end
