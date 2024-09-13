1c745c8fb0b95e2dd4b6fb128aec0136a06a37a3
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2024-09-13 19:46:19 +0200

    Update deDE.lua

--- Delves/Locales/deDE.lua
+++ Delves/Locales/deDE.lua
@@ -1,12 +1,12 @@
 local L = BigWigs:NewBossLocale("Zekvir", "deDE")
 if not L then return end
 if L then
-	--L.zekvir = "Zekvir (Tier 1)"
+	L.zekvir = "Zekvir (Stufe 1)"
 	L.web_terror = "Netzschrecken"
 end
 
 L = BigWigs:NewBossLocale("Zekvir 2", "deDE")
 if L then
-	--L.zekvir = "Zekvir (Tier 2)"
+	L.zekvir = "Zekvir (Stufe 2)"
 	L.web_terror = "Netzschrecken"
 end
