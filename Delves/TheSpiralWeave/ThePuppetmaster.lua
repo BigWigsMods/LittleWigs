8b24afec052fdbf479c6251f67542762605a8f00
funkydude <funkydude@users.noreply.github.com>
2024-09-14 14:12:25 +0100

    Delves/TheSpiralWeave/ThePuppetmaster: Add Stinging Swarm

--- Delves/TheSpiralWeave/ThePuppetmaster.lua
+++ Delves/TheSpiralWeave/ThePuppetmaster.lua
@@ -4,7 +4,7 @@
 
 local mod, CL = BigWigs:NewBoss("The Puppetmaster", 2688)
 if not mod then return end
-mod:RegisterEnableMob(220507) -- The Puppetmaster?
+mod:RegisterEnableMob(220507, 220510) -- The Puppetmaster?, The Puppetmaster? (Final Stage)
 mod:SetEncounterID(3006)
 mod:SetRespawnTime(15)
 mod:SetAllowWin(true)
@@ -29,15 +29,18 @@ end
 function mod:GetOptions()
 	return {
 		451913, -- Grimweave Orb
+		448663, -- Stinging Swarm
 	}
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "GrimweaveOrb", 451913)
+	self:Log("SPELL_CAST_START", "StingingSwarm", 448663)
 end
 
 function mod:OnEngage()
-	self:CDBar(451913, 6.3) -- Grimweave Orb
+	--self:CDBar(451913, 6.3) -- Grimweave Orb
+	self:CDBar(448663, 9.7) -- Stinging Swarm
 end
 
 --------------------------------------------------------------------------------
@@ -47,8 +50,14 @@ end
 function mod:GrimweaveOrb(args)
 	-- this spellId can also be cast by trash
 	if self:MobId(args.sourceGUID) == 220507 then -- The Puppetmaster?
-		self:Message(args.spellId, "red")
+		self:Message(args.spellId, "orange")
 		self:PlaySound(args.spellId, "alarm")
 		self:CDBar(args.spellId, 23.0)
 	end
+end
+
+function mod:StingingSwarm(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "warning")
+	self:CDBar(args.spellId, 31.6)
 end
