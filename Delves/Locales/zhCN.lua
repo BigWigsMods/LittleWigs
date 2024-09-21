e018dbda6c7fc5064cf1c5c8eb2d06c6b938c0fa
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-09-21 12:19:28 +0800

    Update zhCN.lua

--- Delves/Locales/zhCN.lua
+++ Delves/Locales/zhCN.lua
@@ -1,12 +1,12 @@
 local L = BigWigs:NewBossLocale("Zekvir", "zhCN")
 if not L then return end
 if L then
-	--L.zekvir = "Zekvir (Tier 1)"
+	L.zekvir = "泽克维尔（难度1）"
 	L.web_terror = "蛛网恐魔"
 end
 
 L = BigWigs:NewBossLocale("Zekvir 2", "zhCN")
 if L then
-	--L.zekvir = "Zekvir (Tier 2)"
+	L.zekvir = "泽克维尔（难度1）"
 	L.web_terror = "蛛网恐魔"
 end
