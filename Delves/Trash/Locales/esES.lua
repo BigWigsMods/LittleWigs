b946d33be456fdf3eb5ead55a03a083d70933cd2
Zurent <146595017+Zurent@users.noreply.github.com>
2024-09-29 12:27:54 +0200

    Update esES.lua
    Translated the names of the NPCs from Trash/Locales/esES.lua

--- Delves/Trash/Locales/esES.lua
+++ Delves/Trash/Locales/esES.lua
@@ -1,84 +1,84 @@
 local L = BigWigs:NewBossLocale("Fungarian Delve Trash", "esES") or BigWigs:NewBossLocale("Fungarian Delve Trash", "esMX")
 if not L then return end
 if L then
-	--L.fungarian_trash = "Fungarian Trash"
+	L.fungarian_trash = "Fúngicos Trash"
 
-	--L.sporbit = "Sporbit"
-	--L.fungal_speartender = "Fungal Speartender"
-	--L.gnarled_reviver = "Gnarled Reviver"
-	--L.infected_beast = "Infected Beast"
-	--L.fungal_gutter = "Fungal Gutter"
-	--L.fungarian_flinger = "Fungarian Flinger"
+	L.sporbit = "Espórbita"
+	L.fungal_speartender = "Cuidalanzas fúngico"
+	L.gnarled_reviver = "Reanimador retorcido"
+	L.infected_beast = "Bestia infectada"
+	L.fungal_gutter = "Destripador fúngico"
+	L.fungarian_flinger = "Lanzador fungárico"
 	L.fungal_rotcaster = "Putromante fúngico"
-	--L.particularly_bad_guy = "Particularly Bad Guy"
+	L.particularly_bad_guy = "Tipa especialmente mala"
 end
 
 L = BigWigs:NewBossLocale("Kobold Delve Trash", "esES") or BigWigs:NewBossLocale("Kobold Delve Trash", "esMX")
 if L then
-	--L.kobold_trash = "Kobold Trash"
+	L.kobold_trash = "Kobold Trash"
 
-	--L.kobold_taskfinder = "Kobold Taskfinder"
-	--L.spitfire_charger = "Spitfire Charger"
-	--L.spitfire_fusetender = "Spitfire Fusetender"
+	L.kobold_taskfinder = "Buscatareas kóbold"
+	L.spitfire_charger = "Destrera escupefuego"
+	L.spitfire_fusetender = "Cuidamechas escupefuego"
 end
 
 L = BigWigs:NewBossLocale("Kobyss Delve Trash", "esES") or BigWigs:NewBossLocale("Kobyss Delve Trash", "esMX")
 if L then
-	--L.kobyss_trash = "Kobyss Trash"
+	L.kobyss_trash = "Kobyss Trash"
 
-	--L.kobyss_necromancer = "Kobyss Necromancer"
-	--L.kobyss_spearfisher = "Kobyss Spearfisher"
-	--L.kobyss_witherer = "Kobyss Witherer"
-	--L.wandering_gutter = "Wandering Gutter"
-	--L.crazed_predator = "Crazed Predator"
-	--L.deepwater_makura = "Deepwater Makura"
+	L.kobyss_necromancer = "Nigromante kobismal"
+	L.kobyss_spearfisher = "Pescador con lanza kobismal"
+	L.kobyss_witherer = "Marchitador kobismal"
+	L.wandering_gutter = "Destripador errante"
+	L.crazed_predator = "Depredador enloquecido"
+	L.deepwater_makura = "Makrura aguahonda"
 end
 
 L = BigWigs:NewBossLocale("Nerubian Delve Trash", "esES") or BigWigs:NewBossLocale("Nerubian Delve Trash", "esMX")
 if L then
-	--L.nerubian_trash = "Nerubian Trash"
+	L.nerubian_trash = "Nerubian Trash"
 
-	--L.nerubian_lord = "Nerubian Lord"
-	--L.nerubian_darkcaster = "Nerubian Darkcaster"
-	--L.nerubian_captain = "Nerubian Captain"
-	--L.chittering_fearmonger = "Chittering Fearmonger"
-	--L.skittering_swarmer = "Skittering Swarmer"
-	--L.nerubian_webspinner = "Nerubian Webspinner"
+	L.nerubian_lord = "Señor nerubiano"
+	L.nerubian_darkcaster = "Taumaturgo oscuro nerubiano"
+	L.nerubian_captain = "Capitán nerubiano"
+	L.chittering_fearmonger = "Tremebundo chasqueador"
+	L.skittering_swarmer = "Enjambrista escurridizo"
+	L.nerubian_webspinner = "Tejerred nerubiano"
 end
 
 L = BigWigs:NewBossLocale("Nightfall Delve Trash", "esES") or BigWigs:NewBossLocale("Nightfall Delve Trash", "esMX")
 if L then
-	--L.nightfall_trash = "Nightfall Trash"
+	L.nightfall_trash = "Nightfall Trash"
 
-	--L.dark_bombardier = "Dark Bombardier"
-	--L.nightfall_inquisitor = "Nightfall Inquisitor"
-	--L.devouring_shade = "Devouring Shade"
-	--L.weeping_shade = "Weeping Shade"
-	--L.nightfall_shadeguard = "Nightfall Shadeguard"
-	--L.shadow_elemental = "Shadow Elemental"
+	L.dark_bombardier = "Bombardera oscura"
+	L.nightfall_inquisitor = "Inquisidor del Ocaso"
+	L.devouring_shade = "Sombra devoradora"
+	L.weeping_shade = "Sombra sollozante"
+	L.nightfall_shadeguard = "Guardia sombrío del Ocaso"
+	L.shadow_elemental = "Elemental de sombra"
 end
 
 L = BigWigs:NewBossLocale("The Underkeep Trash", "esES") or BigWigs:NewBossLocale("The Underkeep Trash", "esMX")
 if L then
-	--L.ascended_webfriar = "Ascended Webfriar"
-	--L.deepwalker_guardian = "Deepwalker Guardian"
-	--L.crazed_abomination = "Crazed Abomination"
-	--L.web_marauder = "Web Marauder"
+	L.ascended_webfriar = "Fraitelaraña ascendido"
+	L.deepwalker_guardian = "Guardián Andahondo"
+	L.crazed_abomination = "Abominación enloquecida"
+	L.web_marauder = "Maleante de telaraña"
 end
 
 L = BigWigs:NewBossLocale("Delve Rares", "esES") or BigWigs:NewBossLocale("Delve Rares", "esMX")
 if L then
-	--L.rares = "Rares"
+	L.rares = "Rares"
 
-	--L.stolen_loader = "Stolen Loader"
-	--L.invasive_sporecap = "Invasive Sporecap"
-	--L.reno_jackson = "Reno Jackson"
-	--L.sir_finley_mrgglton = "Sir Finley Mrgglton"
-	--L.treasure_wraith = "Treasure Wraith"
-	--L.venombite = "Venombite"
-	--L.kasdru = "Kas'dru"
-	--L.tala = "Tala"
-	--L.velo = "Velo"
-	--L.anubvir = "Anub'vir"
-	--L.zekvir = "Zekvir (Random Spawn)"
+	L.stolen_loader = "Cargador robado"
+	L.invasive_sporecap = "Cabezaespora invasivo"
+	L.reno_jackson = "Reno Jackson"
+	L.sir_finley_mrgglton = "Sir Finley Mrgglton"
+	L.treasure_wraith = "Ánima del tesoro"
+	L.venombite = "Picadura venenosa"
+	L.kasdru = "Kas'dru"
+	L.tala = "Tala"
+	L.velo = "Velo"
+	L.anubvir = "Anub'vir"
+	L.zekvir = "Zekvir (Random Spawn)"
 end
