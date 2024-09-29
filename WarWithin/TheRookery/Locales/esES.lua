3123d98966133db3d513ccb3a67a86e9eba1c326
Zurent <146595017+Zurent@users.noreply.github.com>
2024-09-29 14:08:38 +0200

    Update esES.lua
    Translated the missing parts

--- WarWithin/TheRookery/Locales/esES.lua
+++ WarWithin/TheRookery/Locales/esES.lua
@@ -1,15 +1,15 @@
 local L = BigWigs:NewBossLocale("The Rookery Trash", "esES") or BigWigs:NewBossLocale("The Rookery Trash", "esMX")
 if not L then return end
 if L then
-	--L.quartermaster_koratite = "Quartermaster Koratite"
+	L.quartermaster_koratite = "Intendente Koratite"
 	--L.cursed_stormrider = "Cursed Stormrider"
-	--L.cursed_rooktender = "Cursed Rooktender"
-	--L.unruly_stormrook = "Unruly Stormrook"
-	--L.corrupted_rookguard = "Corrupted Rookguard"
-	--L.corrupted_oracle = "Corrupted Oracle"
+	L.cursed_rooktender = "Cuidagrajos maldito"
+	L.unruly_stormrook = "Tormengrajo indisciplinado"
+	L.corrupted_rookguard = "Triturador maldito del Vacío"
+	L.corrupted_oracle = "Oráculo corrupta"
 	--L.corrupted_thunderer = "Corrupted Thunderer"
-	--L.inflicted_civilian = "Inflicted Civilian"
-	--L.void_ascendant = "Void Ascendant"
-	--L.radiating_voidstone = "Radiating Voidstone"
-	--L.void_fragment = "Void Fragment"
+	L.inflicted_civilian = "Civil afligido"
+	L.void_ascendant = "Ascendiente del Vacío"
+	L.radiating_voidstone = "Piedra del Vacío radiante"
+	L.void_fragment = "Fragmento del Vacío"
 end
