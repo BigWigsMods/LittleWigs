c7dd08ee9baf958ff3136cdf8f604568ef5fb653
Zurent <146595017+Zurent@users.noreply.github.com>
2024-09-29 13:34:36 +0200

    Update esES.lua
    Translated the missing parts

--- WarWithin/CityOfThreads/Locales/esES.lua
+++ WarWithin/CityOfThreads/Locales/esES.lua
@@ -1,23 +1,23 @@
 local L = BigWigs:NewBossLocale("City of Threads Trash", "esES") or BigWigs:NewBossLocale("City of Threads Trash", "esMX")
 if not L then return end
 if L then
-	--L.herald_of_ansurek = "Herald of Ansurek"
-	--L.sureki_silkbinder = "Sureki Silkbinder"
-	--L.royal_swarmguard = "Royal Swarmguard"
-	--L.xephitik = "Xeph'itik"
-	--L.pale_priest = "Pale Priest"
-	--L.eye_of_the_queen = "Eye of the Queen"
-	--L.covert_webmancer = "Covert Webmancer"
-	--L.royal_venomshell = "Royal Venomshell"
-	--L.unstable_test_subject = "Unstable Test Subject"
-	--L.sureki_unnaturaler = "Sureki Unnaturaler"
-	--L.elder_shadeweaver = "Elder Shadeweaver"
+	L.herald_of_ansurek = "Heraldo de Ansurek"
+	L.sureki_silkbinder = "Vinculasedas sureki"
+	L.royal_swarmguard = "Guardaenjambre real"
+	L.xephitik = "Xeph'itik"
+	L.pale_priest = "Sacerdotisa pálida"
+	L.eye_of_the_queen = "Residente de las cuencas"
+	L.covert_webmancer = "Telamante encubierta"
+	L.royal_venomshell = "Conchaveneno real"
+	L.unstable_test_subject = "Sujeto de pruebas inestable"
+	L.sureki_unnaturaler = "Antinaturalista sureki"
+	L.elder_shadeweaver = "Tejesombras anciano"
 	--L.hulking_warshell = "Hulking Warshell"
 
 	--L.xephitik_defeated_trigger = "Enough!"
 	--L.fangs_of_the_queen_warmup_trigger = "The Transformatory was once the home of our sacred evolution."
 	--L.izo_warmup_trigger = "Enough! You've earned a place in my collection. Let me usher you in."
-	--L.custom_on_autotalk_desc = "|cFFFF0000Requires Rogue, Priest, or 25 skill in Khaz Algar Engineering.|r Automatically select the NPC dialog option that grants you the 'Stolen Power' aura.\n\n|T135888:16|tStolen Power\n{448305}"
+	L.custom_on_autotalk_desc = "|cFFFF0000Requiere Pícaro, Sacerdote, o 25 de habilidad en Ingeniería de Khaz Algar.|r Selecciona automáticamente la opción de diálogo del NPC que te otorga el aura 'Poder Robado'.\n\n|T135888:16|tPoder Robado\n{448305}"
 end
 
 L = BigWigs:NewBossLocale("Fangs of the Queen", "esES") or BigWigs:NewBossLocale("Fangs of the Queen", "esMX")
