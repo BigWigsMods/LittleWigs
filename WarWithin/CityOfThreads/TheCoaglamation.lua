4b886825045f775acbc044bedff19d8ede95b151
Nick Towle <ntowle@gmail.com>
2024-09-29 23:02:39 -0400

    WarWithin/CityOfThreads/TheCoaglamation: Timer updates

--- WarWithin/CityOfThreads/TheCoaglamation.lua
+++ WarWithin/CityOfThreads/TheCoaglamation.lua
@@ -35,8 +35,8 @@ function mod:GetOptions()
 		"warmup",
 		441289, -- Viscous Darkness
 		{461842, "TANK_HEALER"}, -- Oozing Smash
-		441395, -- Dark Pulse
 		461880, -- Blood Surge
+		441395, -- Dark Pulse
 		461825, -- Black Blood
 	}
 end
@@ -44,8 +44,8 @@ end
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "ViscousDarkness", 441289, 447146) -- odd casts, even casts
 	self:Log("SPELL_CAST_START", "OozingSmash", 461842)
-	self:Log("SPELL_CAST_START", "DarkPulse", 441395)
 	self:Log("SPELL_CAST_START", "BloodSurge", 438658, 461880) -- Normal/Heroic, Mythic
+	self:Log("SPELL_CAST_START", "DarkPulse", 441395)
 	-- 443311 arena (sides), 462439 arena (stairs), 461825 Mythic pools, 445435 non-Mythic pools
 	self:Log("SPELL_PERIODIC_DAMAGE", "BlackBloodDamage", 443311, 462439, 461825, 445435)
 	self:Log("SPELL_PERIODIC_MISSED", "BlackBloodDamage", 443311, 462439, 461825, 445435)
@@ -58,10 +58,10 @@ function mod:OnEngage()
 	bloodSurgeCount = 1
 	self:StopBar(CL.active)
 	if self:Mythic() then
-		self:CDBar(461842, 3.6, CL.count:format(self:SpellName(461842), oozingSmashCount)) -- Oozing Smash
-		self:CDBar(441289, 10.8, CL.count:format(self:SpellName(441289), viscousDarknessCount)) -- Viscous Darkness
-		self:CDBar(461880, 47.3, CL.count:format(self:SpellName(461880), bloodSurgeCount)) -- Blood Surge
-		self:CDBar(441395, 67.1, CL.count:format(self:SpellName(441395), darkPulseCount)) -- Dark Pulse
+		self:CDBar(461842, 3.2, CL.count:format(self:SpellName(461842), oozingSmashCount)) -- Oozing Smash
+		self:CDBar(441289, 10.5, CL.count:format(self:SpellName(441289), viscousDarknessCount)) -- Viscous Darkness
+		self:CDBar(461880, 19.0, CL.count:format(self:SpellName(461880), bloodSurgeCount)) -- Blood Surge
+		self:CDBar(441395, 47.2, CL.count:format(self:SpellName(441395), darkPulseCount)) -- Dark Pulse
 	else -- Heroic, Normal
 		self:CDBar(441289, 8.2, CL.count:format(self:SpellName(441289), viscousDarknessCount)) -- Viscous Darkness
 		self:CDBar(461880, 20.3, CL.count:format(self:SpellName(461880), bloodSurgeCount)) -- Blood Surge
@@ -82,35 +82,31 @@ function mod:Warmup() -- called from trash module
 	self:Bar("warmup", 9.6, CL.active, L.warmup_icon)
 end
 
-function mod:OozingSmash(args)
-	self:StopBar(CL.count:format(args.spellName, oozingSmashCount))
-	self:Message(args.spellId, "purple", CL.count:format(args.spellName, oozingSmashCount))
-	oozingSmashCount = oozingSmashCount + 1
-	if self:Mythic() then
-		if oozingSmashCount % 2 == 0 then
-			self:CDBar(args.spellId, 15.4, CL.count:format(args.spellName, oozingSmashCount))
-		else
-			self:CDBar(args.spellId, 58.3, CL.count:format(args.spellName, oozingSmashCount))
-		end
-	else
-		self:CDBar(args.spellId, 77.5, CL.count:format(args.spellName, oozingSmashCount))
-	end
-	self:PlaySound(args.spellId, "alert")
-end
-
 function mod:ViscousDarkness(args)
 	self:StopBar(CL.count:format(args.spellName, viscousDarknessCount))
 	self:Message(441289, "red", CL.count:format(args.spellName, viscousDarknessCount))
 	viscousDarknessCount = viscousDarknessCount + 1
-	self:CDBar(441289, 37.2, CL.count:format(args.spellName, viscousDarknessCount))
+	if viscousDarknessCount == 2 then
+		self:CDBar(441289, 21.8, CL.count:format(args.spellName, viscousDarknessCount))
+	elseif viscousDarknessCount == 3 then
+		self:CDBar(441289, 32.8, CL.count:format(args.spellName, viscousDarknessCount))
+	elseif viscousDarknessCount % 2 == 0 then -- 4, 6...
+		self:CDBar(441289, 36.4, CL.count:format(args.spellName, viscousDarknessCount))
+	else -- 5, 7...
+		self:CDBar(441289, 38.9, CL.count:format(args.spellName, viscousDarknessCount))
+	end
 	self:PlaySound(441289, "long")
 end
 
-function mod:DarkPulse(args)
-	self:StopBar(CL.count:format(args.spellName, darkPulseCount))
-	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, darkPulseCount))
-	darkPulseCount = darkPulseCount + 1
-	self:CDBar(args.spellId, 77.2, CL.count:format(args.spellName, darkPulseCount))
+function mod:OozingSmash(args)
+	self:StopBar(CL.count:format(args.spellName, oozingSmashCount))
+	self:Message(args.spellId, "purple", CL.count:format(args.spellName, oozingSmashCount))
+	oozingSmashCount = oozingSmashCount + 1
+	if oozingSmashCount == 2 then
+		self:CDBar(args.spellId, 54.6, CL.count:format(args.spellName, oozingSmashCount))
+	else -- 3+
+		self:CDBar(args.spellId, 75.3, CL.count:format(args.spellName, oozingSmashCount))
+	end
 	self:PlaySound(args.spellId, "alert")
 end
 
@@ -118,8 +114,20 @@ function mod:BloodSurge(args)
 	self:StopBar(CL.count:format(args.spellName, bloodSurgeCount))
 	self:Message(461880, "orange", CL.count:format(args.spellName, bloodSurgeCount))
 	bloodSurgeCount = bloodSurgeCount + 1
-	self:CDBar(461880, 77.2, CL.count:format(args.spellName, bloodSurgeCount))
+	if bloodSurgeCount == 2 then
+		self:CDBar(461880, 68.0, CL.count:format(args.spellName, bloodSurgeCount))
+	else -- 3+
+		self:CDBar(461880, 75.3, CL.count:format(args.spellName, bloodSurgeCount))
+	end
 	self:PlaySound(461880, "alarm")
+end
+
+function mod:DarkPulse(args)
+	self:StopBar(CL.count:format(args.spellName, darkPulseCount))
+	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, darkPulseCount))
+	darkPulseCount = darkPulseCount + 1
+	self:CDBar(args.spellId, 75.3, CL.count:format(args.spellName, darkPulseCount))
+	self:PlaySound(args.spellId, "alert")
 end
 
 do
