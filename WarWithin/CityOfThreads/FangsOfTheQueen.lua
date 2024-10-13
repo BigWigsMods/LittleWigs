6063e5f8fb474ec8fa0c8e82fcd1bdbee24730ea
Nick Towle <ntowle@gmail.com>
2024-10-12 20:06:18 -0400

    WarWithin/CityOfThreads/FangsOfTheQueen: Duskbringer non-Mythic update

--- WarWithin/CityOfThreads/FangsOfTheQueen.lua
+++ WarWithin/CityOfThreads/FangsOfTheQueen.lua
@@ -133,14 +133,11 @@ function mod:Duskbringer(args)
 	self:StopBar(CL.count:format(args.spellName, duskbringerCount))
 	self:Message(args.spellId, "orange", CL.count:format(args.spellName, duskbringerCount))
 	duskbringerCount = duskbringerCount + 1
-	if self:Mythic() then
-		if duskbringerCount % 2 == 0 then
-			self:CDBar(args.spellId, 44.4, CL.count:format(args.spellName, duskbringerCount))
-		else
-			self:CDBar(args.spellId, 50.8, CL.count:format(args.spellName, duskbringerCount))
-		end
-	else
-		self:CDBar(args.spellId, 96.5, CL.count:format(args.spellName, duskbringerCount))
+	-- TODO the Stage 2 Duskbringer casts can be skipped if Nx is busy casting his filler
+	if self:GetStage() == 1 then
+		self:CDBar(args.spellId, 44.4, CL.count:format(args.spellName, duskbringerCount))
+	else -- Stage 2
+		self:CDBar(args.spellId, 50.8, CL.count:format(args.spellName, duskbringerCount))
 	end
 	self:PlaySound(args.spellId, "alarm")
 end
