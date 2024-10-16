53467ca8c29cc54e76a83e832bbae88fe8f8bd14
007bbb <86834094+007bbb@users.noreply.github.com>
2024-10-16 15:42:29 +0900

    Update koKR.lua

--- WarWithin/DarkflameCleft/Locales/koKR.lua
+++ WarWithin/DarkflameCleft/Locales/koKR.lua
@@ -12,12 +12,12 @@ if L then
 	L.shuffling_horror = "부산한 공포"
 	L.creaky_mine_cart = "삐걱거리는 광산 수레"
 
-	--L.minecart = "Mine Cart Minigame"
-	--L.hits = "Hits"
-	--L.minecart_over = "Winner: %s (%d hits)"
+	L.minecart = "광산 수레 미니게임"
+	L.hits = "맞춤"
+	L.minecart_over = "우승자: %s (%d 맞춤)"
 end
 
 L = BigWigs:NewBossLocale("The Darkness", "koKR")
 if L then
-	--L.wriggling_darkspawn = "Wriggling Darkspawn"
+	L.wriggling_darkspawn = "꿈틀거리는 어둠살이"
 end
