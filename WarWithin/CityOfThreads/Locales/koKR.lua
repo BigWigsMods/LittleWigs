9aa0ab1cf976d4a3b8eea1dd63b91b941ceba5c4
007bbb <86834094+007bbb@users.noreply.github.com>
2024-10-16 15:30:41 +0900

    Update koKR.lua

--- WarWithin/CityOfThreads/Locales/koKR.lua
+++ WarWithin/CityOfThreads/Locales/koKR.lua
@@ -1,13 +1,13 @@
 local L = BigWigs:NewBossLocale("City of Threads Trash", "koKR")
 if not L then return end
 if L then
-	--L.herald_of_ansurek = "Herald of Ansurek"
-	--L.sureki_silkbinder = "Sureki Silkbinder"
-	--L.royal_swarmguard = "Royal Swarmguard"
-	--L.xephitik = "Xeph'itik"
-	--L.pale_priest = "Pale Priest"
-	--L.eye_of_the_queen = "Eye of the Queen"
-	--L.covert_webmancer = "Covert Webmancer"
+	--L.herald_of_ansurek = "안수레크의 전령"
+	--L.sureki_silkbinder = "수레키 비단결속사"
+	--L.royal_swarmguard = "왕실 무리수호병"
+	--L.xephitik = "제프이티크"
+	--L.pale_priest = "창백의 사제"
+	--L.eye_of_the_queen = "여왕의 눈"
+	--L.covert_webmancer = "은밀한 그물술사"
 	--L.royal_venomshell = "Royal Venomshell"
 	--L.unstable_test_subject = "Unstable Test Subject"
 	--L.sureki_unnaturaler = "Sureki Unnaturaler"
