445706dde859b45ca8b35739d998ffaa69546ad6
007bbb <86834094+007bbb@users.noreply.github.com>
2024-10-16 11:57:03 +0900

    Update koKR.lua

--- Delves/Locales/koKR.lua
+++ Delves/Locales/koKR.lua
@@ -1,12 +1,12 @@
 local L = BigWigs:NewBossLocale("Zekvir", "koKR")
 if not L then return end
 if L then
-	--L.zekvir = "Zekvir (Tier 1)"
+	L.zekvir = "제크비르 (1단계)"
 	L.web_terror = "거미줄 괴물"
 end
 
 L = BigWigs:NewBossLocale("Zekvir 2", "koKR")
 if L then
-	--L.zekvir = "Zekvir (Tier 2)"
+	L.zekvir = "제크비르 (2단계)"
 	L.web_terror = "거미줄 괴물"
 end
