da51c1915269661127587c4e0e0f943aaf0900cf
Nick Towle <ntowle@gmail.com>
2024-10-15 22:45:56 -0400

    WarWithin/TheStonevault/VoidSpeakerEirich: Timer updates due to hotfixes

--- WarWithin/TheStonevault/VoidSpeakerEirich.lua
+++ WarWithin/TheStonevault/VoidSpeakerEirich.lua
@@ -16,6 +16,7 @@ local voidCorruptionCount = 1
 local nextVoidCorruption = 0
 local entropicReckoningCount = 1
 local nextEntropicReckoning = 0
+local nextUnbridledVoid = 0
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -41,7 +42,8 @@ function mod:OnEngage()
 	local t = GetTime()
 	voidCorruptionCount = 1
 	entropicReckoningCount = 1
-	self:CDBar(427869, 8.2) -- Unbridled Void
+	nextUnbridledVoid = t + 7.3
+	self:CDBar(427869, 7.3) -- Unbridled Void
 	nextVoidCorruption = t + 15.7
 	self:CDBar(427461, 15.7, CL.count:format(self:SpellName(427461), voidCorruptionCount)) -- Void Corruption
 	nextEntropicReckoning = t + 21.7
@@ -59,10 +61,14 @@ function mod:VoidCorruption(args)
 	voidCorruptionCount = voidCorruptionCount + 1
 	nextVoidCorruption = t + 29.1
 	self:CDBar(args.spellId, 29.1, CL.count:format(args.spellName, voidCorruptionCount))
-	-- 3.63 minimum to next Entropic Reckoning
+	-- 3.63 minimum to next ability
 	if nextEntropicReckoning - t < 3.63 then
 		nextEntropicReckoning = t + 3.63
-		self:CDBar(427852, {3.63, 17.0}, CL.count:format(self:SpellName(427852), entropicReckoningCount)) -- Entropic Reckoning
+		self:CDBar(427852, {3.63, 24.3}, CL.count:format(self:SpellName(427852), entropicReckoningCount)) -- Entropic Reckoning
+	end
+	if nextUnbridledVoid - t < 3.63 then
+		nextUnbridledVoid = t + 3.63
+		self:CDBar(427869, {3.63, 20.6}) -- Unbridled Void
 	end
 	-- in Mythic this always applies to everyone so play the warning sound here, this gives time to pre-position
 	if self:Mythic() then
@@ -92,14 +98,24 @@ function mod:EntropicReckoning(args)
 	self:StopBar(CL.count:format(args.spellName, entropicReckoningCount))
 	self:Message(args.spellId, "red", CL.count:format(args.spellName, entropicReckoningCount))
 	entropicReckoningCount = entropicReckoningCount + 1
-	nextEntropicReckoning = t + 17.0
-	self:CDBar(args.spellId, 17.0, CL.count:format(args.spellName, entropicReckoningCount))
+	nextEntropicReckoning = t + 24.3
+	self:CDBar(args.spellId, 24.3, CL.count:format(args.spellName, entropicReckoningCount))
+	-- 6.05 minimum to next ability
+	if nextVoidCorruption - t < 6.05 then
+		nextVoidCorruption = t + 6.05
+		self:CDBar(427461, {6.05, 29.1}, CL.count:format(self:SpellName(427461), voidCorruptionCount)) -- Void Corruption
+	end
+	if nextUnbridledVoid - t < 6.05 then
+		nextUnbridledVoid = t + 6.05
+		self:CDBar(427869, {6.05, 20.6}) -- Unbridled Void
+	end
 	self:PlaySound(args.spellId, "alert")
 end
 
 function mod:UnbridledVoid(args)
 	local t = GetTime()
 	self:Message(args.spellId, "orange")
+	nextUnbridledVoid = t + 20.6
 	self:CDBar(args.spellId, 20.6)
 	-- 6.05 minimum to next ability
 	if nextVoidCorruption - t < 6.05 then
@@ -108,7 +124,7 @@ function mod:UnbridledVoid(args)
 	end
 	if nextEntropicReckoning - t < 6.05 then
 		nextEntropicReckoning = t + 6.05
-		self:CDBar(427852, {6.05, 17.0}, CL.count:format(self:SpellName(427852), entropicReckoningCount)) -- Entropic Reckoning
+		self:CDBar(427852, {6.05, 24.3}, CL.count:format(self:SpellName(427852), entropicReckoningCount)) -- Entropic Reckoning
 	end
 	self:PlaySound(args.spellId, "alarm")
 end
