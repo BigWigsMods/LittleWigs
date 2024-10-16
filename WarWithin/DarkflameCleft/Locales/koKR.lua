9cec117e31f60f2bdb0958ebe2acc2104424ce17
007bbb <86834094+007bbb@users.noreply.github.com>
2024-10-16 15:37:32 +0900

    Update koKR.lua

--- WarWithin/DarkflameCleft/Locales/koKR.lua
+++ WarWithin/DarkflameCleft/Locales/koKR.lua
@@ -1,16 +1,16 @@
 local L = BigWigs:NewBossLocale("Darkflame Cleft Trash", "koKR")
 if not L then return end
 if L then
-	--L.rank_overseer = "Rank Overseer"
-	--L.lowly_moleherd = "Lowly Moleherd"
+	L.rank_overseer = "무리 감독관r"
+	L.lowly_moleherd = "하찮은 두더지목동"
 	L.kobold_taskworker = "코볼트 잡부"
-	--L.wandering_candle = "Wandering Candle"
-	--L.blazing_fiend = "Blazing Fiend"
-	--L.sootsnout = "Sootsnout"
-	--L.torchsnarl = "Torchsnarl"
+	L.wandering_candle = "떠도는 양초"
+	L.blazing_fiend = "타오르는 마귀"
+	L.sootsnout = "숯주둥이"
+	L.torchsnarl = "횃불이빨"
 	L.skittering_darkness = "딸깍거리는 어둠"
-	--L.shuffling_horror = "Shuffling Horror"
-	--L.creaky_mine_cart = "Creaky Mine Cart"
+	L.shuffling_horror = "부산한 공포"
+	L.creaky_mine_cart = "삐걱거리는 광산 수레"
 
 	--L.minecart = "Mine Cart Minigame"
 	--L.hits = "Hits"
