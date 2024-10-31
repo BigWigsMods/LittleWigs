87247ade70632fab5aa4f85504f4229a1ce434c5
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-10-31 20:25:17 +0800

    Update zhCN.lua

--- Classic/Locales/zhCN.lua
+++ Classic/Locales/zhCN.lua
@@ -14,5 +14,5 @@ end
 
 L = BigWigs:NewBossLocale("Lord Aurius Rivendare", "zhCN")
 if L then
-	--L.death_pact_trigger = "attempts to cast Death Pact on his servants!"
+	L.death_pact_trigger = "尝试对他的仆从释放死亡契约！"
 end
