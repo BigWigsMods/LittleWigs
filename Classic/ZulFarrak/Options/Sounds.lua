ce553eda426e1dc69696fbbeb85c2ff7fe0b2061
Nick Towle <ntowle@gmail.com>
2024-11-03 19:26:12 -0500

    Classic/ZulFarrak/Zumrah: Add boss module (#1188)

--- Classic/ZulFarrak/Options/Sounds.lua
+++ Classic/ZulFarrak/Options/Sounds.lua
@@ -16,6 +16,9 @@ BigWigs:AddSounds("Hydromancer Velratha", {
 })
 
 BigWigs:AddSounds("Witch Doctor Zum'rah", {
+	[11086] = "info",
+	[15982] = "alert",
+	[17228] = "alert",
 })
 
 BigWigs:AddSounds("Gahz'rilla", {
