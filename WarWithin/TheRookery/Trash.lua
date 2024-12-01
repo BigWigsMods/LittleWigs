443d20f0e6f06c9693516c22d31bdce33d704e03
Nick Towle <ntowle@gmail.com>
2024-11-30 19:27:23 -0500

    WarWithin/TheRookery/Trash: Show timer on engage

--- WarWithin/TheRookery/Trash.lua
+++ WarWithin/TheRookery/Trash.lua
@@ -45,8 +45,8 @@ end
 function mod:GetOptions()
 	return {
 		-- Quartermaster Koratite
-		426893, -- Bounding Void
-		450628, -- Entropy Shield
+		{426893, "NAMEPLATE"}, -- Bounding Void
+		{450628, "NAMEPLATE"}, -- Entropy Shield
 		-- Cursed Stormrider
 		{427323, "NAMEPLATE"}, -- Charged Bombardment
 		{427404, "NAMEPLATE"}, -- Localized Storm
@@ -88,37 +88,44 @@ end
 
 function mod:OnBossEnable()
 	-- Quartermaster Koratite
+	self:RegisterEngageMob("QuartermasterKoratiteEngaged", 209801)
 	self:Log("SPELL_CAST_START", "BoundingVoid", 426893)
 	self:Log("SPELL_CAST_START", "EntropyShield", 450628)
 	self:Death("QuartermasterKoratiteDeath", 209801)
 
 	-- Cursed Stormrider
+	self:RegisterEngageMob("CursedStormriderEngaged", 212786)
 	self:Log("SPELL_CAST_START", "ChargedBombardment", 427323)
 	self:Log("SPELL_CAST_START", "LocalizedStorm", 427404)
 	self:Death("CursedStormriderDeath", 212786)
 
 	-- Cursed Rooktender
+	self:RegisterEngageMob("CursedRooktenderEngaged", 207199)
 	self:Log("SPELL_CAST_START", "EnrageRook", 427260)
 	self:Log("SPELL_INTERRUPT", "EnrageRookInterrupt", 427260)
 	self:Log("SPELL_CAST_SUCCESS", "EnrageRookSuccess", 427260)
 	self:Death("CursedRooktenderDeath", 207199)
 
 	-- Unruly Stormrook
+	self:RegisterEngageMob("UnrulyStormrookEngaged", 207186)
 	self:Log("SPELL_CAST_START", "Thunderstrike", 430013)
 	self:Log("SPELL_CAST_START", "EnergizedBarrage", 427616)
 	self:Death("UnrulyStormrookDeath", 207186)
 
 	-- Corrupted Rookguard
+	self:RegisterEngageMob("CorruptedRookguardEngaged", 214419)
 	self:Log("SPELL_CAST_START", "Implosion", 423979)
 	self:Death("CorruptedRookguardDeath", 214419)
 
 	-- Corrupted Oracle
+	self:RegisterEngageMob("CorruptedOracleEngaged", 214439)
 	self:Log("SPELL_CAST_START", "VoidShell", 430754)
 	self:Log("SPELL_CAST_SUCCESS", "SeepingCorruption", 430179)
 	self:Log("SPELL_AURA_APPLIED", "SeepingCorruptionApplied", 430179)
 	self:Death("CorruptedOracleDeath", 214439)
 
 	-- Corrupted Thunderer
+	self:RegisterEngageMob("CorruptedThundererEngaged", 214421)
 	self:Log("SPELL_CAST_START", "AttractingShadows", 430812)
 	self:Death("CorruptedThundererDeath", 214421)
 
@@ -126,6 +133,7 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "Instability", 443854)
 
 	-- Void Ascendant
+	self:RegisterEngageMob("VoidAscendantEngaged", 212793)
 	self:Log("SPELL_CAST_START", "VoidVolley", 432959)
 	self:Log("SPELL_INTERRUPT", "VoidVolleyInterrupt", 432959)
 	self:Log("SPELL_CAST_SUCCESS", "VoidVolleySuccess", 432959)
@@ -133,6 +141,7 @@ function mod:OnBossEnable()
 	self:Death("VoidAscendantDeath", 212793)
 
 	-- Radiating Voidstone
+	self:RegisterEngageMob("RadiatingVoidstoneEngaged", 212739)
 	self:Log("SPELL_CAST_START", "EmbraceTheVoid", 432781)
 	self:Log("SPELL_CAST_SUCCESS", "EmbraceTheVoidSuccess", 432781)
 	self:Log("SPELL_AURA_REMOVED", "EmbraceTheVoidRemoved", 432781)
@@ -151,13 +160,22 @@ end
 do
 	local timer
 
+	function mod:QuartermasterKoratiteEngaged(guid)
+		self:CDBar(426893, 5.8) -- Bounding Void
+		self:Nameplate(426893, 5.8, guid) -- Bounding Void
+		self:CDBar(450628, 9.4) -- Entropy Shield
+		self:Nameplate(450628, 9.4, guid) -- Entropy Shield
+		timer = self:ScheduleTimer("QuartermasterKoratiteDeath", 30)
+	end
+
 	function mod:BoundingVoid(args)
 		if timer then
 			self:CancelTimer(timer)
 		end
 		self:Message(args.spellId, "orange")
-		self:PlaySound(args.spellId, "alarm")
 		self:CDBar(args.spellId, 12.1)
+		self:Nameplate(args.spellId, 12.1, args.sourceGUID)
+		self:PlaySound(args.spellId, "alarm")
 		timer = self:ScheduleTimer("QuartermasterKoratiteDeath", 30)
 	end
 
@@ -166,33 +184,42 @@ do
 			self:CancelTimer(timer)
 		end
 		self:Message(args.spellId, "cyan")
-		self:PlaySound(args.spellId, "info")
 		self:CDBar(args.spellId, 26.7)
+		self:Nameplate(args.spellId, 26.7, args.sourceGUID)
+		self:PlaySound(args.spellId, "info")
 		timer = self:ScheduleTimer("QuartermasterKoratiteDeath", 30)
 	end
 
-	function mod:QuartermasterKoratiteDeath()
+	function mod:QuartermasterKoratiteDeath(args)
 		if timer then
 			self:CancelTimer(timer)
 			timer = nil
 		end
 		self:StopBar(426893) -- Bounding Void
 		self:StopBar(450628) -- Entropy Shield
+		if args then
+			self:ClearNameplate(args.destGUID)
+		end
 	end
 end
 
 -- Cursed Stormrider
 
+function mod:CursedStormriderEngaged(guid)
+	self:Nameplate(427323, 4.0, guid) -- Charged Bombardment
+	self:Nameplate(427404, 10.0, guid) -- Localized Storm
+end
+
 function mod:ChargedBombardment(args)
 	self:Message(args.spellId, "orange")
-	self:PlaySound(args.spellId, "alarm")
 	self:Nameplate(args.spellId, 20.6, args.sourceGUID)
+	self:PlaySound(args.spellId, "alarm")
 end
 
 function mod:LocalizedStorm(args)
 	self:Message(args.spellId, "yellow")
-	self:PlaySound(args.spellId, "info")
 	self:Nameplate(args.spellId, 27.9, args.sourceGUID)
+	self:PlaySound(args.spellId, "info")
 end
 
 function mod:CursedStormriderDeath(args)
@@ -201,10 +228,14 @@ end
 
 -- Cursed Rooktender
 
+function mod:CursedRooktenderEngaged(guid)
+	self:Nameplate(427260, 5.7, guid) -- Enrage Rook
+end
+
 function mod:EnrageRook(args)
 	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
-	self:PlaySound(args.spellId, "alert")
 	self:Nameplate(args.spellId, 0, args.sourceGUID)
+	self:PlaySound(args.spellId, "alert")
 end
 
 function mod:EnrageRookInterrupt(args)
@@ -221,29 +252,34 @@ end
 
 -- Unruly Stormrook
 
+function mod:UnrulyStormrookEngaged(guid)
+	self:Nameplate(430013, 5.7, guid) -- Thunderstrike
+	self:Nameplate(427616, 10.5, guid) -- Energized Barrage
+end
+
 do
 	local prev = 0
 	function mod:Thunderstrike(args)
+		self:Nameplate(args.spellId, 18.2, args.sourceGUID)
 		local t = args.time
 		if t - prev > 2 then
 			prev = t
 			self:Message(args.spellId, "orange")
 			self:PlaySound(args.spellId, "alarm")
 		end
-		self:Nameplate(args.spellId, 18.2, args.sourceGUID)
 	end
 end
 
 do
 	local prev = 0
 	function mod:EnergizedBarrage(args)
+		self:Nameplate(args.spellId, 23.0, args.sourceGUID)
 		local t = args.time
 		if t - prev > 2 then
 			prev = t
 			self:Message(args.spellId, "purple")
 			self:PlaySound(args.spellId, "alarm")
 		end
-		self:Nameplate(args.spellId, 23.0, args.sourceGUID)
 	end
 end
 
@@ -253,10 +289,14 @@ end
 
 -- Corrupted Rookguard
 
+function mod:CorruptedRookguardEngaged(guid)
+	self:Nameplate(423979, 5.2, guid) -- Implosion
+end
+
 function mod:Implosion(args)
 	self:Message(args.spellId, "orange")
-	self:PlaySound(args.spellId, "alarm")
 	self:Nameplate(args.spellId, 17.0, args.sourceGUID)
+	self:PlaySound(args.spellId, "alarm")
 end
 
 function mod:CorruptedRookguardDeath(args)
@@ -265,10 +305,16 @@ end
 
 -- Corrupted Oracle
 
+function mod:CorruptedOracleEngaged(guid)
+	self:Nameplate(430179, 9.1, guid) -- Seeping Corruption
+	self:Nameplate(430754, 14.9, guid) -- Void Shell
+end
+
 function mod:VoidShell(args)
 	self:Message(args.spellId, "red")
-	self:PlaySound(args.spellId, "alert")
+	-- unlike most other abilities, this goes on cooldown on cast start
 	self:Nameplate(args.spellId, 20.7, args.sourceGUID)
+	self:PlaySound(args.spellId, "alert")
 end
 
 function mod:SeepingCorruption(args)
@@ -286,10 +332,14 @@ end
 
 -- Corrupted Thunderer
 
+function mod:CorruptedThundererEngaged(guid)
+	self:Nameplate(430812, 8.3, guid) -- Attracting Shadows
+end
+
 function mod:AttractingShadows(args)
 	self:Message(args.spellId, "yellow")
-	self:PlaySound(args.spellId, "info")
 	self:Nameplate(args.spellId, 21.8, args.sourceGUID)
+	self:PlaySound(args.spellId, "info")
 end
 
 function mod:CorruptedThundererDeath(args)
@@ -312,13 +362,18 @@ end
 
 -- Void Ascendant
 
+function mod:VoidAscendantEngaged(guid)
+	self:Nameplate(432638, 6.6, guid) -- Command Void
+	self:Nameplate(432959, 9.5, guid) -- Void Volley
+end
+
 function mod:VoidVolley(args)
 	if self:Friendly(args.sourceFlags) then -- these NPCs can be mind-controlled by Priests
 		return
 	end
 	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
-	self:PlaySound(args.spellId, "alert")
 	self:Nameplate(args.spellId, 0, args.sourceGUID)
+	self:PlaySound(args.spellId, "alert")
 end
 
 function mod:VoidVolleyInterrupt(args)
@@ -340,9 +395,14 @@ end
 
 -- Radiating Voidstone
 
+function mod:RadiatingVoidstoneEngaged(guid)
+	self:Nameplate(432781, 8.0, guid) -- Embrace the Void
+end
+
 do
 	local prev = 0
 	function mod:EmbraceTheVoid(args)
+		self:Nameplate(args.spellId, 0, args.sourceGUID)
 		local t = args.time
 		if t - prev > 2 then
 			prev = t
@@ -353,7 +413,7 @@ do
 end
 
 function mod:EmbraceTheVoidSuccess(args)
-	self:Nameplate(args.spellId, 27.9, args.sourceGUID)
+	self:Nameplate(args.spellId, 25.1, args.sourceGUID)
 end
 
 do
