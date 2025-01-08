5f08d1850b97cf64da04f63ac526aa028d4db18b
Nick Towle <ntowle@gmail.com>
2025-01-08 03:13:41 -0500

    WarWithin/OperationFloodgate/DemolitionDuo: Add boss module

--- WarWithin/OperationFloodgate/Options/Colors.lua
+++ WarWithin/OperationFloodgate/Options/Colors.lua
@@ -3,6 +3,12 @@ BigWigs:AddColors("Big M.O.M.M.A.", {
 })
 
 BigWigs:AddColors("Demolition Duo", {
+	[459779] = {"blue","red"},
+	[459799] = "purple",
+	[460867] = "yellow",
+	[470090] = "cyan",
+	[473713] = {"blue","orange"},
+	[1217653] = "orange",
 })
 
 BigWigs:AddColors("Swampface", {
