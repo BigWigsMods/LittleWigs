e6b8b9c988648261cc05f985bba9dcf45636ce3f
Nick Towle <ntowle@gmail.com>
2025-01-08 20:01:20 -0500

    Classic/Deadmines/Gilnid: Add boss module (#1235)

--- Classic/Deadmines/Options/Colors.lua
+++ Classic/Deadmines/Options/Colors.lua
@@ -11,6 +11,9 @@ BigWigs:AddColors("Sneed", {
 })
 
 BigWigs:AddColors("Gilnid", {
+	[5159] = {"blue","orange","red"},
+	[5213] = {"blue","orange","red"},
+	[450542] = "cyan",
 })
 
 BigWigs:AddColors("Mr. Smite", {
