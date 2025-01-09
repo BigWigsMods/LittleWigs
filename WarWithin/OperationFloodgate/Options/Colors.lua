6508350f2e17d2d7b4171bc7d26c84baf18001e6
Nick Towle <ntowle@gmail.com>
2025-01-08 20:06:21 -0500

    WarWithin/OperationFloodgate/GeezleGigazap: Add boss module (#1245)

--- WarWithin/OperationFloodgate/Options/Colors.lua
+++ WarWithin/OperationFloodgate/Options/Colors.lua
@@ -26,6 +26,11 @@ BigWigs:AddColors("Swampface", {
 })
 
 BigWigs:AddColors("Geezle Gigazap", {
+	[465463] = "orange",
+	[466190] = "purple",
+	[468723] = "blue",
+	[468813] = "yellow",
+	[468841] = {"blue","red"},
 })
 
 BigWigs:AddColors("Operation: Floodgate Trash", {
--- WarWithin/OperationFloodgate/Options/Sounds.lua
+++ WarWithin/OperationFloodgate/Options/Sounds.lua
@@ -26,6 +26,11 @@ BigWigs:AddSounds("Swampface", {
 })
 
 BigWigs:AddSounds("Geezle Gigazap", {
+	[465463] = "long",
+	[466190] = {"alarm","alert"},
+	[468723] = "underyou",
+	[468813] = "info",
+	[468841] = "warning",
 })
 
 BigWigs:AddSounds("Operation: Floodgate Trash", {
