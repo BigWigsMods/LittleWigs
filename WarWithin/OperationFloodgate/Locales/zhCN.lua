449a1afdc9f90322aef47fd326b91398b85ca418
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-01-12 14:36:38 +0800

    Update zhCN/zhTW (#1249)

--- WarWithin/OperationFloodgate/Locales/zhCN.lua
+++ WarWithin/OperationFloodgate/Locales/zhCN.lua
@@ -2,7 +2,7 @@ local L = BigWigs:NewBossLocale("Operation: Floodgate Trash", "zhCN")
 if not L then return end
 if L then
 	L.weapons_stockpiles_pilfered = "掠夺武器物资"
-	L.weapons_stockpiles_pilfered_desc = "当武器物资被掠夺时显示警告。"
+	L.weapons_stockpiles_pilfered_desc = "当掠夺武器物资时显示警告。"
 
 	L.shreddinator_3000 = "撕碎王3000型"
 	--L.mechadrone_sniper = "Mechadrone Sniper"
@@ -12,8 +12,8 @@ if L then
 	L.darkfuse_bloodwarper = "暗索扭血者"
 	L.undercrawler = "幽暗爬行者"
 	L.venture_co_surveyor = "风险投资公司勘探员"
-	--L.venture_co_architect = "Venture Co. Architect"
-	--L.venture_co_diver = "Venture Co. Diver"
+	L.venture_co_architect = "风险投资公司建筑师"
+	L.venture_co_diver = "风险投资公司潜水员"
 	L.disturbed_kelp = "被惊扰的海藻"
 	L.bomb_pile = "一堆炸弹"
 	L.bubbles = "泡泡"
