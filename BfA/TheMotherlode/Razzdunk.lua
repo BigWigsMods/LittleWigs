1958405cc367446d78d0c46863b8f6039d0e217e
Nick Towle <ntowle@gmail.com>
2025-01-22 01:05:17 -0500

    BfA/TheMotherlode/Razdunk: Update for War Within Season 2

--- BfA/TheMotherlode/Razzdunk.lua
+++ BfA/TheMotherlode/Razzdunk.lua
@@ -1,12 +1,20 @@
-
 --------------------------------------------------------------------------------
 -- Module Declaration
 --
 
 local mod, CL = BigWigs:NewBoss("Mogul Razzdunk", 1594, 2116)
 if not mod then return end
-mod:RegisterEnableMob(129232)
-mod.engageId = 2108
+mod:RegisterEnableMob(129232, 132713) -- the vehicle, the actual boss
+mod:SetEncounterID(2108)
+mod:SetRespawnTime(36.4)
+mod:SetStage(1)
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+local gatlingGunCount = 1
+local homingMissileCount = 1
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -15,9 +23,11 @@ mod.engageId = 2108
 function mod:GetOptions()
 	return {
 		"stages",
+		-- Stage One: Big Guns
 		260280, -- Gatling Gun
 		{260829, "ICON", "SAY"}, -- Homing Missile
 		{276229, "CASTBAR"}, -- Micro Missiles
+		-- Stage Two: Drill!
 		271456, -- Drill Smash
 	}, {
 		[260280] = -18916, -- Stage One: Big Guns
@@ -26,84 +36,113 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
+	-- Stage One: Big Guns
 	self:Log("SPELL_CAST_START", "GatlingGun", 260280)
 	self:Log("SPELL_AURA_APPLIED", "HomingMissile", 260829)
 	self:Log("SPELL_AURA_REMOVED", "HomingMissileRemoved", 260829)
-	self:Log("SPELL_CAST_SUCCESS", "ConfigurationDrill", 260189)
-	self:Log("SPELL_CAST_START", "DrillSmash", 271456)
 	self:Log("SPELL_CAST_START", "MicroMissiles", 276229)
-	self:Log("SPELL_CAST_SUCCESS", "ConfigurationCombat", 260190)
+	self:Log("SPELL_CAST_SUCCESS", "ConfigurationDrill", 260189)
 
+	-- Stage Two: Drill!
+	self:Log("SPELL_CAST_START", "DrillSmash", 271456)
+	self:Log("SPELL_CAST_SUCCESS", "ConfigurationCombat", 260190)
+	self:Death("MogulRazdunkVehicleDeath", 129232)
 end
 
 function mod:OnEngage()
-	self:Bar(260829, 5) -- Homing Missile
-	self:Bar(260280, 15) -- Gatling Gun
+	gatlingGunCount = 1
+	homingMissileCount = 1
+	self:SetStage(1)
+	self:CDBar(260829, 7.0) -- Homing Missile
+	self:CDBar(260280, 15.0) -- Gatling Gun
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+-- Stage One: Big Guns
+
 function mod:GatlingGun(args)
 	self:Message(args.spellId, "yellow")
+	gatlingGunCount = gatlingGunCount + 1
+	if gatlingGunCount % 2 == 0 then
+		self:CDBar(args.spellId, 20.0)
+	else
+		self:CDBar(args.spellId, 25.0)
+	end
 	self:PlaySound(args.spellId, "alert")
-	self:Bar(args.spellId, 20)
 end
 
 function mod:HomingMissile(args)
+	self:TargetMessage(args.spellId, "red", args.destName)
+	self:PrimaryIcon(args.spellId, args.destName)
+	homingMissileCount = homingMissileCount + 1
+	if homingMissileCount % 2 == 0 then
+		self:CDBar(args.spellId, 21.0)
+	else
+		self:CDBar(args.spellId, 24.0)
+	end
 	if self:Me(args.destGUID) then
 		self:Say(args.spellId, nil, nil, "Homing Missile")
 	end
-	self:TargetMessage(args.spellId, "red", args.destName)
 	self:PlaySound(args.spellId, "warning", nil, args.destName)
-	self:TargetBar(args.spellId, 10, args.destName)
-	self:PrimaryIcon(args.spellId, args.destName)
-
-	self:CDBar(args.spellId, 21)
 end
 
 function mod:HomingMissileRemoved(args)
 	self:PrimaryIcon(args.spellId)
-	self:StopBar(args.spellName, args.destName)
-end
-
-function mod:ConfigurationDrill(args)
-	self:Message("stages", "cyan", args.spellName, args.spellId)
-	self:PlaySound("stages", "info")
-	self:StopBar(260829) -- Homing Missile
-	self:StopBar(260280) -- Gatling Gun
-	self:StopBar(CL.cast:format(self:SpellName(276229))) --  Micro Missiles Cast Bar
-end
-
-do
-	local function printTarget(self, name, guid)
-		self:TargetMessage(271456, "orange", name)
-		self:PlaySound(271456, "alert", "watchstep", name)
-	end
-
-	function mod:DrillSmash(args)
-		self:GetUnitTarget(printTarget, 0.4, args.sourceGUID)
-		self:Bar(args.spellId, 8.5)
-	end
 end
 
 do
 	local prev = 0
 	function mod:MicroMissiles(args)
 		local t = args.time
-		if t-prev > 2 then
+		if t - prev > 2 then
 			prev = t
 			self:Message(args.spellId, "red")
-			self:PlaySound(args.spellId, "alarm")
 			self:CastBar(args.spellId, 5)
+			self:PlaySound(args.spellId, "alarm")
 		end
 	end
 end
 
-function mod:ConfigurationCombat(args)
+function mod:ConfigurationDrill(args)
+	homingMissileCount = 1
+	gatlingGunCount = 1
+	self:StopBar(260829) -- Homing Missile
+	self:StopBar(260280) -- Gatling Gun
+	self:StopBar(CL.cast:format(self:SpellName(276229))) --  Micro Missiles Cast Bar
+	self:SetStage(2)
 	self:Message("stages", "cyan", args.spellName, args.spellId)
 	self:PlaySound("stages", "info")
-	self:CDBar(260829, 9) -- Homing Missile
-	self:Bar(260280, 17) -- Gatling Gun
+end
+
+-- Stage Two: Drill!
+
+do
+	local function printTarget(self, name, guid)
+		self:TargetMessage(271456, "orange", name)
+		self:PlaySound(271456, "alert", nil, name)
+	end
+
+	function mod:DrillSmash(args)
+		self:GetUnitTarget(printTarget, 0.4, args.sourceGUID)
+		self:CDBar(args.spellId, 10.9)
+	end
+end
+
+function mod:ConfigurationCombat(args)
+	if self:IsEngaged() then -- cast on respawn
+		self:StopBar(271456) -- Drill Smash
+		self:SetStage(1)
+		self:Message("stages", "cyan", args.spellName, args.spellId)
+		self:CDBar(260829, 9.0) -- Homing Missile
+		self:CDBar(260280, 17.0) -- Gatling Gun
+		self:PlaySound("stages", "info")
+	end
+end
+
+function mod:MogulRazdunkVehicleDeath()
+	self:StopBar(260829) -- Homing Missile
+	self:StopBar(260280) -- Gatling Gun
 end
