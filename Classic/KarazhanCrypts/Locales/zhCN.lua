90ca9969990232ce2242e75c37dbb5c29d679c4c
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-02-09 09:11:11 +0800

    Update zhCN.lua

--- Classic/KarazhanCrypts/Locales/zhCN.lua
+++ Classic/KarazhanCrypts/Locales/zhCN.lua
@@ -1,60 +1,60 @@
 local L = BigWigs:NewBossLocale("Kaigy Maryla", "zhCN")
 if not L then return end
 if L then
-	--L.kaigy_maryla = "Kaigy Maryla"
+	L.kaigy_maryla = "凯基·玛丽拉"
 end
 
 L = BigWigs:NewBossLocale("Sairuh Maryla", "zhCN")
 if L then
-	--L.sairuh_maryla = "Sairuh Maryla"
+	L.sairuh_maryla = "萨鲁·玛丽拉"
 end
 
 L = BigWigs:NewBossLocale("Barian Maryla", "zhCN")
 if L then
-	--L.barian_maryla = "Barian Maryla"
+	L.barian_maryla = "巴利安·玛丽拉"
 end
 
 L = BigWigs:NewBossLocale("Creeping Malison", "zhCN")
 if L then
-	--L.creeping_malison = "Creeping Malison"
+	L.creeping_malison = "蠕行诅咒"
 end
 
 L = BigWigs:NewBossLocale("Trizivast", "zhCN")
 if L then
-	--L.trizivast = "Trizivast"
+	L.trizivast = "特里希瓦斯特"
 end
 
 L = BigWigs:NewBossLocale("Hans and Greta", "zhCN")
 if L then
-	--L.hans_and_greta = "Hans and Greta"
-	--L.hans = "Hans"
-	--L.greta = "Greta"
+	L.hans_and_greta = "汉斯和格雷塔"
+	L.hans = "汉斯"
+	L.greta = "格雷塔"
 end
 
 L = BigWigs:NewBossLocale("Goblins", "zhCN")
 if L then
-	--L.goblins = "Goblins"
-	--L.geenkle = "Geenkle"
-	--L.jank = "Jank"
-	--L.gold_rustboot = "Gold Rustboot"
+	L.goblins = "地精"
+	L.geenkle = "金科尔"
+	L.jank = "詹克"
+	L.gold_rustboot = "歌德·锈靴"
 end
 
 L = BigWigs:NewBossLocale("Harbinger of Sin", "zhCN")
 if L then
-	--L.harbinger_of_sin = "Harbinger of Sin"
+	L.harbinger_of_sin = "罪业先驱"
 end
 
 L = BigWigs:NewBossLocale("Kharon", "zhCN")
 if L then
-	--L.kharon = "Kharon"
+	L.kharon = "卡戎"
 end
 
 L = BigWigs:NewBossLocale("Unk'omon", "zhCN")
 if L then
-	--L.unkomon = "Unk'omon"
+	L.unkomon = "昂克欧蒙"
 end
 
 L = BigWigs:NewBossLocale("Dark Rider", "zhCN")
 if L then
-	--L.dark_rider = "Dark Rider"
+	L.dark_rider = "黑暗骑士"
 end
