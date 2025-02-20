d3197efe14b26b57ff922accdb72a88e7708e119
Nick Towle <ntowle@gmail.com>
2025-02-20 02:49:48 -0500

    WarWithin/OperationFloodgate/Swampface: Event update, timer update

--- WarWithin/OperationFloodgate/Swampface.lua
+++ WarWithin/OperationFloodgate/Swampface.lua
@@ -39,7 +39,7 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "SludgeClaws", 469478)
 
 	-- Mythic
-	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1") -- Razorchoke Vines
+	self:Log("SPELL_CAST_SUCCESS", "RazorchokeVines", 470039)
 	self:Log("SPELL_AURA_APPLIED", "RazorchokeVinesPreApplied", 470038)
 	self:Log("SPELL_AURA_APPLIED", "RazorchokeVinesApplied", 472819)
 end
@@ -50,7 +50,7 @@ function mod:OnEngage()
 	if self:Mythic() then
 		self:CDBar(470039, 1.0) -- Razorchoke Vines
 	end
-	self:CDBar(469478, 3.0, CL.count:format(self:SpellName(469478), sludgeClawsCount)) -- Sludge Claws
+	self:CDBar(469478, 2.0, CL.count:format(self:SpellName(469478), sludgeClawsCount)) -- Sludge Claws
 	self:CDBar(473114, 9.0) -- Mudslide
 	self:CDBar(473070, 19.0, CL.count:format(self:SpellName(473070), awakenTheSwampCount)) -- Awaken the Swamp
 end
@@ -86,12 +86,10 @@ end
 do
 	local firstPlayerName, firstPlayerMe = nil, false
 
-	function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
-		if spellId == 470039 then -- Razorchoke Vines
-			firstPlayerName = nil
-			firstPlayerMe = false
-			self:CDBar(spellId, 30.0)
-		end
+	function mod:RazorchokeVines(args)
+		firstPlayerName = nil
+		firstPlayerMe = false
+		self:CDBar(args.spellId, 30.0)
 	end
 
 	function mod:RazorchokeVinesPreApplied(args)
