d8e46ad2c113b40b1348bfdf9d63cf65180433f0
Nick Towle <ntowle@gmail.com>
2025-02-24 20:09:24 -0500

    Delves/SidestreetSluice/GoldElemental: Add boss module

--- Delves/SidestreetSluice/Options/Colors.lua
+++ Delves/SidestreetSluice/Options/Colors.lua
@@ -3,6 +3,7 @@ BigWigs:AddColors("Geargrave", {
 })
 
 BigWigs:AddColors("Gold Elemental", {
+	[473696] = "orange",
 })
 
 BigWigs:AddColors("Vindle Snapcrank", {
--- Delves/SidestreetSluice/Options/Sounds.lua
+++ Delves/SidestreetSluice/Options/Sounds.lua
@@ -3,6 +3,7 @@ BigWigs:AddSounds("Geargrave", {
 })
 
 BigWigs:AddSounds("Gold Elemental", {
+	[473696] = "alarm",
 })
 
 BigWigs:AddSounds("Vindle Snapcrank", {
