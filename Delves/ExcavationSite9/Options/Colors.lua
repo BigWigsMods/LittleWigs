746ee135e5917d1edfb79699b7738213982a1a62
Nick Towle <ntowle@gmail.com>
2025-02-24 20:06:56 -0500

    Delves/ExcavationSite9/HarbingerUlthul: Add boss module

--- Delves/ExcavationSite9/Options/Colors.lua
+++ Delves/ExcavationSite9/Options/Colors.lua
@@ -3,6 +3,9 @@ BigWigs:AddColors("Craggle Fritzbrains", {
 })
 
 BigWigs:AddColors("Harbinger Ul'thul", {
+	[1213700] = {"blue","red"},
+	[1213776] = {"blue","orange"},
+	[1213785] = "yellow",
 })
 
 BigWigs:AddColors("Xel'anegh The Many", {
