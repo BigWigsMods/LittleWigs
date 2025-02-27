264c48f38bb78223507870d006fe552d2c6200e5
Nick Towle <ntowle@gmail.com>
2025-02-26 21:50:24 -0500

    Delves/ExcavationSite9/HarbingerUlthul: Add boss module (#1278)

--- Delves/ExcavationSite9/HarbingerUlthul.lua
+++ Delves/ExcavationSite9/HarbingerUlthul.lua
@@ -28,15 +28,62 @@ end
 
 function mod:GetOptions()
 	return {
+		{1213776, "DISPEL"}, -- Hopeless Curse
+		1213785, -- Tear It Down
+		1213700, -- Unanswered Call
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "HopelessCurse", 1213776)
+	self:Log("SPELL_AURA_APPLIED", "HopelessCurseApplied", 1213776)
+	self:Log("SPELL_CAST_START", "TearItDown", 1213785)
+	self:Log("SPELL_CAST_START", "UnansweredCall", 1213700)
+	self:Log("SPELL_AURA_APPLIED", "UnansweredCallApplied", 1213838)
 end
 
 function mod:OnEngage()
+	self:CDBar(1213776, 4.7) -- Hopeless Curse
+	self:CDBar(1213785, 9.5) -- Tear It Down
+	self:CDBar(1213700, 30.4) -- Unanswered Call
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:HopelessCurse(args)
+	self:Message(args.spellId, "orange", CL.casting:format(args.spellName))
+	self:CDBar(args.spellId, 21.1)
+	self:PlaySound(args.spellId, "alert")
+end
+
+function mod:HopelessCurseApplied(args)
+	if self:Me(args.destGUID) or self:Dispeller("curse", nil, args.spellId) then
+		self:TargetMessage(args.spellId, "orange", args.destName)
+		if self:Dispeller("curse") then
+			self:PlaySound(args.spellId, "warning", nil, args.destName)
+		else
+			self:PlaySound(args.spellId, "alert", nil, args.destName)
+		end
+	end
+end
+
+function mod:TearItDown(args)
+	self:Message(args.spellId, "yellow")
+	self:CDBar(args.spellId, 10.9)
+	self:PlaySound(args.spellId, "info")
+end
+
+function mod:UnansweredCall(args)
+	self:Message(args.spellId, "red")
+	self:CDBar(args.spellId, 33.9)
+	self:PlaySound(args.spellId, "alarm")
+end
+
+function mod:UnansweredCallApplied(args)
+	if self:Me(args.destGUID) then
+		self:PersonalMessage(1213700)
+		self:PlaySound(1213700, "warning")
+	end
+end
--- Delves/ExcavationSite9/Options/Sounds.lua
+++ Delves/ExcavationSite9/Options/Sounds.lua
@@ -5,6 +5,9 @@ BigWigs:AddSounds("Craggle Fritzbrains", {
 })
 
 BigWigs:AddSounds("Harbinger Ul'thul", {
+	[1213700] = {"alarm","warning"},
+	[1213776] = {"alert","warning"},
+	[1213785] = "info",
 })
 
 BigWigs:AddSounds("Xel'anegh The Many", {
