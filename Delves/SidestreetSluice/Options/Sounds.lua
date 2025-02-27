1d03935430c985746c1ddd3e016163ccb21d92f1
Nick Towle <ntowle@gmail.com>
2025-02-26 21:53:27 -0500

    Delves/SidestreetSluice/VindleSnapcrank: Add boss module (#1282)

--- Delves/SidestreetSluice/Options/Sounds.lua
+++ Delves/SidestreetSluice/Options/Sounds.lua
@@ -11,4 +11,7 @@ BigWigs:AddSounds("Gold Elemental", {
 })
 
 BigWigs:AddSounds("Vindle Snapcrank", {
+	[1215337] = "info",
+	[1215374] = "long",
+	[1215870] = "alarm",
 })
--- Delves/SidestreetSluice/VindleSnapcrank.lua
+++ Delves/SidestreetSluice/VindleSnapcrank.lua
@@ -28,15 +28,44 @@ end
 
 function mod:GetOptions()
 	return {
+		1215870, -- Sprocket Smash
+		1215337, -- Clanker Bomb
+		1215374, -- Shock Maintenance
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "SprocketSmash", 1215870)
+	self:Log("SPELL_CAST_START", "ClankerBomb", 1215337)
+	self:Log("SPELL_CAST_START", "ShockMaintenance", 1215374)
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus") -- XXX no encounter events
+	self:Death("Win", 234931) -- Vindle Snapcrank
 end
 
 function mod:OnEngage()
+	self:CDBar(1215870, 4.8) -- Sprocket Smash
+	self:CDBar(1215337, 9.7) -- Clanker Bomb
+	self:CDBar(1215374, 25.5) -- Shock Maintenance
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:SprocketSmash(args)
+	self:Message(args.spellId, "purple")
+	self:CDBar(args.spellId, 10.9)
+	self:PlaySound(args.spellId, "alarm")
+end
+
+function mod:ClankerBomb(args)
+	self:Message(args.spellId, "red")
+	self:CDBar(args.spellId, 17.0)
+	self:PlaySound(args.spellId, "info")
+end
+
+function mod:ShockMaintenance(args)
+	self:Message(args.spellId, "cyan")
+	self:CDBar(args.spellId, 28.7)
+	self:PlaySound(args.spellId, "long")
+end
