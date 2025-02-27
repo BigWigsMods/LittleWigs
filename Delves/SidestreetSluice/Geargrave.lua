66d38ea723c4ef4fadd167d9cef02a250307c424
Nick Towle <ntowle@gmail.com>
2025-02-26 21:52:27 -0500

    Delves/SidestreetSluice/Geargrave: Add boss module (#1280)

--- Delves/SidestreetSluice/Geargrave.lua
+++ Delves/SidestreetSluice/Geargrave.lua
@@ -28,15 +28,58 @@ end
 
 function mod:GetOptions()
 	return {
+		1215957, -- Tremor Claw
+		1215905, -- Carnage Cannon
+		1215912, -- Black Blood
+		1215975, -- Juice It Up!
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "TremorClaw", 1215957)
+	self:Log("SPELL_CAST_START", "CarnageCannon", 1215905)
+	self:Log("SPELL_PERIODIC_DAMAGE", "BlackBloodDamage", 1215912)
+	self:Log("SPELL_PERIODIC_MISSED", "BlackBloodDamage", 1215912)
+	self:Log("SPELL_CAST_START", "JuiceItUp", 1215975)
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus") -- XXX no encounter events
+	self:Death("Win", 234949) -- Geargrave
 end
 
 function mod:OnEngage()
+	self:CDBar(1215957, 6.0) -- Tremor Claw
+	self:CDBar(1215905, 12.1) -- Carnage Cannon
+	self:CDBar(1215975, 18.2) -- Juice It Up!
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:TremorClaw(args)
+	self:Message(args.spellId, "yellow")
+	self:CDBar(args.spellId, 18.2)
+	self:PlaySound(args.spellId, "alarm")
+end
+
+function mod:CarnageCannon(args)
+	self:Message(args.spellId, "orange")
+	self:CDBar(args.spellId, 21.9)
+	self:PlaySound(args.spellId, "alarm")
+end
+
+do
+	local prev = 0
+	function mod:BlackBloodDamage(args)
+		if self:Me(args.destGUID) and args.time - prev > 2 then
+			prev = args.time
+			self:PersonalMessage(args.spellId, "underyou")
+			self:PlaySound(args.spellId, "underyou")
+		end
+	end
+end
+
+function mod:JuiceItUp(args)
+	self:Message(args.spellId, "red")
+	self:CDBar(args.spellId, 41.3)
+	self:PlaySound(args.spellId, "info")
+end
