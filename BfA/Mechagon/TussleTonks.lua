19825271de79973195dab87a1623fde0937d4aba
Nick Towle <ntowle@gmail.com>
2025-02-28 21:05:45 -0500

    BfA/Mechagon/TussleTonks: Timer improvements

--- BfA/Mechagon/TussleTonks.lua
+++ BfA/Mechagon/TussleTonks.lua
@@ -25,6 +25,8 @@ local platinumPummelCount = 1
 local groundPoundCount = 1
 local b4ttl3MineCount = 1
 local foeFlipperCount = 1
+local nextPlatinumPlating = 0
+local platinumPlatingOnBoss = false
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -65,9 +67,13 @@ function mod:OnBossEnable()
 
 	-- The Platinum Pummeler
 	self:Log("SPELL_CAST_START", "PlatinumPlating", 282801)
-	self:Log("SPELL_CAST_START", "PlatinumPummel", 1215065)
+	self:Log("SPELL_CAST_SUCCESS", "PlatinumPlatingSuccess", 282801)
 	self:Log("SPELL_AURA_REMOVED", "PlatinumPlatingRemoved", 282801)
+	self:Log("SPELL_CAST_SUCCESS", "Stunned", 1215194)
+	self:Log("SPELL_CAST_START", "PlatinumPummel", 1215065)
+	self:Log("SPELL_CAST_SUCCESS", "PlatinumPummelSucccess", 1215065)
 	self:Log("SPELL_CAST_START", "GroundPound", 1215102)
+	self:Log("SPELL_CAST_SUCCESS", "GroundPoundSucccess", 1215102)
 	self:Death("ThePlatinumPummelerDeath", 144244)
 
 	-- Gnomercy 4.U.
@@ -84,6 +90,8 @@ function mod:OnEngage()
 	groundPoundCount = 1
 	b4ttl3MineCount = 1
 	foeFlipperCount = 1
+	nextPlatinumPlating = 0 -- can't be stunned until after Platinum Plating is cast
+	platinumPlatingOnBoss = false
 	self:CDBar(285152, 5.8) -- Foe Flipper
 	self:CDBar(1215065, 7.2, CL.count:format(self:SpellName(1215065), platinumPummelCount)) -- Platinum Pummel
 	self:CDBar(1216431, 12.0) -- B.4.T.T.L.3. Mine
@@ -117,39 +125,85 @@ end
 -- The Platinum Pummeler
 
 function mod:PlatinumPlating(args)
-	self:StopBar(CL.count:format(args.spellName, platinumPlatingCount))
 	self:Message(args.spellId, "cyan", CL.count:format(args.spellName, platinumPlatingCount))
-	-- TODO handle boss being stunned during cast
-	platinumPlatingCount = platinumPlatingCount + 1
-	if self:Mythic() then
-		self:CDBar(args.spellId, 40.5, CL.count:format(args.spellName, platinumPlatingCount))
-	else
-		self:CDBar(args.spellId, 36.4, CL.count:format(args.spellName, platinumPlatingCount))
+	if not platinumPlatingOnBoss then
+		self:StopBar(CL.count:format(args.spellName, platinumPlatingCount))
+		platinumPlatingCount = platinumPlatingCount + 1
+		-- cast at 100 energy, 35s energy gain + 0.2s delay
+		nextPlatinumPlating = args.time + 35.2
+		self:CDBar(args.spellId, 35.2, CL.count:format(args.spellName, platinumPlatingCount))
 	end
 	self:PlaySound(args.spellId, "info")
 end
 
+function mod:PlatinumPlatingSuccess(args)
+	if platinumPlatingOnBoss then
+		-- will recast if boss is stunned during the cast
+		self:StopBar(CL.count:format(args.spellName, platinumPlatingCount))
+		platinumPlatingCount = platinumPlatingCount + 1
+		-- cast at 100 energy, 35s energy gain + 0.2s delay - 1s cast
+		nextPlatinumPlating = args.time + 34.2
+		self:CDBar(args.spellId, 34.2, CL.count:format(args.spellName, platinumPlatingCount))
+	else
+		platinumPlatingOnBoss = true
+	end
+end
+
 function mod:PlatinumPlatingRemoved(args)
+	platinumPlatingOnBoss = false
 	self:Message(args.spellId, "green", CL.removed:format(args.spellName))
 	self:PlaySound(args.spellId, "info")
 end
 
+function mod:Stunned(args)
+	if nextPlatinumPlating ~= 0 then -- reload protection
+		-- energy gain is paused, resumes 5s after being stunned
+		local timeUntilPlatinumPlating = nextPlatinumPlating - args.time
+		if timeUntilPlatinumPlating < 0 then
+			timeUntilPlatinumPlating = 5
+		else
+			timeUntilPlatinumPlating = timeUntilPlatinumPlating + 5
+		end
+		self:CDBar(282801, {timeUntilPlatinumPlating, 40.2}, CL.count:format(self:SpellName(282801), platinumPlatingCount)) -- Platinum Plating
+	end
+end
+
 function mod:PlatinumPummel(args)
-	self:StopBar(CL.count:format(args.spellName, platinumPummelCount))
 	self:Message(args.spellId, "purple", CL.count:format(args.spellName, platinumPummelCount))
-	-- TODO handle boss being stunned during cast
-	platinumPummelCount = platinumPummelCount + 1
-	self:CDBar(args.spellId, 15.1, CL.count:format(args.spellName, platinumPummelCount))
+	if not platinumPlatingOnBoss then
+		self:StopBar(CL.count:format(args.spellName, platinumPummelCount))
+		platinumPummelCount = platinumPummelCount + 1
+		self:CDBar(args.spellId, 15.4, CL.count:format(args.spellName, platinumPummelCount))
+	end
 	self:PlaySound(args.spellId, "alarm")
 end
 
+function mod:PlatinumPummelSucccess(args)
+	if platinumPlatingOnBoss then
+		-- will recast if boss is stunned during the cast
+		self:StopBar(CL.count:format(args.spellName, platinumPummelCount))
+		platinumPummelCount = platinumPummelCount + 1
+		self:CDBar(args.spellId, 12.4, CL.count:format(args.spellName, platinumPummelCount))
+	end
+end
+
 function mod:GroundPound(args)
-	self:StopBar(CL.count:format(args.spellName, groundPoundCount))
 	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, groundPoundCount))
-	-- TODO handle boss being stunned during cast
-	groundPoundCount = groundPoundCount + 1
-	self:CDBar(args.spellId, 18.2, CL.count:format(args.spellName, groundPoundCount))
+	if not platinumPlatingOnBoss then
+		self:StopBar(CL.count:format(args.spellName, groundPoundCount))
+		groundPoundCount = groundPoundCount + 1
+		self:CDBar(args.spellId, 18.2, CL.count:format(args.spellName, groundPoundCount))
+	end
 	self:PlaySound(args.spellId, "info")
+end
+
+function mod:GroundPoundSucccess(args)
+	if platinumPlatingOnBoss then
+		-- will recast if boss is stunned during the cast
+		self:StopBar(CL.count:format(args.spellName, groundPoundCount))
+		groundPoundCount = groundPoundCount + 1
+		self:CDBar(args.spellId, 15.2, CL.count:format(args.spellName, groundPoundCount))
+	end
 end
 
 function mod:ThePlatinumPummelerDeath()
@@ -183,7 +237,7 @@ function mod:B4TTL3Mine(args)
 	elseif b4ttl3MineCount == 3 then
 		self:CDBar(args.spellId, 27.9)
 	else -- 4+
-		self:CDBar(args.spellId, 34.8)
+		self:CDBar(args.spellId, 34.1)
 	end
 	self:PlaySound(args.spellId, "long")
 end
@@ -201,11 +255,11 @@ do
 		self:GetUnitTarget(printTarget, 0.4, args.sourceGUID)
 		foeFlipperCount = foeFlipperCount + 1
 		if foeFlipperCount % 2 == 0 then
-			self:CDBar(args.spellId, 15.8)
+			self:CDBar(args.spellId, 15.0)
 		elseif foeFlipperCount == 3 then
 			self:CDBar(args.spellId, 28.0)
 		else -- odd casts (besides 3)
-			self:CDBar(args.spellId, 19.4)
+			self:CDBar(args.spellId, 19.0)
 		end
 	end
 end
