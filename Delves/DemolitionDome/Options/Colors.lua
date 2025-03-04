c39daa1f0f3adbfe3d89f235532883565469fcad
Nick Towle <ntowle@gmail.com>
2025-03-04 02:58:19 -0500

    Delves/DemolitionDome/Underpin: Initial drycode (#1293)

--- Delves/DemolitionDome/Options/Colors.lua
+++ Delves/DemolitionDome/Options/Colors.lua
@@ -1,5 +1,14 @@
 
 BigWigs:AddColors("The Underpin", {
+	[1213852] = "orange",
+	[1213950] = "yellow",
+	[1214043] = "orange",
+	[1214147] = "yellow",
+	[1215521] = "cyan",
+	[1216333] = "green",
+	[1217371] = "red",
+	[1217667] = "cyan",
+	[1218153] = "blue",
 })
 
 BigWigs:AddColors("The Underpin 2", {
--- Delves/DemolitionDome/Options/Sounds.lua
+++ Delves/DemolitionDome/Options/Sounds.lua
@@ -1,5 +1,14 @@
 
 BigWigs:AddSounds("The Underpin", {
+	[1213852] = "alarm",
+	[1213950] = "alarm",
+	[1214043] = "alarm",
+	[1214147] = "info",
+	[1215521] = "long",
+	[1216333] = "info",
+	[1217371] = "alarm",
+	[1217667] = "long",
+	[1218153] = "underyou",
 })
 
 BigWigs:AddSounds("The Underpin 2", {
