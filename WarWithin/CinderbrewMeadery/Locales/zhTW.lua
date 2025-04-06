a7644660cc8d5359fe271cc26f5d4797b3d4e925
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-04-06 15:16:41 +0800

    Update zhTW.lua

--- WarWithin/CinderbrewMeadery/Locales/zhTW.lua
+++ WarWithin/CinderbrewMeadery/Locales/zhTW.lua
@@ -1,20 +1,20 @@
 local L = BigWigs:NewBossLocale("Cinderbrew Meadery Trash", "zhTW")
 if not L then return end
 if L then
-	--L.venture_co_pyromaniac = "Venture Co. Pyromaniac"
-	--L.hired_muscle = "Hired Muscle"
-	--L.tasting_room_attendant = "Tasting Room Attendant"
-	--L.chef_chewie = "Chef Chewie"
-	--L.cooking_pot = "Cooking Pot"
-	--L.flamethrower = "Flamethrower"
-	--L.flavor_scientist = "Flavor Scientist"
-	--L.careless_hopgoblin = "Careless Hopgoblin"
-	--L.brew_drop = "Brew Drop"
-	--L.taste_tester = "Taste Tester"
-	--L.bee_wrangler = "Bee Wrangler"
-	--L.venture_co_honey_harvester = "Venture Co. Honey Harvester"
-	--L.royal_jelly_purveyor = "Royal Jelly Purveyor"
-	--L.yes_man = "Yes Man"
+	L.venture_co_pyromaniac = "風險投資公司縱火魔"
+	L.hired_muscle = "受雇打手"
+	L.tasting_room_attendant = "品酒室侍從"
+	L.chef_chewie = "嚼嚼大廚"
+	L.cooking_pot = "煮菜鍋"
+	L.flamethrower = "火焰噴射器"
+	L.flavor_scientist = "調味科學家"
+	L.careless_hopgoblin = "粗心的蹦跳哥布林"
+	L.brew_drop = "酒水滴"
+	L.taste_tester = "品酒師"
+	L.bee_wrangler = "蜜蜂牧者"
+	L.venture_co_honey_harvester = "風險投資公司蜂蜜收割者"
+	L.royal_jelly_purveyor = "絕佳的蜂王乳供應商"
+	L.yes_man = "應聲蟲"
 
 	--L.custom_on_cooking_autotalk_desc = "|cFFFF0000Requires 25 skill in Khaz Algar Alchemy or Cooking.|r Automatically select the NPC dialog option that grants you 'Sticky Honey' which you can use by clicking your extra action button.\n\n|T451169:16|tSticky Honey\n{438997}"
 	--L.custom_on_flamethrower_autotalk_desc = "|cFFFF0000Requires Gnome, Goblin, Mechagnome, or 25 skill in Khaz Algar Engineering.|r Automatically select the NPC dialog option that grants you 'Flame On' which you can use by clicking your extra action button.\n\n|T135789:16|tFlame On\n{439616}"
@@ -22,5 +22,5 @@ end
 
 L = BigWigs:NewBossLocale("Brew Master Aldryr", "zhTW")
 if L then
-	--L.cinderbrew_delivered = "Cinderbrew delivered"
+	L.cinderbrew_delivered = "燼釀蜜酒已交付"
 end
