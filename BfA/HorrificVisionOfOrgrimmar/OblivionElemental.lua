46980e21b9b590541eacd33b827d93ea74a71462
Nick Towle <ntowle@gmail.com>
2025-04-06 16:09:52 -0400

    BfA/HorrificVisionOfOrgrimmar/OblivionElemental: Update for 11.1.5

--- BfA/HorrificVisionOfOrgrimmar/OblivionElemental.lua
+++ BfA/HorrificVisionOfOrgrimmar/OblivionElemental.lua
@@ -1,13 +1,21 @@
-
 --------------------------------------------------------------------------------
 -- Module Declaration
 --
 
 local mod, CL = BigWigs:NewBoss("Oblivion Elemental", 2212)
 if not mod then return end
-mod:RegisterEnableMob(153244)
+mod:RegisterEnableMob(
+	153244, -- Oblivion Elemental
+	234040 -- Oblivion Elemental (Revisited)
+)
+mod:SetEncounterID({2372, 3088}) -- BFA, Revisited
 mod:SetAllowWin(true)
-mod.engageId = 2372
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+local hopelessnessCount = 1
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -33,7 +41,13 @@ function mod:OnRegister()
 end
 
 function mod:OnBossEnable()
-	self:Log("SPELL_CAST_SUCCESS", "Hopelessness", 297574)
+	self:Log("SPELL_CAST_START", "Hopelessness", 297574)
+	self:Log("SPELL_AURA_APPLIED", "HopelessnessApplied", 297574)
+	self:Log("SPELL_AURA_REMOVED", "HopelessnessRemoved", 297574)
+end
+
+function mod:OnEngage()
+	hopelessnessCount = 1
 end
 
 --------------------------------------------------------------------------------
@@ -41,6 +55,24 @@ end
 --
 
 function mod:Hopelessness(args)
-	self:Message(args.spellId, "yellow")
-	self:PlaySound(args.spellId, "info")
+	if hopelessnessCount == 1 then
+		self:Message(args.spellId, "yellow", CL.percent:format(80, CL.casting:format(args.spellName)))
+	else
+		self:Message(args.spellId, "yellow", CL.percent:format(40, CL.casting:format(args.spellName)))
+	end
+	hopelessnessCount = hopelessnessCount + 1
+end
+
+function mod:HopelessnessApplied(args)
+	if self:Me(args.destGUID) then
+		self:PersonalMessage(args.spellId)
+		self:PlaySound(args.spellId, "alarm")
+	end
+end
+
+function mod:HopelessnessRemoved(args)
+	if self:Me(args.destGUID) then
+		self:Message(args.spellId, "green", CL.removed:format(args.spellName))
+		self:PlaySound(args.spellId, "info")
+	end
 end
