d6d5a8f9630b47ad4e656da6d46485b090ba5d2a
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-04-06 20:44:46 +0800

    Update zhTW.lua

--- WarWithin/AraKara/Locales/zhTW.lua
+++ WarWithin/AraKara/Locales/zhTW.lua
@@ -16,11 +16,11 @@ if L then
 	L.nerubian_hauler = "奈幽搬運工"
 	L.winged_carrier = "飛翼搬運者"
 
-	--L.avanoxx_warmup_trigger = "The Attendants have been silenced... something emerges!"
-	--L.custom_on_autotalk_desc = "|cFFFF0000Requires 25 skill in Khaz Algar Tailoring.|r Automatically select the NPC dialog option that grants you 'Silk Wrap' which you can use by clicking your extra action button."
+	L.avanoxx_warmup_trigger = "侍從安靜下來…有什麼東西冒出來了！"
+	L.custom_on_autotalk_desc = "|cFFFF0000需要至少25點卡茲阿爾加裁縫技能。|r 自動和NPC對話 拉扯蛛絲獲得'絲線纏繞' 使用時需要點擊額外快捷鍵。"
 end
 
 L = BigWigs:NewBossLocale("Anub'zekt", "zhTW")
 if L then
-	--L.bloodstained_webmage_desc = "Anub'zekt summons a Bloodstained Webmage.\n\n{-28975}"
+	L.bloodstained_webmage_desc = "阿努扎克特召喚了染血蟲網法師。\n\n{-28975}"
 end
