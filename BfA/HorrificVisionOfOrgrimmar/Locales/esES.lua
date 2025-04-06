6ae0c7c00d7e3c66c6d56be2d10d8855320b41bb
Nick Towle <ntowle@gmail.com>
2025-04-06 18:29:04 -0400

    BfA/HorrificVisionOfOrgrimmar/Trash: Update for 11.1.5 (#1311)

--- BfA/HorrificVisionOfOrgrimmar/Locales/esES.lua
+++ BfA/HorrificVisionOfOrgrimmar/Locales/esES.lua
@@ -1,44 +1,54 @@
-local L = BigWigs:NewBossLocale("Horrific Vision of Orgrimmar Trash", "esES") or BigWigs:NewBossLocale("Horrific Vision of Orgrimmar Trash", "esMX")
+local L = BigWigs:NewBossLocale("Horrific Vision of Orgrimmar Trash", "esES")
 if not L then return end
 if L then
 	L.voidbound_shaman = "Chamán ligado al Vacío"
+	L.endless_hunger_totem = "Tótem de hambre insaciable"
 	L.crawling_corruption = "Corrupción trepadora"
+	L.void_globule = "Glóbulo del Vacío"
+	L.voidbound_ravager = "Devastador ligado al Vacío"
 	L.voidbound_honor_guard = "Guardia de honor ligado al Vacío"
 	L.voidbound_shieldbearer = "Portaescudos ligado al Vacío"
+	L.naros = "Naros"
+	L.barkeep_morag = "Posadero Morag"
 	L.decimator_shiqvoth = "Diezmador Shiq'voth"
 	L.voidbound_berserker = "Rabioso ligado al Vacío"
+	L.bwemba = "Bwemba"
 	L.greater_void_elemental = "Elemental del Vacío superior"
+	L.faceless_willbreaker = "Quiebravoluntades ignoto"
+	L.burrowing_appendage = "Apéndice cavador"
 	L.annihilator_lakhal = "Aniquilador Lak'hal"
 	L.kthir_dominator = "Dominador k'thir"
 	L.kthir_mindcarver = "Trinchamentes k'thir"
 	L.coagulated_horror = "Horror coagulado"
 	L.voidcrazed_hulk = "Mole trastornada por el Vacío"
 	L.misha = "Misha"
+	L.aqir_scarab = "Escarabajo aqir"
 	L.aqir_bonecrusher = "Machacahuesos aqir"
+	L.aqir_mindhunter = "Cazamentes aqir"
 	L.aqir_venomweaver = "Tejeveneno aqir"
 end
 
-L = BigWigs:NewBossLocale("Thrall", "esES") or BigWigs:NewBossLocale("Thrall", "esMX")
+L = BigWigs:NewBossLocale("Thrall", "esES")
 if L then
 	L.thrall = "Thrall"
 end
 
-L = BigWigs:NewBossLocale("Oblivion Elemental", "esES") or BigWigs:NewBossLocale("Oblivion Elemental", "esMX")
+L = BigWigs:NewBossLocale("Oblivion Elemental", "esES")
 if L then
 	L.oblivion_elemental = "Elemental del olvido"
 end
 
-L = BigWigs:NewBossLocale("Inquisitor Gnshal", "esES") or BigWigs:NewBossLocale("Inquisitor Gnshal", "esMX")
+L = BigWigs:NewBossLocale("Inquisitor Gnshal", "esES")
 if L then
 	L.inquisitor_gnshal = "Inquisidor Gnshal"
 end
 
-L = BigWigs:NewBossLocale("Vez'okk the Lightless", "esES") or BigWigs:NewBossLocale("Vez'okk the Lightless", "esMX")
+L = BigWigs:NewBossLocale("Vez'okk the Lightless", "esES")
 if L then
 	L.vezokk = "Vez'okk el Lóbrego"
 end
 
-L = BigWigs:NewBossLocale("Rexxar", "esES") or BigWigs:NewBossLocale("Rexxar", "esMX")
+L = BigWigs:NewBossLocale("Rexxar", "esES")
 if L then
 	L.rexxar = "Rexxar"
 end
