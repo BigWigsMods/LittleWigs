49f2e50df9ffe1efa33a17aee2a0d2a358a5661b
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-04-09 13:15:01 +0800

    Update zhCN.lua

--- BfA/HorrificVisionOfStormwind/Locales/zhCN.lua
+++ BfA/HorrificVisionOfStormwind/Locales/zhCN.lua
@@ -25,8 +25,8 @@ if L then
 	L.zardeth_of_the_black_claw = "黑爪加尔德斯"
 	L.unstable_servant = "不稳定的仆从"
 
-	--L.therum_deepforge_warmup_trigger = "So ye like tae play with explosives, do ye? Then let's play."
-	--L.alleria_windrunner_warmup_trigger = "Mother... do not listen to the whispers!"
+	L.therum_deepforge_warmup_trigger = "你们喜欢放烟花是吧？那我们就好好玩玩。"
+	L.alleria_windrunner_warmup_trigger = "母亲……别再听那些低语了！"
 end
 
 L = BigWigs:NewBossLocale("Alleria Windrunner", "zhCN")
