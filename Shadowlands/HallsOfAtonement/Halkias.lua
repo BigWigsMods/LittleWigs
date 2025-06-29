7348fe373f084ae234b70b49c6a4fe5b5981ea12
Nick Towle <ntowle@gmail.com>
2025-06-29 18:30:13 -0400

    Shadowlands/HallsOfAtonement/Halkias: Update for War Within Season 3

--- Shadowlands/HallsOfAtonement/Halkias.lua
+++ Shadowlands/HallsOfAtonement/Halkias.lua
@@ -12,12 +12,16 @@ mod:SetRespawnTime(30)
 -- Locals
 --
 
-local refractedSinlightTime = 0
 local crumblingSlamCount = 0
 
 --------------------------------------------------------------------------------
 -- Initialization
 --
+
+function mod:OnRegister()
+	self:SetSpellRename(322711, CL.beams) -- Refracted Sinlight (Beams)
+	self:SetSpellRename(339237, CL.fear) -- Sinlight Visions (Fear)
+end
 
 function mod:GetOptions()
 	return {
@@ -33,21 +37,25 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
-	self:Log("SPELL_AURA_APPLIED", "GlassShardsDamage", 323001)
 	self:Log("SPELL_PERIODIC_DAMAGE", "GlassShardsDamage", 323001)
 	self:Log("SPELL_PERIODIC_MISSED", "GlassShardsDamage", 323001)
 	self:Log("SPELL_CAST_START", "CrumblingSlam", 322936)
-	self:Log("SPELL_CAST_SUCCESS", "HeaveDebris", 322943)
+	self:Log("SPELL_CAST_START", "HeaveDebris", 322943)
 	self:Log("SPELL_CAST_START", "RefractedSinlight", 322711)
+	self:Log("SPELL_CAST_START", "SinlightVisions", 322977) -- Heroic/Normal only
 	self:Log("SPELL_AURA_APPLIED", "SinlightVisionsApplied", 339237, 322977) -- Mythic, Heroic/Normal
 end
 
 function mod:OnEngage()
-	refractedSinlightTime = GetTime() + 49.8
 	crumblingSlamCount = 0
-	self:CDBar(322936, 4.9) -- Crumbling Slam
-	self:CDBar(322943, 13.7) -- Heave Debris
-	self:CDBar(322711, 49.8, CL.beams) -- Refracted Sinlight
+	self:CDBar(322936, 4.5) -- Crumbling Slam
+	self:CDBar(322943, 13.1) -- Heave Debris
+	if self:Mythic() then
+		self:CDBar(322711, 49.8, CL.beams) -- Refracted Sinlight
+	else -- Heroic/Normal
+		self:CDBar(339237, 13.1, CL.fear) -- Sinlight Visions
+		self:CDBar(322711, 32.6, CL.beams) -- Refracted Sinlight
+	end
 end
 
 --------------------------------------------------------------------------------
@@ -57,46 +65,38 @@ end
 do
 	local prev = 0
 	function mod:GlassShardsDamage(args)
-		if self:Me(args.destGUID) then
-			local t = args.time
-			if t - prev > 2 then
-				prev = t
-				self:PersonalMessage(args.spellId, "underyou")
-				self:PlaySound(args.spellId, "underyou", nil, args.destName)
-			end
+		if self:Me(args.destGUID) and args.time - prev > 2.25 then -- 0.5s tick rate
+			prev = args.time
+			self:PersonalMessage(args.spellId, "underyou")
+			self:PlaySound(args.spellId, "underyou")
 		end
 	end
 end
 
 function mod:CrumblingSlam(args)
 	self:Message(args.spellId, "purple")
-	self:PlaySound(args.spellId, "alert")
 	crumblingSlamCount = crumblingSlamCount + 1
-	if refractedSinlightTime - GetTime() > 12.1 then
-		-- only the second Crumbling Slam of the fight is delayed
-		self:CDBar(args.spellId, crumblingSlamCount == 1 and 13.4 or 12.1)
-	else
-		self:StopBar(args.spellId)
-	end
+	-- only the second Crumbling Slam of the fight is delayed
+	self:CDBar(args.spellId, crumblingSlamCount == 1 and 13.4 or 12.1)
+	self:PlaySound(args.spellId, "alert")
 end
 
 function mod:HeaveDebris(args)
 	self:Message(args.spellId, "yellow")
+	self:CDBar(args.spellId, 12.1)
 	self:PlaySound(args.spellId, "alarm")
-	if refractedSinlightTime - GetTime() > 12.1 then
-		self:CDBar(args.spellId, 12.1)
-	else
-		self:StopBar(args.spellId)
-	end
 end
 
 function mod:RefractedSinlight(args)
-	refractedSinlightTime = GetTime() + 45
 	self:Message(args.spellId, "red", CL.beams)
-	self:PlaySound(args.spellId, "warning")
-	self:CDBar(args.spellId, 47.3, CL.beams)
 	self:CDBar(322936, 15.7) -- Crumbling Slam
 	self:CDBar(322943, 17.2) -- Heave Debris
+	self:CDBar(args.spellId, 47.3, CL.beams)
+	self:PlaySound(args.spellId, "warning")
+end
+
+function mod:SinlightVisions(args) -- Heroic/Normal only
+	self:CDBar(339237, 30.1, CL.fear) -- Sinlight Visions
 end
 
 function mod:SinlightVisionsApplied(args)
