6ac4f9ed9620fef502ffc878b05988073e40a4fd
Nick Towle <ntowle@gmail.com>
2025-07-04 14:43:06 -0400

    WarWithin/OperationFloodgate/BigMOMMA: Update for War Within Season 3

--- WarWithin/OperationFloodgate/BigMOMMA.lua
+++ WarWithin/OperationFloodgate/BigMOMMA.lua
@@ -1,3 +1,4 @@
+local isElevenDotTwo = BigWigsLoader.isNext -- XXX remove in 11.2
 --------------------------------------------------------------------------------
 -- Module Declaration
 --
@@ -50,7 +51,11 @@ function mod:OnEngage()
 	self:SetStage(1)
 	self:CDBar(473351, 5.7) -- Electrocrush
 	self:CDBar(473220, 15.4) -- Sonic Boom
-	self:CDBar(469981, 51.0) -- Kill-o-Block Barrier
+	if isElevenDotTwo then -- XXX remove check in 11.2
+		self:CDBar(469981, 55.7) -- Kill-o-Block Barrier
+	else -- XXX remove block in 11.2
+		self:CDBar(469981, 51.0) -- Kill-o-Block Barrier
+	end
 end
 
 --------------------------------------------------------------------------------
@@ -89,7 +94,11 @@ function mod:MobilizeMechadrones(args)
 	self:Message(args.spellId, "cyan")
 	self:CDBar(473351, 9.2) -- Electrocrush
 	self:CDBar(473220, 18.9) -- Sonic Boom
-	self:CDBar(469981, 54.1) -- Kill-o-Block Barrier
+	if isElevenDotTwo then -- XXX remove check in 11.2
+		self:CDBar(469981, 58.9) -- Kill-o-Block Barrier
+	else -- XXX remove block in 11.2
+		self:CDBar(469981, 54.1) -- Kill-o-Block Barrier
+	end
 	self:PlaySound(args.spellId, "info")
 end
 
