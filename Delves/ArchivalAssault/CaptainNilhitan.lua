b367237f30e3023f9703ce4730ac559ad3aef01b
Nick Towle <ntowle@gmail.com>
2025-07-20 19:35:58 -0400

    Delves/ArchivalAssault/CaptainNilhitan: Add boss module (#1386)

--- Delves/ArchivalAssault/CaptainNilhitan.lua
+++ Delves/ArchivalAssault/CaptainNilhitan.lua
@@ -29,15 +29,71 @@ end
 
 function mod:GetOptions()
 	return {
+		1239350, -- All Hands!
+		{1239427, "DISPEL"}, -- Scuttle That One!
+		1239445, -- Broadside
+		-- Ethereal Scallywag
+		{1239407, "NAMEPLATE"}, -- Run Through
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "AllHands", 1239350)
+	self:Log("SPELL_CAST_START", "ScuttleThatOne", 1239427)
+	self:Log("SPELL_AURA_APPLIED", "ScuttleThatOneApplied", 1239427)
+	self:Log("SPELL_CAST_START", "Broadside", 1239445)
+
+	-- Ethereal Scallywag
+	self:Log("SPELL_SUMMON", "AllHandsSummon", 1239859, 1239350)
+	self:Log("SPELL_CAST_START", "RunThrough", 1239407)
+	self:Death("EtherealScallywagDeath", 245722)
 end
 
 function mod:OnEngage()
+	self:CDBar(1239350, 10.7) -- All Hands!
+	self:CDBar(1239427, 20.4) -- Scuttle That One!
+	self:CDBar(1239445, 30.1) -- Broadside
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:AllHands(args)
+	self:Message(args.spellId, "cyan")
+	self:CDBar(args.spellId, 31.9)
+	self:PlaySound(args.spellId, "info")
+end
+
+function mod:ScuttleThatOne(args)
+	self:Message(args.spellId, "red")
+	self:CDBar(args.spellId, 30.3)
+	self:PlaySound(args.spellId, "alert")
+end
+
+function mod:ScuttleThatOneApplied(args)
+	if self:Me(args.destGUID) or self:Dispeller("magic", nil, args.spellId) then
+		self:TargetMessage(args.spellId, "red", args.destName)
+		self:PlaySound(args.spellId, "info", nil, args.destName)
+	end
+end
+
+function mod:Broadside(args)
+	self:Message(args.spellId, "orange")
+	self:CDBar(args.spellId, 32.7)
+	self:PlaySound(args.spellId, "alarm")
+end
+
+function mod:AllHandsSummon(args)
+	self:Nameplate(1239407, 3.2, args.destGUID) -- Run Through
+end
+
+function mod:RunThrough(args)
+	self:Message(args.spellId, "yellow")
+	self:Nameplate(args.spellId, 10.9, args.sourceGUID)
+	self:PlaySound(args.spellId, "alarm")
+end
+
+function mod:EtherealScallywagDeath(args)
+	self:ClearNameplate(args.destGUID)
+end
