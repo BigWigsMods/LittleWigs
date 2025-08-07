aea6ffa9d1f0082e916f58ae183b5042c6d30887
Nick Towle <ntowle@gmail.com>
2025-08-07 00:25:21 -0400

    Delves/SidestreetSluice/Geargrave: Update engage detection

--- Delves/SidestreetSluice/Geargrave.lua
+++ Delves/SidestreetSluice/Geargrave.lua
@@ -5,11 +5,12 @@
 local mod, CL = BigWigs:NewBoss("Geargrave", {2684, 2688, 2826}) -- The Dread Pit, The Spiral Weave, Sidestreet Sluice
 if not mod then return end
 mod:RegisterEnableMob(
-	234949, -- Geargrave (Sidestreet Sluice, The Dread Pit)
-	236531 -- Geargrave (The Spiral Weave)
+	234949, -- Geargrave (The Dread Pit)
+	236531, -- Geargrave (The Spiral Weave)
+	247481, -- Geargrave (Sidestreet Sluice)
+	249829 -- Geargrave (Sidestreet Sluice)
 )
---mod:SetEncounterID(3174) -- encounter event doesn't fire in Sidestreet Sluice
--- 3020 in The Dread Pit (does fire), 3123 in The Spiral Weave (does fire)
+mod:SetEncounterID({3020, 3123, 3174, 3352}) -- The Dread Pit, The Spiral Weave, Sidestreet Sluice, Sidestreet Sluice
 --mod:SetRespawnTime(15) resets, doesn't respawn
 mod:SetAllowWin(true)
 
@@ -48,8 +49,6 @@ function mod:OnBossEnable()
 	self:Log("SPELL_PERIODIC_DAMAGE", "BlackBloodDamage", 1215912)
 	self:Log("SPELL_PERIODIC_MISSED", "BlackBloodDamage", 1215912)
 	self:Log("SPELL_CAST_START", "JuiceItUp", 1215975)
-	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus") -- XXX no encounter events
-	self:Death("Win", 234949, 236531) -- Geargrave (Sidestreet Sluice, The Dread Pit), Geargrave (The Spiral Weave)
 end
 
 function mod:OnEngage()
