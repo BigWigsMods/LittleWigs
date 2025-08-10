8fc03d7dc420b233625cfa85bcc06b5c1f895c5f
Meruski <meryskra@gmail.com>
2025-08-10 20:48:41 +0200

    Update esES.lua

--- Classic/!Locales/esES.lua
+++ Classic/!Locales/esES.lua
@@ -10,7 +10,7 @@ end
 
 L = BigWigs:NewBossLocale("Lord Aurius Rivendare", "esES")
 if L then
-		L.death_pact_trigger = "intenta lanzar un Pacto de muerte a sus sirvientes!"  -- ¡Lord Aurius Osahendido intenta lanzar un Pacto de muerte a sus sirvientes!
+	L.death_pact_trigger = "intenta lanzar un Pacto de muerte a sus sirvientes!"  -- ¡Lord Aurius Osahendido intenta lanzar un Pacto de muerte a sus sirvientes!
 end
 
 -- Blackrock Spire (Vanilla through Mists only)
