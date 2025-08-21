4b59789acc0f667afdefa3694f710bf05298b643
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-08-21 15:15:48 +0800

    Update zhCN.lua

--- Shadowlands/!Locales/zhCN.lua
+++ Shadowlands/!Locales/zhCN.lua
@@ -168,7 +168,7 @@ end
 L = BigWigs:NewBossLocale("Myza's Oasis", "zhCN")
 if L then
 	L.notes = "音符"
-	L.notes_desc = "当音符可用时显示提示信息。使用你的乐器能力击中音符可获得'爵士乐'效果。\n\n|T237554:16|t爵士乐\n{348567}"
+	L.notes_desc = "当音符可用时显示提示信息。使用你的乐器击中音符可获得'爵士乐'增益。\n\n|T237554:16|t爵士乐\n{348567}"
 end
 
 L = BigWigs:NewBossLocale("So'leah", "zhCN")
