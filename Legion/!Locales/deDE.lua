ab49551a786e31d442b5862ccf243bcd48eb043a
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2026-02-14 18:27:54 +0100

    Update deDE (#1458)

--- Legion/!Locales/deDE.lua
+++ Legion/!Locales/deDE.lua
@@ -5,30 +5,30 @@ if L then
 	L.tugar = "Tugar Bluttotem"
 	L.jormog = "Jormog das Ungetüm"
 
-	--L.remaining = "Scales Remaining"
+	L.remaining = "Verbleibende Schuppen"
 
-	--L.submerge = "Submerge"
-	--L.submerge_desc = "Submerges below the ground, summoning spitter eggs and falling spikes."
+	L.submerge = "Untertauchen"
+	L.submerge_desc = "Taucht unter den Boden und beschwört Spuckereier und fallende Stacheln."
 
-	--L.charge_desc = "When Jormog is submerged, he will periodically charge in your direction."
+	L.charge_desc = "Während Jormog untergetaucht ist, stürmt er regelmäßig in Deine Richtung."
 
-	--L.rupture = "{243382} (X)"
-	--L.rupture_desc = "A Fel Rupture in the shape of an X appears under you. After 5 seconds it will rupture the ground, sending spikes into the air and knocking back players on top of it."
+	L.rupture = "{243382} (X)"
+	L.rupture_desc = "Ein X-förmiger Teufelsriss erscheint unter Dir. Nach 5 Sekunden reißt er den Boden auf, wobei Stacheln aus dem Boden schießen und auf ihnen befindliche Spieler zurückstoßen."
 
-	--L.totem_warning = "The totem hit you!"
+	L.totem_warning = "Das Totem trifft Dich!"
 end
 
 L = BigWigs:NewBossLocale("Raest", "deDE")
 if L then
 	L.name = "Raest Magusspeer"
 
-	--L.handFromBeyond = "Hand from Beyond"
+	L.handFromBeyond = "Hand aus den Weiten"
 
-	--L.rune_desc = "Places a Rune of Summoning on the ground. If left unsoaked a Thing of Nightmare will spawn."
+	L.rune_desc = "Platziert eine Beschwörungsrune auf dem Boden. Wenn diese nicht gesoaked wird, erscheint eine Ausgeburt des Alptraums."
 
-	--L.warmup_text = "Karam Magespear Active"
-	--L.warmup_trigger = "You were a fool to follow me, brother. The Twisting Nether feeds my strength. I have become more powerful than you could ever imagine!"
-	--L.warmup_trigger2 = "Kill this interloper, brother!"
+	L.warmup_text = "Karam Magusspeer aktiv"
+	L.warmup_trigger = "Es war dumm, mir zu folgen, Bruder. Der Wirbelnde Nether verleiht mir ungeahnte Macht. Ich bin stärker, als du es dir jemals hättest vorstellen können!"
+	L.warmup_trigger2 = "Töte diesen Eindringling, Bruder!"
 end
 
 L = BigWigs:NewBossLocale("Kruul", "deDE")
@@ -37,21 +37,21 @@ if L then
 	L.inquisitor = "Inquisitor Variss"
 	L.velen = "Prophet Velen"
 
-	--L.warmup_trigger = "Arrogant fools! I am empowered by the souls of a thousand conquered worlds!"
-	--L.win_trigger = "So be it. You will not stand in our way any longer."
+	L.warmup_trigger = "Arrogantes Pack! Ich trage die Seelen von tausend unterworfenen Welten in mir!"
+	L.win_trigger = "So sei es. Ihr werdet uns nicht länger im Weg stehen."
 
-	--L.nether_aberration_desc = "Summons portals around the room, spawning Nether Aberrations."
+	L.nether_aberration_desc = "Beschwört im Raum verteilt Portale, aus denen Netherschrecken erscheinen."
 
-	--L.smoldering_infernal = "Smoldering Infernal"
-	--L.smoldering_infernal_desc = "Summons a Smoldering Infernal."
+	L.smoldering_infernal = "Qualmende Höllenbestie"
+	L.smoldering_infernal_desc = "Beschwört eine Qualmende Höllenbestie."
 end
 
 L = BigWigs:NewBossLocale("Lord Erdris Thorn", "deDE")
 if L then
 	L.erdris = "Lord Erdris Dorn"
 
-	--L.warmup_trigger = "Your arrival is well-timed."
-	--L.warmup_trigger2 = "What's... happening?" --Stage 5 Warm up
+	L.warmup_trigger = "Ihr kommt gerade zur rechten Zeit."
+	L.warmup_trigger2 = "Was... passiert?" --Stage 5 Warm up
 
 	L.mage = "Verderbter auferstandener Magier"
 	L.soldier = "Verderbter auferstandener Soldat"
@@ -63,8 +63,8 @@ if L then
 	L.name = "Erzmagier Xylem"
 	L.corruptingShadows = "Verderbende Schatten"
 
-	--L.warmup_trigger1 = "With the Focusing Iris under my control" -- You are too late, demon hunter! With the Focusing Iris under my control, I can siphon the arcane energy from Azeroth's ley lines directly into my magnificent self!
-	--L.warmup_trigger2 = "Drained of magic, your world will be ripe" -- Drained of magic, your world will be ripe for destruction by my demon masters... and my power will be limitless!
+	L.warmup_trigger1 = "Mit der fokussierenden Iris kann ich die arkane Energie" -- You are too late, demon hunter! With the Focusing Iris under my control, I can siphon the arcane energy from Azeroth's ley lines directly into my magnificent self!
+	L.warmup_trigger2 = "Ihrer Magie beraubt wird diese Welt" -- Drained of magic, your world will be ripe for destruction by my demon masters... and my power will be limitless!
 end
 
 L = BigWigs:NewBossLocale("Agatha", "deDE")
@@ -74,9 +74,9 @@ if L then
 	L.fuming_imp = "Rauchender Wichtel"
 	L.levia = "Levia" -- Shortcut for warmup_trigger1, since the name "Levia" should be unique
 
-	--L.warmup_trigger1 = "You are too late! Levia's power is mine! Using her knowledge, my minions will infiltrate the Kirin Tor and dismantle it from the inside!" -- 35
-	--L.warmup_trigger2 = "Even now, my sayaad tempt your weak-willed mages. Your allies will surrender willingly to the Legion!" -- 16
-	--L.warmup_trigger3 = "But first, you must be punished for taking away my little pet." -- 3
+	L.warmup_trigger1 = "Ihr kommt zu spät! Levias Macht gehört mir! Meine Diener werden ihr Wissen nutzen, um die Kirin Tor zu unterwandern und sie aus dem Inneren zu zerstören." -- 35
+	L.warmup_trigger2 = "Meine Sayaad bringen Eure schwachen Magier bereits in Versuchung. Eure Verbündeten werden sich widerstandslos der Legion beugen." -- 16
+	L.warmup_trigger3 = "Doch zuerst werde ich Euch dafür bestrafen, mir meine kleine Sklavin genommen zu haben." -- 3
 
 	L.stacks = "Stapel"
 end
@@ -87,7 +87,7 @@ if L then
 	L.jarl = "Jarl Velbrand"
 	L.faljar = "Runenseher Faljar"
 
-	--L.warmup_trigger = "What's this? The outsider has come to stop me?"
+	L.warmup_trigger = "Wie? Der Fremdling kommt, mich aufzuhalten?"
 end
 
 -- Assault on Violet Hold
@@ -139,7 +139,7 @@ L = BigWigs:NewBossLocale("Mephistroth", "deDE")
 if L then
 	L.custom_on_time_lost = "Verlorene Zeit während Schattenverblassen"
 	L.custom_on_time_lost_desc = "Zeigt die verlorene Zeit während Schattenverblassen in der Leiste in |cffff0000red|r."
-	--L.time_lost = "%s |cffff0000(+%ds)|r"
+	L.time_lost = "%s |cffff0000(+%ds)|r"
 end
 
 L = BigWigs:NewBossLocale("Domatrax", "deDE")
