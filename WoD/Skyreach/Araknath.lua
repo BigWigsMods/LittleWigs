c73a57811c77f2f4f468087f4526391865fca85d
Nick Towle <ntowle@gmail.com>
2026-02-14 15:31:43 -0500

    WoD/Skyreach/Araknath: Update for Midnight

--- WoD/Skyreach/Araknath.lua
+++ WoD/Skyreach/Araknath.lua
@@ -1,4 +1,3 @@
-
 --------------------------------------------------------------------------------
 -- Module Declaration
 --
@@ -6,8 +5,11 @@
 local mod, CL = BigWigs:NewBoss("Araknath", 1209, 966)
 if not mod then return end
 mod:RegisterEnableMob(76141)
-mod.engageId = 1699
-mod.respawnTime = 23 -- respawns 11s after, unattackable for a while
+mod:SetEncounterID(1699)
+mod:SetRespawnTime(23) -- respawns 11s after, unattackable for a while
+mod:SetPrivateAuraSounds({
+	{154150, sound = "alert"}, -- Light Ray
+})
 
 --------------------------------------------------------------------------------
 -- Locals
@@ -40,29 +42,46 @@ function mod:OnEngage()
 end
 
 --------------------------------------------------------------------------------
+-- Midnight Initialization
+--
+
+if mod:Retail() then -- Midnight+
+	function mod:GetOptions()
+		return {
+			{154150, "PRIVATE"}, -- Light Ray
+		}
+	end
+
+	function mod:OnBossEnable()
+	end
+end
+
+--------------------------------------------------------------------------------
 -- Event Handlers
 --
 
 do
 	local prev = 0
 	function mod:Energize(args)
-		local t = GetTime()
-		if t-prev > 5 then -- More than 1 in Challenge Mode
-			prev = t
-			self:MessageOld(args.spellId, "yellow", "long", CL.incoming:format(args.spellName))
+		if args.time - prev > 5 then -- More than 1 in Challenge Mode
+			prev = args.time
+			self:Message(args.spellId, "yellow", CL.incoming:format(args.spellName))
 			self:Bar(args.spellId, 23)
+			self:PlaySound(args.spellId, "long")
 		end
 	end
 end
 
 function mod:Smash()
-	self:MessageOld(154110, "orange", "warning")
+	self:Message(154110, "orange")
 	smashCount = smashCount + 1
 	self:CDBar(154110, smashCount % 2 == 0 and 14.6 or 8.5)
+	self:PlaySound(154110, "warning")
 end
 
 function mod:Burst(args)
 	burstCount = burstCount + 1
-	self:MessageOld(args.spellId, "red", "info", CL.count:format(args.spellName, burstCount))
+	self:Message(args.spellId, "red", CL.count:format(args.spellName, burstCount))
 	self:CDBar(args.spellId, 23)
+	self:PlaySound(args.spellId, "info")
 end
