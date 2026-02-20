c334d019bf6b577390a2b5fb5bca92140af03026
007bbb <86834094+007bbb@users.noreply.github.com>
2026-02-21 04:46:10 +0900

    Update koKR.lua

--- Midnight/!Locales/koKR.lua
+++ Midnight/!Locales/koKR.lua
@@ -2,22 +2,22 @@
 
 local L = BigWigs:NewBossLocale("Den of Nalorakk Trash", "koKR")
 if L then
-    --L.offerings_acquired = "Offerings Acquired"
-	--L.offerings_acquired_desc = "Show an alert when an offering has been acquired."
+    L.offerings_acquired = "획득한 제물"
+	L.offerings_acquired_desc = "제물이 획득되었을 때 알림을 표시합니다."
 end
 
 -- Maisara Caverns
 
 L = BigWigs:NewBossLocale("Maisara Caverns Trash", "koKR")
 if L then
-	--L.prisoners_freed = "Prisoners Freed"
-	--L.prisoners_freed_desc = "Show an alert when a prisoner has been freed."
+	L.prisoners_freed = "포로 해방"
+	L.prisoners_freed_desc = "포로가 해방되었을 때 알림을 표시합니다."
 end
 
 -- Murder Row
 
 L = BigWigs:NewBossLocale("Murder Row Trash", "koKR")
 if L then
-	--L.snitches_interrogated = "Snitches Interrogated"
-	--L.snitches_interrogated_desc = "Show an alert when a snitch has been interrogated."
+	L.snitches_interrogated = "끄나풀 심문"
+	L.snitches_interrogated_desc = "끄나풀이 심문되었을 때 알림을 표시합니다."
 end
