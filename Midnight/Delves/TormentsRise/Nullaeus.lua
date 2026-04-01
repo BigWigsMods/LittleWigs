003544ec8a98b6c9e321d2875b75c5df342a191b
funkydude <funkydude@users.noreply.github.com>
2026-04-01 03:14:08 +0100

    Midnight/Delves/TormentsRise/Nullaeus: Add backup timers, clean up whitespace

--- Midnight/Delves/TormentsRise/Nullaeus.lua
+++ Midnight/Delves/TormentsRise/Nullaeus.lua
@@ -25,6 +25,7 @@ local implodingStrikeCount = 1
 local devouringEssenceCount = 1
 local addsCount = 1
 local activeBars = {}
+local backupBars = {}
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -33,7 +34,7 @@ local activeBars = {}
 local L = mod:GetLocale()
 if L then
 	L.nullaeus = "Nullaeus"
-    L.adds_icon = "inv_babyvoidwalker_silver"
+	L.adds_icon = "inv_babyvoidwalker_silver"
 end
 
 --------------------------------------------------------------------------------
@@ -45,18 +46,18 @@ function mod:OnRegister()
 end
 
 function mod:GetOptions()
-    return {
-        1280086, -- Emptiness of the Void
-        1280087, -- Imploding Strike
-        1280088, -- Devouring Essence
-        "adds",
-        {1256045, "PRIVATE"}, -- Null Zone
+	return {
+		1280086, -- Emptiness of the Void
+		1280087, -- Imploding Strike
+		1280088, -- Devouring Essence
+		"adds",
+		{1256045, "PRIVATE"}, -- Null Zone
 		{1256167, "PRIVATE"}, -- Void Hole
 		--{1256358, "PRIVATE"}, -- Devouring Essence
 		{1256366, "PRIVATE"}, -- Jagged Rip
 		{1256518, "PRIVATE"}, -- Poisonous Spit
 		{1256526, "PRIVATE"}, -- Curse of Hesitation
-    }
+	}
 end
 
 mod:UseCustomTimers(true)
@@ -64,12 +65,19 @@ function mod:OnEncounterStart()
 	emptinessOfTheVoidCount = 1
 	implodingStrikeCount = 1
 	devouringEssenceCount = 1
-    addsCount = 1
+	addsCount = 1
 	activeBars = {}
+	backupBars = {}
 	if self:ShouldShowBars() then
 		self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_ADDED")
 		self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED")
 		self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_REMOVED")
+	end
+end
+
+function mod:OnBossDisable()
+	for eventID in next, backupBars do
+		self:SendMessage("BigWigs_StopBar", nil, nil, eventID)
 	end
 end
 
@@ -81,16 +89,24 @@ function mod:ENCOUNTER_TIMELINE_EVENT_ADDED(_, eventInfo)
 	if eventInfo.source ~= 0 then return end -- Enum.EncounterTimelineEventSource.Encounter
 	local duration = self:RoundNumber(eventInfo.duration, 1)
 	local barInfo
-    if duration == 7 or duration == 21 or duration == 21.3 then -- Emptiness of the Void
-        barInfo = self:EmptinessOfTheVoidTimeline(eventInfo)
-    elseif duration == 12 or duration == 15.5 or duration == 15.8 then -- Imploding Strike
-        -- this is only cast if a tank is the leader
-        barInfo = self:ImplodingStrikeTimeline(eventInfo)
-    elseif duration == 16 or duration == 18.5 or duration == 18.8 then -- Devouring Essence
-        barInfo = self:DevouringEssenceTimeline(eventInfo)
-    elseif not self:IsWiping() then
-        self:ErrorForTimelineEvent(eventInfo)
-    end
+	if duration == 7 or duration == 21 or duration == 21.3 then -- Emptiness of the Void
+		barInfo = self:EmptinessOfTheVoidTimeline(eventInfo)
+	elseif duration == 12 or duration == 15.5 or duration == 15.8 then -- Imploding Strike
+		-- this is only cast if a tank is the leader
+		barInfo = self:ImplodingStrikeTimeline(eventInfo)
+	elseif duration == 16 or duration == 18.5 or duration == 18.8 then -- Devouring Essence
+		barInfo = self:DevouringEssenceTimeline(eventInfo)
+	elseif not self:IsWiping() then
+		self:ErrorForTimelineEvent(eventInfo)
+
+		backupBars[eventInfo.id] = true
+		self:SendMessage("BigWigs_StartBar", nil, nil, ("[B] %s"):format(eventInfo.spellName), eventInfo.duration, eventInfo.iconFileID, eventInfo.maxQueueDuration, nil, eventInfo.id, eventInfo.id)
+
+		local state = C_EncounterTimeline.GetEventState(eventInfo.id)
+		if state == 1 then -- Enum.EncounterTimelineEventState.Paused = 1
+			self:SendMessage("BigWigs_PauseBar", nil, nil, eventInfo.id)
+		end
+	end
 	if barInfo then
 		activeBars[eventInfo.id] = barInfo
 	end
@@ -103,9 +119,9 @@ function mod:ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED(_, eventID)
 		if state == 0 then -- Active
 			self:ResumeBar(barInfo.key, barInfo.msg)
 		elseif state == 1 then -- Paused
-            -- bars pausing means adds are spawning (and also just stop the bars)
+			-- bars pausing means adds are spawning (and also just stop the bars)
 			self:StopBar(barInfo.msg)
-            self:Adds()
+			self:Adds()
 		elseif state == 2 then -- Finished
 			self:StopBar(barInfo.msg)
 			if barInfo.callback then
@@ -116,6 +132,15 @@ function mod:ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED(_, eventID)
 			self:StopBar(barInfo.msg)
 			activeBars[eventID] = nil
 		end
+	elseif backupBars[eventID] then
+		local newState = C_EncounterTimeline.GetEventState(eventID)
+		if newState == 0 then -- Enum.EncounterTimelineEventState.Active
+			self:SendMessage("BigWigs_ResumeBar", nil, nil, eventID)
+		elseif newState == 1 then -- Enum.EncounterTimelineEventState.Paused
+			self:SendMessage("BigWigs_PauseBar", nil, nil, eventID)
+		else -- Canceled / Finished
+			self:SendMessage("BigWigs_StopBar", nil, nil, eventID)
+		end
 	end
 end
 
@@ -124,6 +149,9 @@ function mod:ENCOUNTER_TIMELINE_EVENT_REMOVED(_, eventID)
 	if barInfo then
 		self:StopBar(barInfo.msg)
 		activeBars[eventID] = nil
+	elseif backupBars[eventID] then
+		backupBars[eventID] = nil
+		self:SendMessage("BigWigs_StopBar", nil, nil, eventID)
 	end
 end
 
@@ -134,10 +162,10 @@ end
 function mod:EmptinessOfTheVoidTimeline(eventInfo)
 	local barText = CL.count:format(self:SpellName(1280086), emptinessOfTheVoidCount)
 	self:CDBar(1280086, eventInfo.duration, barText, nil, eventInfo.id)
-    if emptinessOfTheVoidCount > 1 then
-        self:Message(1280086, "red", CL.casting:format(CL.count:format(self:SpellName(1280086), emptinessOfTheVoidCount - 1)))
-        self:PlaySound(1280086, "warning")
-    end
+	if emptinessOfTheVoidCount > 1 then
+		self:Message(1280086, "red", CL.casting:format(CL.count:format(self:SpellName(1280086), emptinessOfTheVoidCount - 1)))
+		self:PlaySound(1280086, "warning")
+	end
 	emptinessOfTheVoidCount = emptinessOfTheVoidCount + 1
 	return {
 		msg = barText,
@@ -148,10 +176,10 @@ end
 function mod:ImplodingStrikeTimeline(eventInfo)
 	local barText = CL.count:format(self:SpellName(1280087), implodingStrikeCount)
 	self:CDBar(1280087, eventInfo.duration, barText, nil, eventInfo.id)
-    if implodingStrikeCount > 1 then
-        self:Message(1280087, "orange", CL.count:format(self:SpellName(1280087), implodingStrikeCount - 1))
-        self:PlaySound(1280087, "alarm")
-    end
+	if implodingStrikeCount > 1 then
+		self:Message(1280087, "orange", CL.count:format(self:SpellName(1280087), implodingStrikeCount - 1))
+		self:PlaySound(1280087, "alarm")
+	end
 	implodingStrikeCount = implodingStrikeCount + 1
 	return {
 		msg = barText,
@@ -162,10 +190,10 @@ end
 function mod:DevouringEssenceTimeline(eventInfo)
 	local barText = CL.count:format(self:SpellName(1280088), devouringEssenceCount)
 	self:CDBar(1280088, eventInfo.duration, barText, nil, eventInfo.id)
-    if devouringEssenceCount > 1 then
-        self:Message(1280088, "yellow", CL.count:format(self:SpellName(1280088), devouringEssenceCount - 1))
-        self:PlaySound(1280088, "info")
-    end
+	if devouringEssenceCount > 1 then
+		self:Message(1280088, "yellow", CL.count:format(self:SpellName(1280088), devouringEssenceCount - 1))
+		self:PlaySound(1280088, "info")
+	end
 	devouringEssenceCount = devouringEssenceCount + 1
 	return {
 		msg = barText,
@@ -174,13 +202,13 @@ function mod:DevouringEssenceTimeline(eventInfo)
 end
 
 do
-    local prev = 0
-    function mod:Adds()
-        if GetTime() - prev > 2 then
-            prev = GetTime()
-            self:Message("adds", "cyan", CL.count_amount:format(CL.adds_spawning, addsCount, 3), L.adds_icon)
-            addsCount = addsCount + 1
-            self:PlaySound("adds", "long")
-        end
-    end
+	local prev = 0
+	function mod:Adds()
+		if GetTime() - prev > 2 then
+			prev = GetTime()
+			self:Message("adds", "cyan", CL.count_amount:format(CL.adds_spawning, addsCount, 3), L.adds_icon)
+			addsCount = addsCount + 1
+			self:PlaySound("adds", "long")
+		end
+	end
 end
