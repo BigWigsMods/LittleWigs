046e027937e1bdc09c168e87c2b4ca9fe2f3a3dd
Nick Towle <ntowle@gmail.com>
2026-04-05 14:12:29 -0400

    Midnight/MaisaraCaverns/MurojinAndNekraxx: Bestial Wrath, support recreating bars

--- Midnight/MaisaraCaverns/MurojinAndNekraxx.lua
+++ Midnight/MaisaraCaverns/MurojinAndNekraxx.lua
@@ -28,8 +28,12 @@ local barrageCount = 1
 local carrionSwoopCount = 1
 local count45 = 1
 local murojinAlive = true
-local nekraxxAlive = true
+local resuming = 0
+local lastCanceledTime = 0
 local activeBars = {}
+local canceledBars = {}
+local canceledBarsCount = 0
+local backupBars = {}
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -43,6 +47,7 @@ function mod:GetOptions()
 		1243900, -- Fetid Quillstorm
 		1260643, -- Barrage
 		1249479, -- Carrion Swoop
+		1249947, -- Bestial Wrath
 		{1243741, "PRIVATE"}, -- Freezing Trap
 		{1243752, "PRIVATE"}, -- Icy Slick
 		--{1246666, "PRIVATE"}, -- Infected Pinions
@@ -63,8 +68,12 @@ function mod:OnEncounterStart()
 	carrionSwoopCount = 1
 	count45 = 1
 	murojinAlive = true
-	nekraxxAlive = true
+	resuming = 0
+	lastCanceledTime = 0
 	activeBars = {}
+	canceledBars = {}
+	canceledBarsCount = 0
+	backupBars = {}
 	if self:ShouldShowBars() then
 		self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_ADDED")
 		self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED")
@@ -72,15 +81,79 @@ function mod:OnEncounterStart()
 	end
 end
 
+function mod:OnWin()
+	resuming = 0
+	lastCanceledTime = 0
+	activeBars = {}
+	canceledBars = {}
+	canceledBarsCount = 0
+	backupBars = {}
+end
+
+function mod:OnBossDisable()
+	for eventID in next, backupBars do
+		self:SendMessage("BigWigs_StopBar", nil, nil, eventID)
+	end
+end
+
 --------------------------------------------------------------------------------
 -- Timeline Event Handlers
 --
 
+function mod:GetClosestSpell(duration)
+	local closestKey = nil
+    local minDiff = math.huge
+	local threshold = 1 -- only match if within 1 second of expected duration (usually within 0.03)
+    for key, originalExpirationTime in pairs(canceledBars) do
+        local diff = math.abs(originalExpirationTime - lastCanceledTime - duration + 3) -- Blizz adds 3s
+        if diff < minDiff and diff <= threshold then
+            minDiff = diff
+            closestKey = key
+        end
+    end
+	if closestKey then
+        canceledBars[closestKey] = nil
+    end
+    return closestKey
+end
+
 function mod:ENCOUNTER_TIMELINE_EVENT_ADDED(_, eventInfo)
 	if eventInfo.source ~= 0 then return end -- Enum.EncounterTimelineEventSource.Encounter
-	local duration = math.floor(eventInfo.duration + 0.5)
+	local duration = self:RoundNumber(eventInfo.duration, 0)
 	local barInfo
-	if duration == 5 or (duration == 45 and count45 % 6 == 1) then -- Flanking Spear (Muro'jin)
+	-- only try to resume bars if all 6 bars have been canceled and there are still bars left to resume
+	if resuming > 0 and canceledBarsCount % 6 == 0 then
+		local resumingSpell = self:GetClosestSpell(eventInfo.duration)
+		if resumingSpell == 1266480 then -- Flanking Spear
+			resuming = resuming - 1
+			barInfo = self:FlankingSpearTimeline(eventInfo)
+		elseif resumingSpell == 1246666 then -- Infected Pinions
+			resuming = resuming - 1
+			barInfo = self:InfectedPinionsTimeline(eventInfo)
+		elseif resumingSpell == 1260731 then -- Freezing Trap
+			resuming = resuming - 1
+			barInfo = self:FreezingTrapTimeline(eventInfo)
+		elseif resumingSpell == 1243900 then -- Fetid Quillstorm
+			resuming = resuming - 1
+			barInfo = self:FetidQuillstormTimeline(eventInfo)
+		elseif resumingSpell == 1260643 then -- Barrage
+			resuming = resuming - 1
+			barInfo = self:BarrageTimeline(eventInfo)
+		elseif resumingSpell == 1249479 then -- Carrion Swoop
+			resuming = resuming - 1
+			barInfo = self:CarrionSwoopTimeline(eventInfo)
+		elseif not self:IsWiping() then
+			resuming = resuming - 1
+			self:ErrorForTimelineEvent(eventInfo)
+			backupBars[eventInfo.id] = true
+			self:SendMessage("BigWigs_StartBar", nil, nil, ("[B] %s"):format(eventInfo.spellName), eventInfo.duration, eventInfo.iconFileID, eventInfo.maxQueueDuration, nil, eventInfo.id, eventInfo.id)
+		end
+		if resuming == 0 then
+			-- if all bars have been resumed (or backups started), clear the state
+			canceledBarsCount = 0
+			canceledBars = {}
+		end
+	elseif duration == 5 or (duration == 45 and count45 % 6 == 1) then -- Flanking Spear (Muro'jin)
 		barInfo = self:FlankingSpearTimeline(eventInfo)
 	elseif duration == 12 or (duration == 45 and count45 % 6 == 2) then -- Infected Pinions (Nekraxx)
 		barInfo = self:InfectedPinionsTimeline(eventInfo)
@@ -94,15 +167,18 @@ function mod:ENCOUNTER_TIMELINE_EVENT_ADDED(_, eventInfo)
 		barInfo = self:CarrionSwoopTimeline(eventInfo)
 	elseif not self:IsWiping() then
 		self:ErrorForTimelineEvent(eventInfo)
+		backupBars[eventInfo.id] = true
+		self:SendMessage("BigWigs_StartBar", nil, nil, ("[B] %s"):format(eventInfo.spellName), eventInfo.duration, eventInfo.iconFileID, eventInfo.maxQueueDuration, nil, eventInfo.id, eventInfo.id)
 	end
 	if duration == 45 then
-		if murojinAlive and nekraxxAlive then
+		if murojinAlive then
 			count45 = count45 + 1
 		else -- increment counter by 2 to skip the dead boss's abilities
 			count45 = count45 + 2
 		end
 	end
 	if barInfo then
+		barInfo.expirationTime = GetTime() + eventInfo.duration
 		activeBars[eventInfo.id] = barInfo
 	end
 end
@@ -110,7 +186,7 @@ end
 do
 	local murojinAbilities = {
 		[1266480] = true, -- Flanking Spear
-		[1243741] = true, -- Freezing Trap
+		[1260731] = true, -- Freezing Trap
 		[1260643] = true, -- Barrage
 	}
 	local nekraxxAbilities = {
@@ -134,18 +210,35 @@ do
 				activeBars[eventID] = nil
 			elseif state == 3 then -- Canceled
 				self:StopBar(barInfo.msg)
+				if not self:IsWiping() and barInfo.cancelCallback then
+					barInfo.cancelCallback()
+				end
+				lastCanceledTime = GetTime()
+				canceledBars[barInfo.key] = barInfo.expirationTime
+				canceledBarsCount = canceledBarsCount + 1
 				activeBars[eventID] = nil
-				if murojinAbilities[barInfo.key] then
-					murojinAlive = false
-					if count45 % 2 == 1 then -- if Muro'jin died when his ability was next, increment the counter to skip it
-						count45 = count45 + 1
+				if murojinAbilities[barInfo.key] and resuming == 0 then
+					-- if Nekraxx is killed then Nekraxx's abilities will always be canceled first, so if resuming is 0 then
+					-- Muro'jin is definitely dead.
+					if murojinAlive then
+						murojinAlive = false
+						if count45 % 2 == 1 then -- if Muro'jin died when his ability was next, increment the counter to skip it
+							count45 = count45 + 1
+						end
+						self:BestialWrath()
 					end
 				elseif nekraxxAbilities[barInfo.key] then
-					nekraxxAlive = false
-					if count45 % 2 == 0 then -- if Nekraxx died when her ability was next, increment the counter to skip it
-						count45 = count45 + 1
-					end
+					resuming = 6
 				end
+			end
+		elseif backupBars[eventID] then
+			local state = C_EncounterTimeline.GetEventState(eventID)
+			if state == 0 then -- Enum.EncounterTimelineEventState.Active
+				self:SendMessage("BigWigs_ResumeBar", nil, nil, eventID)
+			elseif state == 1 then -- Enum.EncounterTimelineEventState.Paused
+				self:SendMessage("BigWigs_PauseBar", nil, nil, eventID)
+			else -- Canceled / Finished
+				self:SendMessage("BigWigs_StopBar", nil, nil, eventID)
 			end
 		end
 	end
@@ -156,6 +249,9 @@ function mod:ENCOUNTER_TIMELINE_EVENT_REMOVED(_, eventID)
 	if barInfo then
 		self:StopBar(barInfo.msg)
 		activeBars[eventID] = nil
+	elseif backupBars[eventID] then
+		backupBars[eventID] = nil
+		self:SendMessage("BigWigs_StopBar", nil, nil, eventID)
 	end
 end
 
@@ -173,6 +269,9 @@ function mod:FlankingSpearTimeline(eventInfo)
 		callback = function()
 			self:Message(1266480, "purple", barText)
 			self:PlaySound(1266480, "alert")
+		end,
+		cancelCallback = function()
+			flankingSpearCount = flankingSpearCount - 1
 		end
 	}
 end
@@ -187,6 +286,9 @@ function mod:InfectedPinionsTimeline(eventInfo)
 		callback = function()
 			self:Message(1246666, "yellow", barText)
 			self:PlaySound(1246666, "alert")
+		end,
+		cancelCallback = function()
+			infectedPinionsCount = infectedPinionsCount - 1
 		end
 	}
 end
@@ -201,6 +303,9 @@ function mod:FreezingTrapTimeline(eventInfo)
 		callback = function()
 			self:Message(1260731, "orange", barText)
 			self:PlaySound(1260731, "alarm")
+		end,
+		cancelCallback = function()
+			freezingTrapCount = freezingTrapCount - 1
 		end
 	}
 end
@@ -215,6 +320,9 @@ function mod:FetidQuillstormTimeline(eventInfo)
 		callback = function()
 			self:Message(1243900, "red", barText)
 			self:PlaySound(1243900, "alarm")
+		end,
+		cancelCallback = function()
+			fetidQuillstormCount = fetidQuillstormCount - 1
 		end
 	}
 end
@@ -229,6 +337,9 @@ function mod:BarrageTimeline(eventInfo)
 		callback = function()
 			self:Message(1260643, "yellow", barText)
 			self:PlaySound(1260643, "info")
+		end,
+		cancelCallback = function()
+			barrageCount = barrageCount - 1
 		end
 	}
 end
@@ -243,6 +354,14 @@ function mod:CarrionSwoopTimeline(eventInfo)
 		--callback = function() -- has Blizzard message
 			--self:Message(1249479, "fixme", barText)
 			--self:PlaySound(1249479, "fixme")
-		--end
+		--end,
+		cancelCallback = function()
+			carrionSwoopCount = carrionSwoopCount - 1
+		end
 	}
+end
+
+function mod:BestialWrath()
+	self:Message(1249947, "cyan")
+	self:PlaySound(1249947, "long")
 end
