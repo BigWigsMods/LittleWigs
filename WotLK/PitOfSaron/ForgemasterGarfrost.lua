6bb265b07d23ad0d97f04a56911c05785173c77a
Nick Towle <ntowle@gmail.com>
2026-04-26 01:14:13 -0400

    WotLK/PitOfSaron/ForgemasterGarfrost: Redirect Orebreaker message

--- WotLK/PitOfSaron/ForgemasterGarfrost.lua
+++ WotLK/PitOfSaron/ForgemasterGarfrost.lua
@@ -159,6 +159,7 @@ function mod:OrebreakerTimeline(eventInfo)
 		msg = barText,
 		key = 1261546,
 		callback = function()
+			self:TargetMessageFromBlizzMessage(1, 1261546, "blue")
 			self:Message(1261546, "purple", barText)
 			if self:Tank() then
 				self:PlaySound(1261546, "warning")
