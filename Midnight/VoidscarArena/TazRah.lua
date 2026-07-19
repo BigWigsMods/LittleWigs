3da98ec8a8f45b31439c157005a585932563c97e
Nick Towle <ntowle@gmail.com>
2026-07-19 16:51:54 -0400

    Midnight/VoidscarArena/TazRah: Aura sound update

--- Midnight/VoidscarArena/TazRah.lua
+++ Midnight/VoidscarArena/TazRah.lua
@@ -7,8 +7,8 @@ if not mod then return end
 mod:SetEncounterID(3285)
 mod:SetRespawnTime(30)
 mod:SetPrivateAuraSounds({
-	{1222103, sound = "alert"}, -- Nether Dash
-	{1262283, sound = "long"}, -- Dark Rift
+	{1222103, sound = "none"}, -- Nether Dash
+	{1262283, sound = "none"}, -- Dark Rift
 	{1222305, sound = "underyou"}, -- Dark Rift
 })
 
