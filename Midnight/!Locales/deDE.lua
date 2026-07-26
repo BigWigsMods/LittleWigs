8bc413d8292f5901aa82b9dcfbd1ed2395dfdcaa
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2026-07-26 22:39:07 +0200

    Update deDE (#1501)

--- Midnight/!Locales/deDE.lua
+++ Midnight/!Locales/deDE.lua
@@ -2,7 +2,7 @@
 
 local L = BigWigs:NewBossLocale("Altar of Fangs Trash", "deDE")
 if L then
-	--L.custom_on_mixture_autotalk_desc = "|cFFFF0000Requires 25 skill in Midnight Cooking or Midnight Alchemy.|r Automatically select the NPC dialog option to gain the 'Mutating Elixir' buff.\n\n|T136242:16|tMutating Elixir\n{1310012}"
+	L.custom_on_mixture_autotalk_desc = "|cFFFF0000Benötigt 25 Fertigkeitspunkte der Kochkunst von Midnight oder Alchemie von Midnight.|r Wählt automatisch die NSC Dialogoption zur Gewährung des Buffs 'Mutierendes Elixier'.\n\n|T136242:16|tMutierendes Elixier\n{1310012}"
 end
 
 -- Delves: Atal'Aman
