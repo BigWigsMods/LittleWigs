e9c1e51ea83df9cb2f8dd92d89e99f4e8dd8d267
Nick Towle <ntowle@gmail.com>
2026-08-10 03:02:15 -0400

    BfA/TempleOfSethraliss/Avatar: Add aura sounds

--- BfA/TempleOfSethraliss/Avatar.lua
+++ BfA/TempleOfSethraliss/Avatar.lua
@@ -8,6 +8,14 @@ mod:RegisterEnableMob(133392, 137204) -- Avatar of Sethraliss, Hoodoo Hexer (bos
 mod:SetEncounterID(2127)
 if BigWigsLoader.isNext then -- Midnight+ XXX swap to mod:Retail() in 12.1
 	mod:SetRespawnTime(30)
+	mod:SetAuraData({
+		[1300714] = {soundOnApplied = "none"}, -- Shadowlash
+		[1300877] = {soundOnApplied = "none"}, -- Corruption
+		[1303446] = {soundOnApplied = "none"}, -- Tainted Strike
+		[1302618] = {soundOnApplied = "none"}, -- Vile Charge
+		[1311979] = {soundOnApplied = "none"}, -- Latent Hex
+		[1302826] = {soundOnApplied = "none"}, -- Corruption Burst
+	})
 else
 	mod:SetRespawnTime(20)
 end
