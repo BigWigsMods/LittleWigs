6b2e3da82943456610ab00f1db03987ba54eed73
Nick Towle <ntowle@gmail.com>
2026-08-20 23:57:07 -0400

    BfA/KingsRest/Mchimba: Filter extra Entomb timer

--- BfA/KingsRest/Mchimba.lua
+++ BfA/KingsRest/Mchimba.lua
@@ -112,12 +112,12 @@ end
 
 function mod:ENCOUNTER_TIMELINE_EVENT_ADDED(_, eventInfo)
 	if eventInfo.source ~= 0 then return end -- Enum.EncounterTimelineEventSource.Encounter
-	local duration = self:RoundNumber(eventInfo.duration, 1)
+	local duration = self:RoundNumber(eventInfo.duration, 0)
 	local barInfo
 	-- on pull and after each Entomb: DF=5, BC=20, AS=30, ET=60
 	-- follow-ups: DF=32, BC=30, AS=102
 	-- can't skip timers based on Entomb time left because if Entomb is skipped they will be cast as scheduled
-	if duration == 62.5 then -- Entomb placeholder
+	if duration == 63 then -- Entomb placeholder
 		return
 	elseif (initialEvents and duration == 5) or (not initialEvents and duration == 32) then -- Drain Fluids
 		barInfo = self:DrainFluidsTimeline(eventInfo)
